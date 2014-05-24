73255d54c728a80d80c4aa0bb39aec9c4092b514
Ryan Florence <rpflorence@gmail.com>
2014-05-23 23:52:15 -0600

    simple master detail example

---
+++ examples/simple-master-detail/app.js
@@ -0,0 +1,133 @@
+/** @jsx React.DOM */
+var Routes = ReactRouter.Routes;
+var Route = ReactRouter.Route;
+var Link = ReactRouter.Link;
+
+var Main = React.createClass({
+  render: function() {
+    return (
+      <Routes handler={App}>
+        <Route name="state" path="state/:abbr" handler={State} />
+      </Routes>
+    );
+  }
+});
+
+var App = React.createClass({
+  getInitialState: function() {
+    return { states: findStates() }
+  },
+
+  indexTemplate: function() {
+    return <p>Select a state from the left</p>;
+  },
+
+  render: function() {
+    var links = this.state.states.map(function(state) {
+      return <li><Link to="state" abbr={state.abbr}>{state.name}</Link></li>
+    });
+    var content = (this.props.activeRoute) ? this.props.activeRoute : this.indexTemplate();
+    return (
+      <div className="App">
+        <ul className="Master">
+          {links}
+        </ul>
+        <div className="Detail">
+          {content}
+        </div>
+      </div>
+    );
+  }
+});
+
+var State = React.createClass({
+  getInitialState: function() {
+    return findState(this.props.abbr);
+  },
+
+  imageUrl: function() {
+    return "http://www.50states.com/maps/"+underscore(this.state.name)+".gif";
+  },
+
+  render: function() {
+    return (
+      <div className="State">
+        <img src={this.imageUrl()}/>
+      </div>
+    );
+  }
+});
+
+React.renderComponent(<Main/>, document.body);
+
+/*****************************************************************************/
+// data stuff
+
+function findState(abbr) {
+  var states = findStates();
+  for (var i = 0, l = states.length; i < l; i ++) {
+    if (states[i].abbr === abbr) {
+      return states[i];
+    }
+  }
+}
+
+function findStates() {
+  return [
+    { abbr: "AL", name: "Alabama"},
+    { abbr: "AK", name: "Alaska"},
+    { abbr: "AZ", name: "Arizona"},
+    { abbr: "AR", name: "Arkansas"},
+    { abbr: "CA", name: "California"},
+    { abbr: "CO", name: "Colorado"},
+    { abbr: "CT", name: "Connecticut"},
+    { abbr: "DE", name: "Delaware"},
+    { abbr: "FL", name: "Florida"},
+    { abbr: "GA", name: "Georgia"},
+    { abbr: "HI", name: "Hawaii"},
+    { abbr: "ID", name: "Idaho"},
+    { abbr: "IL", name: "Illinois"},
+    { abbr: "IN", name: "Indiana"},
+    { abbr: "IA", name: "Iowa"},
+    { abbr: "KS", name: "Kansas"},
+    { abbr: "KY", name: "Kentucky"},
+    { abbr: "LA", name: "Louisiana"},
+    { abbr: "ME", name: "Maine"},
+    { abbr: "MD", name: "Maryland"},
+    { abbr: "MA", name: "Massachusetts"},
+    { abbr: "MI", name: "Michigan"},
+    { abbr: "MN", name: "Minnesota"},
+    { abbr: "MS", name: "Mississippi"},
+    { abbr: "MO", name: "Missouri"},
+    { abbr: "MT", name: "Montana"},
+    { abbr: "NE", name: "Nebraska"},
+    { abbr: "NV", name: "Nevada"},
+    { abbr: "NH", name: "New Hampshire"},
+    { abbr: "NJ", name: "New Jersey"},
+    { abbr: "NM", name: "New Mexico"},
+    { abbr: "NY", name: "New York"},
+    { abbr: "NC", name: "North Carolina"},
+    { abbr: "ND", name: "North Dakota"},
+    { abbr: "OH", name: "Ohio"},
+    { abbr: "OK", name: "Oklahoma"},
+    { abbr: "OR", name: "Oregon"},
+    { abbr: "PA", name: "Pennsylvania"},
+    { abbr: "RI", name: "Rhode Island"},
+    { abbr: "SC", name: "South Carolina"},
+    { abbr: "SD", name: "South Dakota"},
+    { abbr: "TN", name: "Tennessee"},
+    { abbr: "TX", name: "Texas"},
+    { abbr: "UT", name: "Utah"},
+    { abbr: "VT", name: "Vermont"},
+    { abbr: "VA", name: "Virginia"},
+    { abbr: "WA", name: "Washington"},
+    { abbr: "WV", name: "West Virginia"},
+    { abbr: "WI", name: "Wisconsin"},
+    { abbr: "WY", name: "Wyoming"}
+  ];
+}
+
+function underscore(str) {
+  return str.toLowerCase().replace(/ /, '_');
+}
+
