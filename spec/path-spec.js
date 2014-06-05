29934906acf6c19151e062ae383b2cdacdceee88
Michael Jackson <mjijackson@gmail.com>
2014-06-05 10:24:58 -0700

    More path specs

--- spec/path-spec.js
+++ spec/path-spec.js
@@ -34,3 +34,63 @@ describe('path.extractParams', function () {
     });
   });
 });
+
+describe('path.injectParams', function () {
+  describe('when a pattern does not have dynamic segments', function () {
+    var pattern = 'a/b/c';
+
+    it('returns the pattern', function () {
+      expect(path.injectParams(pattern, {})).toEqual(pattern);
+    });
+  });
+
+  describe('when a pattern has dynamic segments', function () {
+    var pattern = 'comments/:id/edit';
+
+    describe('and a param is missing', function () {
+      it('throws an Error', function () {
+        expect(function () {
+          path.injectParams(pattern, {})
+        }).toThrow(Error);
+      });
+    });
+
+    describe('and all params are present', function () {
+      it('returns the correct path', function () {
+        expect(path.injectParams(pattern, { id: 'abc' })).toEqual('comments/abc/edit');
+      });
+    });
+  });
+});
+
+describe('path.extractQuery', function () {
+  describe('when the path contains a query string', function () {
+    it('returns the parsed query object', function () {
+      expect(path.extractQuery('/a/b/c?id=def&show=true')).toEqual({ id: 'def', show: 'true' });
+    });
+  });
+
+  describe('when the path does not contain a query string', function () {
+    it('returns null', function () {
+      expect(path.extractQuery('/a/b/c')).toBe(null);
+    });
+  });
+});
+
+describe('path.withoutQuery', function () {
+  it('removes the query string', function () {
+    expect(path.withoutQuery('/a/b/c?id=def')).toEqual('/a/b/c');
+  });
+});
+
+describe('path.withQuery', function () {
+  it('appends the query string', function () {
+    expect(path.withQuery('/a/b/c', { id: 'def' })).toEqual('/a/b/c?id=def');
+  });
+});
+
+describe('path.normalize', function () {
+  it('removes slashes from the beginning of a path', function () {
+    expect(path.normalize('//a/b/c')).toEqual('a/b/c');
+  });
+});
