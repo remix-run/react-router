9ee608f89cbdca3594ae55db47cf7a2819d5cd93
Michael Jackson <mjijackson@gmail.com>
2014-06-07 07:17:24 -0700

    Better tests around Link.isActive

--- spec/link-component-spec.js
+++ spec/link-component-spec.js
@@ -1,7 +1,13 @@
 require('./helper');
 var Link = require('../lib/components/link');
 
-describe("when a Link's path matches the URL", function () {
+describe('when a link path does not match the URL', function () {
+  it('is not active', function () {
+    refute(Link.isActive('a/b/c', 'a/:id/c', { id: 'q' }));
+  });
+});
+
+describe('when a link path matches the URL', function () {
   describe('and it has no query', function () {
     it('is active', function () {
       assert(Link.isActive('a/b/c', 'a/:id/c', { id: 'b' }));
@@ -11,6 +17,12 @@ describe("when a Link's path matches the URL", function () {
   describe('and it has a query that matches', function () {
     it('is active', function () {
       assert(Link.isActive('a/b/c?show=true', 'a/:id/c', { id: 'b' }, { show: true }));
+    });
+  });
+
+  describe('and it has a URL-encoded query that matches', function () {
+    it('is active', function () {
+      assert(Link.isActive('a/b/c?list=one%2C%20two', 'a/b/c', {}, { list: 'one, two' }));
     });
   });
 
