b8018b1dabb5a73b4c7113f97d2a90d8c1f967b6
Ryan Florence <rpflorence@gmail.com>
2014-07-25 22:25:30 -0600

    [added] animation example
    abused the [added] flag just for marketing in the
    changelog cause THIS IS EPICLY AWESOME
    
    closes #17

---
+++ examples/animations/app.js
@@ -0,0 +1,41 @@
+/** @jsx React.DOM */
+var React = require('react');
+var Router = require('../../modules/main');
+var Route = Router.Route;
+var Link = Router.Link;
+var Transition = require('react/lib/ReactCSSTransitionGroup');
+
+var App = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <ul>
+          <li><Link to="image" service="kitten">Kitten</Link></li>
+          <li><Link to="image" service="cage">Cage</Link></li>
+        </ul>
+        <Transition transitionName="example">
+          {this.props.activeRouteHandler()}
+        </Transition>
+      </div>
+    );
+  }
+});
+
+var Image = React.createClass({
+  render: function() {
+    var src = "http://place"+this.props.params.service+".com/400/400";
+    return (
+      <div className="Image">
+        <img src={src}/>
+      </div>
+    );
+  }
+});
+
+var routes = (
+  <Route handler={App}>
+    <Route name="image" path="/:service" handler={Image}/>
+  </Route>
+);
+
+React.renderComponent(routes, document.body);
