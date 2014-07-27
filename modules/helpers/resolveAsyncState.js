0f908f1e21b8d817b30d09fc7d21e39cccaa636f
Michael Jackson <mjijackson@gmail.com>
2014-07-27 00:32:36 -0700

    Rename argument

--- modules/helpers/resolveAsyncState.js
+++ modules/helpers/resolveAsyncState.js
@@ -1,18 +1,19 @@
 var Promise = require('es6-promise').Promise;
 
 /**
- * Resolves all values in the given stateDescription object
- * and calls the setState function with new state as they resolve.
+ * Resolves all values in asyncState and calls the setState
+ * function with new state as they resolve. Returns a promise
+ * that resolves after all values are resolved.
  */
-function resolveAsyncState(stateDescription, setState) {
-  if (stateDescription == null)
-    return Promise.resolve({});
+function resolveAsyncState(asyncState, setState) {
+  if (asyncState == null)
+    return Promise.resolve();
 
-  var keys = Object.keys(stateDescription);
+  var keys = Object.keys(asyncState);
   
   return Promise.all(
     keys.map(function (key) {
-      return Promise.resolve(stateDescription[key]).then(function (value) {
+      return Promise.resolve(asyncState[key]).then(function (value) {
         var newState = {};
         newState[key] = value;
         setState(newState);
