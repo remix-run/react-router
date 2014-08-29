66030b2b55c395f8cce01e3cd9330c492b428a35
Michael Jackson <mjijackson@gmail.com>
2014-08-28 23:11:50 -0700

    Add doc comment

--- modules/components/NotFoundRoute.js
+++ modules/components/NotFoundRoute.js
@@ -1,6 +1,13 @@
 var merge = require('react/lib/merge');
 var Route = require('./Route');
 
+/**
+ * A <NotFoundRoute> is a special kind of <Route> that
+ * renders when the beginning of its parent's path matches
+ * but none of its siblings do, including any <DefaultRoute>.
+ * Only one such route may be used at any given level in the
+ * route hierarchy.
+ */
 function NotFoundRoute(props) {
   return Route(
     merge(props, {
