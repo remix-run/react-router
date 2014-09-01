38ffde4fbea8781ef5ffa9b3cc4ee3a76e15f31d
Michael Jackson <mjijackson@gmail.com>
2014-08-31 22:46:29 -0700

    Remove outdated comment

--- specs/main.js
+++ specs/main.js
@@ -1,5 +1,3 @@
-// TODO: this is for webkpack-karma to only create one build instead of a build
-// for every spec file, there must be some sort of config but I can't find it ...
 require('./ActiveStore.spec.js');
 require('./AsyncState.spec.js');
 require('./DefaultRoute.spec.js');
