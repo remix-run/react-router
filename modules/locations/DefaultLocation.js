4aa384895363bae1127b9b9d4047b0bb4500d796
Michael Jackson <mjijackson@gmail.com>
2014-09-02 22:34:42 -0700

    Use MemoryLocation by default in non-DOM environments

--- modules/locations/DefaultLocation.js
+++ modules/locations/DefaultLocation.js
@@ -1,3 +1,5 @@
-module.exports = process.env.NODE_ENV === 'test'
+var canUseDOM = require('react/lib/ExecutionEnvironment').canUseDOM;
+
+module.exports = process.env.NODE_ENV === 'test' || !canUseDOM
   ? require('./MemoryLocation')
   : require('./HashLocation');
