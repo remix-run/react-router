ae6e65ef06b32806c87ca17dcbff50139ba36e31
Dan Abramov <dan.abramov@gmail.com>
2014-09-28 21:46:19 +0400

    Add PayloadSources

--- modules/dispatchers/LocationDispatcher.js
+++ modules/dispatchers/LocationDispatcher.js
@@ -1,5 +1,6 @@
 var copyProperties = require('react/lib/copyProperties');
 var Dispatcher = require('flux').Dispatcher;
+var PayloadSources = require('../constants/PayloadSources');
 
 /**
  * Dispatches actions that modify the URL.
@@ -8,10 +9,17 @@ var LocationDispatcher = copyProperties(new Dispatcher, {
 
   handleViewAction: function (action) {
     this.dispatch({
-      source: 'VIEW_ACTION',
+      source: PayloadSources.VIEW_ACTION,
       action: action
     });
-  }
+  },
+
+  handleBrowserAction: function (action) {
+    this.dispatch({
+      source: PayloadSources.BROWSER_ACTION,
+      action: action
+    });
+  },
 
 });
 
