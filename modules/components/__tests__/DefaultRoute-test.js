59fa140fc99d2438e3c23660a106e2b688435541
Michael Jackson <mjijackson@gmail.com>
2014-10-03 10:26:02 -0700

    More tests

--- modules/components/__tests__/DefaultRoute-test.js
+++ modules/components/__tests__/DefaultRoute-test.js
@@ -1,8 +1,59 @@
 var expect = require('expect');
+var React = require('react/addons');
+var ReactTestUtils = React.addons.TestUtils;
+var RouteContainer = require('../../mixins/RouteContainer');
+var Route = require('../Route');
 var DefaultRoute = require('../DefaultRoute');
 
-describe('DefaultRoute', function () {
+describe('A DefaultRoute', function () {
   it('has a null path', function () {
     expect(DefaultRoute({ path: '/' }).props.path).toBe(null);
+  });
+
+  var App = React.createClass({
+    mixins: [ RouteContainer ],
+    render: function () {
+      return React.DOM.div();
+    }
+  });
+
+  describe('at the root of a container', function () {
+    var component, route;
+    beforeEach(function () {
+      component = ReactTestUtils.renderIntoDocument(
+        App(null,
+          route = DefaultRoute({ handler: App })
+        )
+      );
+    });
+
+    afterEach(function () {
+      React.unmountComponentAtNode(component.getDOMNode());
+    });
+
+    it('becomes its container\'s defaultRoute', function () {
+      expect(component.props.defaultRoute).toBe(route);
+    });
+  });
+
+  describe('nested in another Route', function () {
+    var component, route, defaultRoute;
+    beforeEach(function () {
+      component = ReactTestUtils.renderIntoDocument(
+        App(null,
+          route = Route({ handler: App },
+            defaultRoute = DefaultRoute({ handler: App })
+          )
+        )
+      );
+    });
+
+    afterEach(function () {
+      React.unmountComponentAtNode(component.getDOMNode());
+    });
+
+    it('becomes that route\'s defaultRoute', function () {
+      expect(route.props.defaultRoute).toBe(defaultRoute);
+    });
   });
 });
--- modules/components/__tests__/NotFoundRoute-test.js
+++ modules/components/__tests__/NotFoundRoute-test.js
@@ -1,8 +1,59 @@
 var expect = require('expect');
+var React = require('react/addons');
+var ReactTestUtils = React.addons.TestUtils;
+var RouteContainer = require('../../mixins/RouteContainer');
+var Route = require('../Route');
 var NotFoundRoute = require('../NotFoundRoute');
 
 describe('NotFoundRoute', function () {
   it('has a null path', function () {
     expect(NotFoundRoute({ path: '/' }).props.path).toBe(null);
+  });
+
+  var App = React.createClass({
+    mixins: [ RouteContainer ],
+    render: function () {
+      return React.DOM.div();
+    }
+  });
+
+  describe('at the root of a container', function () {
+    var component, route;
+    beforeEach(function () {
+      component = ReactTestUtils.renderIntoDocument(
+        App(null,
+          route = NotFoundRoute({ handler: App })
+        )
+      );
+    });
+
+    afterEach(function () {
+      React.unmountComponentAtNode(component.getDOMNode());
+    });
+
+    it('becomes its container\'s notFoundRoute', function () {
+      expect(component.props.notFoundRoute).toBe(route);
+    });
+  });
+
+  describe('nested in another Route', function () {
+    var component, route, notFoundRoute;
+    beforeEach(function () {
+      component = ReactTestUtils.renderIntoDocument(
+        App(null,
+          route = Route({ handler: App },
+            notFoundRoute = NotFoundRoute({ handler: App })
+          )
+        )
+      );
+    });
+
+    afterEach(function () {
+      React.unmountComponentAtNode(component.getDOMNode());
+    });
+
+    it('becomes that route\'s notFoundRoute', function () {
+      expect(route.props.notFoundRoute).toBe(notFoundRoute);
+    });
   });
 });
