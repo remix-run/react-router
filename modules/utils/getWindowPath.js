0150f6f5a82adb54b5bb30532e76ccfb645dc721
Michael Jackson <mjijackson@gmail.com>
2014-10-30 08:53:10 -0700

    Add invariant

--- modules/utils/getWindowPath.js
+++ modules/utils/getWindowPath.js
@@ -1,7 +1,15 @@
+var invariant = require('react/lib/invariant');
+var canUseDOM = require('react/lib/ExecutionEnvironment').canUseDOM;
+
 /**
  * Returns the current URL path from `window.location`, including query string
  */
 function getWindowPath() {
+  invariant(
+    canUseDOM,
+    'Cannot get current path without a DOM'
+  );
+
   return window.location.pathname + window.location.search;
 }
 
