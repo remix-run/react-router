d5da1dfca5efec0042dbc790162dd454f68f7157
Ryan Florence <rpflorence@gmail.com>
2014-11-26 08:15:41 -0700

    don't skip scroll tests cause they pass

--- modules/__tests__/Router-test.js
+++ modules/__tests__/Router-test.js
@@ -211,7 +211,7 @@ describe('Router.run', function () {
     });
   });
 
-  describe.skip('ImitateBrowserBehavior scrolling', function () {
+  describe('ImitateBrowserBehavior scrolling', function () {
     var BigPage = React.createClass({
       render: function () {
         return <div style={{ width: 10000, height: 10000, background: 'green' }}/>;
