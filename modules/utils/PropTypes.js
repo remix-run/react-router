f26864792056fdb8c1f9e4eb8fb85fb593729fd8
Michael Jackson <mjijackson@gmail.com>
2014-11-28 23:19:46 -0800

    Better arg name

--- modules/utils/PropTypes.js
+++ modules/utils/PropTypes.js
@@ -3,9 +3,9 @@ var PropTypes = {
   /**
    * Requires that the value of a prop be falsy.
    */
-  falsy: function (props, propName, elementName) {
+  falsy: function (props, propName, componentName) {
     if (props[propName])
-      return new Error('<' + elementName + '> may not have a "' + propName + '" prop');
+      return new Error('<' + componentName + '> may not have a "' + propName + '" prop');
   }
 
 };
