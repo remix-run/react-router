f120d6d1e5c265cbcb261e5db00ed7bfc2671724
Steve McKenzie <steve@stevemckenzie.ca>
2014-12-09 09:11:39 -0500

    Try to use replaceState to verify history API.

--- modules/utils/supportsHistory.js
+++ modules/utils/supportsHistory.js
@@ -10,6 +10,16 @@ function supportsHistory() {
       ua.indexOf('Chrome') === -1) {
     return false;
   }
+
+  try {
+    // TODO: better way to use replaceState that doesn't actually change the current url?
+    var path = window.location.pathname + window.location.search;
+    window.history.replaceState({path: path}, '', path);
+  }
+  catch (e) {
+    return false;
+  }
+
   return (window.history && 'pushState' in window.history);
 }
 
