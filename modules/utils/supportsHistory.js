720aa7eca77560adf969718349f47cc334f891d9
Dan Abramov <dan.abramov@gmail.com>
2014-12-10 20:17:08 +0100

    [fixed] supportsHistory false negatives on WP 8.1

--- modules/utils/supportsHistory.js
+++ modules/utils/supportsHistory.js
@@ -2,12 +2,14 @@ function supportsHistory() {
   /*! taken from modernizr
    * https://github.com/Modernizr/Modernizr/blob/master/LICENSE
    * https://github.com/Modernizr/Modernizr/blob/master/feature-detects/history.js
+   * changed to avoid false negatives for Windows Phones: https://github.com/rackt/react-router/issues/586
    */
   var ua = navigator.userAgent;
   if ((ua.indexOf('Android 2.') !== -1 ||
       (ua.indexOf('Android 4.0') !== -1)) &&
       ua.indexOf('Mobile Safari') !== -1 &&
-      ua.indexOf('Chrome') === -1) {
+      ua.indexOf('Chrome') === -1 &&
+      ua.indexOf('Windows Phone') === -1) {
     return false;
   }
   return (window.history && 'pushState' in window.history);
