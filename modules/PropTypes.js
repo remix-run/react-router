5c32ad8e5fd118482157f71a4966911b64eb0471
Michael Jackson <mjijackson@gmail.com>
2015-02-23 16:43:18 -0800

    Use ES6 function shorthand

--- modules/PropTypes.js
+++ modules/PropTypes.js
@@ -6,7 +6,7 @@ var PropTypes = assign({
   /**
    * Requires that the value of a prop be falsy.
    */
-  falsy: function (props, propName, componentName) {
+  falsy(props, propName, componentName) {
     if (props[propName])
       return new Error('<' + componentName + '> may not have a "' + propName + '" prop');
   }
