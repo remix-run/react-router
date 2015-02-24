67ecd77a89510df1ececa2adc7fc532449473758
Michael Jackson <mjijackson@gmail.com>
2015-02-24 15:48:16 -0800

    Remove dead code

--- modules/Configuration.js
+++ modules/Configuration.js
@@ -1,26 +1,6 @@
-var warning = require('react/lib/warning');
 var invariant = require('react/lib/invariant');
 
-function checkPropTypes(componentName, propTypes, props) {
-  for (var propName in propTypes) {
-    if (propTypes.hasOwnProperty(propName)) {
-      var error = propTypes[propName](props, propName, componentName);
-
-      if (error instanceof Error)
-        warning(false, error.message);
-    }
-  }
-}
-
 var Configuration = {
-
-  statics: {
-
-    validateProps: function (props) {
-      checkPropTypes(this.displayName, this.propTypes, props);
-    }
-
-  },
 
   render: function () {
     invariant(
