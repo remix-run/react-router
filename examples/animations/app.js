6c93099cf81730270af2816467311a9173af94b9
Ryan Florence <rpflorence@gmail.com>
2015-03-11 08:55:07 -0600

    we should not send the real routes array!

--- examples/animations/app.js
+++ examples/animations/app.js
@@ -8,7 +8,7 @@ var App = React.createClass({
   mixins: [ Router.State ],
 
   render: function () {
-    var name = this.getRoutes().reverse()[0].name;
+    var name = this.getRoutes().slice(0).reverse()[0].name;
 
     return (
       <div>
