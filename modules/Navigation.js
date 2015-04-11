e5ad53ba1e377d92c8f416b9c5a96efc285b2bc7
Michael Jackson <mjijackson@gmail.com>
2015-04-10 22:00:53 -0700

    Un-deprecate Navigation and State mixins

--- modules/Navigation.js
+++ modules/Navigation.js
@@ -1,16 +1,4 @@
-var warning = require('react/lib/warning');
 var PropTypes = require('./PropTypes');
-
-function deprecatedMethod(routerMethodName, fn) {
-  return function () {
-    warning(
-      false,
-      `Router.Navigation is deprecated. Please use this.context.router.${routerMethodName}() instead`
-    );
-
-    return fn.apply(this, arguments);
-  };
-}
 
 /**
  * A mixin for components that modify the URL.
@@ -40,40 +28,40 @@ var Navigation = {
    * Returns an absolute URL path created from the given route
    * name, URL parameters, and query values.
    */
-  makePath: deprecatedMethod('makePath', function (to, params, query) {
+  makePath(to, params, query) {
     return this.context.router.makePath(to, params, query);
-  }),
+  },
 
   /**
    * Returns a string that may safely be used as the href of a
    * link to the route with the given name.
    */
-  makeHref: deprecatedMethod('makeHref', function (to, params, query) {
+  makeHref(to, params, query) {
     return this.context.router.makeHref(to, params, query);
-  }),
+  },
 
   /**
    * Transitions to the URL specified in the arguments by pushing
    * a new URL onto the history stack.
    */
-  transitionTo: deprecatedMethod('transitionTo', function (to, params, query) {
+  transitionTo(to, params, query) {
     this.context.router.transitionTo(to, params, query);
-  }),
+  },
 
   /**
    * Transitions to the URL specified in the arguments by replacing
    * the current URL in the history stack.
    */
-  replaceWith: deprecatedMethod('replaceWith', function (to, params, query) {
+  replaceWith(to, params, query) {
     this.context.router.replaceWith(to, params, query);
-  }),
+  },
 
   /**
    * Transitions to the previous URL.
    */
-  goBack: deprecatedMethod('goBack', function () {
+  goBack() {
     return this.context.router.goBack();
-  })
+  }
 
 };
 
