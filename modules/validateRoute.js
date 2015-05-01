827b7c0df72bd9d5705657ff2c89a9d702a7a4d2
Michael Jackson <mjijackson@gmail.com>
2015-05-01 07:58:09 -0700

    Do not give route a default path

--- modules/validateRoute.js
+++ modules/validateRoute.js
@@ -1,8 +1,6 @@
 var invariant = require('react/lib/invariant');
 
 function validateRoute(route) {
-  route.path = route.path || '';
-
   invariant(
     route.handler || route.getHandler,
     'Route %s needs a "handler" or "getHandler" property',
