b21474beb49f42e91d8a9c4aba68dd04da581d3a
Michael Jackson <mjijackson@gmail.com>
2015-04-30 22:45:11 -0700

    [added] Async route matching algorithm

---
+++ modules/findMatch.js
@@ -0,0 +1,169 @@
+var { withoutQuery, extractQuery, extractParams, compilePattern } = require('./PathUtils');
+
+function loopAsync(turns, work, callback) {
+  var currentTurn = 0;
+  var isDone = false;
+
+  function done() {
+    isDone = true;
+    callback.apply(this, arguments);
+  }
+
+  function next() {
+    if (isDone)
+      return;
+
+    if (currentTurn < turns) {
+      currentTurn += 1;
+      work(currentTurn - 1, next, done);
+    } else {
+      done.apply(this, arguments);
+    }
+  }
+
+  next();
+}
+
+function getChildRoutes(route, callback) {
+  if (route.childRoutes) {
+    callback(null, route.childRoutes);
+  } else if (route.getChildRoutes) {
+    route.getChildRoutes(callback);
+  } else {
+    callback(null, null);
+  }
+}
+
+function stripLeadingSlashes(path) {
+  return path ? path.replace(/^\/+/, '') : '';
+}
+
+function stripTrailingSlashes(path) {
+  return path.replace(/\/+$/, '');
+}
+
+function assignParams(params, paramNames, paramValues) {
+  return paramNames.reduceRight(function (params, paramName, index) {
+    var paramValue = paramValues[index];
+
+    if (Array.isArray(params[paramName])) {
+      params[paramName].unshift(paramValue);
+    } else if (paramName in params) {
+      params[paramName] = [ paramValue, params[paramName] ];
+    } else {
+      params[paramName] = paramValue;
+    }
+
+    return params;
+  }, params);
+}
+
+function createParams(paramNames, paramValues) {
+  return assignParams({}, paramNames, paramValues);
+}
+
+function matchPattern(pattern, pathname) {
+  var { escapedSource, paramNames, tokens } = compilePattern(stripLeadingSlashes(pattern));
+
+  escapedSource += '/*'; // Ignore trailing slashes
+
+  var captureRemaining = tokens[tokens.length - 1] !== '*';
+
+  if (captureRemaining)
+    escapedSource += '(.*?)';
+
+  var match = pathname.match(new RegExp('^' + escapedSource + '$', 'i'));
+
+  var remainingPathname, paramValues;
+  if (match != null) {
+    paramValues = Array.prototype.slice.call(match, 1);
+
+    if (captureRemaining) {
+      remainingPathname = paramValues.pop();
+    } else {
+      remainingPathname = pathname.replace(match[0], '');
+    }
+  }
+
+  return {
+    remainingPathname,
+    paramNames,
+    paramValues
+  };
+}
+
+function matchRouteDeep(route, pathname, callback) {
+  var { remainingPathname, paramNames, paramValues } = matchPattern(route.path, pathname);
+
+  if (remainingPathname === '') {
+    // This route matched the whole path!
+    callback(null, {
+      params: createParams(paramNames, paramValues),
+      routes: [ route ]
+    });
+  } else if (remainingPathname != null) {
+    // This route matched at least some of the path.
+    getChildRoutes(route, function (error, childRoutes) {
+      if (error) {
+        callback(error);
+      } else if (childRoutes) {
+        // Check the child routes to see if any of them match.
+        matchRoutes(childRoutes, remainingPathname, function (error, match) {
+          if (error) {
+            callback(error);
+          } else if (match) {
+            // A child route matched! Augment the match and pass it up the stack.
+            assignParams(match.params, paramNames, paramValues);
+            match.routes.unshift(route);
+            callback(null, match);
+          } else {
+            callback();
+          }
+        });
+      } else {
+        callback();
+      }
+    });
+  } else {
+    callback();
+  }
+}
+
+function matchRoutes(routes, pathname, callback) {
+  loopAsync(routes.length, function (index, next, done) {
+    matchRouteDeep(routes[index], pathname, function (error, match) {
+      if (error || match) {
+        done(error, match);
+      } else {
+        next();
+      }
+    });
+  }, callback);
+}
+
+/**
+ * Searches the given tree of routes for a branch that matches
+ * the given path and calls callback(error, match) with the
+ * result. The match object has the following properties:
+ *
+ * routes   An array of route objects that matched, in nested order
+ * params   An object of URL params (contained in the pathname)
+ * query    An object of URL query parameters
+ *
+ * If no match can be made the argument is undefined.
+ */
+function findMatch(routes, path, callback) {
+  if (!Array.isArray(routes))
+    routes = [ routes ]; // Allow a single route
+
+  var pathname = stripLeadingSlashes(withoutQuery(path));
+
+  matchRoutes(routes, pathname, function (error, match) {
+    if (match)
+      match.query = extractQuery(path);
+
+    callback(error, match);
+  });
+}
+
+module.exports = findMatch;
