09b7d32d16f2ca4ddea7283383afccffef2dc88e
Michael Jackson <mjijackson@gmail.com>
2015-05-07 14:52:42 -0700

    Move build to lib directory

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,12 +2,13 @@
 
 babel=node_modules/.bin/babel
 webpack=node_modules/.bin/webpack
+build_dir=lib
 
-rm -rf build
+rm -rf $build_dir
 
-$babel -d build ./modules
-find -X build -type d -name __tests__ | xargs rm -rf
-NODE_ENV=production $webpack modules/index.js build/umd/ReactRouter.js
-NODE_ENV=production COMPRESS=1 $webpack modules/index.js build/umd/ReactRouter.min.js
+$babel -d $build_dir ./modules
+find -X $build_dir -type d -name __tests__ | xargs rm -rf
+NODE_ENV=production $webpack modules/index.js $build_dir/umd/ReactRouter.js
+NODE_ENV=production COMPRESS=1 $webpack modules/index.js $build_dir/umd/ReactRouter.min.js
 
-echo "gzipped, the global build is `gzip -c build/umd/ReactRouter.min.js | wc -c | sed -e 's/^[[:space:]]*//'` bytes"
+echo "gzipped, the global build is `gzip -c $build_dir/umd/ReactRouter.min.js | wc -c | sed -e 's/^[[:space:]]*//'` bytes"
