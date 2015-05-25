cc761593d6a7d667d0afee48919b5e3361718f74
Ryan Florence <rpflorence@gmail.com>
2015-05-25 11:33:59 -0600

    updated exports file
    exporting everything, that way the global build
    gets it all, and DCE will fix things for those
    who actually worry about the size of their JS.

--- modules/index.js
+++ modules/index.js
@@ -1,12 +1,26 @@
-exports.createRouter = require('./createRouter');
-exports.createRoutesFromReactChildren = require('./createRoutesFromReactChildren');
-exports.Navigation = require('./Navigation');
-exports.PropTypes = require('./PropTypes');
-exports.State = require('./State');
-exports.TransitionHook = require('./TransitionHook');
+export Router from './Router';
 
-exports.ImitateBrowserBehavior = require('./behaviors/ImitateBrowserBehavior');
-exports.ScrollToTopBehavior = require('./behaviors/ScrollToTopBehavior');
+export Route from './Route';
 
-exports.Link = require('./Link');
-exports.Route = require('./Route');
+export History from './History';
+export DOMHistory from './DOMHistory';
+export HashHistory from './HashHistory';
+export BrowserHistory from './BrowserHistory';
+export MemoryHistory from './HashHistory';
+
+export Link from './Link';
+export Navigation from './Navigation';
+export TransitionHook from './TransitionHook'
+export Env from './Env';
+
+export AsyncRouting from './AsyncRouting';
+export TransitionManager from './TransitionManager';
+export AsyncProps from './AsyncProps';
+export RouteRenderer from './RouteRenderer';
+export ScrollManager from './ScrollManager';
+export Renderer from './Renderer';
+
+export PropTypes from './PropTypes';
+
+export createRoutesFromReactChildren from './createRoutesFromReactChildren';
+
