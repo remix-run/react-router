0e2180048515cb270522c8424ea7ec4872433aff
BinaryMuse <brandon@brandontilley.com>
2015-06-18 15:32:51 -0700

    Add tests for incorrect onEnter/onLeave Redirect behavior

--- modules/__tests__/Redirect-test.js
+++ modules/__tests__/Redirect-test.js
@@ -26,4 +26,47 @@ describe('A <Redirect>', function () {
       done();
     });
   });
+
+  it('calls onEnter but not onLeave', function (done) {
+    var enterCalled = 0;
+    var leaveCalled = 0;
+    var onEnter = function () { enterCalled++ };
+    var onLeave = function() { leaveCalled++ };
+    render((
+      <Router history={new MemoryHistory('/notes/5')}>
+        <Route onEnter={onEnter} onLeave={onLeave}>
+          <Route path="messages/:id"/>
+          <Redirect from="notes/:id" to="/messages/:id"/>
+        </Route>
+      </Router>
+    ), node, function() {
+      expect(enterCalled).toEqual(1);
+      expect(leaveCalled).toEqual(0);
+      done();
+    });
+  });
+
+  it('doesn\'t call onEnter or onLeave when redirecting inside the same parent', function (done) {
+    var enterCalled = 0;
+    var leaveCalled = 0;
+    var onEnter = function () { enterCalled++ };
+    var onLeave = function() { leaveCalled++ };
+    render((
+      <Router history={new MemoryHistory('/messages/5/details')}>
+        <Route onEnter={onEnter} onLeave={onLeave}>
+          <Redirect from="messages/:id" to="/messages/:id/details"/>
+          <Route path="messages/:id/details"/>
+        </Route>
+      </Router>
+    ), node, function() {
+      expect(this.state.location.pathname).toEqual('/messages/5/details');
+      expect(enterCalled).toEqual(1);
+      expect(leaveCalled).toEqual(0);
+      this.transitionTo("/messages/6");
+      expect(this.state.location.pathname).toEqual('/messages/6/details');
+      expect(enterCalled).toEqual(1);
+      expect(leaveCalled).toEqual(0);
+      done();
+    });
+  });
 });
