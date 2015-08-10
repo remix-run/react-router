6a26d1fc125c380bd67c6aa50dcabc090d6c8ee9
Michael Jackson <mjijackson@gmail.com>
2015-08-10 09:33:41 -0700

    Add BrowserStack build config

--- karma.conf.js
+++ karma.conf.js
@@ -1,15 +1,64 @@
 var webpack = require('webpack');
 
 module.exports = function (config) {
+  // Browsers to run on BrowserStack
+  var customLaunchers = {
+    BS_Chrome: {
+      base: 'BrowserStack',
+      os: 'OS X',
+      os_version: 'Yosemite',
+      browser: 'chrome',
+      browser_version: '39.0',
+    },
+    BS_Firefox: {
+      base: 'BrowserStack',
+      os: 'OS X',
+      os_version: 'Yosemite',
+      browser: 'firefox',
+      browser_version: '32.0',
+    },
+    BS_Safari: {
+      base: 'BrowserStack',
+      os: 'OS X',
+      os_version: 'Yosemite',
+      browser: 'safari',
+      browser_version: '8.0',
+    },
+//    BS_InternetExplorer9: {
+//      base: 'BrowserStack',
+//      os: 'Windows',
+//      os_version: '7',
+//      browser: 'ie',
+//      browser_version: '9.0',
+//    },
+    BS_InternetExplorer10: {
+      base: 'BrowserStack',
+      os: 'Windows',
+      os_version: '8',
+      browser: 'ie',
+      browser_version: '10.0',
+    },
+    BS_InternetExplorer11: {
+      base: 'BrowserStack',
+      os: 'Windows',
+      os_version: '8.1',
+      browser: 'ie',
+      browser_version: '11.0',
+    },
+    BS_MobileSafari: {
+      base: 'BrowserStack',
+      os: 'ios',
+      os_version: '7.0',
+      browser: 'iphone'
+    },
+  };
+
   config.set({
+    customLaunchers: customLaunchers,
 
-    browserNoActivityTimeout: 30000,
-
-    browsers: [ process.env.CONTINUOUS_INTEGRATION ? 'Firefox' : 'Chrome' ],
-
-    singleRun: process.env.CONTINUOUS_INTEGRATION === 'true',
-
+    browsers: [ 'Chrome' ],
     frameworks: [ 'mocha' ],
+    reporters: [ 'mocha' ],
 
     files: [
       'tests.webpack.js'
@@ -18,8 +67,6 @@ module.exports = function (config) {
     preprocessors: {
       'tests.webpack.js': [ 'webpack', 'sourcemap' ]
     },
-
-    reporters: [ 'dots' ],
 
     webpack: {
       devtool: 'inline-source-map',
@@ -38,6 +85,37 @@ module.exports = function (config) {
     webpackServer: {
       noInfo: true
     }
-
   });
+
+  if (process.env.USE_CLOUD) {
+    config.browsers = Object.keys(customLaunchers);
+    config.reporters = [ 'dots' ];
+    config.browserDisconnectTimeout = 10000;
+    config.browserDisconnectTolerance = 3;
+    config.browserNoActivityTimeout = 30000;
+    config.captureTimeout = 120000;
+
+    if (process.env.TRAVIS) {
+      var buildLabel = 'TRAVIS #' + process.env.TRAVIS_BUILD_NUMBER + ' (' + process.env.TRAVIS_BUILD_ID + ')';
+
+      config.browserStack = {
+        username: process.env.BROWSER_STACK_USERNAME,
+        accessKey: process.env.BROWSER_STACK_ACCESS_KEY,
+        pollingTimeout: 10000,
+        startTunnel: false,
+        project: 'react-router',
+        build: buildLabel,
+        name: process.env.TRAVIS_JOB_NUMBER,
+      };
+
+      config.singleRun = true;
+    } else {
+      config.browserStack = {
+        username: process.env.BROWSER_STACK_USERNAME,
+        accessKey: process.env.BROWSER_STACK_ACCESS_KEY,
+        pollingTimeout: 10000,
+        startTunnel: true,
+      };
+    }
+  }
 };
