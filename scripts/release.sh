4c6dc1b93484baec53b37fb4b9619006be7c5623
Michael Jackson <mjijackson@gmail.com>
2015-08-12 22:19:06 -0700

    [fixed] Installing on Windows
    Fixes #1577

--- scripts/release.sh
+++ scripts/release.sh
@@ -7,15 +7,19 @@ update_version() {
   echo "Updated ${1} version to ${2}"
 }
 
+validate_semver() {
+  if ! [[ $1 =~ ^[0-9]\.[0-9]+\.[0-9](-.+)? ]]; then
+    echo "Version $1 is not valid! It must be a valid semver string like 1.0.2 or 2.3.0-beta.1"
+    exit 1
+  fi
+}
+
 current_version=$(node -p "require('./package').version")
 
 printf "Next version (current is $current_version)? "
 read next_version
 
-if ! [[ $next_version =~ ^[0-9]\.[0-9]+\.[0-9](-.+)? ]]; then
-  echo "Version must be a valid semver string, e.g. 1.0.2 or 2.3.0-beta.1"
-  exit 1
-fi
+validate_semver $next_version
 
 next_ref="v$next_version"
 
@@ -26,8 +30,12 @@ update_version 'package.json' $next_version
 $changelog -t $next_ref
 
 npm run build
-git add -A build
+npm run build-umd
+npm run build-min
 
+echo "gzipped, the UMD build is `gzip -c lib/umd/ReactRouter.min.js | wc -c | sed -e 's/^[[:space:]]*//'` bytes"
+
+git add -A build
 git commit -am "Version $next_version"
 
 git tag $next_ref
@@ -37,8 +45,7 @@ git push origin master
 git push origin $next_ref
 git push origin latest -f
 
-npm publish build
+npm publish
 
 echo "# Publishing docs website"
 ./publish-docs.sh
-
