1e8b0e0c2b0ab49007f1d494532c19a0f4e84d9c
Michael Jackson <mjijackson@gmail.com>
2015-09-01 21:26:00 -0700

    match callback expects 3 args

--- modules/useRoutes.js
+++ modules/useRoutes.js
@@ -12,46 +12,32 @@ import matchRoutes from './matchRoutes';
  * - isActive(pathname, query)
  * - registerRouteHook(route, (location) => {})
  * - unregisterRouteHook(route, (location) => {})
- * - listen((error, state) => {})
- * - match(location, (error, state) => {})
+ * - match(location, (error, nextState, redirectInfo) => {})
+ * - listen((error, nextState) => {})
  */
 function useRoutes(createHistory) {
   return function (options={}) {
-    var { routes, ...historyOptions } = options;
-    var history = useQueries(createHistory)(historyOptions);
-    var state = {};
+    let { routes, ...historyOptions } = options;
+    let history = useQueries(createHistory)(historyOptions);
+    let state = {};
 
     function isActive(pathname, query, indexOnly=false) {
       return _isActive(pathname, query, indexOnly, state.location, state.routes, state.params);
     }
 
-    function matchRoutesWithGuaranteedState(routes, location, callback) {
-      matchRoutes(routes, location, function (error, nextState) {
-        if (error || nextState) {
-          callback(error, nextState);
-        } else {
-          warning(
-            false,
-            'Location "%s" did not match any routes',
-            location.pathname + location.search
-          );
-        }
-      });
-    }
-
     // TODO: If we had a way to uniquely identify a route,
     // we could use a plain object here instead...
-    var routeHooks = new Map();
-    var partialNextState;
+    let routeHooks = new Map();
+    let partialNextState;
 
     function match(location, callback) {
       if (partialNextState && partialNextState.location === location) {
         // Continue from where we left off.
         finishMatch(partialNextState, callback);
       } else {
-        matchRoutesWithGuaranteedState(routes, location, function (error, nextState) {
-          if (error) {
-            callback(error);
+        matchRoutes(routes, location, function (error, nextState) {
+          if (error || nextState == null) {
+            callback(error, nextState);
           } else {
             finishMatch({ ...nextState, location }, callback);
           }
@@ -60,7 +46,7 @@ function useRoutes(createHistory) {
     }
 
     function finishMatch(nextState, callback) {
-      var { leaveRoutes, enterRoutes } = computeChangedRoutes(state, nextState);
+      let { leaveRoutes, enterRoutes } = computeChangedRoutes(state, nextState);
 
       runLeaveHooks(leaveRoutes);
 
@@ -68,9 +54,7 @@ function useRoutes(createHistory) {
         if (error) {
           callback(error);
         } else if (redirectInfo) {
-          var { pathname, query, state } = redirectInfo;
-          history.replaceState(state, pathname, query);
-          callback();
+          callback(null, null, redirectInfo);
         } else {
           // TODO: Fetch components after state is updated.
           getComponents(nextState.routes, function (error, components) {
@@ -92,39 +76,42 @@ function useRoutes(createHistory) {
     }
 
     function transitionHook(location, callback) {
-      matchRoutesWithGuaranteedState(routes, location, function (error, nextState) {
-        if (error) {
-          // TODO: Handle the error.
-          callback(false); // Cancel the transition.
-        } else {
-          // Cache some state here so we don't have to
-          // matchRoutes() again in the listen callback.
-          partialNextState = { ...nextState, location };
-
-          var hooks = getRouteHooksForRoutes(
-            computeChangedRoutes(state, nextState).leaveRoutes
-          );
-
-          var result;
-          for (var i = 0, len = hooks.length; result == null && i < len; ++i) {
-            // Passing the location arg here indicates to
-            // the user that this is a transition hook.
-            result = hooks[i](location);
-          }
-
-          callback(result);
+      matchRoutes(routes, location, function (error, nextState) {
+        if (nextState == null) {
+          // TODO: We didn't actually match anything, but hang
+          // onto error/nextState so we don't have to matchRoutes
+          // again in the listen callback.
+          callback();
+          return;
         }
+
+        // Cache some state here so we don't have to
+        // matchRoutes() again in the listen callback.
+        partialNextState = { ...nextState, location };
+
+        let hooks = getRouteHooksForRoutes(
+          computeChangedRoutes(state, nextState).leaveRoutes
+        );
+
+        let result;
+        for (let i = 0, len = hooks.length; result == null && i < len; ++i) {
+          // Passing the location arg here indicates to
+          // the user that this is a transition hook.
+          result = hooks[i](location);
+        }
+
+        callback(result);
       });
     }
 
     function beforeUnloadHook() {
       // Synchronously check to see if any route hooks want to 
       // prevent the current window/tab from closing.
-      if (state && state.routes) {
-        var hooks = getRouteHooksForRoutes(state.routes);
+      if (state.routes) {
+        let hooks = getRouteHooksForRoutes(state.routes);
 
-        var message;
-        for (var i = 0, len = hooks.length; typeof message !== 'string' && i < len; ++i) {
+        let message;
+        for (let i = 0, len = hooks.length; typeof message !== 'string' && i < len; ++i) {
           // Passing no args indicates to the user that this is a
           // beforeunload hook. We don't know the next location.
           message = hooks[i]();
@@ -138,7 +125,7 @@ function useRoutes(createHistory) {
       // TODO: Warn if they register for a route that isn't currently
       // active. They're probably doing something wrong, like re-creating
       // route objects on every location change.
-      var hooks = routeHooks.get(route);
+      let hooks = routeHooks.get(route);
 
       if (hooks == null) {
         routeHooks.set(route, (hooks = [ hook ]));
@@ -155,10 +142,10 @@ function useRoutes(createHistory) {
     }
 
     function unregisterRouteHook(route, hook) {
-      var hooks = routeHooks.get(route);
+      let hooks = routeHooks.get(route);
       
       if (hooks != null) {
-        var newHooks = hooks.filter(item => item !== hook);
+        let newHooks = hooks.filter(item => item !== hook);
 
         if (newHooks.length === 0) {
           routeHooks.delete(route);
@@ -175,24 +162,36 @@ function useRoutes(createHistory) {
       }
     }
 
-    function dispatch(location, callback) {
-      if (state && state.location === location) {
-        callback(null, state);
-        return;
-      }
-
-      match(location, function (error, nextState) {
-        if (error) {
-          callback(error);
-        } else if (nextState) {
-          callback(null, (state = nextState));
-        }
-      });
-    }
-
+    /**
+     * This is the API for stateful environments. As the location changes,
+     * we update state and call the listener. Benefits of this API are:
+     *
+     * - We automatically manage state on the client
+     * - We automatically handle redirects on the client
+     * - We warn when the location doesn't match any routes
+     */
     function listen(listener) {
       return history.listen(function (location) {
-        dispatch(location, listener);
+        if (state.location === location) {
+          listener(null, state);
+        } else {
+          match(location, function (error, nextState, redirectInfo) {
+            if (error) {
+              listener(error);
+            } else if (nextState) {
+              listener(null, (state = nextState));
+            } else if (redirectInfo) {
+              let { pathname, query, state } = redirectInfo;
+              history.replaceState(state, pathname, query);
+            } else {
+              warning(
+                false,
+                'Location "%s" did not match any routes',
+                location.pathname + location.search
+              );
+            }
+          });
+        }
       });
     }
 
