d049625e25116423badb3c0f8135f7dd61bbb65b
C. T. Lin <chentsulin@gmail.com>
2015-09-03 16:54:49 +0800

    fix import PropTypes and get undefined bug

--- modules/PropTypes.js
+++ modules/PropTypes.js
@@ -2,19 +2,19 @@ import { PropTypes } from 'react';
 
 var { func, object, arrayOf, oneOfType, element, shape, string } = PropTypes;
 
-export function falsy(props, propName, componentName) {
+function falsy(props, propName, componentName) {
   if (props[propName])
     return new Error(`<${componentName}> should not have a "${propName}" prop`);
 }
 
-export var history = shape({
+var history = shape({
   listen: func.isRequired,
   pushState: func.isRequired,
   replaceState: func.isRequired,
   go: func.isRequired
 });
 
-export var location = shape({
+var location = shape({
   pathname: string.isRequired,
   search: string.isRequired,
   state: object,
@@ -22,7 +22,17 @@ export var location = shape({
   key: string
 });
 
-export var component = oneOfType([ func, string ]);
-export var components = oneOfType([ component, object ]);
-export var route = oneOfType([ object, element ]);
-export var routes = oneOfType([ route, arrayOf(route) ]);
+var component = oneOfType([ func, string ]);
+var components = oneOfType([ component, object ]);
+var route = oneOfType([ object, element ]);
+var routes = oneOfType([ route, arrayOf(route) ]);
+
+export default {
+  falsy,
+  history,
+  location,
+  component,
+  components,
+  route,
+  router
+};
