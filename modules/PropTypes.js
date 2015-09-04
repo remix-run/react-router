4fd5d4a05dcabfe684098ff848797c4e58730202
C. T. Lin <chentsulin@gmail.com>
2015-09-05 02:34:49 +0800

    add others export back.

--- modules/PropTypes.js
+++ modules/PropTypes.js
@@ -2,19 +2,19 @@ import { PropTypes } from 'react';
 
 var { func, object, arrayOf, oneOfType, element, shape, string } = PropTypes;
 
-function falsy(props, propName, componentName) {
+export function falsy(props, propName, componentName) {
   if (props[propName])
     return new Error(`<${componentName}> should not have a "${propName}" prop`);
 }
 
-var history = shape({
+export var history = shape({
   listen: func.isRequired,
   pushState: func.isRequired,
   replaceState: func.isRequired,
   go: func.isRequired
 });
 
-var location = shape({
+export var location = shape({
   pathname: string.isRequired,
   search: string.isRequired,
   state: object,
@@ -22,10 +22,10 @@ var location = shape({
   key: string
 });
 
-var component = oneOfType([ func, string ]);
-var components = oneOfType([ component, object ]);
-var route = oneOfType([ object, element ]);
-var routes = oneOfType([ route, arrayOf(route) ]);
+export var component = oneOfType([ func, string ]);
+export var components = oneOfType([ component, object ]);
+export var route = oneOfType([ object, element ]);
+export var routes = oneOfType([ route, arrayOf(route) ]);
 
 export default {
   falsy,
