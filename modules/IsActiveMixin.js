e548ec3830c6746f7f27822bf334168daca9e656
Grgur Grisogono <grgur@grgur.com>
2015-09-07 13:31:12 +0200

    IsActiveDecorator

---
+++ modules/IsActiveMixin.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+var { object } = React.PropTypes;
+
+/**
+ * The IsActive mixin provides components with an isActive(pathname, query)
+ * method they can use to check if a given pathname/query are active.
+ *
+ * Example:
+ *
+ *   import { IsActive } from 'react-router';
+ *
+ *   var AboutLink = React.createClass({
+ *     mixins: [ IsActive ],
+ *     render() {
+ *       var className = this.props.className;
+ *
+ *       if (this.isActive('/about'))
+ *         className += ' is-active';
+ *
+ *       return React.createElement('a', { className }, this.props.children);
+ *     }
+ *   });
+ */
+var IsActive = {
+
+  contextTypes: {
+    history: object.isRequired
+  },
+
+  isActive(pathname, query, indexOnly) {
+    return this.context.history.isActive(pathname, query, indexOnly);
+  }
+
+};
+
+export const IsActiveDecorator = createDecorator(IsActive);
+
+export default IsActive;
