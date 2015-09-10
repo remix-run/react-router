234be13a7502ee19e8a6b681222c6779f7022e56
Ryan Florence <rpflorence@gmail.com>
2015-09-10 16:58:46 -0600

    Revert "add `match` for one-shot routing"
    This reverts commit 446c30c28f4710f06106c52caf7e00e992afef13.
    
    last minute change to API, don't want to block anybody

--- modules/__tests__/serverRendering-test.js
+++ modules/__tests__/serverRendering-test.js
@@ -1,29 +1,13 @@
 import expect from 'expect';
 import React from 'react';
+import createHistory from 'history/lib/createMemoryHistory';
 import createLocation from 'history/lib/createLocation';
 import RoutingContext from '../RoutingContext';
-import match from '../match';
-import Link from '../Link';
+import useRoutes from '../useRoutes';
 
 describe('server rendering', function () {
-  var App, Dashboard, About, AppRoute, RedirectRoute, AboutRoute, DashboardRoute, routes;
-
+  var Dashboard, DashboardRoute, routes;
   beforeEach(function () {
-    App = React.createClass({
-      render() {
-        return (
-          <div className="App">
-            <h1>App</h1>
-            <Link to="/about" activeClassName="about-is-active">About</Link>{' '}
-            <Link to="/dashboard" activeClassName="dashboard-is-active">Dashboard</Link>
-            <div>
-              {this.props.children}
-            </div>
-          </div>
-        );
-      }
-    })
-
     Dashboard = React.createClass({
       render() {
         return (
@@ -34,83 +18,28 @@ describe('server rendering', function () {
       }
     });
 
-    About = React.createClass({
-      render() {
-        return (
-          <div className="About">
-            <h1>About</h1>
-          </div>
-        );
-      }
-    });
-
     DashboardRoute = {
-      path: '/dashboard',
+      path: '/',
       component: Dashboard
     };
 
-    AboutRoute = {
-      path: '/about',
-      component: About
-    };
-
-    RedirectRoute = {
-      path: '/company',
-      onEnter (nextState, redirectTo) {
-        redirectTo('/about')
-      }
-    };
-
-    AppRoute = routes = {
-      path: '/',
-      component: App,
-      childRoutes: [ DashboardRoute, AboutRoute, RedirectRoute ]
-    };
+    routes = [
+      DashboardRoute
+    ];
   });
-
+ 
   it('works', function (done) {
-    var location = createLocation('/dashboard');
-    match({ routes, location }, function (error, redirectLocation, renderProps) {
+    var history = useRoutes(createHistory)({ routes });
+    var location = createLocation('/');
+
+    history.match(location, function (error, state) {
       var string = React.renderToString(
-        <RoutingContext {...renderProps} />
+        <RoutingContext history={history} {...state} />
       );
+
       expect(string).toMatch(/The Dashboard/);
+
       done();
     });
   });
-
-  it('renders active Links as active', function (done) {
-    var location = createLocation('/about');
-    match({ routes, location }, function (error, redirectLocation, renderProps) {
-      var string = React.renderToString(
-        <RoutingContext {...renderProps} />
-      );
-      expect(string).toMatch(/about-is-active/);
-      //expect(string).toNotMatch(/dashboard-is-active/); TODO add toNotMatch to expect
-      done();
-    });
-  });
-
-  it('sends the redirect location', function (done) {
-    var location = createLocation('/company');
-    match({ routes, location }, function (error, redirectLocation) {
-      expect(redirectLocation).toExist();
-      expect(redirectLocation.pathname).toEqual('/about')
-      expect(redirectLocation.search).toEqual('')
-      expect(redirectLocation.state).toEqual(null)
-      expect(redirectLocation.action).toEqual('REPLACE')
-      done();
-    });
-  });
-
-  it('sends an error when no routes match', function (done) {
-    var location = createLocation('/no-match');
-    match({ routes, location }, function (error, redirect, state) {
-      expect(error).toExist();
-      expect(error.name).toEqual('NoMatchError');
-      expect(error.message).toMatch(/\/no-match/);
-      done();
-    });
-  });
-
 });
--- modules/useRoutes.js
+++ modules/useRoutes.js
@@ -8,15 +8,6 @@ import { default as _isActive } from './isActive';
 import getComponents from './getComponents';
 import matchRoutes from './matchRoutes';
 
-class NoMatchError extends Error {
-  constructor (location) {
-    super()
-    this.name = this.constructor.name;
-    this.message = `Location ${location.pathname}${location.search} did not match any routes.`;
-    this.stack = (new Error()).stack;
-  }
-}
-
 function hasAnyProperties(object) {
   for (var p in object)
     if (object.hasOwnProperty(p))
@@ -53,17 +44,10 @@ function useRoutes(createHistory) {
         finishMatch(partialNextState, callback);
       } else {
         matchRoutes(routes, location, function (error, nextState) {
-          if (error) {
-            callback(error, null, null);
-          } else if (nextState) {
-            finishMatch({ ...nextState, location }, function (err, nextLocation, nextState) {
-              if (nextState)
-                state = nextState;
-              callback(err, nextLocation, nextState);
-            });
+          if (error || nextState == null) {
+            callback(error, nextState);
           } else {
-            console.log(4)
-            callback(new NoMatchError(location));
+            finishMatch({ ...nextState, location }, callback);
           }
         });
       }
@@ -84,14 +68,14 @@ function useRoutes(createHistory) {
         if (error) {
           callback(error);
         } else if (redirectInfo) {
-          callback(null, createLocationFromRedirectInfo(redirectInfo), null);
+          callback(null, null, createLocationFromRedirectInfo(redirectInfo));
         } else {
           // TODO: Fetch components after state is updated.
           getComponents(nextState.routes, function (error, components) {
             if (error) {
               callback(error);
             } else {
-              callback(null, null, { ...nextState, components });
+              callback(null, { ...nextState, components });
             }
           });
         }
@@ -143,7 +127,7 @@ function useRoutes(createHistory) {
     }
 
     function beforeUnloadHook() {
-      // Synchronously check to see if any route hooks want to
+      // Synchronously check to see if any route hooks want to 
       // prevent the current window/tab from closing.
       if (state.routes) {
         let hooks = getRouteHooksForRoutes(state.routes);
@@ -185,7 +169,7 @@ function useRoutes(createHistory) {
     function unregisterRouteHook(route, hook) {
       let routeID = getRouteID(route);
       let hooks = RouteHooks[routeID];
-
+      
       if (hooks != null) {
         let newHooks = hooks.filter(item => item !== hook);
 
@@ -217,17 +201,19 @@ function useRoutes(createHistory) {
         if (state.location === location) {
           listener(null, state);
         } else {
-          match(location, function (error, nextLocation, nextState) {
+          match(location, function (error, nextState, nextLocation) {
             if (error) {
-              if (error.name === 'NoMatchError') {
-                warning(false, error.message);
-              } else {
-                listener(error);
-              }
+              listener(error);
             } else if (nextState) {
-              listener(null, state); // match mutates state to nextState
+              listener(null, (state = nextState));
             } else if (nextLocation) {
               history.transitionTo(nextLocation);
+            } else {
+              warning(
+                false,
+                'Location "%s" did not match any routes',
+                location.pathname + location.search
+              );
             }
           });
         }
