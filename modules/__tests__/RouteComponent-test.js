0095cc320067c926e22d503b9316d0377ee4bfd3
Ryan Florence <rpflorence@gmail.com>
2015-09-10 12:00:05 -0600

    add failing test for props.routes

---
+++ modules/__tests__/RouteComponent-test.js
@@ -0,0 +1,43 @@
+import expect from 'expect';
+import React from 'react';
+import createHistory from 'history/lib/createMemoryHistory';
+import Router from '../Router';
+import Route from '../Route';
+
+describe('a Route Component', function () {
+
+  var node;
+  beforeEach(function () {
+    node = document.createElement('div');
+  });
+
+  afterEach(function () {
+    React.unmountComponentAtNode(node);
+  });
+
+  it.only('injects the right props', function (done) {
+    var Parent = React.createClass({
+      componentDidMount() {
+        assertProps(this.props)
+      },
+      render() { return null }
+    });
+
+    var Child = React.createClass({
+      render() { return null }
+    });
+
+    var child = { path: 'child', component: Child }
+    var parent = { path: '/', component: Parent, childRoutes: [ child ] }
+
+    function assertProps(props) {
+      expect(props.route).toEqual(parent)
+      expect(props.routes).toEqual([parent, child])
+    }
+
+    React.render((
+      <Router history={createHistory('/child')} routes={parent}/>
+    ), node, done)
+  });
+
+});
