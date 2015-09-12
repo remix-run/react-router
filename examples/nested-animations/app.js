48041570f299d6db6c99cf4175ee6182c38ddecf
Thibaud Colas <thibaudcolas@gmail.com>
2015-09-13 00:17:36 +1200

    Update nested animations example for 1.0 API & new code style

--- examples/nested-animations/app.js
+++ examples/nested-animations/app.js
@@ -1,80 +1,67 @@
-var React = require('react');
-var TransitionGroup = require('react/lib/ReactCSSTransitionGroup');
-var Router = require('react-router');
-var { Route, RouteHandler, Link } = Router;
+import React from 'react/addons';
+import { Router, Route, Link } from 'react-router';
+
+var { CSSTransitionGroup } = React.addons;
 
 var App = React.createClass({
+  render() {
 
-  contextTypes: {
-    router: React.PropTypes.func
-  },
-
-  render: function () {
-    var name = this.context.router.getCurrentPath();
     // Only take the first-level part of the path as key, instead of the whole path.
-    var key = name.split('/')[1] || 'root';
+    var pathname = this.props.location.pathname;
+    var key = pathname.split('/')[1] || 'root';
+
     return (
       <div>
         <ul>
-          <li><Link to="page1">Page 1</Link></li>
-          <li><Link to="page2">Page 2</Link></li>
+          <li><Link to="/page1">Page 1</Link></li>
+          <li><Link to="/page2">Page 2</Link></li>
         </ul>
-        <TransitionGroup component="div" transitionName="swap">
-          <RouteHandler key={key}/>
-        </TransitionGroup>
+        <CSSTransitionGroup component="div" transitionName="swap">
+          {React.cloneElement(this.props.children || <div />, { key: key })}
+        </CSSTransitionGroup>
       </div>
     );
   }
 });
 
 var Page1 = React.createClass({
-
-    contextTypes: {
-      router: React.PropTypes.func
-    },
-
-  render: function () {
-    var name = this.context.router.getCurrentPath();
+  render() {
+    var key = this.props.location.pathname;
     return (
       <div className="Image">
         <h1>Page 1</h1>
         <ul>
-          <li><Link to="page1-tab1">Tab 1</Link></li>
-          <li><Link to="page1-tab2">Tab 2</Link></li>
+          <li><Link to="/page1/tab1">Tab 1</Link></li>
+          <li><Link to="/page1/tab2">Tab 2</Link></li>
         </ul>
-        <TransitionGroup component="div" transitionName="example">
-          <RouteHandler key={name}/>
-        </TransitionGroup>
+        <CSSTransitionGroup component="div" transitionName="example">
+          {React.cloneElement(this.props.children || <div/>, { key: key })}
+        </CSSTransitionGroup>
       </div>
     );
   }
 });
 
 var Page2 = React.createClass({
-
-    contextTypes: {
-      router: React.PropTypes.func
-    },
-
-  render: function () {
-    var name = this.context.router.getCurrentPath();
+  render() {
+    var key = this.props.location.pathname;
     return (
       <div className="Image">
         <h1>Page 2</h1>
         <ul>
-          <li><Link to="page2-tab1">Tab 1</Link></li>
-          <li><Link to="page2-tab2">Tab 2</Link></li>
+          <li><Link to="/page2/tab1">Tab 1</Link></li>
+          <li><Link to="/page2/tab2">Tab 2</Link></li>
         </ul>
-        <TransitionGroup component="div" transitionName="example">
-          <RouteHandler key={name}/>
-        </TransitionGroup>
+        <CSSTransitionGroup component="div" transitionName="example">
+          {React.cloneElement(this.props.children || <div/>, { key: key })}
+        </CSSTransitionGroup>
       </div>
     );
   }
 });
 
 var Tab1 = React.createClass({
-  render: function () {
+  render() {
     return (
       <div className="Image">
         <h2>Tab 1</h2>
@@ -85,7 +72,7 @@ var Tab1 = React.createClass({
 });
 
 var Tab2 = React.createClass({
-  render: function () {
+  render() {
     return (
       <div className="Image">
         <h2>Tab 2</h2>
@@ -95,19 +82,17 @@ var Tab2 = React.createClass({
   }
 });
 
-var routes = (
-  <Route handler={App}>
-    <Route name="page1" handler={Page1}>
-      <Route name="page1-tab1" path="tab1" handler={Tab1} />
-      <Route name="page1-tab2" path="tab2" handler={Tab2} />
+React.render((
+  <Router>
+    <Route path="/" component={App}>
+      <Route path="page1" component={Page1}>
+        <Route path="tab1" component={Tab1} />
+        <Route path="tab2" component={Tab2} />
+      </Route>
+      <Route path="page2" component={Page2}>
+        <Route path="tab1" component={Tab1} />
+        <Route path="tab2" component={Tab2} />
+      </Route>
     </Route>
-    <Route name="page2" handler={Page2}>
-      <Route name="page2-tab1" path="tab1" handler={Tab1} />
-      <Route name="page2-tab2" path="tab2" handler={Tab2} />
-    </Route>
-  </Route>
-);
-
-Router.run(routes, function (Handler) {
-  React.render(<Handler/>, document.getElementById('example'));
-});
+  </Router>
+), document.getElementById('example'));
