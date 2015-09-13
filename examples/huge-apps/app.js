9d1d50a7f64802975afaddc45fcc00ab9d09a931
Barrett Sonntag <barrett@sosuke.com>
2015-09-13 01:41:56 -0500

    add comment showing the unrolled loop of the huge-apps Router

--- examples/huge-apps/app.js
+++ examples/huge-apps/app.js
@@ -21,3 +21,48 @@ React.render(
   <Router routes={rootRoute} />,
   document.getElementById('example')
 );
+
+// I've unrolled the recursive directory loop that is happening above to get a
+// better idea of just what this huge-apps Router looks like
+//
+// import { Route } from 'react-router'
+
+// import App from './components/App';
+// import Course from './routes/Course/components/Course';
+// import AnnouncementsSidebar from './routes/Course/routes/Announcements/components/Sidebar';
+// import Announcements from './routes/Course/routes/Announcements/components/Announcements';
+// import Announcement from './routes/Course/routes/Announcements/routes/Announcement/components/Announcement';
+// import AssignmentsSidebar from './routes/Course/routes/Assignments/components/Sidebar';
+// import Assignments from './routes/Course/routes/Assignments/components/Assignments';
+// import Assignment from './routes/Course/routes/Assignments/routes/Assignment/components/Assignment';
+// import CourseGrades from './routes/Course/routes/Grades/components/Grades';
+// import Calendar from './routes/Calendar/components/Calendar';
+// import Grades from './routes/Grades/components/Grades';
+// import Messages from './routes/Messages/components/Messages';
+
+// React.render(
+//   <Router>
+//     <Route path="/" component={App}>
+//       <Route path="calendar" component={Calendar} />
+//       <Route path="course/:courseId" component={Course}>
+//         <Route path="announcements" components={{
+//           sidebar: AnnouncementsSidebar,
+//           main: Announcements
+//         }}>
+//           <Route path=":announcementId" component={Announcement} />
+//         </Route>
+//         <Route path="assignments" components={{
+//           sidebar: AssignmentsSidebar,
+//           main: Assignments
+//         }}>
+//           <Route path=":assignmentId" component={Assignment} />
+//         </Route>
+//         <Route path="grades" component={CourseGrades} />
+//       </Route>
+//       <Route path="grades" component={Grades} />
+//       <Route path="messages" component={Messages} />
+//       <Route path="profile" component={Calendar} />
+//     </Route>
+//   </Router>,
+//   document.getElementById('example')
+// );
