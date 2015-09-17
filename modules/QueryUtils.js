3f759ac3c5f9c371c5dd05ae6630e62401112d97
Sabra Pratt <pratt.sabra@gmail.com>
2015-09-17 13:25:41 -0700

    Remove unused QueryUtils

--- modules/QueryUtils.js
+++
@@ -1,9 +0,0 @@
-import qs from 'qs'
-
-export function stringifyQuery(query) {
-  return qs.stringify(query, { arrayFormat: 'brackets' })
-}
-
-export function parseQueryString(queryString) {
-  return qs.parse(queryString)
-}
