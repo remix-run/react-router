06ff577ed9e1f13864d5d1e13afb7ba535a7066b
Michael Jackson <mjijackson@gmail.com>
2015-09-24 22:42:23 -0700

    Remove usage of Object.assign

--- examples/huge-apps/components/GlobalNav.js
+++ examples/huge-apps/components/GlobalNav.js
@@ -1,7 +1,28 @@
-import React from 'react';
-import { Link } from 'react-router';
+import React from 'react'
+import { Link } from 'react-router'
 
-const styles = {};
+const dark = 'hsl(200, 20%, 20%)'
+const light = '#fff'
+const styles = {}
+
+styles.wrapper = {
+  padding: '10px 20px',
+  overflow: 'hidden',
+  background: dark,
+  color: light
+}
+
+styles.link = {
+  padding: 11,
+  color: light,
+  fontWeight: 200
+}
+
+styles.activeLink = {
+  ...styles.link,
+  background: light,
+  color: dark
+}
 
 class GlobalNav extends React.Component {
 
@@ -10,19 +31,19 @@ class GlobalNav extends React.Component {
       id: 1,
       name: 'Ryan Florence'
     }
-  };
-
-  constructor (props, context) {
-    super(props, context);
-    this.logOut = this.logOut.bind(this);
   }
 
-  logOut () {
-    alert('log out');
+  constructor(props, context) {
+    super(props, context)
+    this.logOut = this.logOut.bind(this)
   }
 
-  render () {
-    var { user } = this.props;
+  logOut() {
+    alert('log out')
+  }
+
+  render() {
+    var { user } = this.props
     return (
       <div style={styles.wrapper}>
         <div style={{float: 'left'}}>
@@ -35,29 +56,8 @@ class GlobalNav extends React.Component {
           <Link style={styles.link} to="/profile">{user.name}</Link> <button onClick={this.logOut}>log out</button>
         </div>
       </div>
-    );
+    )
   }
 }
 
-var dark = 'hsl(200, 20%, 20%)';
-var light = '#fff';
-
-styles.wrapper = {
-  padding: '10px 20px',
-  overflow: 'hidden',
-  background: dark,
-  color: light
-};
-
-styles.link = {
-  padding: 11,
-  color: light,
-  fontWeight: 200
-}
-
-styles.activeLink = Object.assign({}, styles.link, {
-  background: light,
-  color: dark
-});
-
-export default GlobalNav;
+export default GlobalNav
--- examples/huge-apps/routes/Course/components/Nav.js
+++ examples/huge-apps/routes/Course/components/Nav.js
@@ -1,53 +1,50 @@
-import React from 'react';
-import { Link } from 'react-router';
-import AnnouncementsRoute from '../routes/Announcements';
-import AssignmentsRoute from '../routes/Assignments';
-import GradesRoute from '../routes/Grades';
+import React from 'react'
+import { Link } from 'react-router'
+import AnnouncementsRoute from '../routes/Announcements'
+import AssignmentsRoute from '../routes/Assignments'
+import GradesRoute from '../routes/Grades'
 
-const styles = {};
+const styles = {}
 
 styles.nav = {
   borderBottom: '1px solid #aaa'
-};
+}
 
 styles.link = {
   display: 'inline-block',
   padding: 10,
-  textDecoration: 'none',
-};
+  textDecoration: 'none'
+}
 
-styles.activeLink = Object.assign({}, styles.link, {
+styles.activeLink = {
+  ...styles.link,
   //color: 'red'
-});
+}
 
 class Nav extends React.Component {
 
   render () {
-    var { course } = this.props;
-    var pages = [
+    const { course } = this.props
+    const pages = [
       ['announcements', 'Announcements'],
       ['assignments', 'Assignments'],
       ['grades', 'Grades'],
-    ];
+    ]
+
     return (
       <nav style={styles.nav}>
         {pages.map((page, index) => (
           <Link
             key={page[0]}
-            activeStyle={index === 0 ?
-              Object.assign({}, styles.activeLink, { paddingLeft: 0 }) :
-              styles.activeLink}
-            style={index === 0 ?
-              Object.assign({}, styles.link, { paddingLeft: 0 }) :
-              styles.link }
+            activeStyle={index === 0 ? { ...styles.activeLink, paddingLeft: 0 } : styles.activeLink}
+            style={index === 0 ? { ...styles.link, paddingLeft: 0 } : styles.link}
             to={`/course/${course.id}/${page[0]}`}
           >{page[1]}</Link>
         ))}
       </nav>
-    );
+    )
   }
 
 }
 
-export default Nav;
-
+export default Nav
