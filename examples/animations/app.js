e4a19e8773cafb8fad52d4a7e88b187aea2fdd7e
Mateusz Zatorski <mateuszzatorski@gmail.com>
2015-11-01 13:17:13 +0000

    fix animation example  ReactCSSTransitionGroup warning

--- examples/animations/app.js
+++ examples/animations/app.js
@@ -20,7 +20,11 @@ class App extends React.Component {
           <li><Link to="/page1">Page 1</Link></li>
           <li><Link to="/page2">Page 2</Link></li>
         </ul>
-        <ReactCSSTransitionGroup component="div" transitionName="example">
+        <ReactCSSTransitionGroup component="div"
+                                 transitionName="example"
+                                 transitionEnterTimeout={500}
+                                 transitionLeaveTimeout={500}
+        >
           {React.cloneElement(this.props.children || <div />, { key: pathname })}
         </ReactCSSTransitionGroup>
       </div>
