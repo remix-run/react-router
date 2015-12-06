f87367dfe8c0956dd081c124163d2fb5983d4711
Ryan Florence <rpflorence@gmail.com>
2015-12-06 04:47:09 -0800

    use createClass
    didn’t realize we were back to createClass again!

--- modules/RoutingContext.js
+++ modules/RoutingContext.js
@@ -2,14 +2,14 @@ import React from 'react'
 import RouterContext from './RouterContext'
 import warning from 'warning'
 
-class RoutingContext extends React.Component {
+const RoutingContext = React.createClass({
   componentWillMount() {
     warning(false, '`RoutingContext` has been renamed to `RouterContext`. Please use `import { RouterContext } from \'react-router\'.`')
-  }
+  },
 
   render() {
     return <RouterContext {...this.props}/>
   }
-}
+})
 
 export default RoutingContext
