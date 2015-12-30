54ec6a81d759bb78b17687d6b0689b0fc6e49873
Ryan Florence <rpflorence@gmail.com>
2015-12-30 15:27:34 -0800

    don’t create a hashHistory if there is no DOM

--- modules/hashHistory.js
+++ modules/hashHistory.js
@@ -1,5 +1,15 @@
 import useRouterHistory from './useRouterHistory'
 import createHashHistory from 'history/lib/createHashHistory'
 
-export default useRouterHistory(createHashHistory)()
+const canUseDOM = !!(
+  typeof window !== 'undefined' && window.document && window.document.createElement
+)
+
+let history
+
+if (canUseDOM) {
+  history = useRouterHistory(createHashHistory)()
+}
+
+export default history
 
