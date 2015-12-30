56d44968a5bf8b22d2c20fc01fe28b8a19ea3d96
Ryan Florence <rpflorence@gmail.com>
2015-12-30 14:45:16 -0800

    don’t create browserHistory on the server
    if you simply `require(‘react-router’)` on the
    server you get an error, because the singleton
    is actually created.
    
    maybe we should export a memory history?

--- modules/browserHistory.js
+++ modules/browserHistory.js
@@ -1,4 +1,14 @@
 import useRouterHistory from './useRouterHistory'
 import createBrowserHistory from 'history/lib/createBrowserHistory'
 
-export default useRouterHistory(createBrowserHistory)()
+const canUseDOM = !!(
+  typeof window !== 'undefined' && window.document && window.document.createElement
+)
+
+let history
+
+if (canUseDOM) {
+  history = useRouterHistory(createBrowserHistory)()
+}
+
+export default history
