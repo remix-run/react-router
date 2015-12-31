20598e8a88f0eded575a66ae41a91dc2a2fe5be3
Ryan Florence <rpflorence@gmail.com>
2015-12-31 13:13:08 -0800

    pass all args to `warning`
    fixes #2805

--- modules/warning.js
+++ modules/warning.js
@@ -1,6 +1,6 @@
 import warning from 'warning'
 
-export default function routerWarning(falseToWarn, message) {
+export default function routerWarning(falseToWarn, message, ...args) {
   message = `[react-router] ${message}`
-  warning(falseToWarn, message)
+  warning(falseToWarn, message, ...args)
 }
