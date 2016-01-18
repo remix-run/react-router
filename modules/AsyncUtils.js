6c441bec47dab73a1968dc43279d1059eb9c7a34
Jimmy Jia <tesrin@gmail.com>
2016-01-18 13:15:38 -0500

    Unwind the done callback as well

--- modules/AsyncUtils.js
+++ modules/AsyncUtils.js
@@ -1,15 +1,24 @@
 export function loopAsync(turns, work, callback) {
   let currentTurn = 0, isDone = false
-  let sync = false, hasNext = false
+  let sync = false, hasNext = false, doneArgs
 
   function done() {
     isDone = true
+    if (sync) {
+      // Iterate instead of recursing if possible.
+      doneArgs = [ ...arguments ]
+      return
+    }
+
     callback.apply(this, arguments)
   }
 
   function next() {
-    hasNext = true
+    if (isDone) {
+      return
+    }
 
+    hasNext = true
     if (sync) {
       // Iterate instead of recursing if possible.
       return
@@ -25,10 +34,14 @@ export function loopAsync(turns, work, callback) {
     sync = false
 
     if (isDone) {
+      // This means the loop finished synchronously.
+      callback.apply(this, doneArgs)
       return
     }
+
     if (currentTurn >= turns && hasNext) {
-      done()
+      isDone = true
+      callback()
     }
   }
 
