2d7b3bb549de328b7da8f65808af366366d25155
Jimmy Jia <tesrin@gmail.com>
2016-04-13 15:14:00 -0400

    Do not include deprecation code in production builds

--- modules/deprecateObjectProperties.js
+++ modules/deprecateObjectProperties.js
@@ -1,41 +1,56 @@
-/*eslint no-empty: 0*/
 import warning from './routerWarning'
 
-let useMembrane = false
+// No-op by default.
+let deprecateObjectProperties = object => object
 
 if (__DEV__) {
+  let useMembrane = false
+
   try {
     if (Object.defineProperty({}, 'x', { get() { return true } }).x) {
       useMembrane = true
     }
-  } catch(e) { }
-}
+  /* eslint-disable no-empty */
+  } catch(e) {}
+  /* eslint-enable no-empty */
 
-// wraps an object in a membrane to warn about deprecated property access
-export default function deprecateObjectProperties(object, message) {
-  if (!useMembrane)
-    return object
+  if (useMembrane) {
+    deprecateObjectProperties = (object, message) => {
+      // Wrap the deprecated object in a membrane to warn on property access.
+      const membrane = {}
 
-  const membrane = {}
-
-  for (let prop in object) {
-    if (typeof object[prop] === 'function') {
-      membrane[prop] = function () {
-        warning(false, message)
-        return object[prop].apply(object, arguments)
-      }
-    } else {
-      Object.defineProperty(membrane, prop, {
-        configurable: false,
-        enumerable: false,
-        get() {
-          warning(false, message)
-          return object[prop]
+      for (const prop in object) {
+        if (!Object.prototype.hasOwnProperty.call(object, prop)) {
+          continue
         }
-      })
+
+        if (typeof object[prop] === 'function') {
+          // Can't use fat arrow here because of use of arguments below.
+          membrane[prop] = function () {
+            warning(false, message)
+            return object[prop].apply(object, arguments)
+          }
+          continue
+        }
+
+        // These properties are non-enumerable to prevent React dev tools from
+        // seeing them and causing spurious warnings when accessing them. In
+        // principle this could be done with a proxy, but support for the
+        // ownKeys trap on proxies is not universal, even among browsers that
+        // otherwise support proxies.
+        Object.defineProperty(membrane, prop, {
+          configurable: false,
+          enumerable: false,
+          get() {
+            warning(false, message)
+            return object[prop]
+          }
+        })
+      }
+
+      return membrane
     }
   }
-
-  return membrane
 }
 
+export default deprecateObjectProperties
