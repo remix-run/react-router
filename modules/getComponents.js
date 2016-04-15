793dfaa89bc84ec987d7e8ece32651794fb9953b
Jimmy Jia <tesrin@gmail.com>
2016-04-15 16:02:37 -0400

    Don't use Object.assign

--- modules/getComponents.js
+++ modules/getComponents.js
@@ -9,35 +9,37 @@ function getComponentsForRoute(nextState, route, callback) {
   }
 
   const getComponent = route.getComponent || route.getComponents
-  if (getComponent) {
-    let nextStateWithLocation = { ...nextState }
-    const { location } = nextState
-
-    if (__DEV__ && canUseMembrane) {
-      // I don't use deprecateObjectProperties here because I want to keep the
-      // same code path between development and production, in that we just
-      // assign extra properties to the copy of the state object in both cases.
-      for (const prop in location) {
-        if (!Object.prototype.hasOwnProperty.call(location, prop)) {
-          continue
-        }
-
-        Object.defineProperty(nextStateWithLocation, prop, {
-          get() {
-            warning(false, 'Accessing location properties from the first argument to `getComponent` and `getComponents` is deprecated. That argument is now the router state (`nextState`) rather than the location. To access the location, use `nextState.location`.')
-            return location[prop]
-          }
-        })
-      }
-    } else {
-      Object.assign(nextStateWithLocation, location)
-    }
-
-    getComponent.call(route, nextStateWithLocation, callback)
+  if (!getComponent) {
+    callback()
     return
   }
 
-  callback()
+  const { location } = nextState
+  let nextStateWithLocation
+
+  if (__DEV__ && canUseMembrane) {
+    nextStateWithLocation = { ...nextState }
+
+    // I don't use deprecateObjectProperties here because I want to keep the
+    // same code path between development and production, in that we just
+    // assign extra properties to the copy of the state object in both cases.
+    for (const prop in location) {
+      if (!Object.prototype.hasOwnProperty.call(location, prop)) {
+        continue
+      }
+
+      Object.defineProperty(nextStateWithLocation, prop, {
+        get() {
+          warning(false, 'Accessing location properties from the first argument to `getComponent` and `getComponents` is deprecated. That argument is now the router state (`nextState`) rather than the location. To access the location, use `nextState.location`.')
+          return location[prop]
+        }
+      })
+    }
+  } else {
+    nextStateWithLocation = { ...nextState, ...location }
+  }
+
+  getComponent.call(route, nextStateWithLocation, callback)
 }
 
 /**
