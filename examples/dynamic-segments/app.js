62c688563c57820e0a4fe08c67513e8cf206da28
Dylan Companjen <info@dylancompanjen.com>
2016-06-04 11:11:38 +0200

    Define presentational components as stateless functional components

--- examples/dynamic-segments/app.js
+++ examples/dynamic-segments/app.js
@@ -1,49 +1,41 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { render } from 'react-dom'
 import { browserHistory, Router, Route, Link, Redirect } from 'react-router'
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <ul>
-          <li><Link to="/user/123" activeClassName="active">Bob</Link></li>
-          <li><Link to="/user/abc" activeClassName="active">Sally</Link></li>
-        </ul>
-        {this.props.children}
-      </div>
-    )
-  }
+const App = (props) => (
+  <div>
+    <ul>
+      <li><Link to="/user/123" activeClassName="active">Bob</Link></li>
+      <li><Link to="/user/abc" activeClassName="active">Sally</Link></li>
+    </ul>
+    {props.children}
+  </div>
+)
+
+const User = (props) => {
+  const { userID } = props.params
+
+  return (
+    <div className="User">
+      <h1>User id: {userID}</h1>
+      <ul>
+        <li><Link to={`/user/${userID}/tasks/foo`} activeClassName="active">foo task</Link></li>
+        <li><Link to={`/user/${userID}/tasks/bar`} activeClassName="active">bar task</Link></li>
+      </ul>
+      {props.children}
+    </div>
+  )
 }
 
-class User extends Component {
-  render() {
-    const { userID } = this.props.params
+const Task = (props) => {
+  const { userID, taskID } = props.params
 
-    return (
-      <div className="User">
-        <h1>User id: {userID}</h1>
-        <ul>
-          <li><Link to={`/user/${userID}/tasks/foo`} activeClassName="active">foo task</Link></li>
-          <li><Link to={`/user/${userID}/tasks/bar`} activeClassName="active">bar task</Link></li>
-        </ul>
-        {this.props.children}
-      </div>
-    )
-  }
-}
-
-class Task extends Component {
-  render() {
-    const { userID, taskID } = this.props.params
-
-    return (
-      <div className="Task">
-        <h2>User ID: {userID}</h2>
-        <h3>Task ID: {taskID}</h3>
-      </div>
-    )
-  }
+  return (
+    <div className="Task">
+      <h2>User ID: {userID}</h2>
+      <h3>Task ID: {taskID}</h3>
+    </div>
+  )
 }
 
 render((
