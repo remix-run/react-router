cf74ecb69f200921a4fadd594493e8c98242d639
Dylan Companjen <info@dylancompanjen.com>
2016-06-04 11:36:25 +0200

    Define presentational components as stateless functional components

--- examples/query-params/app.js
+++ examples/query-params/app.js
@@ -1,36 +1,30 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { render } from 'react-dom'
 import { browserHistory, Router, Route, Link } from 'react-router'
 
-class User extends Component {
-  render() {
-    let { userID } = this.props.params
-    let { query } = this.props.location
-    let age = query && query.showAge ? '33' : ''
+const User = (props) => {
+  let { userID } = props.params
+  let { query } = props.location
+  let age = query && query.showAge ? '33' : ''
 
-    return (
-      <div className="User">
-        <h1>User id: {userID}</h1>
-        {age}
-      </div>
-    )
-  }
+  return (
+    <div className="User">
+      <h1>User id: {userID}</h1>
+      {age}
+    </div>
+  )
 }
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <ul>
-          <li><Link to="/user/bob" activeClassName="active">Bob</Link></li>
-          <li><Link to={{ pathname: '/user/bob', query: { showAge: true } }} activeClassName="active">Bob With Query Params</Link></li>
-          <li><Link to="/user/sally" activeClassName="active">Sally</Link></li>
-        </ul>
-        {this.props.children}
-      </div>
-    )
-  }
-}
+const App = (props) => (
+  <div>
+    <ul>
+      <li><Link to="/user/bob" activeClassName="active">Bob</Link></li>
+      <li><Link to={{ pathname: '/user/bob', query: { showAge: true } }} activeClassName="active">Bob With Query Params</Link></li>
+      <li><Link to="/user/sally" activeClassName="active">Sally</Link></li>
+    </ul>
+    {props.children}
+  </div>
+)
 
 render((
   <Router history={browserHistory}>
