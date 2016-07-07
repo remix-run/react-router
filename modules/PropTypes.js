ab82466472e9d4c3bfd792f97cba465519b67527
Michael Jackson <mjijackson@gmail.com>
2016-07-06 21:41:48 -0600

    Better location prop type

--- modules/PropTypes.js
+++ modules/PropTypes.js
@@ -1,6 +1,11 @@
 import { PropTypes } from 'react'
 
-export const location = PropTypes.object
+export const location = PropTypes.shape({
+  pathname: PropTypes.string.isRequired,
+  search: PropTypes.string.isRequired,
+  hash: PropTypes.string.isRequired,
+  state: PropTypes.any
+})
 
 export const router = PropTypes.shape({
   createHref: PropTypes.func.isRequired,
