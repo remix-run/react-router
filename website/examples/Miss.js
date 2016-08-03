1fc7ae8bad1828866f314fe6db88b4b0c2ae83e2
Ryan Florence <rpflorence@gmail.com>
2016-08-03 15:20:14 -0700

    fix Miss example

--- website/examples/Miss.js
+++ website/examples/Miss.js
@@ -25,7 +25,7 @@ const Home = () => (
 
 const WillMatch = () => <h3>Matched!</h3>
 
-const NoMatch = () => ({ location }) => (
+const NoMatch = ({ location }) => (
   <div>
     <h3>No match for <code>{location.pathname}</code></h3>
   </div>
