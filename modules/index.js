b1694e0fc96490d1282c14a28d7ab0ba0d54e960
Michael Jackson <mjijackson@gmail.com>
2016-08-18 12:14:46 -0700

    Add various routers to top-level exports

--- modules/index.js
+++ modules/index.js
@@ -6,3 +6,12 @@ export Miss from './Miss'
 export Redirect from './Redirect'
 export Router from './Router'
 export matchPattern from './matchPattern'
+
+// High-level wrappers
+export BrowserRouter from './BrowserRouter'
+export HashRouter from './HashRouter'
+export MemoryRouter from './MemoryRouter'
+
+// Low-level building blocks
+export HistoryRouter from './HistoryRouter'
+export StaticRouter from './StaticRouter'
