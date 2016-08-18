ae24bcffce8ac54e4dd5e1e3c1240012c9eb720b
Thomas Florin <thoflo002@gmail.com>
2016-08-18 11:22:09 -0500

    Updating withRouter based on comments

--- modules/withRouter.js
+++ modules/withRouter.js
@@ -1,18 +1,28 @@
 import React from 'react'
 import hoistStatics from 'hoist-non-react-statics'
 import { routerShape } from './PropTypes'
+import warning from './routerWarning'
+
 
 function getDisplayName(WrappedComponent) {
   return WrappedComponent.displayName || WrappedComponent.name || 'Component'
 }
 
-export default function withRouter(WrappedComponent) {
+export default function withRouter(WrappedComponent, options) {
   const WithRouter = React.createClass({
     contextTypes: { router: routerShape },
     propTypes: { router: routerShape },
+    getWrappedInstance() {
+      warning(options && options.withRef, 'To access the wrappedInstance you must provide {withRef : true} as the first argument of the withRouter call')
+      return this.refs.wrappedInstance
+    },
     render() {
       const router = this.props.router || this.context.router
-      return <WrappedComponent {...this.props} router={router} />
+      if(options && options.withRef) {
+        return <WrappedComponent {...this.props} ref="wrappedInstance" router={router} />
+      }else{
+        return <WrappedComponent {...this.props} router={router} />
+      }
     }
   })
 
