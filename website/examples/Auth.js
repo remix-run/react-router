f9a657a35c9bd3ce59a2903749938efaa7e1aef8
Sunil Pai <threepointone@gmail.com>
2016-09-14 10:16:29 +0530

    Update Auth.js

--- website/examples/Auth.js
+++ website/examples/Auth.js
@@ -7,7 +7,7 @@ import Router from 'react-router/BrowserRouter'
 
 ////////////////////////////////////////////////////////////
 // 1. Click the public page
-// 2. Click the protected page<
+// 2. Click the protected page
 // 3. Log in
 // 4. Click the back button, note the url each time
 
