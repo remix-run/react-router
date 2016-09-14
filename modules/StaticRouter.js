07725d7ef566a9a4bcfcf91b1f0816dd1f546393
C.T. Lin <chentsulin@gmail.com>
2016-09-14 17:00:56 +0800

    Pass basename to StaticRouter for createHref

--- modules/StaticRouter.js
+++ modules/StaticRouter.js
@@ -20,6 +20,7 @@ class StaticRouter extends React.Component {
     children: PropTypes.oneOfType([ PropTypes.node, PropTypes.func ]),
     createHref: PropTypes.func.isRequired,
     location: PropTypes.oneOfType([ PropTypes.object, PropTypes.string ]).isRequired,
+    basename: PropTypes.string,
     onPush: PropTypes.func.isRequired,
     onReplace: PropTypes.func.isRequired,
     stringifyQuery: PropTypes.func.isRequired,
@@ -45,7 +46,10 @@ class StaticRouter extends React.Component {
 
   getChildContext() {
     const createHref = (to) => {
-      const path = createRouterPath(to, this.props.stringifyQuery)
+      const path = createRouterPath(
+        this.props.basename ? this.props.basename + to : to,
+        this.props.stringifyQuery
+      )
       return this.props.createHref(path)
     }
 
