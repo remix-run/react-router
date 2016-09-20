cabc74adf1d0df6a0a4b2faed737ad0a9120f390
Robin Tollisen <al4crity@gmail.com>
2016-09-20 08:32:43 +0200

    Made the test verify that correct route is chosen rather than just verifying no error.

--- modules/__tests__/MatchProvider-test.js
+++ modules/__tests__/MatchProvider-test.js
@@ -10,11 +10,11 @@ describe('MatchProvider', () => {
 
   it('works with multiple routes', () => {
     const TEXT = 'TEXT'
-    const location = { pathname: '/', state: { test: TEXT } }
+    const location = { pathname: '/test', state: { test: TEXT } }
     const html = renderToString(
       <MatchProvider>
         <Match exactly pattern="/" location={location} render={() => (
-          <div>{location.state.test}</div>
+          <div></div>
         )}/>
         <Match exactly pattern="/test" location={location} render={() => (
           <div>{location.state.test}</div>
