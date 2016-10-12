c85733a316b18ad54690be324be1d1e89fe9a3e4
Kristof Dreier <kristof.dreier@diva-e.com>
2016-10-12 13:13:33 +0200

    added tests for different hashtypes

--- modules/__tests__/HashRouter-test.js
+++ modules/__tests__/HashRouter-test.js
@@ -5,29 +5,50 @@ import HashRouter from '../HashRouter'
 import Link from '../Link'
 
 describe('HashRouter', () => {
-  describe('empty hashType', () => {
-    it('adds a hash to the link', () => {
-      const div = document.createElement('div')
-      render((
-        <HashRouter>
-          <Link to="/foo" />
-        </HashRouter>
-      ), div)
-      const a = div.querySelector('a')
-      expect(a.getAttribute('href')).toEqual('#/foo')
+  const linkCreator = (hashType, to) => {
+    const div = document.createElement('div')
+    render((
+      <HashRouter hashType={hashType}>
+        <Link to={to} />
+      </HashRouter>
+    ), div)
+    const a = div.querySelector('a')
+    return a
+  }
+
+  describe('hashType slash', () => {
+    it('adds a hash link', () => {
+      const link = linkCreator('slash', '/foo')
+      expect(link.getAttribute('href')).toEqual('#/foo')
+    })
+
+    it('adds a hash link with a leading slash if it is missing', () => {
+      const link = linkCreator('slash', 'foo')
+      expect(link.getAttribute('href')).toEqual('#/foo')
     })
   })
 
   describe('hashType hashbang', () => {
     it('adds a hashbang to the link', () => {
-      const div = document.createElement('div')
-      render((
-        <HashRouter hashType="hashbang">
-          <Link to="/foo" />
-        </HashRouter>
-      ), div)
-      const a = div.querySelector('a')
-      expect(a.getAttribute('href')).toEqual('#!/foo')
+      const link = linkCreator('hashbang', '/foo')
+      expect(link.getAttribute('href')).toEqual('#!/foo')
+    })
+
+    it('adds a hashbang to the link with a leading slash if it is missing', () => {
+      const link = linkCreator('hashbang', 'foo')
+      expect(link.getAttribute('href')).toEqual('#!/foo')
+    })
+  })
+
+  describe('hashType noslash', () => {
+    it('adds a hash link', () => {
+      const link = linkCreator('noslash', 'foo')
+      expect(link.getAttribute('href')).toEqual('#foo')
+    })
+
+    it('adds a hash link and removes the leading slash', () => {
+      const link = linkCreator('noslash', '/foo')
+      expect(link.getAttribute('href')).toEqual('#foo')
     })
   })
 })
