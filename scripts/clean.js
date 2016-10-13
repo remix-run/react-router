6f246839570b03a76864aac959b80279fe3fb06f
Ali Sheehan-Dare <alisd23@live.co.uk>
2016-10-13 02:13:01 +0100

    Fix scripts on windows (#4038)
    * fix tasks on windows
    
    * add watch task
    
    * remove delete dep and ignore scripts folder from eslint

---
+++ scripts/clean.js
@@ -0,0 +1,27 @@
+const fs = require('fs')
+const path = require('path')
+
+const root = (...args) => path.join(__dirname, '..', ...args)
+
+const files = fs.readdirSync(root('modules'))
+  .filter(file => file.includes('.js'))
+
+const promises = Promise.all(
+  files.map(file => new Promise((resolve, reject) => {
+    try {
+      fs.unlinkSync(root(file))
+      resolve()
+    } catch (e) {
+      reject(e)
+    }
+  }))
+)
+
+promises
+  .then(() => {
+    console.log('Clean task complete')
+  })
+  .catch(err => {
+    console.log('Clean task failed')
+    console.log(err)
+  })
