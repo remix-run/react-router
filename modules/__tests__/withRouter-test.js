6baf261b6e641b345fc23b6d1a4001eed877d38e
Ali Sheehan-Dare <alisd23@live.co.uk>
2016-10-14 11:38:40 +0100

    feat: add further features and tests

--- modules/__tests__/withRouter-test.js
+++ modules/__tests__/withRouter-test.js
@@ -12,11 +12,16 @@ describe('withRouter', () => {
     </MemoryRouter>
   )
 
-  it('passes history, router, and location as props', (done) => {
-    const div = document.createElement('div')
-    const location = { pathname: '/url-1' }
+  let div, location
 
-    const component = (props) => {
+  beforeEach(() => {
+    div = document.createElement('div')
+  })
+
+  it('passes history, router, and location as props', (done) => {
+    location = { pathname: '/url-1' }
+
+    const Component = (props) => {
       expect(props.history).toNotBe(undefined)
       expect(props.router).toNotBe(undefined)
       expect(props.location).toNotBe(undefined)
@@ -27,18 +32,17 @@ describe('withRouter', () => {
     }
 
     render(
-      <App location={location} Component={withRouter(component)} />,
+      <App location={location} Component={withRouter(Component)} />,
       div,
       () => done()
     )
   })
 
   it('re-renders and passes down new props when location changes', (done) => {
-    const div = document.createElement('div')
-    const location = { pathname: '/url-1' }
+    location = { pathname: '/url-1' }
     let redirected = false
 
-    const component = (props) => {
+    const Component = (props) => {
       if (redirected) {
         expect(props.location.pathname).toBe('/url-2')
         redirected = true
@@ -50,9 +54,59 @@ describe('withRouter', () => {
     }
 
     render(
-      <App location={location} Component={withRouter(component)} />,
+      <App location={location} Component={withRouter(Component)} />,
       div,
       () => done()
     )
+  })
+
+  it('can be used with or without options', () => {
+    const Component = () => <div />
+    const withOptions = withRouter({})(Component)
+    const withoutOptions = withRouter(Component)
+    expect(typeof withOptions).toBe('function')
+    expect(typeof withoutOptions).toBe('function')
+  })
+
+  it('hoists statics', () => {
+    class Component extends React.Component {
+      static Test = 'test'
+      render() { return null }
+    }
+
+    expect(withRouter(Component).Test).toBe('test')
+  })
+
+  it('exposes a displayName property', () => {
+    class MyComponent extends React.Component {
+      static displayName = 'MyComponent'
+      render() { return null }
+    }
+    expect(withRouter(MyComponent).displayName).toBe('withRouter(MyComponent)')
+  })
+
+  it('exposes a WrappedComponent property', () => {
+    class MyComponent extends React.Component {
+      render() { return null }
+    }
+    expect(withRouter(MyComponent).WrappedComponent).toBe(MyComponent)
+  })
+
+  it('can expose a wrappedInstance property on the instance', (done) => {
+    class MyComponent extends React.Component {
+      render() { return null }
+    }
+    const EnhancedComponent = withRouter({ withRef: true })(MyComponent)
+
+    class App extends React.Component {
+      checkRef(ref) {
+        expect(ref.wrappedInstance instanceof MyComponent).toBe(true)
+      }
+      render() {
+        return <EnhancedComponent ref={e => this.checkRef(e)} />
+      }
+    }
+
+    render(<App />, div, () => done())
   })
 })
--- modules/withRouter.js
+++ modules/withRouter.js
@@ -1,35 +1,78 @@
 import React, { Component } from 'react'
+import invariant from 'invariant'
+import hoistStatics from 'hoist-non-react-statics'
 import { historyContext } from 'react-history/PropTypes'
 import { routerContext } from './PropTypes'
 import { LocationSubscriber } from './Broadcasts'
 
-const withRouter = (WrappedComponent) => {
-  class WithRouter extends Component {
-    static contextTypes = {
-      history: historyContext,
-      router: routerContext
+function getDisplayName(WrappedComponent) {
+  return WrappedComponent.displayName || WrappedComponent.name || 'Component'
+}
+
+const withRouter = (options) => {
+  const hoc = (WrappedComponent) => {
+    const withRef = options && options.withRef
+
+    class WithRouter extends Component {
+      static contextTypes = {
+        history: historyContext,
+        router: routerContext
+      }
+
+      getWrappedInstance() {
+        invariant(
+          withRef,
+          'To access the wrapped instance, you need to specify ' +
+          '`{ withRef: true }` as an option to withRouter - ' +
+          '@withRouter(opts) OR withRouter(opts)(Component)'
+        )
+
+        return this.wrappedInstance
+      }
+
+      render() {
+        const { router, history } = this.context
+        const modifiedHistory = { ...history }
+        delete modifiedHistory.location
+        const props = {
+          ...this.props,
+          history: modifiedHistory,
+          router
+        }
+        if (withRef) {
+          props.ref = (c) => { this.wrappedInstance = c }
+        }
+
+        return (
+          <LocationSubscriber>
+            {(locationContext) => {
+              return (
+                <WrappedComponent
+                  {...props}
+                  location={locationContext}
+                />
+              )
+            }}
+          </LocationSubscriber>
+        )
+      }
     }
 
-    render() {
-      return (
-        <LocationSubscriber>
-          {(locationContext) => {
-            const history = { ...this.context.history }
-            delete history.location
-            return (
-              <WrappedComponent
-                {...this.props}
-                location={locationContext}
-                history={history}
-                router={this.context.router}
-              />
-            )
-          }}
-        </LocationSubscriber>
-      )
-    }
+    WithRouter.displayName = `withRouter(${getDisplayName(WrappedComponent)})`
+    WithRouter.WrappedComponent = WrappedComponent
+
+    return hoistStatics(WithRouter, WrappedComponent)
   }
-  return WithRouter
+
+  // Allow using decorator with or without options
+  // @withRouter **OR** @withRouter(opts)
+  // withRouter(Component) **OR** withRouter(opts)(Component)
+  if (typeof options === 'function') {
+    // Calling like: withRouter(Component)
+    return hoc(options)
+  } else {
+    return hoc
+  }
 }
 
 export default withRouter
