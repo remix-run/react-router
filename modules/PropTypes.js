eb7d0e2e3c8d1034301102ac92c08cb8cd40054c
Asa Ayers <Asa@AsaAyers.com>
2016-10-20 16:35:52 -0700

    Export dummy proptypes for production.

--- modules/PropTypes.js
+++ modules/PropTypes.js
@@ -1,48 +1,61 @@
 import { PropTypes } from 'react'
 
-export const action = PropTypes.oneOf([
-  'PUSH',
-  'REPLACE',
-  'POP'
-])
+const placeholder = function() {}
+placeholder.isRequired = function() {}
 
-export const matchContext = PropTypes.shape({
-  addMatch: PropTypes.func.isRequired,
-  removeMatch: PropTypes.func.isRequired
-})
+export let action = placeholder
+export let history = placeholder
+export let location = placeholder
+export let matchContext = placeholder
+export let historyContext = placeholder
+export let routerContext = placeholder
 
-export const history = PropTypes.shape({
-  listen: PropTypes.func.isRequired,
-  listenBefore: PropTypes.func.isRequired,
-  push: PropTypes.func.isRequired,
-  replace: PropTypes.func.isRequired,
-  go: PropTypes.func.isRequired
-})
+if (__DEV__) {
 
-export const location = PropTypes.shape({
-  pathname: PropTypes.string.isRequired,
-  search: PropTypes.string.isRequired,
-  hash: PropTypes.string.isRequired,
-  state: PropTypes.any,
-  key: PropTypes.string
-})
+  action = PropTypes.oneOf([
+    'PUSH',
+    'REPLACE',
+    'POP'
+  ])
 
-export const historyContext = PropTypes.shape({
-  action: action.isRequired,
-  location: location.isRequired,
-  push: PropTypes.func.isRequired,
-  replace: PropTypes.func.isRequired,
-  go: PropTypes.func.isRequired,
-  goBack: PropTypes.func.isRequired,
-  goForward: PropTypes.func.isRequired,
-  canGo: PropTypes.func,
-  block: PropTypes.func.isRequired
-})
+  history = PropTypes.shape({
+    listen: PropTypes.func.isRequired,
+    listenBefore: PropTypes.func.isRequired,
+    push: PropTypes.func.isRequired,
+    replace: PropTypes.func.isRequired,
+    go: PropTypes.func.isRequired
+  })
 
-export const routerContext = PropTypes.shape({
-  transitionTo: PropTypes.func.isRequired,
-  replaceWith: PropTypes.func.isRequired,
-  blockTransitions: PropTypes.func.isRequired,
-  createHref: PropTypes.func.isRequired
-})
+  location = PropTypes.shape({
+    pathname: PropTypes.string.isRequired,
+    search: PropTypes.string.isRequired,
+    hash: PropTypes.string.isRequired,
+    state: PropTypes.any,
+    key: PropTypes.string
+  })
 
+  matchContext = PropTypes.shape({
+    addMatch: PropTypes.func.isRequired,
+    removeMatch: PropTypes.func.isRequired
+  })
+
+  historyContext = PropTypes.shape({
+    action: action.isRequired,
+    location: location.isRequired,
+    push: PropTypes.func.isRequired,
+    replace: PropTypes.func.isRequired,
+    go: PropTypes.func.isRequired,
+    goBack: PropTypes.func.isRequired,
+    goForward: PropTypes.func.isRequired,
+    canGo: PropTypes.func,
+    block: PropTypes.func.isRequired
+  })
+
+  routerContext = PropTypes.shape({
+    transitionTo: PropTypes.func.isRequired,
+    replaceWith: PropTypes.func.isRequired,
+    blockTransitions: PropTypes.func.isRequired,
+    createHref: PropTypes.func.isRequired
+  })
+
+}
