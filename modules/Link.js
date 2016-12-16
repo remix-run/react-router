21219fe25fb32b6704029976a2e0b8163b671a56
Michael Jackson <mjijackson@gmail.com>
2016-12-16 15:57:35 -0800

    Do not pass action/location props to <a>

--- modules/Link.js
+++ modules/Link.js
@@ -50,7 +50,7 @@ class Link extends React.Component {
   }
 
   render() {
-    const { history, replace, to, ...props } = this.props // eslint-disable-line no-unused-vars
+    const { action, location, history, replace, to, ...props } = this.props // eslint-disable-line no-unused-vars
     const href = typeof to === 'string' ? to : history.createHref(to)
 
     return (
