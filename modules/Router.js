5435a2cb40249aef0599a46aa03ef8abd355b835
Michael Jackson <mjijackson@gmail.com>
2016-12-16 14:37:33 -0800

    <Router> renders the first child that matches

--- modules/Router.js
+++ modules/Router.js
@@ -1,7 +1,6 @@
 import React, { PropTypes } from 'react'
 import matchRoutes from './matchRoutes'
 import withHistory from './withHistory'
-import Route from './Route'
 import {
   location as locationType
 } from './PropTypes'
@@ -18,23 +17,11 @@ class Router extends React.Component {
   render() {
     const { location, children } = this.props
 
-    const routes = []
-
-    React.Children.forEach(children, child => {
-      if (child.type === Route) {
-        routes.push({
-          pattern: child.props.pattern,
-          exact: child.props.exact,
-          element: child
-        })
-      }
-    })
-
-    // This covers the case when someone renders e.g. a <BrowserRouter>
-    // just to get the right context w/out actually passing any <Route>s
-    // as children. In that case, we just render the children.
-    if (routes.length === 0)
-      return React.Children.only(children)
+    const routes = React.Children.map(children, child => ({
+      pattern: child.props.pattern,
+      exact: child.props.exact,
+      element: child
+    }))
 
     const { match, route } = matchRoutes(routes, location.pathname)
 
