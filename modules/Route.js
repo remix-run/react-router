d358f2d1371e4bf6f6f5c6535311ea4b3bc9b505
Michael Jackson <mjijackson@gmail.com>
2016-12-19 10:03:17 -0800

    Check for existence of child

--- modules/Route.js
+++ modules/Route.js
@@ -26,7 +26,9 @@ class Route extends React.Component {
   handleRouteChange({ action, location }, callback) {
     const child = this.child
 
-    if (typeof child.routeWillChange === 'function') {
+    // Need to check for existence of child because
+    // functional components don't have instances.
+    if (child && typeof child.routeWillChange === 'function') {
       const { path, exact } = this.props
       const match = matchPattern(path, exact, location.pathname)
 
