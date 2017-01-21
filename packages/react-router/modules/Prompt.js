acdfc5c06410957e6328a18c27a2d54c6e20149f
Garry Taylor <giz@gbdesign.net>
2017-01-21 14:22:18 +0000

    Prompt beforeUnload (#4272) Implement beforeUnload, allowing custom and standard alerts if a user tries to leave the site. Includes docs and example.

--- packages/react-router/modules/Prompt.js
+++ packages/react-router/modules/Prompt.js
@@ -1,4 +1,5 @@
 import React, { PropTypes } from 'react'
+const BEFOREUNLOAD = "beforeunload"; // see: http://stackoverflow.com/questions/39094138/reactjs-event-listener-beforeunload-added-but-not-removed
 
 /**
  * The public API for prompting the user before navigating away
@@ -16,7 +17,11 @@ class Prompt extends React.Component {
     message: PropTypes.oneOfType([
       PropTypes.func,
       PropTypes.string
-    ]).isRequired
+    ]).isRequired,
+    beforeUnload: PropTypes.oneOfType([
+      PropTypes.func,
+      PropTypes.bool
+    ])
   }
 
   static defaultProps = {
@@ -37,9 +42,28 @@ class Prompt extends React.Component {
     }
   }
 
+  onbeforeunload(e) {
+    var dialogText = "Changes you made may not be saved."
+    e.returnValue = dialogText
+    return dialogText
+  }
+
+  enableUnload(_onbeforeunload) {
+    if(this._subscribed) { return }
+    this._subscribed = _onbeforeunload ? _onbeforeunload : this.onbeforeunload;
+    window.addEventListener(BEFOREUNLOAD, this._subscribed)
+  }
+
+  disableUnload() {
+    window.removeEventListener(BEFOREUNLOAD, this._subscribed)
+    delete this._subscribed
+  }
+
   componentWillMount() {
     if (this.props.when)
       this.enable(this.props.message)
+    if (this.props.beforeUnload)
+      this.enableUnload();
   }
 
   componentWillReceiveProps(nextProps) {
@@ -49,10 +73,22 @@ class Prompt extends React.Component {
     } else {
       this.disable()
     }
+
+    if (nextProps.beforeUnload === true && this.props.beforeUnload === false) {
+      this.enableUnload()
+    } else if (nextProps.beforeUnload === false && 
+      (this.props.beforeUnload === true || typeof this.props.beforeUnload === "function")) {
+      this.disableUnload()
+    }
+
+    if (typeof nextProps.beforeUnload === "function") {
+      this.enableUnload(nextProps.beforeUnload)
+    }    
   }
 
   componentWillUnmount() {
     this.disable()
+    this.disableUnload()
   }
 
   render() {
