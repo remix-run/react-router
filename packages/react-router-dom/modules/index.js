1cb8b8d9de65a88b6ba735263da3275f282f1c3d
Michael Jackson <mjijackson@gmail.com>
2017-01-23 14:09:47 -0800

    Re-export stuff from react-router

--- packages/react-router-dom/modules/index.js
+++ packages/react-router-dom/modules/index.js
@@ -2,3 +2,13 @@ export BrowserRouter from './BrowserRouter'
 export HashRouter from './HashRouter'
 export Link from './Link'
 export NavLink from './NavLink'
+
+// Re-export a bunch of stuff from react-router for convenience.
+export MemoryRouter from 'react-router/MemoryRouter'
+export ServerRouter from 'react-router/ServerRouter'
+export Redirect from 'react-router/Redirect'
+export Route from 'react-router/Route'
+export Router from 'react-router/Router'
+export Switch from 'react-router/Switch'
+export matchPath from 'react-router/matchPath'
+export withRouter from 'react-router/withRouter'
