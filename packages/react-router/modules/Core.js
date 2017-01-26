570788191649f935021cdf2bcd6a3a45c5df692b
Michael Jackson <mjijackson@gmail.com>
2017-01-25 20:34:18 -0800

    Put matchPath in its own file

--- packages/react-router/modules/Core.js
+++ packages/react-router/modules/Core.js
@@ -1,57 +1,5 @@
 import React, { PropTypes } from 'react'
-import pathToRegexp from 'path-to-regexp'
-
-const patternCache = {}
-const cacheLimit = 10000
-let cacheCount = 0
-
-const compilePath = (pattern, options) => {
-  const cacheKey = options.end + options.strict
-  const cache = patternCache[cacheKey] || (patternCache[cacheKey] = {})
-
-  if (cache[pattern])
-    return cache[pattern]
-
-  const keys = []
-  const re = pathToRegexp(pattern, keys, options)
-  const compiledPattern = { re, keys }
-
-  if (cacheCount < cacheLimit) {
-    cache[pattern] = compiledPattern
-    cacheCount++
-  }
-
-  return compiledPattern
-}
-
-/**
- * Public API for matching a URL pathname to a path pattern.
- */
-const matchPath = (pathname, path, { exact = false, strict = false }) => {
-  if (!path)
-    return { url: pathname, isExact: true, params: {} }
-  const { re, keys } = compilePath(path, { end: exact, strict })
-  const match = re.exec(pathname)
-
-  if (!match)
-    return null
-
-  const [ url, ...values ] = match
-  const isExact = pathname === url
-
-  if (exact && !isExact)
-    return null
-
-  return {
-    path, // the path pattern used to match
-    url, // the matched portion of the URL
-    isExact, // whether or not we matched exactly
-    params: keys.reduce((memo, key, index) => {
-      memo[key.name] = values[index]
-      return memo
-    }, {})
-  }
-}
+import matchPath from './matchPath'
 
 class RouterProvider extends React.Component {
   static propTypes = {
@@ -216,7 +164,6 @@ const ConnectedRoute = withRouter(Route)
 const ConnectedSwitch = withRouter(Switch)
 
 export {
-  matchPath,
   withRouter,
   ConnectedRoute as Route,
   ConnectedSwitch as Switch,
--- packages/react-router/modules/index.js
+++ packages/react-router/modules/index.js
@@ -1,11 +1,11 @@
 export {
-  matchPath,
   withRouter,
   Route,
   Switch,
   Router
 } from './Core'
 
+export matchPath from './matchPath'
 export MemoryRouter from './MemoryRouter'
 export StaticRouter from './StaticRouter'
 export Redirect from './Redirect'
