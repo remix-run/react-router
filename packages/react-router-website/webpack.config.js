43d74e4c9037885856d6e046ff863b7c3600b000
Michael Jackson <mjijackson@gmail.com>
2017-01-27 21:19:47 -0800

    Use production build of React

--- packages/react-router-website/webpack.config.js
+++ packages/react-router-website/webpack.config.js
@@ -23,7 +23,7 @@ module.exports = {
 
   plugins: [
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
     }),
     new webpack.optimize.CommonsChunkPlugin('vendor', `vendor-[chunkHash].js`),
     new HTMLWebpackPlugin({
