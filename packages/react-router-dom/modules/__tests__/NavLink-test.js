b300c5946224d95048e05ff8badfb0ff25e1fb86
Paul Sherman <paul.sherman.88@gmail.com>
2017-02-01 05:15:51 -0600

    NavLink tests (#4421)
    * NavLink tests
    
    * mobile safari doesn't like classList.contains?
    
    * test isActive prop

--- packages/react-router-dom/modules/__tests__/NavLink-test.js
+++ packages/react-router-dom/modules/__tests__/NavLink-test.js
@@ -1,9 +1,112 @@
-describe('When a <NavLink> is active', () => {
-  it('applies its activeClassName')
-  it('applies its activeStyle')
-})
+import expect from 'expect'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import MemoryRouter from 'react-router/MemoryRouter'
+import NavLink from '../NavLink'
 
-describe('When a <NavLink> is not active', () => {
-  it('does not apply its activeClassName')
-  it('does not apply its activeStyle')
+describe('NavLink', () => {
+  const node = document.createElement('div')
+
+  afterEach(() => {
+    ReactDOM.unmountComponentAtNode(node)
+  })
+
+  describe('When a <NavLink> is active', () => {
+    it('applies its activeClassName', () => {
+      ReactDOM.render((
+        <MemoryRouter initialEntries={['/pizza']}>
+          <NavLink to='/pizza' activeClassName='active'>Pizza!</NavLink>
+        </MemoryRouter>
+      ), node)
+      const a = node.getElementsByTagName('a')[0]
+      expect(a.className).toContain('active')
+    })
+
+    it('applies its activeStyle', () => {
+      const defaultStyle = { color: 'black' }
+      const activeStyle = { color: 'red' }
+
+      ReactDOM.render((
+        <MemoryRouter initialEntries={['/pizza']}>
+          <NavLink
+            to='/pizza'
+            style={defaultStyle}
+            activeStyle={activeStyle}
+            >
+            Pizza!
+          </NavLink>
+        </MemoryRouter>
+      ), node)
+      const a = node.getElementsByTagName('a')[0]
+      expect(a.style.color).toBe(activeStyle.color)
+    })
+  })
+
+  describe('When a <NavLink> is not active', () => {
+    it('does not apply its activeClassName', () => {
+      ReactDOM.render((
+        <MemoryRouter initialEntries={['/pizza']}>
+          <NavLink to='/salad' activeClassName='active'>Salad?</NavLink>
+        </MemoryRouter>
+      ), node)
+      const a = node.getElementsByTagName('a')[0]
+      expect(a.className).toNotContain('active')
+    })
+
+    it('does not apply its activeStyle', () => {
+      const defaultStyle = { color: 'black' }
+      const activeStyle = { color: 'red' }
+
+      ReactDOM.render((
+        <MemoryRouter initialEntries={['/pizza']}>
+          <NavLink
+            to='/salad'
+            style={defaultStyle}
+            activeStyle={activeStyle}
+            >
+            Salad?
+          </NavLink>
+        </MemoryRouter>
+      ), node)
+      const a = node.getElementsByTagName('a')[0]
+      expect(a.style.color).toBe(defaultStyle.color)
+    })
+  })
+  
+  describe('isActive', () => {
+    const activeExact = (match, location) => !!match && match.isExact
+
+    it('applies active props when isActive returns true', () => {
+      ReactDOM.render((
+        <MemoryRouter initialEntries={['/pizza']}>
+          <NavLink
+            to='/pizza'
+            activeClassName='active'
+            isActive={activeExact}
+            >
+            Pizza!
+          </NavLink>
+        </MemoryRouter>
+      ), node)
+      const a = node.getElementsByTagName('a')[0]
+      expect(a.className).toContain('active')
+    })
+
+    it('does not apply active props when isActive returns false', () => {
+      ReactDOM.render((
+        <MemoryRouter initialEntries={['/pizza']}>
+          <NavLink
+            to='/pizza/anchovies'
+            activeClassName='active'
+            isActive={activeExact}
+            >
+            Pizza!
+          </NavLink>
+        </MemoryRouter>
+      ), node)
+      const a = node.getElementsByTagName('a')[0]
+      expect(a.className).toNotContain('active')
+    })
+  })
+
 })
