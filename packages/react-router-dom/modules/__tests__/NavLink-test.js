ba280fe85ffcc693e7bc5e892ad056c6d07de8e1
Paul Sherman <paul.sherman.88@gmail.com>
2017-01-31 22:59:32 -0600

    NavLink tests

--- packages/react-router-dom/modules/__tests__/NavLink-test.js
+++ packages/react-router-dom/modules/__tests__/NavLink-test.js
@@ -1,9 +1,76 @@
-describe('When a <NavLink> is active', () => {
-  it('applies its activeClassName')
-  it('applies its activeStyle')
-})
+import expect from 'expect'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import MemoryRouter from 'react-router/MemoryRouter'
+import NavLink from '../NavLink'
 
-describe('When a <NavLink> is not active', () => {
-  it('does not apply its activeClassName')
-  it('does not apply its activeStyle')
+describe('NavLink', () => {
+  const node = document.createElement('div')
+
+  afterEach(() => {
+    ReactDOM.unmountComponentAtNode(node)
+  })
+
+  describe('When a <NavLink> is active', () => {
+    it('applies its activeClassName', () => {
+      ReactDOM.render((
+        <MemoryRouter initialEntries={['/pizza']}>
+          <NavLink to='/pizza' activeClassName='active'>Pizza!</NavLink>
+        </MemoryRouter>
+      ), node)
+      const a = node.getElementsByTagName('a')[0]
+      expect(a.classList.contains('active')).toBe(true)
+    })
+
+    it('applies its activeStyle', () => {
+      const defaultStyle = { color: 'black' }
+      const activeStyle = { color: 'red' }
+
+      ReactDOM.render((
+        <MemoryRouter initialEntries={['/pizza']}>
+          <NavLink
+            to='/pizza'
+            style={defaultStyle}
+            activeStyle={activeStyle}
+            >
+            Pizza!
+          </NavLink>
+        </MemoryRouter>
+      ), node)
+      const a = node.getElementsByTagName('a')[0]
+      expect(a.style.color).toBe(activeStyle.color)
+    })
+  })
+
+  describe('When a <NavLink> is not active', () => {
+    it('does not apply its activeClassName', () => {
+      ReactDOM.render((
+        <MemoryRouter initialEntries={['/pizza']}>
+          <NavLink to='/salad' activeClassName='active'>Salad?</NavLink>
+        </MemoryRouter>
+      ), node)
+      const a = node.getElementsByTagName('a')[0]
+      expect(a.classList.contains('acative')).toBe(false)
+    })
+
+    it('does not apply its activeStyle', () => {
+      const defaultStyle = { color: 'black' }
+      const activeStyle = { color: 'red' }
+
+      ReactDOM.render((
+        <MemoryRouter initialEntries={['/pizza']}>
+          <NavLink
+            to='/salad'
+            style={defaultStyle}
+            activeStyle={activeStyle}
+            >
+            Salad?
+          </NavLink>
+        </MemoryRouter>
+      ), node)
+      const a = node.getElementsByTagName('a')[0]
+      expect(a.style.color).toBe(defaultStyle.color)
+    })
+  })
+  
 })
