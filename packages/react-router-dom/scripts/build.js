9fdf267fc3a9c8c6bbe5f5acb229d66c3759533b
Michael Jackson <mjijackson@gmail.com>
2017-02-03 22:03:45 -0500

    More informative build scripts

--- packages/react-router-dom/scripts/build.js
+++ packages/react-router-dom/scripts/build.js
@@ -1,5 +1,4 @@
 const fs = require('fs')
-const path = require('path')
 const execSync = require('child_process').execSync
 const inInstall = require('in-publish').inInstall
 const prettyBytes = require('pretty-bytes')
@@ -8,20 +7,38 @@ const gzipSize = require('gzip-size')
 if (inInstall())
   process.exit(0)
 
-const exec = (command, env) =>
-  execSync(command, { stdio: 'inherit', env })
+const exec = (command, extraEnv) =>
+  execSync(command, {
+    stdio: 'inherit',
+    env: Object.assign({}, process.env, extraEnv)
+  })
 
-const webpackEnv = Object.assign({}, process.env, {
+console.log('Building CommonJS modules ...')
+
+exec('babel modules -d . --ignore __tests__', {
+  BABEL_ENV: 'cjs'
+})
+
+console.log('\nBuilding ES modules ...')
+
+exec('babel modules -d . --ignore __tests__', {
+  BABEL_ENV: 'es'
+})
+
+console.log('\nBuilding react-router-dom.js ...')
+
+exec('webpack modules/index.js umd/react-router-dom.js', {
   NODE_ENV: 'production'
 })
 
-exec('npm run build-es')
-exec('npm run build-lib')
-exec('npm run build-umd', webpackEnv)
-exec('npm run build-min', webpackEnv)
+console.log('\nBuilding react-router-dom.min.js ...')
+
+exec('webpack -p modules/index.js umd/react-router-dom.min.js', {
+  NODE_ENV: 'production'
+})
 
 const size = gzipSize.sync(
-  fs.readFileSync(path.resolve(__dirname, '../umd/react-router-dom.min.js'))
+  fs.readFileSync('umd/react-router-dom.min.js')
 )
 
 console.log('\ngzipped, the UMD build is %s', prettyBytes(size))
