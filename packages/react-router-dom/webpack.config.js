a8c4c9302a5110b0954e5851c13ed1e4e46e25bb
Paul Sherman <paul.sherman.88@gmail.com>
2017-02-07 22:54:48 -0600

    Remove react-router as webpack external

--- packages/react-router-dom/webpack.config.js
+++ packages/react-router-dom/webpack.config.js
@@ -12,12 +12,6 @@ module.exports = {
       commonjs2: 'react',
       commonjs: 'react',
       amd: 'react'
-    },
-    'react-router': {
-      root: 'ReactRouter',
-      commonjs2: 'react-router',
-      commonjs: 'react-router',
-      amd: 'react-router'
     }
   },
 
