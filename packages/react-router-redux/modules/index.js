9bba6fc8d0446b077525909ae1c0804185a41e00
Tim Dorr <timdorr@timdorr.com>
2017-03-08 22:12:23 -0500

    Reduxtion! :boom:

---
+++ packages/react-router-redux/modules/index.js
@@ -0,0 +1,2 @@
+export ConnectedRouter from './ConnectedRouter'
+export { LOCATION_CHANGE, routerReducer } from './reducer'
---
+++ packages/react-router-redux/rollup.config.js
@@ -0,0 +1,42 @@
+import babel from 'rollup-plugin-babel'
+import uglify from 'rollup-plugin-uglify'
+
+var config = {
+  format: 'umd',
+  moduleName: 'ReactRouterRedux',
+  plugins: [
+    babel({
+      exclude: 'node_modules/**'
+    })
+  ],
+  external: [
+    'react',
+    'react-router'
+  ],
+  globals: {
+    react: 'React',
+    'react-router': 'ReactRouter'
+  }
+}
+
+if (process.env.NODE_ENV === 'production') {
+  config.plugins.push(
+    uglify({
+      compress: {
+        pure_getters: true,
+        unsafe: true,
+        unsafe_comps: true,
+        warnings: false,
+        screw_ie8: false
+      },
+      mangle: {
+        screw_ie8: false
+      },
+      output: {
+        screw_ie8: false
+      }
+    })
+  )
+}
+
+export default config
