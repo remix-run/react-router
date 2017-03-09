f5a69b9a9d7dfa4a3634193a1a527796225195dd
Tim Dorr <timdorr@timdorr.com>
2017-03-08 22:20:50 -0500

    Whoops, that shouldn't have been commented out...

--- packages/react-router-redux/tools/build.js
+++ packages/react-router-redux/tools/build.js
@@ -13,17 +13,17 @@ const exec = (command, extraEnv) =>
     env: Object.assign({}, process.env, extraEnv)
   })
 
-// console.log('Building CommonJS modules ...')
-//
-// exec('babel modules -d . --ignore __tests__', {
-//   BABEL_ENV: 'cjs'
-// })
-//
-// console.log('\nBuilding ES modules ...')
-//
-// exec('babel modules -d es --ignore __tests__', {
-//   BABEL_ENV: 'es'
-// })
+console.log('Building CommonJS modules ...')
+
+exec('babel modules -d . --ignore __tests__', {
+  BABEL_ENV: 'cjs'
+})
+
+console.log('\nBuilding ES modules ...')
+
+exec('babel modules -d es --ignore __tests__', {
+  BABEL_ENV: 'es'
+})
 
 console.log('\nBuilding react-router-redux.js ...')
 
