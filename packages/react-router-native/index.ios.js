2f833b77a9e2e7a8e35900b656afc1ef97ca5b80
Tyler McGinnis <tylermcginnis33@gmail.com>
2017-03-10 02:02:09 -0700

    Remove my Prompt and reference original core Prompt

--- packages/react-router-native/index.ios.js
+++ packages/react-router-native/index.ios.js
@@ -1,6 +1,6 @@
 import { AppRegistry } from 'react-native'
 
-import App from './examples/BoringExample'
+import App from './examples/BasicExample'
 //import App from './examples/ExperimentalExample'
 
 AppRegistry.registerComponent('ReactRouterNative', () => App)
