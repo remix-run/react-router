30dd4f175b26f0ea2f1da17787ba26271df139a8
Ryan Florence <rpflorence@gmail.com>
2017-03-10 11:34:28 -0800

    switch animation example to simpler TransitionGroup

--- packages/react-router-website/modules/examples/Animation.js
+++ packages/react-router-website/modules/examples/Animation.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { TransitionMotion, spring } from 'react-motion'
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
 import {
   BrowserRouter as Router,
   Route,
@@ -7,56 +7,57 @@ import {
   Redirect
 } from 'react-router-dom'
 
+/* you'll need this CSS somewhere
+.fade-enter {
+  opacity: 0;
+  z-index: 1;
+}
+
+.fade-enter.fade-enter-active {
+  opacity: 1;
+  transition: opacity 250ms ease-in;
+}
+*/
+
 const AnimationExample = () => (
   <Router>
-    <div style={styles.fill}>
-      <ul style={styles.nav}>
-        <NavLink to="/10/90/50">Red</NavLink>
-        <NavLink to="/120/100/40">Green</NavLink>
-        <NavLink to="/200/100/40">Blue</NavLink>
-        <NavLink to="/310/100/50">Pink</NavLink>
-      </ul>
+    <Route render={({ location }) => (
+      <div style={styles.fill}>
+        <Route exact path="/" render={() => (
+          <Redirect to="/10/90/50"/>
+        )}/>
 
-      <div style={styles.content}>
-        <FadeRoute path="/:h/:s/:l" component={HSL}/>
+        <ul style={styles.nav}>
+          <NavLink to="/10/90/50">Red</NavLink>
+          <NavLink to="/120/100/40">Green</NavLink>
+          <NavLink to="/200/100/40">Blue</NavLink>
+          <NavLink to="/310/100/50">Pink</NavLink>
+        </ul>
+
+        <div style={styles.content}>
+          <ReactCSSTransitionGroup
+            transitionName="fade"
+            transitionEnterTimeout={300}
+            transitionLeaveTimeout={300}
+          >
+            {/* no different than other usage of
+                ReactCSSTransitionGroup, just make
+                sure to pass `location` to `Route`
+                so it can match the old location
+                as it animates out
+            */}
+            <Route
+              location={location}
+              key={location.key}
+              path="/:h/:s/:l"
+              component={HSL}
+            />
+          </ReactCSSTransitionGroup>
+        </div>
       </div>
-
-      <Route exact path="/" render={() => (
-        <Redirect to="/10/90/50"/>
-      )}/>
-    </div>
+    )}/>
   </Router>
 )
-
-const FadeRoute = ({ component: Component, ...rest }) => {
-  const willLeave = () => ({ zIndex: 1, opacity: spring(0) })
-
-  return (
-    <Route {...rest} children={({ location, match }) => (
-      <TransitionMotion
-        willLeave={willLeave}
-        styles={match ? [ {
-          key: location.pathname,
-          style: { opacity: 1 },
-          data: match
-        } ] : []}
-      >
-        {interpolatedStyles => (
-          <div>
-            {interpolatedStyles.map(config => (
-              <div
-                key={config.key}
-                style={{ ...styles.fill, ...config.style }}
-              >
-                <Component {...config.data}/>
-              </div>
-            ))}
-          </div>
-        )}
-      </TransitionMotion>
-    )}/>
-  )
-}
 
 const NavLink = (props) => (
   <li style={styles.navItem}>
@@ -64,8 +65,9 @@ const NavLink = (props) => (
   </li>
 )
 
-const HSL = ({ params }) => (
+const HSL = ({ match: { params } }) => (
   <div style={{
+    ...styles.fill,
     ...styles.hsl,
     background: `hsl(${params.h}, ${params.s}%, ${params.l}%)`
   }}>hsl({params.h}, {params.s}%, {params.l}%)</div>
