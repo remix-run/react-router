25442ee3a0d49d837f329a3e9b6841e615a6615b
Chris Morrell <inxilpro@users.noreply.github.com>
2017-03-17 10:28:46 -0400

    Removes ConnectedRouter in favor of dispatching in middleware

--- packages/react-router-redux/modules/__tests__/middleware-test.js
+++ packages/react-router-redux/modules/__tests__/middleware-test.js
@@ -1,17 +1,21 @@
 import { push, replace } from '../actions'
 import routerMiddleware from '../middleware'
+import { LOCATION_CHANGE } from '../reducer'
 
 describe('routerMiddleware', () => {
-  let history, next, dispatch
+  let history, next, dispatch, middleware, listener, storeDispatch
 
   beforeEach(() => {
     history = {
       push: jest.fn(),
-      replace: jest.fn()
+      replace: jest.fn(),
+      listen: jest.fn().mockImplementation(arg => listener = arg)
     }
     next = jest.fn()
 
-    dispatch = routerMiddleware(history)()(next)
+    storeDispatch = jest.fn()
+    middleware = routerMiddleware(history)
+    dispatch = middleware({ dispatch: storeDispatch })(next)
   })
 
 
@@ -28,5 +32,21 @@ describe('routerMiddleware', () => {
   it('ignores other actions', () => {
     dispatch({ type: 'FOO' })
     expect(next).toHaveBeenCalled()
+  })
+
+  it('dispatches change actions on history change', () => {
+    expect(history.listen).toHaveBeenCalledTimes(1)
+    expect(typeof listener).toBe('function')
+    const location = {
+      pathname: '/testpath',
+      search: '',
+      hash: '',
+      key: 'abc123'
+    }
+    listener(location)
+    expect(storeDispatch).toHaveBeenCalledWith({
+      type: LOCATION_CHANGE,
+      payload: location
+    })
   })
 })
--- packages/react-router-redux/modules/middleware.js
+++ packages/react-router-redux/modules/middleware.js
@@ -1,17 +1,26 @@
 import { CALL_HISTORY_METHOD } from './actions'
+import { LOCATION_CHANGE } from './reducer'
 
 /**
  * This middleware captures CALL_HISTORY_METHOD actions to redirect to the
  * provided history object. This will prevent these actions from reaching your
  * reducer or any middleware that comes after this one.
+ *
+ * It also listens to history changes and dispatches LOCATION_CHANGE actions
  */
-export default function routerMiddleware(history) {
-  return () => next => action => {
-    if (action.type !== CALL_HISTORY_METHOD) {
-      return next(action)
-    }
+ export default function routerMiddleware(history) {
+  return ({ dispatch }) => {
+    history.listen(payload => dispatch({
+      type: LOCATION_CHANGE,
+      payload
+    }))
+    return next => action => {
+      if (action.type !== CALL_HISTORY_METHOD) {
+        return next(action)
+      }
 
-    const { payload: { method, args } } = action
-    history[method](...args)
+      const { method, args } = action.payload
+      history[method](...args)
+    }
   }
 }
