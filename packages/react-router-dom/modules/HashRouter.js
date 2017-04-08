1bdb2fc82d5242ffd101c18368157613e54b2778
Yasser Kaddour <yasser.kaddour@gmail.com>
2017-04-08 15:48:20 +0100

    Migrate from the deprecated React.propTypes to prop-types package

--- packages/react-router-dom/modules/HashRouter.js
+++ packages/react-router-dom/modules/HashRouter.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react'
+import React from 'react'
+import PropTypes from 'prop-types'
 import createHistory from 'history/createHashHistory'
 import { Router } from 'react-router'
 
--- packages/react-router-dom/modules/Link.js
+++ packages/react-router-dom/modules/Link.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react'
+import React from 'react'
+import PropTypes from 'prop-types'
 
 const isModifiedEvent = (event) =>
   !!(event.metaKey || event.altKey || event.ctrlKey || event.shiftKey)
