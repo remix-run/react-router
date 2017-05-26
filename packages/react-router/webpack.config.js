006e2b340f0bac1a65e2fc55ce340704284e44e9
Jochen Berger <fooberger@gmail.com>
2017-05-26 11:25:26 +0200

    upgrade to Webpack 2.6.1

--- packages/react-router/webpack.config.js
+++ packages/react-router/webpack.config.js
@@ -16,8 +16,8 @@ module.exports = {
   },
 
   module: {
-    loaders: [
-      { test: /\.js$/, exclude: /node_modules/, loader: 'babel' }
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' }
     ]
   },
 
