2d6e1f66f10e2fca95a503cf56db3feb78bb746d
Alex Ilyaev <alexilyaev@gmail.com>
2017-07-17 16:13:44 +0300

    Redirect: Support props changes on Redirect to handle edge cases

--- packages/react-router/modules/Redirect.js
+++ packages/react-router/modules/Redirect.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import warning from 'warning'
 import invariant from 'invariant'
+import { locationsAreEqual } from 'history'
 
 /**
- * The public API for updating the location programatically
+ * The public API for updating the location programmatically
  * with a component.
  */
 class Redirect extends React.Component {
@@ -13,7 +15,7 @@ class Redirect extends React.Component {
     to: PropTypes.oneOfType([
       PropTypes.string,
       PropTypes.object
-    ])
+    ]).isRequired
   }
 
   static defaultProps = {
@@ -47,6 +49,24 @@ class Redirect extends React.Component {
   componentDidMount() {
     if (!this.isStatic())
       this.perform()
+  }
+
+  componentDidUpdate(prevProps) {
+    let prevTo = prevProps.to
+    let nextTo = this.props.to
+
+    prevTo = typeof prevTo === 'string' ? { pathname: prevTo } : prevTo
+    nextTo = typeof nextTo === 'string' ? { pathname: nextTo } : nextTo
+
+    if (locationsAreEqual(prevTo, nextTo))
+      return warning(
+        false,
+        `You tried to redirect to the same route you're currently on: "%s%s"`,
+        nextTo.pathname,
+        nextTo.search || ''
+      )
+
+    this.perform()
   }
 
   perform() {
