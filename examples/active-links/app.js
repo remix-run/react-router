e8176f52b1690452b825eb8ec5e077b4a5e88153
Ryan Florence <rpflorence@gmail.com>
2017-09-11 20:58:55 -0700

    Oops, didn’t mean to commit this.

--- examples/active-links/app.js
+++ examples/active-links/app.js
@@ -6,31 +6,26 @@ import withExampleBasename from '../withExampleBasename'
 
 const ACTIVE = { color: 'red' }
 
-class App extends React.Component {
-  render() {
-    console.log('render')
-    return (
-      <div>
-        <h1>APP!</h1>
-        <ul>
-          <li><Link      to="/"           activeStyle={ACTIVE}>/</Link></li>
-          <li><IndexLink to="/"           activeStyle={ACTIVE}>/ IndexLink</IndexLink></li>
+const App = ({ children }) => (
+  <div>
+    <h1>APP!</h1>
+    <ul>
+      <li><Link      to="/"           activeStyle={ACTIVE}>/</Link></li>
+      <li><IndexLink to="/"           activeStyle={ACTIVE}>/ IndexLink</IndexLink></li>
 
-          <li><Link      to="/users"      activeStyle={ACTIVE}>/users</Link></li>
-          <li><IndexLink to="/users"      activeStyle={ACTIVE}>/users IndexLink</IndexLink></li>
+      <li><Link      to="/users"      activeStyle={ACTIVE}>/users</Link></li>
+      <li><IndexLink to="/users"      activeStyle={ACTIVE}>/users IndexLink</IndexLink></li>
 
-          <li><Link      to="/users/ryan" activeStyle={ACTIVE}>/users/ryan</Link></li>
-          <li><Link      to={{ pathname: '/users/ryan', query: { foo: 'bar' } }}
-                                          activeStyle={ACTIVE}>/users/ryan?foo=bar</Link></li>
+      <li><Link      to="/users/ryan" activeStyle={ACTIVE}>/users/ryan</Link></li>
+      <li><Link      to={{ pathname: '/users/ryan', query: { foo: 'bar' } }}
+                                      activeStyle={ACTIVE}>/users/ryan?foo=bar</Link></li>
 
-          <li><Link      to="/about"      activeStyle={ACTIVE}>/about</Link></li>
-        </ul>
+      <li><Link      to="/about"      activeStyle={ACTIVE}>/about</Link></li>
+    </ul>
 
-        {this.props.children}
-      </div>
-    )
-  }
-}
+    {children}
+  </div>
+)
 
 const Index = () => (
   <div>
