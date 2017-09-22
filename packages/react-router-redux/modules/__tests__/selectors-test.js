6a5d48b6740a849cfcc99258cb8b2b3f382cc226
Eloy Toro <eloytoro@gmail.com>
2017-09-22 10:35:46 -0500

    react-router-redux useful selectors (#5543)
    * add feature
    
    * Remove an extra linebreak
    
    * add tests
    
    * use === instead of deep comparison
    
    * remove duplicated test
    
    * remove unused var

---
+++ packages/react-router-redux/modules/__tests__/selectors-test.js
@@ -0,0 +1,74 @@
+import { getLocation, createMatchSelector } from '../selectors'
+import { createStore, combineReducers } from 'redux'
+import { routerReducer, LOCATION_CHANGE } from '../reducer'
+
+describe('selectors', () => {
+
+  let store
+
+  beforeEach(() => {
+    store = createStore(combineReducers({
+      router: routerReducer
+    }))
+  })
+
+  describe('getLocation', () => {
+    it('gets the location from the state', () => {
+      const location = { pathname: '/' }
+      store.dispatch({
+        type: LOCATION_CHANGE,
+        payload: location
+      })
+      const state = store.getState()
+      expect(getLocation(state)).toBe(location)
+    })
+  })
+
+  describe('createMatchSelector', () => {
+    it('matches correctly', () => {
+      const matchSelector = createMatchSelector('/')
+      store.dispatch({
+        type: LOCATION_CHANGE,
+        payload: { pathname: '/test' }
+      })
+      const state = store.getState()
+      expect(matchSelector(state)).toEqual({
+        isExact: false,
+        params: {},
+        path: '/',
+        url: '/'
+      })
+    })
+
+    it('does not update if the match is the same', () => {
+      const matchSelector = createMatchSelector('/')
+      store.dispatch({
+        type: LOCATION_CHANGE,
+        payload: { pathname: '/test1' }
+      })
+      const match1 = matchSelector(store.getState())
+      store.dispatch({
+        type: LOCATION_CHANGE,
+        payload: { pathname: '/test2' }
+      })
+      const match2 = matchSelector(store.getState())
+      expect(match1).toBe(match2)
+    })
+
+    it('updates if the match is different', () => {
+      const matchSelector = createMatchSelector('/sushi/:type')
+      store.dispatch({
+        type: LOCATION_CHANGE,
+        payload: { pathname: '/sushi/california' }
+      })
+      const match1 = matchSelector(store.getState())
+      store.dispatch({
+        type: LOCATION_CHANGE,
+        payload: { pathname: '/sushi/dynamite' }
+      })
+      const match2 = matchSelector(store.getState())
+      expect(match1).not.toBe(match2)
+    })
+  })
+
+})
---
+++ packages/react-router-redux/modules/selectors.js
@@ -0,0 +1,20 @@
+import { matchPath } from 'react-router'
+
+export const getLocation = state => state.router.location
+
+export const createMatchSelector = (path) => {
+  let lastPathname = null
+  let lastMatch = null
+  return (state) => {
+    const { pathname } = getLocation(state)
+    if (pathname === lastPathname) {
+      return lastMatch
+    }
+    lastPathname = pathname
+    const match = matchPath(pathname, path)
+    if (!match || !lastMatch || match.url !== lastMatch.url) {
+      lastMatch = match
+    }
+    return lastMatch
+  }
+}
