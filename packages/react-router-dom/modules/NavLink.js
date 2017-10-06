a43e855edffca9566a714b8b757f24138bf257a0
Eric Siegel <siegel.eric@gmail.com>
2017-10-05 22:58:06 -0700

    Escape NavLink path to allow special characters in path.  Fixes #5584

--- packages/react-router-dom/modules/NavLink.js
+++ packages/react-router-dom/modules/NavLink.js
@@ -18,27 +18,40 @@ const NavLink = ({
   isActive: getIsActive,
   ariaCurrent,
   ...rest
-}) => (
-  <Route
-    path={typeof to === 'object' ? to.pathname : to}
-    exact={exact}
-    strict={strict}
-    location={location}
-    children={({ location, match }) => {
-      const isActive = !!(getIsActive ? getIsActive(match, location) : match)
+}) => {
+  // Escape special Route path matching characters since `to` is expected to be an exact url.
+  let unescapedPath;
+  if (typeof to === 'object') {
+    unescapedPath = to.pathname;
+  } else {
+    unescapedPath = to;
+  }
 
-      return (
-        <Link
-          to={to}
-          className={isActive ? [ className, activeClassName ].filter(i => i).join(' ') : className}
-          style={isActive ? { ...style, ...activeStyle } : style}
-          aria-current={isActive && ariaCurrent}
-          {...rest}
-        />
-      )
-    }}
-  />
-)
+  // Regex taken from: https://github.com/pillarjs/path-to-regexp/blob/master/index.js#L202
+  const path = unescapedPath.replace(/([.+*?=^!:${}()[\]|/\\])/g, '\\$1');
+
+  return (
+    <Route
+      path={path}
+      exact={exact}
+      strict={strict}
+      location={location}
+      children={({ location, match }) => {
+        const isActive = !!(getIsActive ? getIsActive(match, location) : match)
+
+        return (
+          <Link
+            to={to}
+            className={isActive ? [ className, activeClassName ].filter(i => i).join(' ') : className}
+            style={isActive ? { ...style, ...activeStyle } : style}
+            aria-current={isActive && ariaCurrent}
+            {...rest}
+          />
+        )
+      }}
+    />
+  );
+}
 
 NavLink.propTypes = {
   to: Link.propTypes.to,
