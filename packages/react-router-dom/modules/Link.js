9e13e738e71643d817ce13ec34c29f6bafb6a72d
mohamed-elmahallawy <mohamed.elmahallawy@airbnb.com>
2017-10-24 08:48:34 -0700

    Remove history methods from render of Link

--- packages/react-router-dom/modules/Link.js
+++ packages/react-router-dom/modules/Link.js
@@ -38,6 +38,20 @@ class Link extends React.Component {
     }).isRequired
   }
 
+  getHref = () => {
+    const { replace, to } = this.props // eslint-disable-line no-unused-vars
+    
+    invariant(
+      this.context.router,
+      'You should not use <Link> outside a <Router>'
+    )
+
+    const { history } = this.context.router
+    const location = typeof to === 'string' ? createLocation(to, null, null, history.location) : to
+
+    return history.createHref(location)
+  }
+
   handleClick = (event) => {
     if (this.props.onClick)
       this.props.onClick(event)
@@ -62,17 +76,9 @@ class Link extends React.Component {
   }
 
   render() {
-    const { replace, to, innerRef, ...props } = this.props // eslint-disable-line no-unused-vars
-
-    invariant(
-      this.context.router,
-      'You should not use <Link> outside a <Router>'
-    )
-
-    const { history } = this.context.router
-    const location = typeof to === 'string' ? createLocation(to, null, null, history.location) : to
-
-    const href = history.createHref(location)
+    const { innerRef, ...props } = this.props // eslint-disable-line no-unused-vars
+    const href = this.getHref()
+    
     return <a {...props} onClick={this.handleClick} href={href} ref={innerRef}/>
   }
 }
