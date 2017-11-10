c47ffd71022e1f94eee0a326b5c607aec0629326
Renan Vaz <renan.piresvz@gmail.com>
2017-11-10 14:26:15 -0300

    Improvements to react-router-redux examples (#5647)
    * general improvements to react-router-redux examples
    
    * remove unnecessary arrow function
    
    * use Redirect for private route

--- packages/react-router-redux/examples/BasicExample.js
+++ packages/react-router-redux/examples/BasicExample.js
@@ -1,44 +1,41 @@
-import React from 'react';
-import { render } from 'react-dom';
-import { Provider } from 'react-redux';
-import { ConnectedRouter } from 'react-router-redux';
+import React from 'react'
+import { render } from 'react-dom'
+import { connect, Provider } from 'react-redux'
+import { ConnectedRouter, routerReducer, routerMiddleware } from 'react-router-redux'
 
-import { createStore, applyMiddleware } from 'redux';
-import createHistory from 'history/createBrowserHistory';
-import { routerReducer, routerMiddleware } from 'react-router-redux';
+import { createStore, applyMiddleware } from 'redux'
+import createHistory from 'history/createBrowserHistory'
 
-import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
-import { Route, Switch } from 'react-router';
+import { Link } from 'react-router-dom'
+import { Route, Switch } from 'react-router'
 
-const history = createHistory();
+const history = createHistory()
 
 const store = createStore(
-    routerReducer,
-    applyMiddleware(routerMiddleware(history)),
-);
+  routerReducer,
+  applyMiddleware(routerMiddleware(history)),
+)
 
 const ConnectedSwitch = connect(state => ({
-	location: state.location
-}))(Switch);
+  location: state.location
+}))(Switch)
 
-const AppContainer = ({ location }) => (
-    <ConnectedSwitch>
-        <Route exact path="/" component={(props) => (<h1>Home <Link to="/about">About</Link></h1>)} />
-        <Route path="/about" component={(props) => (<h1>About <Link to="/">Home</Link></h1>)} />
-    </ConnectedSwitch>
-);
+const AppContainer = () => (
+  <ConnectedSwitch>
+    <Route exact path="/" component={() => (<h1>Home <Link to="/about">About</Link></h1>)} />
+    <Route path="/about" component={() => (<h1>About <Link to="/">Home</Link></h1>)} />
+  </ConnectedSwitch>
+)
 
 const App = connect(state => ({
-    location: state.location,
+  location: state.location,
 }))(AppContainer)
 
 render(
-    <Provider store={store}>
-        <ConnectedRouter history={history}>
-            <App />
-        </ConnectedRouter>
-    </Provider>,
-    document.getElementById('root'),
-);
-  
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <App />
+    </ConnectedRouter>
+  </Provider>,
+  document.getElementById('root'),
+)
