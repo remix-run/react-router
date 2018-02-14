bdcfec6dbfea4dbab009f3c10d42e8fdfd8e7871
MICHAEL JACKSON <mj@mjackson.me>
2018-02-13 16:17:45 -0800

    Prettify scripts

--- scripts/build.js
+++ scripts/build.js
@@ -1,13 +1,13 @@
-const execSync = require('child_process').execSync
+const execSync = require("child_process").execSync;
 
 const exec = (cmd, env) =>
   execSync(cmd, {
-    stdio: 'inherit',
+    stdio: "inherit",
     env: Object.assign({}, process.env, env)
-  })
+  });
 
 if (process.env.CI) {
-  exec('lerna run build --stream --ignore react-router-website')
+  exec("lerna run build --stream --ignore react-router-website");
 } else {
-  exec('lerna run build --stream')
+  exec("lerna run build --stream");
 }
--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,13 +1,17 @@
-const execSync = require('child_process').execSync
+const execSync = require("child_process").execSync;
 
 const exec = (cmd, env) =>
   execSync(cmd, {
-    stdio: 'inherit',
+    stdio: "inherit",
     env: Object.assign({}, process.env, env)
-  })
+  });
 
 if (process.env.CI) {
-  exec('lerna bootstrap --stream --ignore react-router-website --hoist --nohoist react-native --nohoist react-test-renderer')
+  exec(
+    "lerna bootstrap --stream --ignore react-router-website --hoist --nohoist react-native --nohoist react-test-renderer"
+  );
 } else {
-  exec('lerna bootstrap  --stream --hoist --nohoist react-native --nohoist react-test-renderer')
+  exec(
+    "lerna bootstrap  --stream --hoist --nohoist react-native --nohoist react-test-renderer"
+  );
 }
