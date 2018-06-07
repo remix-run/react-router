5787a758bba16468680a841ebfcb7be5e67fd3f7
jharris4 <harris.jb@gmail.com>
2018-06-07 07:17:25 -0400

    add addParamProps option as a convenience to inject all match.params.props as props

--- packages/react-router-config/modules/renderRoutes.js
+++ packages/react-router-config/modules/renderRoutes.js
@@ -8,6 +8,7 @@ const EMPTY = {};
 const DEFAULT_ROUTE_PROP = "route";
 const DEFAULT_RENDER_CHILD_PROP = "renderChild";
 const DEFAULT_MATCH_PROP = "match";
+const DEFAULT_ADD_PARAM_PROPS = false;
 
 const renderRoutes = (
   routes,
@@ -15,72 +16,83 @@ const renderRoutes = (
     extraProps = EMPTY,
     switchProps = EMPTY,
     routeProp = DEFAULT_ROUTE_PROP,
-    renderChildProp = DEFAULT_RENDER_CHILD_PROP,
     matchProp = DEFAULT_MATCH_PROP,
-    noRenderChildComponent = false,
+    renderChildProp = DEFAULT_RENDER_CHILD_PROP,
+    addParamProps = DEFAULT_ADD_PARAM_PROPS,
+    noRenderChildComponent = () => false,
     overrideRenderRoutes = false
   } = {}
 ) => {
-  if (routes) {
-    let renderChild = route => ({
-      [routeProp]: stripRoute,
-      [matchProp]: stripMatch,
-      ...otherProps
-    } = {}) =>
-      route.routes
-        ? (overrideRenderRoutes ? overrideRenderRoutes : renderRoutes).call(
-            null,
-            route.routes,
-            {
-              extraProps: otherProps,
-              switchProps,
-              renderChildProp,
-              noRenderChildComponent,
-              overrideRenderRoutes
-            }
-          )
-        : noRenderChildComponent;
-    let routeComponentProps = null;
-    return (
-      <Switch {...switchProps}>
-        {routes.map((route, i) => (
-          <Route
-            key={route.key || i}
-            path={route.path}
-            exact={route.exact}
-            strict={route.strict}
-            render={props =>
-              route.redirect !== void 0 ? (
-                <Redirect
-                  from={route.path}
-                  to={generatePath(route.redirect, props.match.params)}
-                />
-              ) : (
-                ((routeComponentProps = {
-                  ...(route.props ? route.props : EMPTY),
-                  ...props,
-                  ...extraProps,
-                  ...(route.forcedProps ? route.forcedProps : EMPTY)
-                }),
-                route.component ? (
-                  <route.component
-                    {...routeComponentProps}
-                    {...(routeProp ? { [routeProp]: route } : EMPTY)}
-                    {...(renderChildProp
-                      ? { [renderChildProp]: renderChild(route) }
-                      : EMPTY)}
-                  />
-                ) : (
-                  renderChild(route)(routeComponentProps)
-                ))
-              )
-            }
-          />
-        ))}
-      </Switch>
-    );
+  if (!routes) {
+    return null;
   }
-  return null;
+
+  const renderChild = route => ({
+    [routeProp]: stripRouteProp,
+    [matchProp]: stripMatchProp,
+    ...otherProps
+  } = {}) =>
+    route.routes
+      ? (overrideRenderRoutes ? overrideRenderRoutes : renderRoutes).call(
+          null,
+          route.routes,
+          {
+            extraProps: otherProps,
+            switchProps,
+            routeProp,
+            matchProp,
+            renderChildProp,
+            addParamProps,
+            noRenderChildComponent,
+            overrideRenderRoutes
+          }
+        )
+      : noRenderChildComponent;
+
+  const mergeRouteProps = (route, props) => {
+    const { route: stirpRoute, match, ...otherProps } = props;
+    return {
+      ...(route.props ? route.props : EMPTY),
+      ...otherProps,
+      ...extraProps,
+      ...(route.forcedProps ? route.forcedProps : EMPTY),
+      ...(routeProp ? { [routeProp]: route } : EMPTY),
+      ...(matchProp ? { [matchProp]: match } : EMPTY),
+      ...(addParamProps
+        ? match
+          ? match.params
+            ? match.params
+            : EMPTY
+          : EMPTY
+        : EMPTY),
+      ...(renderChildProp ? { [renderChildProp]: renderChild(route) } : EMPTY)
+    };
+  };
+
+  return (
+    <Switch {...switchProps}>
+      {routes.map((route, i) => (
+        <Route
+          key={route.key || i}
+          path={route.path}
+          exact={route.exact}
+          strict={route.strict}
+          render={props =>
+            route.redirect !== void 0 ? (
+              <Redirect
+                from={route.path}
+                to={generatePath(route.redirect, props.match.params)}
+              />
+            ) : route.component ? (
+              <route.component {...mergeRouteProps(route, props)} />
+            ) : (
+              renderChild(route)(mergeRouteProps(route, props))
+            )
+          }
+        />
+      ))}
+    </Switch>
+  );
 };
 
 export default renderRoutes;
