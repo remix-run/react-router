9a9dde7fe6228f9bcd897649896ee3e11ce06f0c
Paul Sherman <paul.sherman.88@gmail.com>
2018-07-07 18:55:37 -0500

    Missed this somehow

--- packages/react-router/modules/matchPath.js
+++ packages/react-router/modules/matchPath.js
@@ -26,22 +26,20 @@ const compilePath = (pattern, options) => {
  * Public API for matching a URL pathname to a path pattern.
  */
 const matchPath = (pathname, options = {}, parentMatch = null) => {
-  if (typeof options === 'string')
-    options = { path: options }
+  if (typeof options === "string") options = { path: options };
 
-  const { exact = false, strict = false, sensitive = false } = options
-  let path = options.path != null ? options.path : options.from
+  const { exact = false, strict = false, sensitive = false } = options;
+  let path = options.path != null ? options.path : options.from;
 
-  const absolute = isAbsolute(path)
+  const absolute = isAbsolute(path);
 
-  if (path == null) 
-    return parentMatch ? parentMatch : { url: '/', isExact: true, params: {} }
+  if (path == null)
+    return parentMatch ? parentMatch : { url: "/", isExact: true, params: {} };
 
-  if (!absolute)
-    path = resolvePath(path, parentMatch && parentMatch.url)
+  if (!absolute) path = resolvePath(path, parentMatch && parentMatch.url);
 
-  const { re, keys } = compilePath(path, { end: exact, strict, sensitive })
-  const match = re.exec(pathname)
+  const { re, keys } = compilePath(path, { end: exact, strict, sensitive });
+  const match = re.exec(pathname);
 
   if (path == null) return parent;
 
@@ -56,58 +54,46 @@ const matchPath = (pathname, options = {}, parentMatch = null) => {
   if (exact && !isExact) return null;
 
   const matchParams = keys.reduce((memo, key, index) => {
-    memo[key.name] = values[index]
-    return memo
-  }, {})
+    memo[key.name] = values[index];
+    return memo;
+  }, {});
 
   // merge parent match's params for partial paths
   // this allows us to join paths using the parent match's url instead of path
   const params = absolute
     ? matchParams
-    : Object.assign({}, parentMatch && parentMatch.params, matchParams)
+    : Object.assign({}, parentMatch && parentMatch.params, matchParams);
 
   return {
     path, // the path pattern used to match
     url: path === "/" && url === "" ? "/" : url, // the matched portion of the URL
     isExact, // whether or not we matched exactly
-<<<<<<< 2b94b8f9e115bec6426be06b309b6963f4a96004
-    params: keys.reduce((memo, key, index) => {
-      memo[key.name] = values[index];
-      return memo;
-    }, {})
+    params
   };
 };
 
-export default matchPath;
-=======
-    params
-  }
-}
-
 const resolvePath = (pathname, base) => {
   if (pathname === undefined || isAbsolute(pathname)) {
-    return pathname
+    return pathname;
   }
 
   if (!base) {
-    base = '/'
+    base = "/";
   }
 
-  if (pathname === '') {
-    return base
+  if (pathname === "") {
+    return base;
   } else {
-    return `${addTrailingSlash(base)}${pathname}`
+    return `${addTrailingSlash(base)}${pathname}`;
   }
-}
+};
 
-const isAbsolute = pathname => !!(pathname && pathname.charAt(0) === '/')
+const isAbsolute = pathname => !!(pathname && pathname.charAt(0) === "/");
 
 const addTrailingSlash = pathname =>
-  hasTrailingSlash(pathname) ? pathname : pathname + '/'
+  hasTrailingSlash(pathname) ? pathname : pathname + "/";
 
-const hasTrailingSlash = pathname => 
-  !!pathname && pathname.charAt(pathname.length-1) === '/'
+const hasTrailingSlash = pathname =>
+  !!pathname && pathname.charAt(pathname.length - 1) === "/";
 
-
-export default matchPath
->>>>>>> Resolve relative paths in matchPath
+export default matchPath;
