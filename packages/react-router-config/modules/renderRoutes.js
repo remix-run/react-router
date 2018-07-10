befb3d4eaf2a92cab7c2f923ad99f92d53ad5468
edward32tnt <edward32tnt@gmail.com>
2018-07-10 13:31:57 +0800

    Fix SyntaxError

--- packages/react-router-config/modules/renderRoutes.js
+++ packages/react-router-config/modules/renderRoutes.js
@@ -2,8 +2,8 @@ import React from "react";
 import Switch from "react-router/Switch";
 import Route from "react-router/Route";
 
-const renderRoutes = (routes, extraProps = {}, switchProps = {}) =>
-  routes ? (
+const renderRoutes = (routes, extraProps = {}, switchProps = {}) => {
+  return routes ? (
     <Switch {...switchProps}>
       {routes.map((route, i) => (
         <Route
@@ -13,18 +13,21 @@ const renderRoutes = (routes, extraProps = {}, switchProps = {}) =>
           strict={route.strict}
           render={props => {
             if (route.render) {
-              return route.render({ ...props, ...extraProps, route: route })
+              return route.render({ ...props, ...extraProps, route: route });
             } else {
-              return route.routes
-                ? ( <route.component {...props} {...extraProps} route={route}>
-                      {renderRoutes(route.routes, extraProps, switchProps)}
-                    </route.component> )
-                : ( <route.component {...props} {...extraProps} route={route} /> )
+              return route.routes ? (
+                <route.component {...props} {...extraProps} route={route}>
+                  {renderRoutes(route.routes, extraProps, switchProps)}
+                </route.component>
+              ) : (
+                <route.component {...props} {...extraProps} route={route} />
+              );
             }
-          }
+          }}
         />
       ))}
     </Switch>
   ) : null;
+}
 
 export default renderRoutes;
