e0b48f01cf99ee6c06e93df13a6517541cf6391e
Michael Jackson <michael@jackson.us>
2018-09-21 10:46:25 -0700

    Prettify build scripts

--- packages/react-router-config/tools/build.js
+++ packages/react-router-config/tools/build.js
@@ -1,42 +1,41 @@
-const fs = require('fs')
-const execSync = require('child_process').execSync
-const prettyBytes = require('pretty-bytes')
-const gzipSize = require('gzip-size')
+const fs = require("fs");
+const execSync = require("child_process").execSync;
+const prettyBytes = require("pretty-bytes");
+const gzipSize = require("gzip-size");
 
-const exec = (command, extraEnv) =>
+function exec(command, extraEnv) {
   execSync(command, {
-    stdio: 'inherit',
+    stdio: "inherit",
     env: Object.assign({}, process.env, extraEnv)
-  })
+  });
+}
 
-console.log('Building CommonJS modules ...')
+console.log("Building CommonJS modules ...");
 
-exec('babel modules -d . --ignore __tests__', {
-  BABEL_ENV: 'cjs'
-})
+exec("babel modules -d . --ignore __tests__", {
+  BABEL_ENV: "cjs"
+});
 
-console.log('\nBuilding ES modules ...')
+console.log("\nBuilding ES modules ...");
 
-exec('babel modules -d es --ignore __tests__', {
-  BABEL_ENV: 'es'
-})
+exec("babel modules -d es --ignore __tests__", {
+  BABEL_ENV: "es"
+});
 
-console.log('\nBuilding react-router-config.js ...')
+console.log("\nBuilding react-router-config.js ...");
 
-exec('rollup -c -f umd -o umd/react-router-config.js', {
-  BABEL_ENV: 'umd',
-  NODE_ENV: 'development'
-})
+exec("rollup -c -f umd -o umd/react-router-config.js", {
+  BABEL_ENV: "umd",
+  NODE_ENV: "development"
+});
 
-console.log('\nBuilding react-router.min.js ...')
+console.log("\nBuilding react-router.min.js ...");
 
-exec('rollup -c -f umd -o umd/react-router-config.min.js', {
-  BABEL_ENV: 'umd',
-  NODE_ENV: 'production'
-})
+exec("rollup -c -f umd -o umd/react-router-config.min.js", {
+  BABEL_ENV: "umd",
+  NODE_ENV: "production"
+});
 
-const size = gzipSize.sync(
-  fs.readFileSync('umd/react-router-config.min.js')
-)
+const size = gzipSize.sync(fs.readFileSync("umd/react-router-config.min.js"));
 
-console.log('\ngzipped, the UMD build is %s', prettyBytes(size))
+console.log("\ngzipped, the UMD build is %s", prettyBytes(size));
