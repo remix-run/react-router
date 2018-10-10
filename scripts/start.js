efed40b4bbb5cc981af9775f29cb3f87dab99aaa
Michael Jackson <michael@jackson.us>
2018-10-10 06:17:53 -0700

    Make start script work cross-platform

---
+++ scripts/start.js
@@ -0,0 +1,9 @@
+const path = require("path");
+const execSync = require("child_process").execSync;
+
+function exec(cmd) {
+  execSync(cmd, { stdio: "inherit", env: process.env });
+}
+
+process.chdir(path.resolve(__dirname, "../website"));
+exec("npm start");
