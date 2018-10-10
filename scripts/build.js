93ceb483622dacb75a3ed8c79a849c02cafb58e6
Michael Jackson <michael@jackson.us>
2018-10-10 05:27:27 -0700

    Build packages before building the website
    The website uses the pre-built versions of react-router and
    react-router-dom, so we need to make sure they are built first.

--- scripts/build.js
+++ scripts/build.js
@@ -1,11 +1,30 @@
+const path = require("path");
 const execSync = require("child_process").execSync;
 
-function exec(cmd, env) {
+function exec(cmd) {
   execSync(cmd, { stdio: "inherit", env: process.env });
 }
 
-if (process.env.CI) {
-  exec("lerna run build --stream --ignore react-router-website");
-} else {
-  exec("lerna run build --stream");
+const cwd = process.cwd();
+
+// Note: We don't currently have a build step for react-router-native.
+// Instead, we use the source files directly.
+["react-router", "react-router-dom", "react-router-config"].forEach(
+  packageName => {
+    console.log("Building %s ...", packageName);
+    process.chdir(path.resolve(__dirname, "../packages/" + packageName));
+    exec("npm run build");
+    console.log();
+  }
+);
+
+if (!process.env.CI) {
+  // Don't bother building the website now. Instead, build it in
+  // deploy-website.sh when we're on the "website" branch.
+  console.log("Building the website ...");
+  process.chdir(path.resolve(__dirname, "../website"));
+  exec("npm run build");
+  console.log();
 }
+
+process.chdir(cwd);
