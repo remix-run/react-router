e80d1ba6f0636a04e57e37e95804fafd034c879e
Bogdan Chadkin <trysound@yandex.ru>
2018-10-14 13:52:35 +0300

    Build with multi configs and fix esm entry point
    As you know esm distribution for different environments is not trivial
    if we go with "react" way. I suggest to provide only one esm entry
    point. User bundler will solve environments. Webpack requires only one
    flag for this.
    
    In this diff I also replaced build script with rollup multi config,
    added build artifacts cleaning and added size snapshot plugin to track
    package size and esm treeshakability.

--- packages/react-router-config/rollup.config.js
+++ packages/react-router-config/rollup.config.js
@@ -1,35 +1,112 @@
 import babel from "rollup-plugin-babel";
-import uglify from "rollup-plugin-uglify";
 import replace from "rollup-plugin-replace";
 import commonjs from "rollup-plugin-commonjs";
-import resolve from "rollup-plugin-node-resolve";
+import nodeResolve from "rollup-plugin-node-resolve";
+import { sizeSnapshot } from "rollup-plugin-size-snapshot";
+import { uglify } from "rollup-plugin-uglify";
+import pkg from "./package.json";
 
-const config = {
-  output: {
-    name: "ReactRouterConfig",
-    globals: {
-      react: "React",
-      "react-router": "ReactRouter"
-    }
-  },
-  external: ["react", "react-router"],
-  plugins: [
-    babel({
-      exclude: "node_modules/**",
-      plugins: ["external-helpers"]
-    }),
-    resolve(),
-    commonjs({
-      include: "node_modules/**"
-    }),
-    replace({
-      "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV)
-    })
-  ]
+const input = "modules/index.js";
+const name = "ReactRouterConfig";
+const globals = {
+  react: "React",
+  "react-router": "ReactRouter"
+};
+const babelOptions = {
+  exclude: "node_modules/**",
+  plugins: ["external-helpers"]
+};
+const commonjsOptions = {
+  include: "node_modules/**"
 };
 
-if (process.env.NODE_ENV === "production") {
-  config.plugins.push(uglify());
-}
+export default [
+  {
+    input,
+    output: {
+      file: `cjs/${pkg.name}.js`,
+      format: "cjs",
+      name,
+      globals
+    },
+    external: Object.keys(globals),
+    plugins: [
+      babel(babelOptions),
+      nodeResolve(),
+      commonjs(commonjsOptions),
+      replace({ "process.env.NODE_ENV": JSON.stringify("development") })
+    ]
+  },
 
-export default config;
+  {
+    input,
+    output: {
+      file: `cjs/${pkg.name}.min.js`,
+      format: "cjs",
+      name,
+      globals
+    },
+    external: Object.keys(globals),
+    plugins: [
+      babel(babelOptions),
+      nodeResolve(),
+      commonjs(commonjsOptions),
+      replace({ "process.env.NODE_ENV": JSON.stringify("production") }),
+      uglify()
+    ]
+  },
+
+  {
+    input,
+    output: {
+      file: `esm/${pkg.name}.js`,
+      format: "esm",
+      name,
+      globals
+    },
+    external: Object.keys(globals),
+    plugins: [
+      babel(babelOptions),
+      nodeResolve(),
+      commonjs(commonjsOptions),
+      sizeSnapshot()
+    ]
+  },
+
+  {
+    input,
+    output: {
+      file: `umd/${pkg.name}.js`,
+      format: "umd",
+      name,
+      globals
+    },
+    external: Object.keys(globals),
+    plugins: [
+      babel(babelOptions),
+      nodeResolve(),
+      commonjs(commonjsOptions),
+      replace({ "process.env.NODE_ENV": JSON.stringify("development") }),
+      sizeSnapshot()
+    ]
+  },
+
+  {
+    input,
+    output: {
+      file: `umd/${pkg.name}.min.js`,
+      format: "umd",
+      name,
+      globals
+    },
+    external: Object.keys(globals),
+    plugins: [
+      babel(babelOptions),
+      nodeResolve(),
+      commonjs(commonjsOptions),
+      replace({ "process.env.NODE_ENV": JSON.stringify("production") }),
+      sizeSnapshot(),
+      uglify()
+    ]
+  }
+];
