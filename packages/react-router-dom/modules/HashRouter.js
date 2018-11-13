559e0eeae77bc2ba0b70288aa29fe00511982bd2
Bogdan Chadkin <trysound@yandex.ru>
2018-11-13 23:14:07 +0300

    Convert HashRouter to function component

--- packages/react-router-dom/modules/HashRouter.js
+++ packages/react-router-dom/modules/HashRouter.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Router } from "react-router";
+import { Router, __Lifecycle as Lifecycle } from "react-router";
 import { createHashHistory as createHistory } from "history";
 import PropTypes from "prop-types";
 import warning from "tiny-warning";
@@ -7,12 +7,22 @@ import warning from "tiny-warning";
 /**
  * The public API for a <Router> that uses window.location.hash.
  */
-class HashRouter extends React.Component {
-  history = createHistory(this.props);
-
-  render() {
-    return <Router history={this.history} children={this.props.children} />;
-  }
+function HashRouter(props) {
+  return (
+    <Lifecycle>
+      {instance => {
+        if (!instance.history) {
+          instance.history = createHistory(props);
+          warning(
+            !props.history,
+            "<HashRouter> ignores the history prop. To use a custom history, " +
+              "use `import { Router }` instead of `import { HashRouter as Router }`."
+          );
+        }
+        return <Router history={instance.history} children={props.children} />;
+      }}
+    </Lifecycle>
+  );
 }
 
 if (__DEV__) {
@@ -21,14 +31,6 @@ if (__DEV__) {
     children: PropTypes.node,
     getUserConfirmation: PropTypes.func,
     hashType: PropTypes.oneOf(["hashbang", "noslash", "slash"])
-  };
-
-  HashRouter.prototype.componentDidMount = function() {
-    warning(
-      !this.props.history,
-      "<HashRouter> ignores the history prop. To use a custom history, " +
-        "use `import { Router }` instead of `import { HashRouter as Router }`."
-    );
   };
 }
 
