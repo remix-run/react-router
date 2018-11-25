e386eaeb5b3472e2c6d30c555b2d703b09b8ef71
Michael Jackson <michael@jackson.us>
2018-11-24 22:35:30 -0800

    Use .babelrc files instead of .babelrc.js

--- packages/react-router-config/modules/.babelrc.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  presets: [["@babel/env", { loose: true }], "@babel/react"],
-  plugins: ["dev-expression"]
-};
--- packages/react-router/modules/.babelrc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  presets: [["@babel/env", { loose: true }], "@babel/react"],
-  plugins: [
-    "dev-expression",
-    ["@babel/proposal-class-properties", { loose: true }]
-  ]
-};
