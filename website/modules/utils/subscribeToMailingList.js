8e280445f37797c87764d54715301e2d66409340
Brad Westfall <brad@azpixels.com>
2019-02-26 13:30:34 -0700

    new mailing list service

--- website/modules/utils/subscribeToMailingList.js
+++ website/modules/utils/subscribeToMailingList.js
@@ -2,16 +2,15 @@ import fetch from "isomorphic-fetch";
 import qs from "query-string";
 
 function subscribeToMailingList(email) {
-  return fetch("https://reacttraining.us16.list-manage.com/subscribe/post", {
+  return fetch("https://api.convertkit.com/v3/forms/852704/subscribe", {
     method: "POST",
     mode: "no-cors",
     headers: {
       "Content-Type": "application/x-www-form-urlencoded"
     },
     body: qs.stringify({
-      u: "47df8ab2101d1dd83353427fc",
-      id: "83f54bda60",
-      MERGE0: email
+      api_key: "617ze_vOpzLKL9yB_BbsXQ",
+      email
     })
   });
 }
