63f8234e5ddff5c77467e8a3f360ec081cb25abb
Michael Jackson <michael@jackson.us>
2019-02-27 14:01:44 -0800

    Update deploy script

--- scripts/deploy-website.sh
+++ scripts/deploy-website.sh
@@ -2,19 +2,20 @@
 
 PUBLIC_PATH="${PUBLIC_PATH:-/react-router/}"
 
-root_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
+root_dir="$(dirname "$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)")"
 tmp_dir="/tmp/reacttraining.com"
 
+# Clone reacttraining.com repo into the tmp dir
 rm -rf $tmp_dir
 git clone --depth 2 --branch master "git@github.com:ReactTraining/reacttraining.com.git" $tmp_dir
 
-cd "$root_dir/website"
-
+# Build the website into the public/react-router dir
 rm -rf "$tmp_dir/public/react-router"
+cd "$root_dir/website"
 npm run build -- --output-path "$tmp_dir/public/react-router" --output-public-path $PUBLIC_PATH
 
+# Commit all changes
 cd $tmp_dir
-
 git add -A
 git commit \
   -m "Update react-router website
@@ -22,4 +23,5 @@ git commit \
 https://travis-ci.org/$TRAVIS_REPO_SLUG/builds/$TRAVIS_BUILD_ID" \
   --author "Travis CI <travis@reacttraining.com>"
 
+# Deploy
 git push origin master
