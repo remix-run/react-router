dbc9481e3775c6b7f63fc3d982041a20c31f526f
Paul Sherman <paul.sherman.88@gmail.com>
2019-03-18 13:12:49 -0500

    Get react-router-native tests working

--- packages/react-router-native/jest.config.js
+++ packages/react-router-native/jest.config.js
@@ -1,8 +1,10 @@
 module.exports = {
   preset: "react-native",
+  rootDir: "../..",
   testRunner: "jest-circus/runner",
+  testMatch: ["<rootDir>/packages/react-router-native/__tests__/*.js"],
   restoreMocks: true,
   moduleNameMapper: {
-    "^react-router$": "<rootDir>/../react-router/cjs/react-router.js"
+    "^react-router$": "<rootDir>/node_modules/react-router/cjs/react-router.js"
   }
 };
