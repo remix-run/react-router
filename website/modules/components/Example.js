d302953b76edd75409335117b2e3e7dee0777df2
Brad Westfall <brad@azpixels.com>
2019-03-21 11:20:47 -0700

    should be float left

--- website/modules/components/Example.js
+++ website/modules/components/Example.js
@@ -1,59 +1,53 @@
-import React, { Component } from "react";
+import React from "react";
 import { Redirect } from "react-router-dom";
 import PropTypes from "prop-types";
 import SandboxExample from "./SandboxExample";
 
-class Example extends Component {
-  static propTypes = {
-    data: PropTypes.object,
-    match: PropTypes.shape({
-      params: PropTypes.shape({
-        example: PropTypes.string,
-        environment: PropTypes.string
-      })
-    })
-  };
+function Example({ data, match }) {
+  const { example: exampleParam, environment } = match.params;
+  const example = data.examples.find(e => e.slug === exampleParam);
+  const isNative = environment === "native";
 
-  render() {
-    const {
-      data,
-      match: {
-        params: { example: exampleParam, environment }
-      }
-    } = this.props;
-    const example = data.examples.find(e => e.slug === exampleParam);
-    const isNative = environment === "native";
-    return example ? (
-      isNative ? (
-        <SandboxExample
-          label={example.label}
-          path={example.path}
-          dependencies={{
-            "react-router-native": "latest",
-            "react-native-web": "latest",
-            "react-art": "latest",
-            "react-scripts": "2.0.0",
-            ...(example.extraDependencies || {})
-          }}
-          code={example.code}
-          extraEmbedOptions={{ editorsize: 66, hidenavigation: true }}
-        />
-      ) : (
-        <SandboxExample
-          label={example.label}
-          path={example.path}
-          dependencies={{
-            "react-router-dom": "latest",
-            "react-scripts": "2.0.0",
-            ...(example.extraDependencies || {})
-          }}
-          code={example.code}
-        />
-      )
+  return example ? (
+    isNative ? (
+      <SandboxExample
+        label={example.label}
+        path={example.path}
+        dependencies={{
+          "react-router-native": "latest",
+          "react-native-web": "latest",
+          "react-art": "latest",
+          "react-scripts": "2.0.0",
+          ...(example.extraDependencies || {})
+        }}
+        code={example.code}
+        extraEmbedOptions={{ editorsize: 66, hidenavigation: true }}
+      />
     ) : (
-      <Redirect to={`/${environment}/example/${data.examples[0].slug}`} />
-    );
-  }
+      <SandboxExample
+        label={example.label}
+        path={example.path}
+        dependencies={{
+          "react-router-dom": "latest",
+          "react-scripts": "2.0.0",
+          ...(example.extraDependencies || {})
+        }}
+        code={example.code}
+      />
+    )
+  ) : (
+    <Redirect to={`/${environment}/example/${data.examples[0].slug}`} />
+  );
 }
+
+Example.propTypes = {
+  data: PropTypes.object,
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      example: PropTypes.string,
+      environment: PropTypes.string
+    })
+  })
+};
 
 export default Example;
