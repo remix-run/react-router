323e3e9d22f0a316a92c63d38f35113dde2fb720
StringEpsilon <StringEpsilon@gmail.com>
2019-04-06 13:20:39 +0200

    Switch to mini-create-router-context

--- packages/react-router/modules/RouterContext.js
+++ packages/react-router/modules/RouterContext.js
@@ -1,12 +1,12 @@
 // TODO: Replace with React.createContext once we can assume React 16+
-import createContext from "create-react-context";
+import createContext from "mini-create-react-context";
 
 const createNamedContext = name => {
   const context = createContext();
   context.displayName = name;
 
   return context;
-}
+};
 
-const context = /*#__PURE__*/ createNamedContext('Router');
+const context = /*#__PURE__*/ createNamedContext("Router");
 export default context;
