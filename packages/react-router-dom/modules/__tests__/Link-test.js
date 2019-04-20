a09d6ab64bae86496a174502ce3f2eca203f8116
MeiKatz <gregor.mitzka@gmail.com>
2019-04-20 13:46:27 +0200

    remove unused Component import in <Link /> test

--- packages/react-router-dom/modules/__tests__/Link-test.js
+++ packages/react-router-dom/modules/__tests__/Link-test.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
 import { MemoryRouter, Router, HashRouter, Link } from "react-router-dom";
 import { createMemoryHistory } from "history";
