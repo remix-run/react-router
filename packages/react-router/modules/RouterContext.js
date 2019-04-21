8786e39879a707c5e71252eae1e340d4d39c314d
Phap <centralparkloop@yahoo.com>
2019-04-21 14:10:59 -0400

    remove mini-create-react-context

--- packages/react-router/modules/RouterContext.js
+++ packages/react-router/modules/RouterContext.js
@@ -1,8 +1,7 @@
-// TODO: Replace with React.createContext once we can assume React 16+
-import createContext from "mini-create-react-context";
+import React from "react";
 
 const createNamedContext = name => {
-  const context = createContext();
+  const context = React.createContext();
   context.displayName = name;
 
   return context;
