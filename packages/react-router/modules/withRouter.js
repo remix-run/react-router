56c829bfad227ac8c096610e089871d231bf89ea
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2019-05-15 13:36:33 +0200

    fix(react-router): Allow string and object refs in withRouter (#6680)
    * fix(react-router): Don't warn about ref objects
    
    * fix(react-router): Don't warn about string refs

--- packages/react-router/modules/withRouter.js
+++ packages/react-router/modules/withRouter.js
@@ -36,7 +36,11 @@ function withRouter(Component) {
 
   if (__DEV__) {
     C.propTypes = {
-      wrappedComponentRef: PropTypes.func
+      wrappedComponentRef: PropTypes.oneOfType([
+        PropTypes.string,
+        PropTypes.func,
+        PropTypes.object
+      ])
     };
   }
 
