395edf8b403586fdf8259aa9b0c105a837aa5f90
Paul Sherman <paul.sherman.88@gmail.com>
2019-06-18 09:39:20 -0500

    Remove basename before pushing internal links

--- website/modules/components/DelegateMarkdownLinks.js
+++ website/modules/components/DelegateMarkdownLinks.js
@@ -2,6 +2,14 @@ import { Component } from "react";
 import PropTypes from "prop-types";
 import { withRouter } from "react-router-dom";
 
+import basename from "../basename";
+
+const matchBase = new RegExp(`^${basename}`);
+
+function removeBase(href) {
+  return href.replace(matchBase, "");
+}
+
 let delegate = history => {
   document.body.addEventListener("click", e => {
     let node = e.target;
@@ -10,7 +18,7 @@ let delegate = history => {
       if (typeof node.className === "string") {
         if (node.className.match(/internal-link/)) {
           e.preventDefault();
-          const href = node.getAttribute("href");
+          const href = removeBase(node.getAttribute("href"));
           history.push(href);
           break;
         }
