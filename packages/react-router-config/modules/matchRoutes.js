2ebf8491a42c663f987666805cf133cb25f06256
MeiKatz <gregor.mitzka@gmail.com>
2019-06-27 19:58:53 +0200

    [fix] use parentMatch.url instead of parentMatch in matchRoutes()
    missed to change matchRoutes() after change in previous commit

--- packages/react-router-config/modules/matchRoutes.js
+++ packages/react-router-config/modules/matchRoutes.js
@@ -6,7 +6,7 @@ function matchRoutes(routes, pathname, /*not public API*/ branch = []) {
       branch[branch.length - 1] && branch[branch.length - 1].match;
 
     const match = route.path
-      ? matchPath(pathname, route, parentMatch)
+      ? matchPath(pathname, route, parentMatch && parentMatch.url)
       : parentMatch
         ? parentMatch // use parent match
         : Router.computeRootMatch(pathname); // use default "root" match
