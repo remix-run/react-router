6ecfde2321c9adbbaa7335a0fcfda6fa233b51ba
Phap <centralparkloop@yahoo.com>
2019-08-04 21:00:41 -0400

    linted some files

--- packages/react-router-native/rn-cli.config.js
+++ packages/react-router-native/rn-cli.config.js
@@ -5,16 +5,16 @@ const config = require("react-native/packager/rn-cli.config");
 const rootPackagePath = path.join(__dirname, "..", "..");
 
 config.getBlacklist = () => [
-  new RegExp(`^${escapeRegExp(rootPackagePath)}\/node_modules(.*)`),
+  new RegExp(`^${escapeRegExp(rootPackagePath)}/node_modules(.*)`),
   new RegExp(
     `^${escapeRegExp(
       rootPackagePath
-    )}\/packages\/(react-router-dom|react-router-website(-*)?)\/(.*)`
+    )}/packages/(react-router-dom|react-router-website(-*)?)/(.*)`
   ),
   new RegExp(
     `^${escapeRegExp(
       rootPackagePath
-    )}\/packages\/react-router-native\/node_modules\/react-router(.*)`
+    )}/packages/react-router-native/node_modules/react-router(.*)`
   )
   //new RegExp(`^${escapeRegExp(rootPackagePath)}\/packages\/react-router\/node_modules\/(.*)`),
 ];
