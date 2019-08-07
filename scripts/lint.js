f3ea7ebfade2acedeb20cfb9a0437816f1afd309
Phap <centralparkloop@yahoo.com>
2019-08-06 21:04:32 -0400

    add script for linting

---
+++ scripts/lint.js
@@ -0,0 +1,19 @@
+const path = require("path");
+const execSync = require("child_process").execSync;
+
+function exec(cmd) {
+  execSync(cmd, { stdio: "inherit", env: process.env });
+}
+
+const cwd = process.cwd();
+
+[
+  "react-router",
+  "react-router-dom",
+  "react-router-config"
+].forEach(packageName => {
+  process.chdir(path.resolve(__dirname, "../packages/" + packageName));
+  exec("npm run lint");
+});
+
+process.chdir(cwd);
