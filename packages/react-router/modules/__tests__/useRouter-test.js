6762221370e1280103b81e92a8afc739f036814c
Daniel Skelton <daniel_skelton@rapid7.com>
2019-09-24 15:59:56 +0100

    update useRouter test description

--- packages/react-router/modules/__tests__/useRouter-test.js
+++ packages/react-router/modules/__tests__/useRouter-test.js
@@ -4,7 +4,7 @@ import { MemoryRouter, Route, useRouter } from "react-router";
 
 import renderStrict from "./utils/renderStrict.js";
 
-describe("useLocation", () => {
+describe("useRouter", () => {
   const node = document.createElement("div");
 
   afterEach(() => {
