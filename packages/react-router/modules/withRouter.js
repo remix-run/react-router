70088750a9f92ee09af073bc7feb94b0e7c2e59d
Kingsley Ochu <ochu.kc@gmail.com>
2019-10-13 22:10:42 +0100

    Forward withRouter ref using `React.forwardRef` api

--- packages/react-router/modules/withRouter.js
+++ packages/react-router/modules/withRouter.js
@@ -4,13 +4,17 @@ import hoistStatics from "hoist-non-react-statics";
 import invariant from "tiny-invariant";
 
 import RouterContext from "./RouterContext.js";
+let { forwardRef } = React;
+if (typeof forwardRef === "undefined") {
+  forwardRef = C => C;
+}
 
 /**
  * A public higher-order component to access the imperative API
  */
 function withRouter(Component) {
   const displayName = `withRouter(${Component.displayName || Component.name})`;
-  const C = props => {
+  const C = forwardRef((props, forwardedRef) => {
     const { wrappedComponentRef, ...remainingProps } = props;
 
     return (
@@ -24,13 +28,13 @@ function withRouter(Component) {
             <Component
               {...remainingProps}
               {...context}
-              ref={wrappedComponentRef}
+              ref={forwardedRef || wrappedComponentRef}
             />
           );
         }}
       </RouterContext.Consumer>
     );
-  };
+  });
 
   C.displayName = displayName;
   C.WrappedComponent = Component;
