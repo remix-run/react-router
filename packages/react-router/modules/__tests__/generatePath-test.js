4e7daea9ae33b6fb35386f39b941dee6651a8c33
Mehmet Sarıoğlu <mehmet.sarioglu@dcipheranalytics.com>
2019-12-15 22:11:34 +0300

    Add modifier usage tests to generatePath

--- packages/react-router/modules/__tests__/generatePath-test.js
+++ packages/react-router/modules/__tests__/generatePath-test.js
@@ -65,4 +65,89 @@ describe("generatePath", () => {
       );
     });
   });
+
+  describe("with optional params=/posts/:id?", () => {
+    it("returns correct url with optional params", () => {
+      const pattern = "/posts/:id?";
+      const params = { id: "post-id" };
+
+      const generated = generatePath(pattern, params);
+      expect(generated).toBe("/posts/post-id");
+    });
+
+    it("returns correct url without optional params", () => {
+      const pattern = "/posts/:id?";
+
+      const generated = generatePath(pattern);
+      expect(generated).toBe("/posts");
+    });
+  });
+
+  describe("with zero or more params modifier=/some/:path*", () => {
+    it("returns correct url without params", () => {
+      const pattern = "/some/:path*";
+
+      const generated = generatePath(pattern);
+      expect(generated).toBe("/some");
+    });
+
+    it("returns correct url with empty array", () => {
+      const pattern = "/some/:path*";
+      const params = { path: [] };
+
+      const generated = generatePath(pattern, params);
+      expect(generated).toBe("/some");
+    });
+
+    it("returns correct url with primitive params", () => {
+      const pattern = "/some/:path*";
+      const params = { path: "primitive" };
+
+      const generated = generatePath(pattern, params);
+      expect(generated).toBe("/some/primitive");
+    });
+
+    it("returns correct url with array of params", () => {
+      const pattern = "/some/:path*";
+      const params = { path: ["array", "of", "params"] };
+
+      const generated = generatePath(pattern, params);
+      expect(generated).toBe("/some/array/of/params");
+    });
+  });
+
+  describe("with one or more params modifier=/some/:path+", () => {
+    it("throws error without params", () => {
+      const pattern = "/some/:path+";
+
+      expect(() => {
+        generatePath(pattern);
+      }).toThrow();
+    });
+
+    it("throws error with empty array", () => {
+      const pattern = "/some/:path+";
+      const params = { path: [] };
+
+      expect(() => {
+        generatePath(pattern, params);
+      }).toThrow();
+    });
+
+    it("returns correct url with primitive params", () => {
+      const pattern = "/some/:path+";
+      const params = { path: "primitive" };
+
+      const generated = generatePath(pattern, params);
+      expect(generated).toBe("/some/primitive");
+    });
+
+    it("returns correct url with array of params", () => {
+      const pattern = "/some/:path+";
+      const params = { path: ["array", "of", "params"] };
+
+      const generated = generatePath(pattern, params);
+      expect(generated).toBe("/some/array/of/params");
+    });
+  });
 });
