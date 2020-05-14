af8fcd945a043354e6574493b5c9e5411c5c6e8d
Dominik Engel <DE@nea-x.net>
2020-05-14 10:00:46 +0200

    Enable Linter
    THis PR correctly sets up the eslinter for react-router typescirpt files

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  parser: "@typescript-eslint/parser", // Specifies the ESLint parser
+  parserOptions: {
+    ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
+    sourceType: "module" // Allows for the use of imports
+  },
+  extends: [
+    "plugin:@typescript-eslint/recommended" // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+  ],
+  rules: {
+    // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
+    // e.g. "@typescript-eslint/explicit-function-return-type": "off",
+  }
+};
