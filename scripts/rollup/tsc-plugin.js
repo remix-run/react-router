814ce0e7f04818badd5b50586545512b4d9f726a
Michael Jackson <michael@jackson.us>
2020-06-18 14:10:51 -0700

    Add back missing .d.ts files to the output

--- scripts/rollup/tsc-plugin.js
+++ scripts/rollup/tsc-plugin.js
@@ -4,8 +4,8 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 
 const dirname = path.dirname(fileURLToPath(import.meta.url));
-const repoRoot = path.resolve(dirname, '../..');
-const tscOutputRoot = path.join(repoRoot, '.tsc-output');
+const repoDir = path.resolve(dirname, '../..');
+const tscOutputDir = path.join(repoDir, '.tsc-output');
 
 const extRe = /\.tsx?$/;
 
@@ -39,18 +39,18 @@ export default function tscPlugin({ build, watch } = {}) {
       return promise;
     },
     async resolveId(id, importer) {
-      if (!importer && id.startsWith('packages/')) {
+      if (!importer) {
         // This is one of our entry points. Resolve the
         // id to the compiled file in the output dir.
-        let outFile = id.replace(/^packages/, tscOutputRoot);
+        let outFile = id.replace(/^packages/, tscOutputDir);
 
-        if (build) {
-          // Also, emit the d.ts file
-          let dtsFile = outFile.replace(extRe, '.d.ts');
-          let fileName = path.basename(dtsFile);
-          let source = await fsp.readFile(dtsFile, { encoding: 'utf8' });
-          this.emitFile({ type: 'asset', fileName, source });
-        }
+        // Also, emit the d.ts file
+        // TODO: Should we be doing this in buildStart instead to
+        // avoid calling emitFile() each time this id is resolved?
+        let dtsFile = outFile.replace(extRe, '.d.ts');
+        let fileName = path.basename(dtsFile);
+        let source = await fsp.readFile(dtsFile, { encoding: 'utf8' });
+        this.emitFile({ type: 'asset', fileName, source });
 
         return outFile.replace(extRe, '.js');
       }
