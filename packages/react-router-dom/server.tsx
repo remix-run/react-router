a2fb3d09a7a84150c50b627d6124293ba60907d2
Michael Jackson <michael@jackson.us>
2020-06-18 13:06:13 -0700

    Add missing space

--- packages/react-router-dom/server.tsx
+++ packages/react-router-dom/server.tsx
@@ -50,11 +50,11 @@ export function StaticRouter({
           `in your app.`
       );
     },
-    go(n: number) {
+    go(delta: number) {
       throw new Error(
-        `You cannot use navigator.go(${n}) on the server because it is a stateless` +
+        `You cannot use navigator.go() on the server because it is a stateless ` +
           `environment. This error was probably triggered when you did a ` +
-          `\`navigate(${n})\` somewhere in your app.`
+          `\`navigate(${delta})\` somewhere in your app.`
       );
     },
     back() {
