ba7d9c1747eadf96cf5aac6747752f1d5f6b8afc
Michael Jackson <michael@jackson.us>
2020-07-02 18:06:39 -0700

    Update website repo

--- scripts/deploy-website.sh
+++ scripts/deploy-website.sh
@@ -2,20 +2,21 @@
 
 set -e
 
-PUBLIC_PATH="${PUBLIC_PATH:-/react-router/}"
+PUBLIC_PATH="${PUBLIC_PATH:-/}"
 
 root_dir="$(dirname "$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)")"
-tmp_dir="/tmp/reacttraining.com"
+tmp_dir="/tmp/reactrouter.com"
+output_dir="$tmp_dir/public"
 
 # Clone reacttraining.com repo into the tmp dir
 rm -rf $tmp_dir
-git clone --depth 2 --branch master "git@github.com:ReactTraining/reacttraining.com.git" $tmp_dir
+git clone --depth 2 --branch master "git@github.com:ReactTraining/reactrouter.com.git" $tmp_dir
 
 # Build the website into the static/react-router dir
-rm -rf "$tmp_dir/static/react-router"
+rm -rf "$output_dir"
 cd "$root_dir/website"
 yarn
-yarn build --output-path "$tmp_dir/static/react-router" --output-public-path $PUBLIC_PATH
+yarn build --output-path "$output_dir" --output-public-path $PUBLIC_PATH
 
 # Commit all changes
 cd $tmp_dir
