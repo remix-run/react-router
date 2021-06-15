cfd1a188cbdb4dd4ca3fd430b161a46b6eb708f9
Brooks Lybrand <brooks.lybrand@business-laboratory.com>
2021-06-15 17:37:54 -0500

    Replace undefined matches with ''

--- packages/react-router/index.tsx
+++ packages/react-router/index.tsx
@@ -884,7 +884,7 @@ export function matchPath(
   let matchedPathname = match[1];
   let values = match.slice(2);
   let params = paramNames.reduce((memo, paramName, index) => {
-    memo[paramName] = safelyDecodeURIComponent(values[index], paramName);
+    memo[paramName] = safelyDecodeURIComponent(values[index] || '', paramName);
     return memo;
   }, {} as Params);
 
