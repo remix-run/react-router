3b7481c544b03603a1c2425f95fa56bc9b071d5d
Chance Strickland <chance.strickland@gmail.com>
2021-06-24 21:03:29 -0700

    prettier

--- packages/react-router/jest.config.js
+++ packages/react-router/jest.config.js
@@ -1,12 +1,12 @@
 module.exports = {
-  testMatch: ['**/__tests__/*-test.[jt]s?(x)'],
+  testMatch: ["**/__tests__/*-test.[jt]s?(x)"],
   transform: {
-    '\\.[jt]sx?$': './jest-transformer.js'
+    "\\.[jt]sx?$": "./jest-transformer.js",
   },
   globals: {
-    __DEV__: true
+    __DEV__: true,
   },
   moduleNameMapper: {
-    "^react-router$": "<rootDir>/../../build/react-router"
-  }
+    "^react-router$": "<rootDir>/../../build/react-router",
+  },
 };
