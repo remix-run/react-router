055a0fb956ec74a902a3c97f7e8463c58f6f6c2b
zhangqiming897 <38695894+zhangqiming897@users.noreply.github.com>
2021-07-05 11:18:51 +0800

    更新
    增加路由缓存

--- packages/react-router-config/modules/renderRoutes.js
+++ packages/react-router-config/modules/renderRoutes.js
@@ -1,8 +1,10 @@
 import React from "react";
 import { Switch, Route } from "react-router";
+import KeepAlive, { AliveScope } from 'react-activation';
 
 function renderRoutes(routes, extraProps = {}, switchProps = {}) {
   return routes ? (
+   <AliveScope> 
     <Switch {...switchProps}>
       {routes.map((route, i) => (
         <Route
@@ -14,13 +16,17 @@ function renderRoutes(routes, extraProps = {}, switchProps = {}) {
             route.render ? (
               route.render({ ...props, ...extraProps, route: route })
             ) : (
+             <KeepAlive when={route.cache}> 
               <route.component {...props} {...extraProps} route={route} />
+             </KeepAlive> 
             )
           }
         />
       ))}
     </Switch>
+ </AliveScope>
   ) : null;
 }
 
 export default renderRoutes;
+
