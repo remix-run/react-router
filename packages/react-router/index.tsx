5dc6ca370a2b95836b6ba6df00ab8a3e83ec01eb
issac <issac@novonetworks.com>
2021-07-23 13:20:29 +0900

    Add missing generic types to useLocation

--- packages/react-router/index.tsx
+++ packages/react-router/index.tsx
@@ -347,7 +347,7 @@ export function useInRouterContext(): boolean {
  *
  * @see https://reactrouter.com/api/useLocation
  */
-export function useLocation(): Location {
+export function useLocation<S extends State = State>(): Location<S> {
   invariant(
     useInRouterContext(),
     // TODO: This error is probably because they somehow have 2 versions of the
