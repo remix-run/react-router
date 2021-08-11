43045de42bda277c997182cdecfec1447d14cd1b
Chance Strickland <chance.strickland@gmail.com>
2021-08-11 14:14:58 -0700

    add tests for useResolvedPath

---
+++ packages/react-router/__tests__/useResolvedPath-test.tsx
@@ -0,0 +1,59 @@
+import * as React from "react";
+import { create as createTestRenderer } from "react-test-renderer";
+import {
+  MemoryRouter as Router,
+  Routes,
+  Route,
+  useResolvedPath
+} from "react-router";
+import type { Path } from "history";
+
+describe("useResolvedPath", () => {
+  it("path string resolves to Path object", () => {
+    let path!: Path;
+    function Home() {
+      path = useResolvedPath("/home?user=mj#welcome");
+      return <h1>Home</h1>;
+    }
+
+    createTestRenderer(
+      <Router initialEntries={["/home"]}>
+        <Routes>
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </Router>
+    );
+
+    expect(typeof path).toBe("object");
+    expect(path).toMatchObject({
+      pathname: "/home",
+      search: "?user=mj",
+      hash: "#welcome"
+    });
+  });
+
+  describe("given a hash with a ? character", () => {
+    it("hash is not parsed as a search string", () => {
+      let path!: Path;
+      function Home() {
+        path = useResolvedPath("/home#welcome?user=mj");
+        return <h1>Home</h1>;
+      }
+
+      createTestRenderer(
+        <Router initialEntries={["/home"]}>
+          <Routes>
+            <Route path="/home" element={<Home />} />
+          </Routes>
+        </Router>
+      );
+
+      expect(typeof path).toBe("object");
+      expect(path).toMatchObject({
+        pathname: "/home",
+        search: "",
+        hash: "#welcome?user=mj"
+      });
+    });
+  });
+});
