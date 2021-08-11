a78c6ddd80d2fd697500b907849b449358734e2e
Chance Strickland <chance.strickland@gmail.com>
2021-08-11 15:29:44 -0700

    update website deploy script

--- scripts/deploy-website.sh
+++ scripts/deploy-website.sh
@@ -9,7 +9,7 @@ tmp_dir="/tmp/reacttraining.com"
 
 # Clone reacttraining.com repo into the tmp dir
 rm -rf $tmp_dir
-git clone --depth 2 --branch master "git@github.com:ReactTraining/reacttraining.com.git" $tmp_dir
+git clone --depth 2 --branch main "git@github.com:remix-run/reactrouter.com.git" $tmp_dir
 
 # Build the website into the static/react-router dir
 rm -rf "$tmp_dir/static/react-router"
@@ -24,7 +24,7 @@ git commit \
 	--author "Travis CI <travis-ci@reacttraining.com>" \
   -m "Update react-router website
 
-https://travis-ci.com/$TRAVIS_REPO_SLUG/builds/$TRAVIS_BUILD_ID"
+$GITHUB_SERVER_URL/$GITHUB_REPOSITORY/actions/runs/$GITHUB_RUN_ID"
 
 # Deploy
-git push origin master
+git push origin main
