a2e219720e743e2a489e8b6cfb60b00e13c4e673
Chance Strickland <chance.strickland@gmail.com>
2021-08-12 16:59:01 -0700

    remove location prop from "Routes"

--- packages/react-router/__tests__/useRoutes-test.tsx
+++ packages/react-router/__tests__/useRoutes-test.tsx
@@ -1,8 +1,6 @@
 import * as React from "react";
-import * as ReactDOM from "react-dom";
 import { create as createTestRenderer } from "react-test-renderer";
 import { MemoryRouter as Router, useRoutes } from "react-router";
-import { act } from "react-dom/test-utils";
 import type { PartialRouteObject } from "react-router";
 
 describe("useRoutes", () => {
@@ -28,40 +26,14 @@ describe("useRoutes", () => {
 
     expect(renderer.toJSON()).toMatchSnapshot();
   });
-
-  it("Uses the `location` prop instead of context location`", () => {
-    let node = document.createElement("div");
-    document.body.appendChild(node);
-
-    let routes = [
-      { path: "one", element: <h1>one</h1> },
-      { path: "two", element: <h1>two</h1> }
-    ];
-
-    act(() => {
-      ReactDOM.render(
-        <Router initialEntries={["/one"]}>
-          <RoutesRenderer routes={routes} location={{ pathname: "/two" }} />
-        </Router>,
-        node
-      );
-    });
-
-    expect(node.innerHTML).toMatch(/two/);
-
-    // cleanup
-    document.body.removeChild(node);
-  });
 });
 
 function RoutesRenderer({
   routes,
-  basename,
-  location
+  basename
 }: {
   routes: PartialRouteObject[];
   basename?: string;
-  location?: Partial<Location>;
 }) {
-  return useRoutes(routes, basename, location);
+  return useRoutes(routes, basename);
 }
