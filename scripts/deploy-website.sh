3df481f8b5d37ef59fd5ecfe2b6f4505a24c1465
Chance Strickland <chance.strickland@gmail.com>
2021-08-14 11:38:37 -0700

    fix website deploy script

--- scripts/deploy-website.sh
+++ scripts/deploy-website.sh
@@ -15,7 +15,7 @@ git clone --depth 2 --branch main "git@github.com:remix-run/reactrouter.com.git"
 rm -rf "$tmp_dir/static/react-router"
 cd "$root_dir/website"
 npm install
-npm run build build --output-path "$tmp_dir/static/react-router" --output-public-path $PUBLIC_PATH
+npm run build --output-path "$tmp_dir/static/react-router" --output-public-path $PUBLIC_PATH
 
 # Commit all changes
 cd $tmp_dir
