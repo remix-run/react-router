remix-run/remix

useTransition, useActionData, useFetcher, shouldReload
This is kinda big but opens up a lot of use cases,

- can build better pending navigation UI with useTransition telling app more detailed information (state, type)
- replaces usePendingFormSubmit and usePendingLocation
- actually aborts stale submissions/loads
- fixes bugs around interrupted submissions/navigations
- actions can return data now
- super useful for form validation, no more screwing around with sessions
- allows apps to call loaders and actions outside of navigation
- manages cancellation of stale submissions and loads
- reloads route data after actions
- commits the freshest reloaded data along the way when there are multiple inflight

allows route modules to decide if they should reload or not

- after submissions
- when the search params change
- when the same href is navigated to

other stuff

- reloads route data when the same href is navigated to
- does not create ghost history entries on interrupted navigation

These old hooks still work, but have been deprecated for the new hooks.

- useRouteData -> useLoaderData
- usePendingFormSubmit -> useTransition().submission
- usePendingLocation -> useTransition().location

Also includes a helping of docs updates

Closes #169, #151, #175, #128, #54, #208

packages/remix-server-runtime/routeModules.ts
@@ -15,10 +15,9 @@ export interface RouteModules<RouteModule> {
  */
 export interface ActionFunction<TRequest = Request, TResponse = Response> {
   (args: { request: TRequest; context: AppLoadContext; params: Params }):
-    | Promise<TResponse | Response | string>
+    | Promise<TResponse | Response>
     | TResponse
-    | Response
-    | string;
+    | Response;
 }
 
 /**
@@ -34,9 +33,11 @@ export interface HeadersFunction<
   THeaders = Headers,
   THeadersInit = HeadersInit
 > {
-  (args: { loaderHeaders: THeaders; parentHeaders: THeaders }):
-    | THeaders
-    | THeadersInit;
+  (args: {
+    actionHeaders: THeaders;
+    loaderHeaders: THeaders;
+    parentHeaders: THeaders;
+  }): THeaders | THeadersInit;
 }
 
 /**

remix-run/react-router

packages/remix-server-runtime/routeModules.ts
@@ -15,9 +15,8 @@ export interface RouteModules<RouteModule> {
  */
 export interface ActionFunction {
   (args: { request: Request; context: AppLoadContext; params: Params }):
-    | Promise<Response | string>
-    | Response
-    | string;
+    | Promise<Response>
+    | Response;
 }
 
 /**
@@ -30,9 +29,11 @@ export type ErrorBoundaryComponent = ComponentType<{ error: Error }>;
  * will be merged with (and take precedence over) headers from parent routes.
  */
 export interface HeadersFunction {
-  (args: { loaderHeaders: Headers; parentHeaders: Headers }):
-    | Headers
-    | HeadersInit;
+  (args: {
+    loaderHeaders: Headers;
+    parentHeaders: Headers;
+    actionHeaders: Headers;
+  }): Headers | HeadersInit;
 }
 
 /**