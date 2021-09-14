f20f5fb831828eb6e87e52ee1af2bf25f522d31c
iamsergo <sergio820015@yandex.ru>
2021-09-14 18:15:23 +0300

    Fix typo in file name

--- packages/react-router/modules/HistoryContext.js
+++ packages/react-router/modules/HistoryContext.js
@@ -1,4 +1,4 @@
-import createNamedContext from "./createNameContext";
+import createNamedContext from "./createNamedContext";
 
 const historyContext = /*#__PURE__*/ createNamedContext("Router-History");
 export default historyContext;
