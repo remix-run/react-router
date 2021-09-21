97236cfc87122b80900ad8e8fe17c623bac30583
Logan McAnsh <logan@mcan.sh>
2021-09-21 17:38:51 -0400

    test: add support for --watchAll
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- packages/react-router-dom/jest.config.js
+++ packages/react-router-dom/jest.config.js
@@ -5,11 +5,5 @@ module.exports = {
   },
   globals: {
     __DEV__: true
-  },
-  moduleNameMapper: {
-    "^react-router$": "<rootDir>/../../build/react-router",
-    "^react-router-dom$": "<rootDir>/../../build/react-router-dom",
-    "^react-router-dom\\/server$":
-      "<rootDir>/../../build/react-router-dom/server"
   }
 };
