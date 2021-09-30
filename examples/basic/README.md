c8acb746c17170b419dab0c43318f2dd652b0a0f
Michael Jackson <michael@jackson.us>
2021-09-30 16:59:55 -0700

    Update basic example

--- examples/basic/README.md
+++ examples/basic/README.md
@@ -1,5 +1,13 @@
 # Basic Example
 
+This example demonstrates some of the basic features of React Router, including:
+
+- Layouts and nested `<Route>`s
+- Index `<Route>`s
+- Catch-all `<Route>`s
+- Using `<Outlet>` as a placeholder for child routes
+- Using `<Link>`s for navigation
+
 ## Preview
 
 Open this example on [StackBlitz](https://stackblitz.com):
--- examples/basic/src/App.tsx
+++ examples/basic/src/App.tsx
@@ -1,10 +1,44 @@
 import * as React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Outlet,
+  Link
+} from "react-router-dom";
 
-function App() {
+export default function App() {
   return (
     <Router>
       <div>
+        <h1>Welcome to the app!</h1>
+
+        {/* Routes nest inside one another. Nested route paths build upon
+            parent route paths, and nested route elements render inside
+            parent route elements. See the note about <Outlet> below. */}
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="about" element={<About />} />
+            <Route path="dashboard" element={<Dashboard />} />
+
+            {/* Using path="*"" means "match anything", so this route
+                acts like a catch-all for URLs that we don't have explicit
+                routes for. */}
+            <Route path="*" element={<NoMatch />} />
+          </Route>
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+function Layout() {
+  return (
+    <div>
+      {/* A "layout route" is a good place to put markup you want to
+          share across all the pages on your site, like navigation. */}
+      <nav>
         <ul>
           <li>
             <Link to="/">Home</Link>
@@ -15,23 +49,19 @@ function App() {
           <li>
             <Link to="/dashboard">Dashboard</Link>
           </li>
+          <li>
+            <Link to="/nothing-here">Nothing Here</Link>
+          </li>
         </ul>
+      </nav>
 
-        <hr />
+      <hr />
 
-        <Routes>
-          <Route path="/">
-            <Home />
-          </Route>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/dashboard">
-            <Dashboard />
-          </Route>
-        </Routes>
-      </div>
-    </Router>
+      {/* An <Outlet> renders whatever child route is currently active,
+          so you can think about this <Outlet> as a placeholder for
+          the child routes we defined above. */}
+      <Outlet />
+    </div>
   );
 }
 
@@ -59,4 +89,13 @@ function Dashboard() {
   );
 }
 
-export default App;
+function NoMatch() {
+  return (
+    <div>
+      <h2>Nothing to see here!</h2>
+      <p>
+        <Link to="/">Go to the home page</Link>
+      </p>
+    </div>
+  );
+}
