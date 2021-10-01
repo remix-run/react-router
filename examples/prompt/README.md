9852034c4fe7ca65dd7424d6a395c17fe649593a
Logan McAnsh <logan@mcan.sh>
2021-10-01 13:20:00 -0400

    add prompt example
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

---
+++ examples/prompt/README.md
@@ -0,0 +1,9 @@
+# Prompt Example
+
+Sometimes you want to prevent the user from navigating away from a page. The most common use case is when they have entered some data into a form but haven't submitted it yet, and you don't want them to lose it.
+
+## Preview
+
+Open this example on [StackBlitz](https://stackblitz.com):
+
+[![Open in StackBlitz](https://developer.stackblitz.com/img/open_in_stackblitz.svg)](https://stackblitz.com/github/remix-run/react-router/tree/dev/examples/prompt)
---
+++ examples/prompt/src/App.tsx
@@ -0,0 +1,123 @@
+import * as React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Outlet,
+  Link,
+  Prompt
+} from "react-router-dom";
+
+export default function App() {
+  return (
+    <Router>
+      <div>
+        <h1>Welcome to the app!</h1>
+
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="about" element={<About />} />
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="*" element={<NoMatch />} />
+          </Route>
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+function Layout() {
+  return (
+    <div>
+      {/* A "layout route" is a good place to put markup you want to
+          share across all the pages on your site, like navigation. */}
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/dashboard">Dashboard</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <hr />
+
+      <Outlet />
+    </div>
+  );
+}
+
+function Home() {
+  let [isBlocking, setIsBlocking] = React.useState(false);
+
+  return (
+    <div>
+      <h2>Home</h2>
+
+      <form
+        onSubmit={event => {
+          event.preventDefault();
+          event.currentTarget.reset();
+          setIsBlocking(false);
+        }}
+      >
+        <Prompt
+          when={isBlocking}
+          message={`Are you sure you want to go to leave?`}
+        />
+
+        <p>
+          Blocking?{" "}
+          {isBlocking ? "Yes, click a link or the back button" : "Nope"}
+        </p>
+
+        <p>
+          <input
+            size={50}
+            placeholder="type something to block transitions"
+            onChange={event => {
+              setIsBlocking(event.target.value.length > 0);
+            }}
+          />
+        </p>
+
+        <p>
+          <button>Submit to stop blocking</button>
+        </p>
+      </form>
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div>
+      <h2>About</h2>
+    </div>
+  );
+}
+
+function Dashboard() {
+  return (
+    <div>
+      <h2>Dashboard</h2>
+    </div>
+  );
+}
+
+function NoMatch() {
+  return (
+    <div>
+      <h2>Nothing to see here!</h2>
+      <p>
+        <Link to="/">Go to the home page</Link>
+      </p>
+    </div>
+  );
+}
