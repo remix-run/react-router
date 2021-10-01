d16cd5b84aaecc4d777022275e77d2fa0ac27ab3
Logan McAnsh <logan@mcan.sh>
2021-10-01 11:47:53 -0400

    chore: add @rollup/plugin-replace
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- examples/ssr/vite.config.js
+++ examples/ssr/vite.config.js
@@ -1,10 +1,20 @@
 import * as path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import rollupReplace from "@rollup/plugin-replace";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    rollupReplace({
+      preventAssignment: true,
+      values: {
+        __DEV__: JSON.stringify(true),
+        "process.env.NODE_ENV": JSON.stringify("development")
+      }
+    }),
+    react()
+  ],
   build: {
     minify: false
   },
