4995f622b9f1bf916b54b5c7780324c2b1fb40ae
Logan McAnsh <logan@mcan.sh>
2021-10-01 17:32:53 -0400

    Update App.tsx

--- examples/prompt/src/App.tsx
+++ examples/prompt/src/App.tsx
@@ -30,8 +30,6 @@ export default function App() {
 function Layout() {
   return (
     <div>
-      {/* A "layout route" is a good place to put markup you want to
-          share across all the pages on your site, like navigation. */}
       <nav>
         <ul>
           <li>
