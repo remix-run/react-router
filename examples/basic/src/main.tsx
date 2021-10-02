0b0a8dbe2b9ce9298981ebb31b86407e76a154b8
Logan McAnsh <logan@mcan.sh>
2021-10-02 14:37:45 -0400

    updates to basic example
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- examples/basic/src/main.tsx
+++ examples/basic/src/main.tsx
@@ -2,10 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
