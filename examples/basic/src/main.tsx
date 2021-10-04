fdd13fbfcb3a875c4cd27d74fe07404e3bad4201
Michael Jackson <michael@jackson.us>
2021-10-04 12:27:55 -0700

    Change import order

--- examples/basic/src/main.tsx
+++ examples/basic/src/main.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+
 import "./index.css";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
