f35048c415c9e37cbdd4bc2930dedae51baf69b6
Logan McAnsh <logan@mcan.sh>
2021-10-07 19:10:34 -0400

    add useRoutes example (#8100)
    * chore: add useRoutes example
    
    Signed-off-by: Logan McAnsh <logan@mcan.sh>
    
    * Update index.html
    
    * update example
    
    Signed-off-by: Logan McAnsh <logan@mcan.sh>
    
    * chore: remove router from app.tsx
    
    Signed-off-by: Logan McAnsh <logan@mcan.sh>
    
    * chore: revert basic example changes
    
    accidentally got slipped into this example, changes are re-added in #8102
    
    * normalize example with basic

---
+++ examples/useroutes/README.md
@@ -0,0 +1,9 @@
+# useRoutes Example
+
+This example demonstrates how to use the useRoutes hook to define routes.
+
+## Preview
+
+Open this example on [StackBlitz](https://stackblitz.com):
+
+[![Open in StackBlitz](https://developer.stackblitz.com/img/open_in_stackblitz.svg)](https://stackblitz.com/github/remix-run/react-router/tree/dev/examples/useroutes?file=src/App.tsx)
---
+++ examples/useroutes/src/App.tsx
@@ -0,0 +1,127 @@
+import * as React from "react";
+import type { RouteObject } from "react-router-dom";
+import { Outlet, Link, useRoutes, useParams } from "react-router-dom";
+
+let routes: RouteObject[] = [
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      { index: true, element: <Home /> },
+      {
+        path: "/courses",
+        element: <Courses />,
+        children: [
+          { index: true, element: <CoursesIndex /> },
+          { path: "/courses/:id", element: <Course /> }
+        ]
+      },
+      { path: "*", element: <NoMatch /> }
+    ]
+  }
+];
+
+export default function App() {
+  let element = useRoutes(routes);
+
+  return (
+    <div>
+      <h1>Welcome to the app!</h1>
+      {element}
+    </div>
+  );
+}
+
+function Layout() {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/courses">Courses</Link>
+          </li>
+          <li>
+            <Link to="/nothing-here">Nothing Here</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <hr />
+
+      <Outlet />
+    </div>
+  );
+}
+
+function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
+    </div>
+  );
+}
+
+function Courses() {
+  return (
+    <div>
+      <h2>Courses</h2>
+      <Outlet />
+    </div>
+  );
+}
+
+function CoursesIndex() {
+  return (
+    <div>
+      <p>Please choose a course:</p>
+
+      <nav>
+        <ul>
+          <li>
+            <Link to="react-fundamentals">React Fundamentals</Link>
+          </li>
+          <li>
+            <Link to="advanced-react">Advanced React</Link>
+          </li>
+          <li>
+            <Link to="react-router">React Router</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+
+function Course() {
+  let { id } = useParams<"id">();
+
+  return (
+    <div>
+      <h2>
+        Welcome to the {id!.split("-").map(capitalizeString).join(" ")} course!
+      </h2>
+
+      <p>This is a great course. You're gonna love it!</p>
+
+      <Link to="/courses">See all courses</Link>
+    </div>
+  );
+}
+
+function capitalizeString(s: string): string {
+  return s.charAt(0).toUpperCase() + s.slice(1);
+}
+
+function NoMatch() {
+  return (
+    <div>
+      <h2>It looks like you're lost...</h2>
+      <p>
+        <Link to="/">Go to the home page</Link>
+      </p>
+    </div>
+  );
+}
