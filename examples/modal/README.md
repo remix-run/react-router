ceedf687583d4a13937bfda21b5ef6eef20422fb
Logan McAnsh <logan@mcan.sh>
2021-10-07 18:24:24 -0400

    add modal example (#8107)
    * [WIP]: add modal example
    
    Signed-off-by: Logan McAnsh <logan@mcan.sh>
    
    * chore: skipLibCheck
    
    Signed-off-by: Logan McAnsh <logan@mcan.sh>
    
    * modal
    
    Signed-off-by: Logan McAnsh <logan@mcan.sh>
    
    * normalize example with basic
    
    Signed-off-by: Logan McAnsh <logan@mcan.sh>
    
    * rename image to pinnedLocation
    
    * Update package.json
    
    * chore: use @reach/dialog for modal
    
    Signed-off-by: Logan McAnsh <logan@mcan.sh>
    
    * chore: update comment printWidth, add alt text
    
    Signed-off-by: Logan McAnsh <logan@mcan.sh>
    
    * fix: styling
    
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

---
+++ examples/modal/README.md
@@ -0,0 +1,7 @@
+# Modal Example
+
+## Preview
+
+Open this example on [StackBlitz](https://stackblitz.com):
+
+[![Open in StackBlitz](https://developer.stackblitz.com/img/open_in_stackblitz.svg)](https://stackblitz.com/github/remix-run/react-router/tree/dev/examples/modal?file=src/App.tsx)
---
+++ examples/modal/src/App.tsx
@@ -0,0 +1,204 @@
+import * as React from "react";
+import {
+  Routes,
+  Route,
+  Outlet,
+  Link,
+  useLocation,
+  useNavigate,
+  useParams
+} from "react-router-dom";
+import { Dialog } from "@reach/dialog";
+import "@reach/dialog/styles.css";
+
+import { IMAGES, getImageById } from "./images";
+
+export default function App() {
+  let location = useLocation();
+
+  /*
+    This piece of state is set when one of the gallery links is clicked.
+    The `image` state is the location that we were at when one of the gallery
+    links was clicked. If it's there, use it as the location for the <Routes>
+    so we show the gallery in the background, behind the modal.
+  */
+  let state = location.state as { pinnedLocation?: Location };
+  let pinnedLocation = state?.pinnedLocation;
+
+  return (
+    <div>
+      <h1>Welcome to the app!</h1>
+      <Routes location={pinnedLocation || location}>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="gallery" element={<Gallery />} />
+          <Route path="/img/:id" element={<ImageView />} />
+          <Route path="*" element={<NoMatch />} />
+        </Route>
+      </Routes>
+
+      {/* Show the modal when a image page is set */}
+      {pinnedLocation && (
+        <Routes>
+          <Route path="/img/:id" element={<Modal />} />
+        </Routes>
+      )}
+    </div>
+  );
+}
+
+function Layout() {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/gallery">Gallery</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <hr />
+
+      <Outlet />
+    </div>
+  );
+}
+
+function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
+
+      <h3>Featured Images</h3>
+      <ul>
+        <li>
+          <Link to="/img/1">Image 1</Link>
+        </li>
+        <li>
+          <Link to="/img/2">Image 2</Link>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+function Gallery() {
+  let location = useLocation();
+
+  return (
+    <div style={{ padding: "0 24px" }}>
+      <h2>Gallery</h2>
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "repeat(auto-fit, minmax(200px, 1fr))",
+          gap: "24px"
+        }}
+      >
+        {IMAGES.map(image => (
+          <Link
+            key={image.id}
+            to={`/img/${image.id}`}
+            // This is the trick! This link sets
+            // the `background` in location state.
+            state={{ pinnedLocation: location }}
+          >
+            <img
+              width={200}
+              height={200}
+              style={{
+                width: "100%",
+                aspectRatio: "1 / 1",
+                height: "auto",
+                borderRadius: "8px"
+              }}
+              src={image.src}
+              alt={image.title}
+            />
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+function ImageView() {
+  let { id } = useParams<"id">();
+  let image = getImageById(Number(id));
+
+  if (!image) return <div>Image not found</div>;
+
+  return (
+    <div>
+      <h1>{image.title}</h1>
+      <img width={400} height={400} src={image.src} alt="" />
+    </div>
+  );
+}
+
+function Modal() {
+  let navigate = useNavigate();
+  let { id } = useParams<"id">();
+  let image = getImageById(Number(id));
+  let buttonRef = React.useRef<HTMLButtonElement>(null);
+
+  function onDismiss() {
+    navigate(-1);
+  }
+
+  if (!image) return null;
+
+  return (
+    <Dialog
+      aria-labelledby="label"
+      onDismiss={onDismiss}
+      initialFocusRef={buttonRef}
+    >
+      <div
+        style={{
+          display: "grid",
+          justifyContent: "center",
+          padding: "8px 8px"
+        }}
+      >
+        <h1 id="label" style={{ margin: 0 }}>
+          {image.title}
+        </h1>
+        <img
+          style={{
+            margin: "16px 0",
+            borderRadius: "8px",
+            width: "100%",
+            height: "auto"
+          }}
+          width={400}
+          height={400}
+          src={image.src}
+          alt=""
+        />
+        <button
+          style={{ display: "block" }}
+          ref={buttonRef}
+          onClick={onDismiss}
+        >
+          Close
+        </button>
+      </div>
+    </Dialog>
+  );
+}
+
+function NoMatch() {
+  return (
+    <div>
+      <h2>Nothing to see here!</h2>
+      <p>
+        <Link to="/">Go to the home page</Link>
+      </p>
+    </div>
+  );
+}
