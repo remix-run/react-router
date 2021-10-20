5a0e56646707c331e297d95420beaebc37ddc6c3
Logan McAnsh <logan@mcan.sh>
2021-10-20 17:36:29 -0400

    chore: remove extra types
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- examples/custom-query-parsing/src/@types/index.d.ts
+++
@@ -1,9 +0,0 @@
-export type Nullable<T> = T | null | undefined;
-
-export type AnyJson = boolean | number | string | null | JsonArray | JsonMap;
-
-export interface JsonMap {
-  [key: string]: AnyJson;
-}
-
-export interface JsonArray extends Array<AnyJson> {}
