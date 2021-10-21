15aa84c3351cac1f90411e92f252df80bf1f7a91
Jacob Ebey <jacob.ebey@live.com>
2021-10-21 14:47:58 -0700

    fix: updated route convention test (#343)
    also added tests for layout

--- packages/remix-dev/__tests__/routesConvention-test.ts
+++ packages/remix-dev/__tests__/routesConvention-test.ts
@@ -12,7 +12,12 @@ describe("createRoutePath", () => {
       ["flat.sub", "flat/sub"],
       ["nested/index", "nested"],
       ["flat.index", "flat"],
-      ["index", ""]
+      ["index", undefined],
+      ["__layout/index", undefined],
+      ["__layout/test", "test"],
+      ["__layout.test", "test"],
+      ["__layout/$slug", ":slug"],
+      ["nested/__layout/$slug", "nested/:slug"]
     ];
 
     for (let [input, expected] of tests) {
