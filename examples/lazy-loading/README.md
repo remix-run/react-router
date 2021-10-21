289e8c2367cb2cacab7c7088e7830eabf3a7181f
Logan McAnsh <logan@mcan.sh>
2021-10-20 20:14:41 -0400

    add lazy loading example (#8158)
    * add code splitting example
    
    Signed-off-by: Logan McAnsh <logan@mcan.sh>
    
    * chore: rename example, add link to messages page
    
    * chore: rename things to be lazy loading
    
    * chore: one last name change

---
+++ examples/lazy-loading/README.md
@@ -0,0 +1,9 @@
+# Code Splitting Example
+
+This example demonstrates how to do code splitting, not only of your application code, but of your routes.
+
+## Preview
+
+Open this example on [StackBlitz](https://stackblitz.com):
+
+[![Open in StackBlitz](https://developer.stackblitz.com/img/open_in_stackblitz.svg)](https://stackblitz.com/github/remix-run/react-router/tree/v6.0.0-beta.7/examples/lazy-loading?file=src/App.tsx)
---
+++ examples/lazy-loading/src/views/dashboard.tsx
@@ -0,0 +1,54 @@
+import * as React from "react";
+import { Routes, Route, Outlet, Link } from "react-router-dom";
+
+export default function Dashboard() {
+  return (
+    <Routes>
+      <Route path="/" element={<DashboardLayout />}>
+        <Route index element={<DashboardIndex />} />
+        <Route path="messages" element={<Messages />} />
+      </Route>
+    </Routes>
+  );
+}
+
+function DashboardLayout() {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/dashboard">Dashboard Home</Link>
+          </li>
+          <li>
+            <Link to="/dashboard/messages">Messages</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <hr />
+
+      <Outlet />
+    </div>
+  );
+}
+
+function DashboardIndex() {
+  return (
+    <div>
+      <h2>Dashboard Index</h2>
+    </div>
+  );
+}
+
+function Messages() {
+  return (
+    <div>
+      <h2>Messages</h2>
+      <ul>
+        <li>Message 1</li>
+        <li>Message 2</li>
+      </ul>
+    </div>
+  );
+}
