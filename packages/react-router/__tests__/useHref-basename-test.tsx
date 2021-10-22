7d45ae222fb1a7413619c5e8acee3bd6b917cd27
Jacob Ebey <jacob.ebey@live.com>
2021-10-21 18:37:14 -0700

    updated tests to use test renderer

--- packages/react-router/__tests__/useHref-basename-test.tsx
+++ packages/react-router/__tests__/useHref-basename-test.tsx
@@ -1,352 +1,365 @@
 import * as React from "react";
-import { act, create as createTestRenderer } from "react-test-renderer";
+import { create as createTestRenderer } from "react-test-renderer";
 import { MemoryRouter, Routes, Route, useHref } from "react-router";
 
 describe("useHref under a basename", () => {
   describe("to an absolute route", () => {
     it("returns the correct href a", () => {
-      let state;
-      let href = "";
       function Admin() {
-        state = React.useState({});
-        href = useHref("/invoices");
-        return <h1>Admin</h1>;
+        let href = useHref("/invoices");
+        return <p>{href}</p>;
       }
 
-      createTestRenderer(
+      let element = (
         <MemoryRouter basename="/app" initialEntries={["/app/admin"]}>
           <Routes>
             <Route path="admin" element={<Admin />} />
           </Routes>
         </MemoryRouter>
       );
+      let renderer = createTestRenderer(element);
+      renderer.update(element);
 
-      expect(href).toBe("/app/invoices");
-      act(() => state[1]({}));
-      expect(href).toBe("/app/invoices");
+      expect(renderer.toJSON()).toMatchInlineSnapshot(`
+        <p>
+          /app/invoices
+        </p>
+      `);
     });
   });
 
   describe("to a child route", () => {
     it("returns the correct href", () => {
-      let state;
-      let href = "";
       function Admin() {
-        state = React.useState({});
-        href = useHref("invoices");
-        return <h1>Admin</h1>;
+        let href = useHref("invoices");
+        return <p>{href}</p>;
       }
-
-      createTestRenderer(
+      let element = (
         <MemoryRouter basename="/app" initialEntries={["/app/admin"]}>
           <Routes>
             <Route path="admin" element={<Admin />} />
           </Routes>
         </MemoryRouter>
       );
+      let renderer = createTestRenderer(element);
+      renderer.update(element);
 
-      expect(href).toBe("/app/admin/invoices");
-      act(() => state[1]({}));
-      expect(href).toBe("/app/admin/invoices");
+      expect(renderer.toJSON()).toMatchInlineSnapshot(`
+        <p>
+          /app/admin/invoices
+        </p>
+      `);
     });
 
     describe("when the URL has a trailing slash", () => {
       it("returns the correct href", () => {
-        let state;
-        let href = "";
         function Admin() {
-          state = React.useState({});
-          href = useHref("invoices");
-          return <h1>Admin</h1>;
+          let href = useHref("invoices");
+          return <p>{href}</p>;
         }
 
-        createTestRenderer(
+        let element = (
           <MemoryRouter basename="/app" initialEntries={["/app/admin/"]}>
             <Routes>
               <Route path="admin" element={<Admin />} />
             </Routes>
           </MemoryRouter>
         );
+        let renderer = createTestRenderer(element);
+        renderer.update(element);
 
-        expect(href).toBe("/app/admin/invoices");
-        act(() => state[1]({}));
-        expect(href).toBe("/app/admin/invoices");
+        expect(renderer.toJSON()).toMatchInlineSnapshot(`
+          <p>
+            /app/admin/invoices
+          </p>
+        `);
       });
     });
 
     describe("when the href has a trailing slash", () => {
       it("returns the correct href", () => {
-        let state;
-        let href = "";
         function Admin() {
-          state = React.useState({});
-          href = useHref("invoices/");
-          return <h1>Admin</h1>;
+          let href = useHref("invoices/");
+          return <p>{href}</p>;
         }
 
-        createTestRenderer(
+        let element = (
           <MemoryRouter basename="/app" initialEntries={["/app/admin"]}>
             <Routes>
               <Route path="admin" element={<Admin />} />
             </Routes>
           </MemoryRouter>
         );
+        let renderer = createTestRenderer(element);
+        renderer.update(element);
 
-        expect(href).toBe("/app/admin/invoices/");
-        act(() => state[1]({}));
-        expect(href).toBe("/app/admin/invoices/");
+        expect(renderer.toJSON()).toMatchInlineSnapshot(`
+          <p>
+            /app/admin/invoices/
+          </p>
+        `);
       });
     });
   });
 
   describe("to a sibling route", () => {
     it("returns the correct href", () => {
-      let state;
-      let href = "";
       function Admin() {
-        state = React.useState({});
-        href = useHref("../dashboard");
-        return <h1>Admin</h1>;
+        let href = useHref("../dashboard");
+        return <p>{href}</p>;
       }
 
-      createTestRenderer(
+      let element = (
         <MemoryRouter basename="/app" initialEntries={["/app/admin"]}>
           <Routes>
             <Route path="admin" element={<Admin />} />
           </Routes>
         </MemoryRouter>
       );
+      let renderer = createTestRenderer(element);
+      renderer.update(element);
 
-      expect(href).toBe("/app/dashboard");
-      act(() => state[1]({}));
-      expect(href).toBe("/app/dashboard");
+      expect(renderer.toJSON()).toMatchInlineSnapshot(`
+        <p>
+          /app/dashboard
+        </p>
+      `);
     });
 
     describe("when the URL has a trailing slash", () => {
       it("returns the correct href", () => {
-        let state;
-        let href = "";
         function Admin() {
-          state = React.useState({});
-          href = useHref("../dashboard");
-          return <h1>Admin</h1>;
+          let href = useHref("../dashboard");
+          return <p>{href}</p>;
         }
 
-        createTestRenderer(
+        let element = (
           <MemoryRouter basename="/app" initialEntries={["/app/admin/"]}>
             <Routes>
               <Route path="admin" element={<Admin />} />
             </Routes>
           </MemoryRouter>
         );
+        let renderer = createTestRenderer(element);
+        renderer.update(element);
 
-        expect(href).toBe("/app/dashboard");
-        act(() => state[1]({}));
-        expect(href).toBe("/app/dashboard");
+        expect(renderer.toJSON()).toMatchInlineSnapshot(`
+          <p>
+            /app/dashboard
+          </p>
+        `);
       });
     });
 
     describe("when the href has a trailing slash", () => {
       it("returns the correct href", () => {
-        let state;
-        let href = "";
         function Admin() {
-          state = React.useState({});
-          href = useHref("../dashboard/");
-          return <h1>Admin</h1>;
+          let href = useHref("../dashboard/");
+          return <p>{href}</p>;
         }
 
-        createTestRenderer(
+        let element = (
           <MemoryRouter basename="/app" initialEntries={["/app/admin"]}>
             <Routes>
               <Route path="admin" element={<Admin />} />
             </Routes>
           </MemoryRouter>
         );
+        let renderer = createTestRenderer(element);
+        renderer.update(element);
 
-        expect(href).toBe("/app/dashboard/");
-        act(() => state[1]({}));
-        expect(href).toBe("/app/dashboard/");
+        expect(renderer.toJSON()).toMatchInlineSnapshot(`
+          <p>
+            /app/dashboard/
+          </p>
+        `);
       });
     });
   });
 
   describe("to a parent route", () => {
     it("returns the correct href", () => {
-      let state;
-      let href = "";
       function Admin() {
-        state = React.useState({});
-        href = useHref("..");
-        return <h1>Admin</h1>;
+        let href = useHref("..");
+        return <p>{href}</p>;
       }
 
-      createTestRenderer(
+      let element = (
         <MemoryRouter basename="/app" initialEntries={["/app/admin"]}>
           <Routes>
             <Route path="admin" element={<Admin />} />
           </Routes>
         </MemoryRouter>
       );
+      let renderer = createTestRenderer(element);
+      renderer.update(element);
 
-      expect(href).toBe("/app");
-      act(() => state[1]({}));
-      expect(href).toBe("/app");
+      expect(renderer.toJSON()).toMatchInlineSnapshot(`
+        <p>
+          /app
+        </p>
+      `);
     });
 
     describe("when the URL has a trailing slash", () => {
       it("returns the correct href", () => {
-        let state;
-        let href = "";
         function Admin() {
-          state = React.useState({});
-          href = useHref("..");
-          return <h1>Admin</h1>;
+          let href = useHref("..");
+          return <p>{href}</p>;
         }
 
-        createTestRenderer(
+        let element = (
           <MemoryRouter basename="/app" initialEntries={["/app/admin/"]}>
             <Routes>
               <Route path="admin" element={<Admin />} />
             </Routes>
           </MemoryRouter>
         );
+        let renderer = createTestRenderer(element);
+        renderer.update(element);
 
-        expect(href).toBe("/app");
-        act(() => state[1]({}));
-        expect(href).toBe("/app");
+        expect(renderer.toJSON()).toMatchInlineSnapshot(`
+          <p>
+            /app
+          </p>
+        `);
       });
     });
 
     describe("when the href has a trailing slash", () => {
       it("returns the correct href", () => {
-        let state;
-        let href = "";
         function Admin() {
-          state = React.useState({});
-          href = useHref("../");
-          return <h1>Admin</h1>;
+          let href = useHref("../");
+          return <p>{href}</p>;
         }
 
-        createTestRenderer(
+        let element = (
           <MemoryRouter basename="/app" initialEntries={["/app/admin"]}>
             <Routes>
               <Route path="admin" element={<Admin />} />
             </Routes>
           </MemoryRouter>
         );
+        let renderer = createTestRenderer(element);
+        renderer.update(element);
 
-        expect(href).toBe("/app/");
-        act(() => state[1]({}));
-        expect(href).toBe("/app/");
+        expect(renderer.toJSON()).toMatchInlineSnapshot(`
+          <p>
+            /app/
+          </p>
+        `);
       });
     });
   });
 
   describe("with a to value that has more .. segments than the current URL", () => {
     it("returns the correct href", () => {
-      let state;
-      let href = "";
       function Admin() {
-        state = React.useState({});
-        href = useHref("../../../dashboard");
-        return <h1>Admin</h1>;
+        let href = useHref("../../../dashboard");
+        return <p>{href}</p>;
       }
 
-      createTestRenderer(
+      let element = (
         <MemoryRouter basename="/app" initialEntries={["/app/admin"]}>
           <Routes>
             <Route path="admin" element={<Admin />} />
           </Routes>
         </MemoryRouter>
       );
+      let renderer = createTestRenderer(element);
+      renderer.update(element);
 
       // This is correct because the basename works like a chroot "jail".
       // Relative <Link to> values cannot "escape" into a higher level URL since
       // they would be linking to a URL that the <Router> cannot render. To link
       // to a higher URL path, use a plain <a>.
-      expect(href).toBe("/app/dashboard");
-      act(() => state[1]({}));
-      expect(href).toBe("/app/dashboard");
+      expect(renderer.toJSON()).toMatchInlineSnapshot(`
+        <p>
+          /app/dashboard
+        </p>
+      `);
     });
 
     describe("and no additional segments", () => {
       it("returns the correct href", () => {
-        let state;
-        let href = "";
         function Admin() {
-          state = React.useState({});
-          href = useHref("../../..");
-          return <h1>Admin</h1>;
+          let href = useHref("../../..");
+          return <p>{href}</p>;
         }
 
-        createTestRenderer(
+        let element = (
           <MemoryRouter basename="/app" initialEntries={["/app/admin"]}>
             <Routes>
               <Route path="admin" element={<Admin />} />
             </Routes>
           </MemoryRouter>
         );
+        let renderer = createTestRenderer(element);
+        renderer.update(element);
 
         // This is correct because the basename works like a chroot "jail".
         // Relative <Link to> values cannot "escape" into a higher level URL
         // since they would be linking to a URL that the <Router> cannot render.
         // To link to a higher URL path, use a plain <a>.
-        expect(href).toBe("/app");
-        act(() => state[1]({}));
-        expect(href).toBe("/app");
+        expect(renderer.toJSON()).toMatchInlineSnapshot(`
+          <p>
+            /app
+          </p>
+        `);
       });
     });
 
     describe("when the URL has a trailing slash", () => {
       it("returns the correct href", () => {
-        let state;
-        let href = "";
         function Admin() {
-          state = React.useState({});
-          href = useHref("../../../dashboard");
-          return <h1>Admin</h1>;
+          let href = useHref("../../../dashboard");
+          return <p>{href}</p>;
         }
 
-        createTestRenderer(
+        let element = (
           <MemoryRouter basename="/app" initialEntries={["/app/admin/"]}>
             <Routes>
               <Route path="admin" element={<Admin />} />
             </Routes>
           </MemoryRouter>
         );
+        let renderer = createTestRenderer(element);
+        renderer.update(element);
 
         // This is correct because the basename works like a chroot "jail".
         // Relative <Link to> values cannot "escape" into a higher level URL
         // since they would be linking to a URL that the <Router> cannot render.
         // To link to a higher URL path, use a plain <a>.
-        expect(href).toBe("/app/dashboard");
-        act(() => state[1]({}));
-        expect(href).toBe("/app/dashboard");
+        expect(renderer.toJSON()).toMatchInlineSnapshot(`
+          <p>
+            /app/dashboard
+          </p>
+        `);
       });
     });
 
     describe("when the href has a trailing slash", () => {
       it("returns the correct href", () => {
-        let state;
-        let href = "";
         function Admin() {
-          state = React.useState({});
-          href = useHref("../../../dashboard/");
-          return <h1>Admin</h1>;
+          let href = useHref("../../../dashboard/");
+          return <p>{href}</p>;
         }
 
-        createTestRenderer(
+        let element = (
           <MemoryRouter basename="/app" initialEntries={["/app/admin"]}>
             <Routes>
               <Route path="admin" element={<Admin />} />
             </Routes>
           </MemoryRouter>
         );
+        let renderer = createTestRenderer(element);
+        renderer.update(element);
 
-        expect(href).toBe("/app/dashboard/");
-        act(() => state[1]({}));
-        expect(href).toBe("/app/dashboard/");
+        expect(renderer.toJSON()).toMatchInlineSnapshot(`
+          <p>
+            /app/dashboard/
+          </p>
+        `);
       });
     });
   });
