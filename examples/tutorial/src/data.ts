4da0ae67ccd4daf5194e307672756685650ab4dd
Chance Strickland <hi@chance.dev>
2021-11-01 07:47:36 -0700

    Add tutorial files

---
+++ examples/tutorial/src/data.ts
@@ -0,0 +1,44 @@
+let invoices = [
+  {
+    name: "Santa Monica",
+    number: 1995,
+    amount: "$10,800",
+    due: "12/05/1995"
+  },
+  {
+    name: "Stankonia",
+    number: 2000,
+    amount: "$8,000",
+    due: "10/31/2000"
+  },
+  {
+    name: "Ocean Avenue",
+    number: 2003,
+    amount: "$9,500",
+    due: "07/22/2003"
+  },
+  {
+    name: "Tubthumper",
+    number: 1997,
+    amount: "$14,000",
+    due: "09/01/1997"
+  },
+  {
+    name: "Wide Open Spaces",
+    number: 1998,
+    amount: "$4,600",
+    due: "01/27/2998"
+  }
+];
+
+export function getInvoices() {
+  return invoices;
+}
+
+export function getInvoice(number: number) {
+  return invoices.find(invoice => invoice.number === number);
+}
+
+export function deleteInvoice(number: number) {
+  invoices = invoices.filter(invoice => invoice.number !== number);
+}
---
+++ examples/tutorial/src/main.tsx
@@ -0,0 +1,6 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import App from "./App";
+
+let rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
