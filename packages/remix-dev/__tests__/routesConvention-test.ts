455668a3267664fc2f47d139bcdbe6df8a2c6e29
Jacob Ebey <jacob.ebey@live.com>
2021-11-17 23:21:27 -0800

    feat: add escape convention `[]` for routes (#392)
    Co-authored-by: Kent C. Dodds <me+github@kentcdodds.com>

--- packages/remix-dev/__tests__/routesConvention-test.ts
+++ packages/remix-dev/__tests__/routesConvention-test.ts
@@ -3,6 +3,12 @@ import { createRoutePath } from "../config/routesConvention";
 describe("createRoutePath", () => {
   describe("creates proper route paths", () => {
     let tests = [
+      ["routes/$", "routes/*"],
+      ["routes/sub/$", "routes/sub/*"],
+      ["routes.sub/$", "routes/sub/*"],
+      ["routes/$slug", "routes/:slug"],
+      ["routes/sub/$slug", "routes/sub/:slug"],
+      ["routes.sub/$slug", "routes/sub/:slug"],
       ["$", "*"],
       ["nested/$", "nested/*"],
       ["flat.$", "flat/*"],
@@ -17,7 +23,19 @@ describe("createRoutePath", () => {
       ["__layout/test", "test"],
       ["__layout.test", "test"],
       ["__layout/$slug", ":slug"],
-      ["nested/__layout/$slug", "nested/:slug"]
+      ["nested/__layout/$slug", "nested/:slug"],
+      ["$slug[.]json", ":slug.json"],
+      ["sub/[sitemap.xml]", "sub/sitemap.xml"],
+      ["posts/$slug/[image.jpg]", "posts/:slug/image.jpg"],
+      ["$[$dollabills].[.]lol[/]what/[$].$", ":$dollabills/.lol/what/$/*"],
+      ["sub.[[]", "sub/["],
+      ["sub.]", "sub/]"],
+      ["sub.[[]]", "sub/[]"],
+      ["sub.[[]", "sub/["],
+      ["beef]", "beef]"],
+      ["[index]", "index"],
+      ["test/inde[x]", "test/index"],
+      ["[i]ndex/[[].[[]]", "index/[/[]"]
     ];
 
     for (let [input, expected] of tests) {
--- packages/remix-dev/config/routesConvention.ts
+++ packages/remix-dev/config/routesConvention.ts
@@ -98,28 +98,88 @@ export function defineConventionalRoutes(appDir: string): RouteManifest {
   return defineRoutes(defineNestedRoutes);
 }
 
+let escapeStart = "[";
+let escapeEnd = "]";
+
 // TODO: Cleanup and write some tests for this function
 export function createRoutePath(partialRouteId: string): string | undefined {
-  let path = normalizeSlashes(partialRouteId)
-    // routes/$ -> routes/*
-    // routes/nested/$.tsx (with a "routes/nested.tsx" layout)
-    .replace(/^\$$/, "*")
-    // routes/docs.$ -> routes/docs/*
-    // routes/docs/$ -> routes/docs/*
-    .replace(/(\/|\.)\$$/, "/*")
-    // routes/$user -> routes/:user
-    .replace(/\$/g, ":")
-    // routes/not.nested -> routes/not/nested
-    .replace(/\./g, "/");
-  path = /\b\/?index$/.test(path) ? path.replace(/\/?index$/, "") : path;
+  let result = "";
+  let rawSegmentBuffer = "";
 
-  // remove "__" layout segments
-  path = path
-    .split("/")
-    .filter(segment => !segment.startsWith("__"))
-    .join("/");
+  let inEscapeSequence = 0;
+  let skipSegment = false;
+  for (let i = 0; i < partialRouteId.length; i++) {
+    let char = partialRouteId.charAt(i);
+    let lastChar = i > 0 ? partialRouteId.charAt(i - 1) : undefined;
+    let nextChar =
+      i < partialRouteId.length - 1 ? partialRouteId.charAt(i + 1) : undefined;
 
-  return path ? path : undefined;
+    function isNewEscapeSequence() {
+      return (
+        !inEscapeSequence && char === escapeStart && lastChar !== escapeStart
+      );
+    }
+
+    function isCloseEscapeSequence() {
+      return inEscapeSequence && char === escapeEnd && nextChar !== escapeEnd;
+    }
+
+    function isStartOfLayoutSegment() {
+      return char === "_" && nextChar === "_" && !rawSegmentBuffer;
+    }
+
+    if (skipSegment) {
+      if (char === "/" || char === "." || char === path.win32.sep) {
+        skipSegment = false;
+      }
+      continue;
+    }
+
+    if (isNewEscapeSequence()) {
+      inEscapeSequence++;
+      continue;
+    }
+
+    if (isCloseEscapeSequence()) {
+      inEscapeSequence--;
+      continue;
+    }
+
+    if (inEscapeSequence) {
+      result += char;
+      continue;
+    }
+
+    if (char === "/" || char === path.win32.sep || char === ".") {
+      if (rawSegmentBuffer === "index" && result.endsWith("index")) {
+        result = result.replace(/\/?index$/, "");
+      } else {
+        result += "/";
+      }
+      rawSegmentBuffer = "";
+      continue;
+    }
+
+    if (isStartOfLayoutSegment()) {
+      skipSegment = true;
+      continue;
+    }
+
+    rawSegmentBuffer += char;
+
+    if (char === "$") {
+      result += typeof nextChar === "undefined" ? "*" : ":";
+      continue;
+    }
+
+    result += char;
+  }
+
+  if (rawSegmentBuffer === "index" && result.endsWith("index")) {
+    result = result.replace(/\/?index$/, "");
+  }
+
+  return result || undefined;
 }
 
 function findParentRouteId(
