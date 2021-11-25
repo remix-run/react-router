38c52e139b4f87d04395e4a6c6a2fa2061a06b43
Luke Morales <lukemorales@live.com>
2021-11-25 20:46:37 -0300

    test: create tests for useNavigate to assert hash behavior

--- packages/react-router/__tests__/useNavigate-test.tsx
+++ packages/react-router/__tests__/useNavigate-test.tsx
@@ -97,4 +97,195 @@ describe("useNavigate", () => {
       `);
     });
   });
+
+  describe("with hash", () => {
+    function Home() {
+      let navigate = useNavigate();
+
+      function handleClick() {
+        navigate("/about");
+      }
+
+      return (
+        <div>
+          <h1>Home</h1>
+          <p>location.hash: {useLocation().hash}</p>
+          <button onClick={handleClick}>click me</button>
+        </div>
+      );
+    }
+
+    function ShowLocationHash() {
+      return <p>location.hash: {useLocation().hash}</p>;
+    }
+
+    let renderer: TestRenderer.ReactTestRenderer;
+
+    it("transitions to new location without the hash", () => {
+      TestRenderer.act(() => {
+        renderer = TestRenderer.create(
+          <MemoryRouter initialEntries={["/home#test"]}>
+            <Routes>
+              <Route path="home" element={<Home />} />
+              <Route path="about" element={<ShowLocationHash />} />
+            </Routes>
+          </MemoryRouter>
+        );
+      });
+
+      let button = renderer.root.findByType("button");
+
+      TestRenderer.act(() => {
+        button.props.onClick();
+      });
+
+      expect(renderer.toJSON()).toMatchInlineSnapshot(`
+        <p>
+          location.hash: 
+          
+        </p>
+      `);
+    });
+
+    describe("updates the current location search and preserves the hash", () => {
+      function StringSearch({ object = false }) {
+        let navigate = useNavigate();
+        let location = useLocation();
+
+        function handleClick() {
+          const search = "?query=search";
+          navigate(object ? { search } : search);
+        }
+
+        return (
+          <div>
+            <h1>Home</h1>
+            <p>location.search: {location.search}</p>
+            <p>location.hash: {location.hash}</p>
+            <button onClick={handleClick}>click me</button>
+          </div>
+        );
+      }
+
+      it("behaves correctly when path is a string", () => {
+        TestRenderer.act(() => {
+          renderer = TestRenderer.create(
+            <MemoryRouter initialEntries={["/home#test"]}>
+              <Routes>
+                <Route path="home" element={<StringSearch />} />
+              </Routes>
+            </MemoryRouter>
+          );
+        });
+
+        expect(renderer.toJSON()).toMatchInlineSnapshot(`
+          <div>
+            <h1>
+              Home
+            </h1>
+            <p>
+              location.search: 
+              
+            </p>
+            <p>
+              location.hash: 
+              #test
+            </p>
+            <button
+              onClick={[Function]}
+            >
+              click me
+            </button>
+          </div>
+        `);
+
+        let button = renderer.root.findByType("button");
+
+        TestRenderer.act(() => {
+          button.props.onClick();
+        });
+
+        expect(renderer.toJSON()).toMatchInlineSnapshot(`
+          <div>
+            <h1>
+              Home
+            </h1>
+            <p>
+              location.search: 
+              ?query=search
+            </p>
+            <p>
+              location.hash: 
+              #test
+            </p>
+            <button
+              onClick={[Function]}
+            >
+              click me
+            </button>
+          </div>
+        `);
+      });
+
+      it("behaves correctly when path is an object", () => {
+        TestRenderer.act(() => {
+          renderer = TestRenderer.create(
+            <MemoryRouter initialEntries={["/home#test"]}>
+              <Routes>
+                <Route path="home" element={<StringSearch object={true} />} />
+              </Routes>
+            </MemoryRouter>
+          );
+        });
+
+        expect(renderer.toJSON()).toMatchInlineSnapshot(`
+          <div>
+            <h1>
+              Home
+            </h1>
+            <p>
+              location.search: 
+              
+            </p>
+            <p>
+              location.hash: 
+              #test
+            </p>
+            <button
+              onClick={[Function]}
+            >
+              click me
+            </button>
+          </div>
+        `);
+
+        let button = renderer.root.findByType("button");
+
+        TestRenderer.act(() => {
+          button.props.onClick();
+        });
+
+        expect(renderer.toJSON()).toMatchInlineSnapshot(`
+          <div>
+            <h1>
+              Home
+            </h1>
+            <p>
+              location.search: 
+              ?query=search
+            </p>
+            <p>
+              location.hash: 
+              #test
+            </p>
+            <button
+              onClick={[Function]}
+            >
+              click me
+            </button>
+          </div>
+        `);
+      });
+    });
+  });
 });
