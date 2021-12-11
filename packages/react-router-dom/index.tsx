8e3fdb5263e5341b0912824582557d4febbc2e2c
Chance Strickland <hi@chance.dev>
2021-12-11 09:10:34 -0800

    chore: comments

--- packages/react-router-dom/index.tsx
+++ packages/react-router-dom/index.tsx
@@ -204,8 +204,9 @@ export interface HistoryRouterProps {
 
 /**
  * A `<Router>` that accepts a pre-instantiated history object. It's important
- * to note that using your own history object is highly discouraged and can
- * potentially cause issues in your dependency tree.
+ * to note that using your own history object is highly discouraged and may add
+ * two versions of the history library to your bundles unless you use the same
+ * version of the history library that React Router uses internally.
  */
 function HistoryRouter({ basename, children, history }: HistoryRouterProps) {
   const [state, setState] = React.useState({
