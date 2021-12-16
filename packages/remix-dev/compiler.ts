b6e49ecce0753f0e17f340e8d12abdf8d3c7d683
Logan McAnsh <logan@remix.run>
2021-12-16 16:00:34 -0500

    fix(dev/compiler): use inline sourcemaps for server build (#1087)
    Signed-off-by: Logan McAnsh <logan@mcan.sh>
    (cherry picked from commit 1d3f18402a6fd89be883dc7eb7292feaf07e9582)
    (cherry picked from commit d0b5edab5a73a8c49bbcbc9634676a5adc092ae2)

--- packages/remix-dev/compiler.ts
+++ packages/remix-dev/compiler.ts
@@ -352,11 +352,11 @@ async function createServerBuild(
     bundle: true,
     logLevel: "silent",
     incremental: options.incremental,
-    sourcemap: true,
+    sourcemap: options.sourcemap ? "inline" : false,
     // The server build needs to know how to generate asset URLs for imports
     // of CSS and other files.
     assetNames: "_assets/[name]-[hash]",
-    publicPath: "./",
+    publicPath: config.publicPath,
     define: {
       "process.env.NODE_ENV": JSON.stringify(options.mode)
     },
