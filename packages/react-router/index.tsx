6e6aeb7a74b2b753ab314d523340998d0ddc8e17
Alex Mitchell <shamsup@users.noreply.github.com>
2022-01-06 09:24:30 -0800

    expand url-encoded entity matching to include any two hex characters

--- packages/react-router/index.tsx
+++ packages/react-router/index.tsx
@@ -1225,7 +1225,7 @@ function compilePath(
         // Additionally, allow paths starting with `.`, `-`, `~`, and url-encoded entities,
         // but do not consume the character in the matched path so they can match against
         // nested paths.
-        "(?:(?=[.~-]|%[0-7][0-9A-F])|(?:\\b|\\/|$))";
+        "(?:(?=[.~-]|%[0-9A-F]{2})|(?:\\b|\\/|$))";
   }
 
   let matcher = new RegExp(regexpSource, caseSensitive ? undefined : "i");
