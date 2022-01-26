6826037bf26aeedd0ebe00ea77f81082625720c7
Jacob Ebey <jacob.ebey@live.com>
2022-01-26 09:50:02 -0800

    chore: add useful error message if virtual module is used directly (#1656)

--- packages/remix-dev/server-build.ts
+++ packages/remix-dev/server-build.ts
@@ -1,5 +1,11 @@
 import type { ServerBuild } from "@remix-run/server-runtime";
 
+throw new Error(
+  "@remix-run/dev/server-build is not meant to be used directly from node_modules." +
+    " It is exists to provide type definitions for a virtual module provided" +
+    " the Remix compiler at build time."
+);
+
 export const assets: ServerBuild["assets"] = undefined!;
 export const entry: ServerBuild["entry"] = undefined!;
 export const routes: ServerBuild["routes"] = undefined!;
