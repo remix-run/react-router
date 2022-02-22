cb6c8530c540d81a9a43592a4bc6b83e1318bf96
Chance Strickland <hi@chance.dev>
2022-02-22 09:42:10 -0800

    fix: allow `process.env.NODE_ENV === "test"` in build (#2059)

--- packages/remix-dev/build.ts
+++ packages/remix-dev/build.ts
@@ -1,10 +1,15 @@
 export enum BuildMode {
   Development = "development",
-  Production = "production"
+  Production = "production",
+  Test = "test"
 }
 
 export function isBuildMode(mode: any): mode is BuildMode {
-  return mode === BuildMode.Development || mode === BuildMode.Production;
+  return (
+    mode === BuildMode.Development ||
+    mode === BuildMode.Production ||
+    mode === BuildMode.Test
+  );
 }
 
 export enum BuildTarget {
