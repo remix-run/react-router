747e6a020fe4cd0f3e46291500b552f08b8b3f1e
Pedro Cattori <pcattori@gmail.com>
2022-03-14 20:32:59 -0500

    test(@remix-run/dev): test tarball uses tsx, not jsx

--- packages/remix-dev/__tests__/cli-test.ts
+++ packages/remix-dev/__tests__/cli-test.ts
@@ -205,7 +205,7 @@ describe("remix cli", () => {
         `💿 That's it! \`cd\` into "${projectDir}" and check the README for development and deploy instructions!`
       );
       expect(fs.existsSync(path.join(projectDir, "package.json"))).toBeTruthy();
-      expect(fs.existsSync(path.join(projectDir, "app/root.jsx"))).toBeTruthy();
+      expect(fs.existsSync(path.join(projectDir, "app/root.tsx"))).toBeTruthy();
     });
 
     it("works for a path to a tarball on disk", async () => {
@@ -222,7 +222,7 @@ describe("remix cli", () => {
         `💿 That's it! \`cd\` into "${projectDir}" and check the README for development and deploy instructions!`
       );
       expect(fs.existsSync(path.join(projectDir, "package.json"))).toBeTruthy();
-      expect(fs.existsSync(path.join(projectDir, "app/root.jsx"))).toBeTruthy();
+      expect(fs.existsSync(path.join(projectDir, "app/root.tsx"))).toBeTruthy();
     });
 
     it("works for a file URL to a tarball on disk", async () => {
@@ -241,7 +241,7 @@ describe("remix cli", () => {
         `💿 That's it! \`cd\` into "${projectDir}" and check the README for development and deploy instructions!`
       );
       expect(fs.existsSync(path.join(projectDir, "package.json"))).toBeTruthy();
-      expect(fs.existsSync(path.join(projectDir, "app/root.jsx"))).toBeTruthy();
+      expect(fs.existsSync(path.join(projectDir, "app/root.tsx"))).toBeTruthy();
     });
 
     it("works for a file path to a directory on disk", async () => {
