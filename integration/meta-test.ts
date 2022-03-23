b83c6654e8f8332005624bfab24fe8f981030e65
Michaël De Boey <info@michaeldeboey.be>
2022-03-23 03:59:42 +0100

    feat(remix-react): add support for `http-equiv` in `meta` function (#2331)

--- integration/meta-test.ts
+++ integration/meta-test.ts
@@ -10,20 +10,24 @@ describe("meta", () => {
       files: {
         "app/root.jsx": js`
           import { json, Meta, Links, Outlet, Scripts } from "remix";
-          
+
           export const loader = async () =>
             json({
               description: "This is a meta page",
               title: "Meta Page",
             });
-          
+
           export const meta = ({ data }) => ({
+            charset: "utf-8",
             description: data.description,
             "og:image": "https://picsum.photos/200/200",
             "og:type": data.contentType, // undefined
+            refresh: {
+              httpEquiv: "refresh",
+              content: "3;url=https://www.mozilla.org",
+            },
             title: data.title,
           });
-
 
           export default function Root() {
             return (
@@ -52,8 +56,26 @@ describe("meta", () => {
     app = await createAppFixture(fixture);
   });
 
-  afterAll(async () => {
-    await app.close();
+  afterAll(async () => await app.close());
+
+  test("empty meta does not render a tag", async () => {
+    let enableJavaScript = await app.disableJavaScript();
+
+    await app.goto("/");
+
+    await expect(app.getHtml('meta[property="og:type"]')).rejects.toThrowError(
+      'No element matches selector "meta[property="og:type"]"'
+    );
+    await enableJavaScript();
+  });
+
+  test("meta { charset } adds a <meta charset='utf-8' />", async () => {
+    let enableJavaScript = await app.disableJavaScript();
+
+    await app.goto("/");
+
+    expect(await app.getHtml('meta[charset="utf-8"]')).toBeTruthy();
+    await enableJavaScript();
   });
 
   test("meta { title } adds a <title />", async () => {
@@ -61,20 +83,7 @@ describe("meta", () => {
 
     await app.goto("/");
 
-    expect(await app.getHtml("title")).toEqual(
-      expect.stringMatching(/<title>/s)
-    );
-    await enableJavaScript();
-  });
-
-  test("meta { description } adds a <meta name='description' />", async () => {
-    let enableJavaScript = await app.disableJavaScript();
-
-    await app.goto("/");
-
-    expect(await app.getHtml("meta")).toEqual(
-      expect.stringMatching(/<meta\s+name="description"/s)
-    );
+    expect(await app.getHtml("title")).toBeTruthy();
     await enableJavaScript();
   });
 
@@ -83,20 +92,29 @@ describe("meta", () => {
 
     await app.goto("/");
 
-    expect(await app.getHtml("meta")).toEqual(
-      expect.stringMatching(/<meta\s+property="og:*/s)
-    );
+    expect(await app.getHtml('meta[property="og:image"]')).toBeTruthy();
     await enableJavaScript();
   });
 
-  test("empty meta does not render a tag", async () => {
+  test("meta { description } adds a <meta name='description' />", async () => {
     let enableJavaScript = await app.disableJavaScript();
 
     await app.goto("/");
 
-    expect(await app.getHtml("meta")).not.toEqual(
-      expect.stringMatching(/<meta\s+property="og:type/s)
-    );
+    expect(await app.getHtml('meta[name="description"]')).toBeTruthy();
+    await enableJavaScript();
+  });
+
+  test("meta { refresh } adds a <meta http-equiv='refresh' content='3;url=https://www.mozilla.org' />", async () => {
+    let enableJavaScript = await app.disableJavaScript();
+
+    await app.goto("/");
+
+    expect(
+      await app.getHtml(
+        'meta[http-equiv="refresh"][content="3;url=https://www.mozilla.org"]'
+      )
+    ).toBeTruthy();
     await enableJavaScript();
   });
 });
--- packages/remix-server-runtime/routeModules.ts
+++ packages/remix-server-runtime/routeModules.ts
@@ -92,7 +92,16 @@ export interface MetaFunction {
  * `name` attribute.
  */
 export interface HtmlMetaDescriptor {
-  [name: string]: string | string[] | null | undefined;
+  charset?: "utf-8";
+  charSet?: "utf-8";
+  title?: string;
+  [name: string]:
+    | null
+    | string
+    | string[]
+    | undefined
+    | Record<string, string>
+    | Record<string, string>[];
 }
 
 export type MetaDescriptor = HtmlMetaDescriptor;
