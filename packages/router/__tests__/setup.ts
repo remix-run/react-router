f312cbf390c3b9d77b1ec88dd34e95a29e6575b9
Matt Brophy <matt@brophy.org>
2022-04-15 10:36:17 -0400

    fix request patch

--- packages/router/__tests__/setup.ts
+++ packages/router/__tests__/setup.ts
@@ -7,4 +7,21 @@ if (!globalThis.fetch) {
   // @ts-expect-error
   globalThis.Response = Response;
   globalThis.FormData = FormData;
+
+  // Temporary patch util @web-std/fetch properly supports this
+  // See: https://github.com/web-std/io/pull/60
+  let oldRequestFormData = globalThis.Request.prototype.formData;
+  globalThis.Request.prototype.formData = async function formDataShim() {
+    let contentType = this.headers?.get("Content-Type") || "";
+    if (
+      contentType.startsWith("application/x-www-form-urlencoded") &&
+      this.body != null
+    ) {
+      const form = new FormData();
+      let bodyText = await this.text();
+      new URLSearchParams(bodyText).forEach((v, k) => form.append(k, v));
+      return form;
+    }
+    return oldRequestFormData.call(this);
+  };
 }
