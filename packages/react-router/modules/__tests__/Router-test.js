a68510f4ae12f48c859d4dccedadfd064e45a240
John Gozde <john@gozde.ca>
2022-05-09 10:10:55 -0600

    Fix: make v5 Router compatible with v18 StrictMode (#8831)

--- packages/react-router/modules/__tests__/Router-test.js
+++ packages/react-router/modules/__tests__/Router-test.js
@@ -46,4 +46,66 @@ describe("A <Router>", () => {
       }).not.toThrow();
     });
   });
+
+  describe("with react v18 StrictMode semantics", () => {
+    function simulateV18StrictModeRender(props, afterRender) {
+      // Constructor gets called twice
+      let router = new Router(props);
+      router = new Router(props);
+
+      // Swap out setState since we're not actually mounting the component
+      router.setState = jest.fn();
+
+      // Render is called twice
+      router.render();
+      router.render();
+
+      if (afterRender) afterRender();
+
+      router.componentDidMount();
+
+      // Effects are invoked twice
+      router.componentWillUnmount();
+      router.componentDidMount();
+
+      return router;
+    }
+
+    it("responds to history changes", () => {
+      const history = createHistory();
+      const props = { history, children: <p>Foo</p> };
+
+      const router = simulateV18StrictModeRender(props);
+
+      history.push("/foo");
+
+      expect(router.setState).toHaveBeenCalledTimes(1);
+      expect(router.setState).toHaveBeenCalledWith({
+        location: expect.objectContaining({ pathname: "/foo" })
+      });
+    });
+
+    it("can handle path changes on child mount", () => {
+      const history = createHistory();
+      const props = { history, children: <p>Foo</p> };
+
+      const router = simulateV18StrictModeRender(props, () => {
+        history.push("/baz");
+        history.push("/qux");
+        history.push("/foo");
+      });
+
+      expect(router.setState).toHaveBeenCalledTimes(1);
+      expect(router.setState).toHaveBeenCalledWith({
+        location: expect.objectContaining({ pathname: "/foo" })
+      });
+
+      history.push("/bar");
+
+      expect(router.setState).toHaveBeenCalledTimes(2);
+      expect(router.setState).toHaveBeenCalledWith({
+        location: expect.objectContaining({ pathname: "/bar" })
+      });
+    });
+  });
 });
