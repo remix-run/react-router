d543c21f139512e7ab9b1385bf6183f629b3344d
John Memmott <jcmemmott20@outlook.com>
2022-05-31 21:27:19 -0600

    fix(remix-dev): fix typos in `server-build.ts`' error message (#3345)

--- packages/remix-dev/server-build.ts
+++ packages/remix-dev/server-build.ts
@@ -3,8 +3,8 @@ import type { ServerBuild } from "@remix-run/server-runtime";
 
 throw new Error(
   "@remix-run/dev/server-build is not meant to be used directly from node_modules." +
-    " It is exists to provide type definitions for a virtual module provided" +
-    " the Remix compiler at build time."
+    " It exists to provide type definitions for a virtual module provided" +
+    " by the Remix compiler at build time."
 );
 
 export const assets: ServerBuild["assets"] = undefined!;
