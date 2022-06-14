fbcd6ecdb78ef7ab0b1767a4dda4258b3dfa610f
Matt Brophy <matt@brophy.org>
2022-06-14 16:28:30 -0400

    chore: clean up API surface by using router.navigate promise

--- examples/ssr-data-router/src/entry.server.tsx
+++ examples/ssr-data-router/src/entry.server.tsx
@@ -1,5 +1,4 @@
 import { installGlobals } from "@remix-run/node";
-import type { Router as DataRouter } from "@remix-run/router";
 import { createMemoryRouter } from "@remix-run/router";
 import * as React from "react";
 import ReactDOMServer from "react-dom/server";
@@ -10,42 +9,28 @@ import { routes } from "./App";
 installGlobals();
 
 export async function render(url: string) {
-  let router: DataRouter = await new Promise((resolve, reject) => {
-    try {
-      // Create a single-use server router and perform the singular navigation
-      createMemoryRouter({
-        routes,
-        initialEntries: [url],
-        ssrCallback(_router, redirect) {
-          if (redirect) {
-            reject(redirect);
-          } else {
-            resolve(_router);
-          }
-        },
-      }).navigate(url, { replace: true });
-    } catch (e) {
-      reject(e);
-    }
+  let router = createMemoryRouter({
+    routes,
+    initialEntries: [url],
+    isSSR: true,
   });
 
-  let hydrationData = {
-    loaderData: router.state.loaderData,
-    actionData: router.state.actionData,
-    errors: router.state.errors,
-  };
+  try {
+    await router.navigate(url, { replace: true });
 
-  let html = ReactDOMServer.renderToString(
-    <React.StrictMode>
-      <DataStaticRouter router={router} />
-    </React.StrictMode>
-  );
-
-  router.dispose();
-  router = null;
-
-  return {
-    hydrationData,
-    html,
-  };
+    return {
+      hydrationData: {
+        loaderData: router.state.loaderData,
+        actionData: router.state.actionData,
+        errors: router.state.errors,
+      },
+      html: ReactDOMServer.renderToString(
+        <React.StrictMode>
+          <DataStaticRouter router={router} />
+        </React.StrictMode>
+      ),
+    };
+  } finally {
+    router.dispose();
+  }
 }
