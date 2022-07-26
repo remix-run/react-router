c3ccb121463c45fbff08ce0c94124c25be6eb1c5
Matt Brophy <matt@brophy.org>
2022-07-26 12:18:58 -0400

    Update all examples to use react 18

--- examples/data-router/src/main.tsx
+++ examples/data-router/src/main.tsx
@@ -1,10 +1,10 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom/client";
 
 import "./index.css";
 import App from "./App";
 
-createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
