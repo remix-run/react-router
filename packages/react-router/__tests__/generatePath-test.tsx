c282a5dd6ca41450136fb90605d335bfb3363812
Matt Brophy <matt@brophy.org>
2022-09-12 11:14:56 -0400

    fix: ensure consistency in generatePath/compilePath for partial splat params

--- packages/react-router/__tests__/generatePath-test.tsx
+++ packages/react-router/__tests__/generatePath-test.tsx
@@ -51,6 +51,8 @@ describe("generatePath", () => {
   });
 
   it("only interpolates and does not add slashes", () => {
+    let consoleWarn = jest.spyOn(console, "warn").mockImplementation(() => {});
+
     expect(generatePath("*")).toBe("");
     expect(generatePath("/*")).toBe("/");
 
@@ -66,7 +68,28 @@ describe("generatePath", () => {
     expect(generatePath("foo:bar", { bar: "baz" })).toBe("foobaz");
     expect(generatePath("/foo:bar", { bar: "baz" })).toBe("/foobaz");
 
-    expect(generatePath("foo*", { "*": "bar" })).toBe("foobar");
-    expect(generatePath("/foo*", { "*": "bar" })).toBe("/foobar");
+    // Partial splats are treated as independent path segments
+    expect(generatePath("foo*", { "*": "bar" })).toBe("foo/bar");
+    expect(generatePath("/foo*", { "*": "bar" })).toBe("/foo/bar");
+
+    // Ensure we warn on partial splat usages
+    expect(consoleWarn.mock.calls).toMatchInlineSnapshot(`
+      Array [
+        Array [
+          "Route path \\"foo*\\" will be treated as if it were \\"foo/*\\" because the \`*\` character must always follow a \`/\` in the pattern. To get rid of this warning, please change the route path to \\"foo/*\\".",
+        ],
+        Array [
+          "Route path \\"/foo*\\" will be treated as if it were \\"/foo/*\\" because the \`*\` character must always follow a \`/\` in the pattern. To get rid of this warning, please change the route path to \\"/foo/*\\".",
+        ],
+        Array [
+          "Route path \\"foo*\\" will be treated as if it were \\"foo/*\\" because the \`*\` character must always follow a \`/\` in the pattern. To get rid of this warning, please change the route path to \\"foo/*\\".",
+        ],
+        Array [
+          "Route path \\"/foo*\\" will be treated as if it were \\"/foo/*\\" because the \`*\` character must always follow a \`/\` in the pattern. To get rid of this warning, please change the route path to \\"/foo/*\\".",
+        ],
+      ]
+    `);
+
+    consoleWarn.mockRestore();
   });
 });
