989ec7c0acce20cd9110829c30c6a9876c90501d
Michaël De Boey <info@michaeldeboey.be>
2022-12-21 19:53:20 +0100

    chore: fix ESLint warning (#4936)

--- integration/revalidate-test.ts
+++ integration/revalidate-test.ts
@@ -1,7 +1,7 @@
 import { test, expect } from "@playwright/test";
 
 import { createAppFixture, createFixture, js } from "./helpers/create-fixture";
-import type { Fixture, AppFixture } from "./helpers/create-fixture";
+import type { AppFixture } from "./helpers/create-fixture";
 import { PlaywrightFixture } from "./helpers/playwright-fixture";
 
 test.describe("Revalidation", () => {
