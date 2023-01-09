8cfc7d3c4d66490fd0dd07aa7ce15ca4d155dfed
Matt Brophy <matt@brophy.org>
2023-01-09 11:42:58 -0500

    Add navigation blocking example

---
+++ examples/navigation-blocking/README.md
@@ -0,0 +1,15 @@
+---
+title: Navigation Blocking
+toc: false
+order: 1
+---
+
+# Navigation Blocking
+
+This example demonstrates using `useBlocker` to prevent navigating away from a page where you might lose user-entered form data. A potential better UX for this is storing user-entered information in `sessionStorage` and pre-populating the form on return.
+
+## Preview
+
+Open this example on [StackBlitz](https://stackblitz.com):
+
+[![Open in StackBlitz](https://developer.stackblitz.com/img/open_in_stackblitz.svg)](https://stackblitz.com/github/remix-run/react-router/tree/main/examples/navigation-blocking?file=src/App.tsx)
---
+++ examples/navigation-blocking/src/app.tsx
@@ -0,0 +1,145 @@
+import React from "react";
+import { Blocker, useLocation } from "react-router-dom";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Form,
+  json,
+  Link,
+  Outlet,
+  Route,
+  RouterProvider,
+  useBlocker,
+} from "react-router-dom";
+
+let router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route index element={<h2>Index</h2>} />
+      <Route path="one" element={<h2>One</h2>} />
+      <Route path="two" element={<h2>Two</h2>} />
+      <Route
+        path="three"
+        action={() => json({ ok: true })}
+        element={
+          <>
+            <h2>Three</h2>
+            <ImportantForm />
+          </>
+        }
+      />
+      <Route path="four" element={<h2>Four</h2>} />
+      <Route path="five" element={<h2>Five</h2>} />
+    </Route>
+  )
+);
+
+if (import.meta.hot) {
+  import.meta.hot.dispose(() => router.dispose());
+}
+
+export default function App() {
+  return <RouterProvider router={router} />;
+}
+
+function Layout() {
+  let [historyIndex, setHistoryIndex] = React.useState(
+    window.history.state?.idx
+  );
+  let location = useLocation();
+
+  // Expose the underlying history index in the UI for debugging
+  React.useEffect(() => {
+    setHistoryIndex(window.history.state?.idx);
+  }, [location]);
+
+  // Give us meaningful document titles for popping back/forward more than 1 entry
+  React.useEffect(() => {
+    document.title = location.pathname;
+  }, [location]);
+
+  return (
+    <>
+      <h1>Navigation Blocking Example</h1>
+      <nav>
+        <Link to="/">Index</Link>&nbsp;&nbsp;
+        <Link to="/one">One</Link>&nbsp;&nbsp;
+        <Link to="/two">Two</Link>&nbsp;&nbsp;
+        <Link to="/three">Three (Form)</Link>&nbsp;&nbsp;
+        <Link to="/four">Four</Link>&nbsp;&nbsp;
+        <Link to="/five">Five</Link>&nbsp;&nbsp;
+        <a href="https://remix.run">External link to Remix Docs</a>&nbsp;&nbsp;
+      </nav>
+      <p>
+        Current location (index): {location.pathname} ({historyIndex})
+      </p>
+      <Outlet />
+    </>
+  );
+}
+
+function ImportantForm() {
+  let [value, setValue] = React.useState("");
+  let isBlocked = value !== "";
+
+  let [beforeUnload, setBeforeUnload] = React.useState(false);
+  let blocker = useBlocker(() => isBlocked, { beforeUnload });
+
+  // Reset the blocker if the user cleans the form
+  React.useEffect(() => {
+    if (blocker.state === "blocked" && !isBlocked) {
+      blocker.reset();
+    }
+  }, [blocker, isBlocked]);
+
+  // Display our confirmation UI
+  const blockerUI: Record<Blocker["state"], React.ReactElement> = {
+    unblocked: <p style={{ color: "green" }}>Blocker is currently unblocked</p>,
+    blocked: (
+      <>
+        <p style={{ color: "red" }}>Blocked the last navigation</p>
+        <button onClick={() => blocker.proceed?.()}>Let me through</button>
+        <button onClick={() => blocker.reset?.()}>Keep me here</button>
+      </>
+    ),
+    proceeding: (
+      <p style={{ color: "orange" }}>Proceeding through blocked navigation</p>
+    ),
+  };
+
+  return (
+    <>
+      <label>
+        <input
+          type="checkbox"
+          checked={beforeUnload}
+          onChange={(e) => setBeforeUnload(e.target.checked)}
+        />
+        Block external links with <code>beforeunload</code> event listener?
+      </label>
+
+      <p>
+        Is the form dirty?{" "}
+        {isBlocked ? (
+          <span style={{ color: "red" }}>Yes</span>
+        ) : (
+          <span style={{ color: "green" }}>No</span>
+        )}
+      </p>
+
+      <Form method="post">
+        <label>
+          Enter some important data:
+          <input
+            name="data"
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+          />
+        </label>
+        <button type="submit">Save</button>
+      </Form>
+
+      {blockerUI[blocker.state]}
+    </>
+  );
+}
