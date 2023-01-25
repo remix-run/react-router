397ce99766bb8db16d8b851724d6192c5c2baf2a
Nicolas Hervé <nicolas.herve.pessac@gmail.com>
2023-01-25 20:16:55 +0100

    fix: update auth example

--- examples/auth/src/App.tsx
+++ examples/auth/src/App.tsx
@@ -128,10 +128,9 @@ function AuthStatus() {
 }
 
 function RequireAuth({ children }: { children: JSX.Element }) {
-  let auth = useAuth();
   let location = useLocation();
 
-  if (!auth.user) {
+  if (!fakeAuthProvider.isAuthenticated) {
     // Redirect them to the /login page, but save the current location they were
     // trying to go to when they were redirected. This allows us to send them
     // along to that page after they login, which is a nicer user experience
