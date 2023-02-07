28601b1fe31c6350519e74880fd39cd1da4b5c98
Matt Brophy <matt@brophy.org>
2023-02-07 17:46:00 -0500

    Move invariant to an UNSAFE_ export

--- packages/react-router-dom/server.tsx
+++ packages/react-router-dom/server.tsx
@@ -10,7 +10,7 @@ import {
   IDLE_FETCHER,
   IDLE_NAVIGATION,
   Action,
-  invariant,
+  UNSAFE_invariant as invariant,
   isRouteErrorResponse,
   UNSAFE_convertRoutesToDataRoutes as convertRoutesToDataRoutes,
 } from "@remix-run/router";
