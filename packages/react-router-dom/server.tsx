5706fbd9975ce6d3c6e4201644a7936be4c3f3c2
郑闯闯 <zhengchuang_kindle@163.com>
2023-02-15 00:59:57 +0800

    __staticRouterHydrationData html tag escape (#10068)
    Co-authored-by: Matt Brophy <matt@brophy.org>

--- packages/react-router-dom/server.tsx
+++ packages/react-router-dom/server.tsx
@@ -114,7 +114,7 @@ export function StaticRouterProvider({
     // up parsing on the client.  Dual-stringify is needed to ensure all quotes
     // are properly escaped in the resulting string.  See:
     //   https://v8.dev/blog/cost-of-javascript-2019#json
-    let json = JSON.stringify(JSON.stringify(data));
+    let json = htmlEscape(JSON.stringify(JSON.stringify(data)));
     hydrateScript = `window.__staticRouterHydrationData = JSON.parse(${json});`;
   }
 
@@ -322,4 +322,20 @@ function encodeLocation(to: To): Path {
     search: path.search || "",
     hash: path.hash || "",
   };
+}
+
+// This utility is based on https://github.com/zertosh/htmlescape
+// License: https://github.com/zertosh/htmlescape/blob/0527ca7156a524d256101bb310a9f970f63078ad/LICENSE
+const ESCAPE_LOOKUP: { [match: string]: string } = {
+  "&": "\\u0026",
+  ">": "\\u003e",
+  "<": "\\u003c",
+  "\u2028": "\\u2028",
+  "\u2029": "\\u2029",
+};
+
+const ESCAPE_REGEX = /[&><\u2028\u2029]/g;
+
+function htmlEscape(str: string): string {
+  return str.replace(ESCAPE_REGEX, (match) => ESCAPE_LOOKUP[match]);
 }
