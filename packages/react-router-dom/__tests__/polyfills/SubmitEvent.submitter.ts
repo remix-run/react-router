6bc783a6cf89994fa92b1e6de6a8d417ea8376a8
Thomas <thomasverleye@live.be>
2023-02-18 18:48:44 +0100

    Create test for Link in StaticRouter Context

--- packages/react-router-dom/__tests__/polyfills/SubmitEvent.submitter.ts
+++ packages/react-router-dom/__tests__/polyfills/SubmitEvent.submitter.ts
@@ -6,23 +6,25 @@ if (
   const setImmediate = (fn, ...args) => global.setTimeout(fn, 0, ...args);
 
   let maybeSubmitter;
-  window.addEventListener(
-    "click",
-    (event) => {
-      if ((event.target as any)?.form) maybeSubmitter = event.target;
-      setImmediate(() => {
-        // if this click doesn't imminently trigger a submit event, then forget it
-        maybeSubmitter = undefined;
-      });
-    },
-    { capture: true }
-  );
-  window.addEventListener(
-    "submit",
-    (event: any) => {
-      if (maybeSubmitter?.form === event.target)
-        event.submitter = maybeSubmitter;
-    },
-    { capture: true }
-  );
+  if (typeof window !== "undefined") {
+    window.addEventListener(
+      "click",
+      (event) => {
+        if ((event.target as any)?.form) maybeSubmitter = event.target;
+        setImmediate(() => {
+          // if this click doesn't imminently trigger a submit event, then forget it
+          maybeSubmitter = undefined;
+        });
+      },
+      { capture: true }
+    );
+    window.addEventListener(
+      "submit",
+      (event: any) => {
+        if (maybeSubmitter?.form === event.target)
+          event.submitter = maybeSubmitter;
+      },
+      { capture: true }
+    );
+  }
 }
