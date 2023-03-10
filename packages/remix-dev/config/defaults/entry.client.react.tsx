67d0cd92672b3858b643f0c0426173186468e15b
Logan McAnsh <logan@mcan.sh>
2023-03-10 11:38:28 -0500

    chore(remix-dev): update default `entry.client` (#5749)

--- packages/remix-dev/config/defaults/entry.client.react.tsx
+++ packages/remix-dev/config/defaults/entry.client.react.tsx
@@ -2,21 +2,11 @@ import { RemixBrowser } from "@remix-run/react";
 import { startTransition, StrictMode } from "react";
 import { hydrateRoot } from "react-dom/client";
 
-function hydrate() {
-  startTransition(() => {
-    hydrateRoot(
-      document,
-      <StrictMode>
-        <RemixBrowser />
-      </StrictMode>
-    );
-  });
-}
-
-if (typeof requestIdleCallback === "function") {
-  requestIdleCallback(hydrate);
-} else {
-  // Safari doesn't support requestIdleCallback
-  // https://caniuse.com/requestidlecallback
-  setTimeout(hydrate, 1);
-}
+startTransition(() => {
+  hydrateRoot(
+    document,
+    <StrictMode>
+      <RemixBrowser />
+    </StrictMode>
+  );
+});
