6a2179fd7e0d04c3a17053ccdc712d2af67d77b2
Matt Brophy <matt@brophy.org>
2023-03-13 10:30:51 -0400

    Add v2_errorBoundary tests (#5762)

--- integration/catch-boundary-data-test.ts
+++ integration/catch-boundary-data-test.ts
@@ -228,3 +228,219 @@ test("renders self boundary with layout data available on transition", async ({
   await page.waitForSelector(`#layout-data:has-text("${LAYOUT_DATA}")`);
   await page.waitForSelector(`#own-boundary:has-text("${OWN_BOUNDARY_TEXT}")`);
 });
+
+// Copy/Paste of the above tests altered to use v2_errorBoundary.  In v2 we can:
+// - delete the above tests
+// - remove this describe block
+// - remove the v2_errorBoundary flag
+test.describe("v2_errorBoundary", () => {
+  test.beforeAll(async () => {
+    fixture = await createFixture({
+      future: {
+        v2_routeConvention: true,
+        v2_errorBoundary: true,
+      },
+      files: {
+        "app/root.jsx": js`
+          import { json } from "@remix-run/node";
+          import {
+            Links,
+            Meta,
+            Outlet,
+            Scripts,
+            useLoaderData,
+            useMatches,
+          } from "@remix-run/react";
+
+          export const loader = () => json("${ROOT_DATA}");
+
+          export default function Root() {
+            const data = useLoaderData();
+
+            return (
+              <html lang="en">
+                <head>
+                  <Meta />
+                  <Links />
+                </head>
+                <body>
+                  <div id="root-data">{data}</div>
+                  <Outlet />
+                  <Scripts />
+                </body>
+              </html>
+            );
+          }
+
+          export function ErrorBoundary() {
+            let matches = useMatches();
+            let { data } = matches.find(match => match.id === "root");
+
+            return (
+              <html>
+                <head />
+                <body>
+                  <div id="root-boundary">${ROOT_BOUNDARY_TEXT}</div>
+                  <div id="root-boundary-data">{data}</div>
+                  <Scripts />
+                </body>
+              </html>
+            );
+          }
+        `,
+
+        "app/routes/_index.jsx": js`
+          import { Link } from "@remix-run/react";
+          export default function Index() {
+            return (
+              <div>
+                <Link to="${NO_BOUNDARY_LOADER}">${NO_BOUNDARY_LOADER}</Link>
+                <Link to="${HAS_BOUNDARY_LAYOUT_NESTED_LOADER}">${HAS_BOUNDARY_LAYOUT_NESTED_LOADER}</Link>
+                <Link to="${HAS_BOUNDARY_NESTED_LOADER}">${HAS_BOUNDARY_NESTED_LOADER}</Link>
+              </div>
+            );
+          }
+        `,
+
+        [`app/routes${NO_BOUNDARY_LOADER_FILE}.jsx`]: js`
+          export function loader() {
+            throw new Response("", { status: 401 });
+          }
+          export default function Index() {
+            return <div/>;
+          }
+        `,
+
+        [`app/routes${HAS_BOUNDARY_LAYOUT_NESTED_LOADER_FILE}.jsx`]: js`
+          import { useMatches } from "@remix-run/react";
+          export function loader() {
+            return "${LAYOUT_DATA}";
+          }
+          export default function Layout() {
+            return <div/>;
+          }
+          export function ErrorBoundary() {
+            let matches = useMatches();
+            let { data } = matches.find(match => match.id === "routes${HAS_BOUNDARY_LAYOUT_NESTED_LOADER_FILE}");
+
+            return (
+              <div>
+                <div id="layout-boundary">${LAYOUT_BOUNDARY_TEXT}</div>
+                <div id="layout-boundary-data">{data}</div>
+              </div>
+            );
+          }
+        `,
+
+        [`app/routes${HAS_BOUNDARY_LAYOUT_NESTED_LOADER_FILE}._index.jsx`]: js`
+          export function loader() {
+            throw new Response("", { status: 401 });
+          }
+          export default function Index() {
+            return <div/>;
+          }
+        `,
+
+        [`app/routes${HAS_BOUNDARY_NESTED_LOADER_FILE}.jsx`]: js`
+          import { Outlet, useLoaderData } from "@remix-run/react";
+          export function loader() {
+            return "${LAYOUT_DATA}";
+          }
+          export default function Layout() {
+            let data = useLoaderData();
+            return (
+              <div>
+                <div id="layout-data">{data}</div>
+                <Outlet/>
+              </div>
+            );
+          }
+        `,
+
+        [`app/routes${HAS_BOUNDARY_NESTED_LOADER_FILE}._index.jsx`]: js`
+          export function loader() {
+            throw new Response("", { status: 401 });
+          }
+          export default function Index() {
+            return <div/>;
+          }
+          export function ErrorBoundary() {
+            return (
+              <div id="own-boundary">${OWN_BOUNDARY_TEXT}</div>
+            );
+          }
+        `,
+      },
+    });
+
+    appFixture = await createAppFixture(fixture);
+  });
+
+  test.afterAll(() => {
+    appFixture.close();
+  });
+
+  test("renders root boundary with data available", async () => {
+    let res = await fixture.requestDocument(NO_BOUNDARY_LOADER);
+    expect(res.status).toBe(401);
+    let html = await res.text();
+    expect(html).toMatch(ROOT_BOUNDARY_TEXT);
+    expect(html).toMatch(ROOT_DATA);
+  });
+
+  test("renders root boundary with data available on transition", async ({
+    page,
+  }) => {
+    let app = new PlaywrightFixture(appFixture, page);
+    await app.goto("/");
+    await app.clickLink(NO_BOUNDARY_LOADER);
+    await page.waitForSelector("#root-boundary");
+    await page.waitForSelector(`#root-boundary-data:has-text("${ROOT_DATA}")`);
+  });
+
+  test("renders layout boundary with data available", async () => {
+    let res = await fixture.requestDocument(HAS_BOUNDARY_LAYOUT_NESTED_LOADER);
+    expect(res.status).toBe(401);
+    let html = await res.text();
+    expect(html).toMatch(ROOT_DATA);
+    expect(html).toMatch(LAYOUT_BOUNDARY_TEXT);
+    expect(html).toMatch(LAYOUT_DATA);
+  });
+
+  test("renders layout boundary with data available on transition", async ({
+    page,
+  }) => {
+    let app = new PlaywrightFixture(appFixture, page);
+    await app.goto("/");
+    await app.clickLink(HAS_BOUNDARY_LAYOUT_NESTED_LOADER);
+    await page.waitForSelector(`#root-data:has-text("${ROOT_DATA}")`);
+    await page.waitForSelector(
+      `#layout-boundary:has-text("${LAYOUT_BOUNDARY_TEXT}")`
+    );
+    await page.waitForSelector(
+      `#layout-boundary-data:has-text("${LAYOUT_DATA}")`
+    );
+  });
+
+  test("renders self boundary with layout data available", async () => {
+    let res = await fixture.requestDocument(HAS_BOUNDARY_NESTED_LOADER);
+    expect(res.status).toBe(401);
+    let html = await res.text();
+    expect(html).toMatch(ROOT_DATA);
+    expect(html).toMatch(LAYOUT_DATA);
+    expect(html).toMatch(OWN_BOUNDARY_TEXT);
+  });
+
+  test("renders self boundary with layout data available on transition", async ({
+    page,
+  }) => {
+    let app = new PlaywrightFixture(appFixture, page);
+    await app.goto("/");
+    await app.clickLink(HAS_BOUNDARY_NESTED_LOADER);
+    await page.waitForSelector(`#root-data:has-text("${ROOT_DATA}")`);
+    await page.waitForSelector(`#layout-data:has-text("${LAYOUT_DATA}")`);
+    await page.waitForSelector(
+      `#own-boundary:has-text("${OWN_BOUNDARY_TEXT}")`
+    );
+  });
+});
--- integration/link-test.ts
+++ integration/link-test.ts
@@ -32,7 +32,10 @@ test.describe("route module link export", () => {
 
   test.beforeAll(async () => {
     fixture = await createFixture({
-      future: { v2_routeConvention: true },
+      future: {
+        v2_routeConvention: true,
+        v2_errorBoundary: true,
+      },
       files: {
         "app/favicon.ico": js``,
 
@@ -88,7 +91,8 @@ test.describe("route module link export", () => {
             Meta,
             Outlet,
             Scripts,
-            useCatch,
+            useRouteError,
+            isRouteErrorResponse
           } from "@remix-run/react";
           import resetHref from "./reset.css";
           import stylesHref from "./app.css";
@@ -123,72 +127,72 @@ test.describe("route module link export", () => {
             );
           }
 
-          export function CatchBoundary() {
-            let caught = useCatch();
+          export function ErrorBoundary() {
+            let error = useRouteError();
 
-            switch (caught.status) {
-              case 404:
+            if (isRouteErrorResponse()) {
+              switch (error.status) {
+                case 404:
+                  return (
+                    <html lang="en">
+                      <head>
+                        <meta charSet="utf-8" />
+                        <title>404 Not Found</title>
+                        <Links />
+                      </head>
+                      <body>
+                        <div>
+                          <h1>404 Not Found</h1>
+                        </div>
+                        <Scripts />
+                      </body>
+                    </html>
+                  );
+                default:
+                  console.warn("Unexpected catch", error);
+
+                  return (
+                    <html lang="en">
+                      <head>
+                        <meta charSet="utf-8" />
+                        <title>{error.status} Uh-oh!</title>
+                        <Links />
+                      </head>
+                      <body>
+                        <div>
+                          <h1>
+                            {error.status} {error.statusText}
+                          </h1>
+                          {error.data ? (
+                            <pre>
+                              <code>{JSON.stringify(error.data, null, 2)}</code>
+                            </pre>
+                          ) : null}
+                        </div>
+                        <Scripts />
+                      </body>
+                    </html>
+                  );
+                }
+              } else {
+                console.error(error);
                 return (
                   <html lang="en">
                     <head>
                       <meta charSet="utf-8" />
-                      <title>404 Not Found</title>
+                      <title>Oops!</title>
                       <Links />
                     </head>
                     <body>
                       <div>
-                        <h1>404 Not Found</h1>
+                        <h1>App Error Boundary</h1>
+                        <pre>{error.message}</pre>
                       </div>
                       <Scripts />
                     </body>
                   </html>
                 );
-              default:
-                console.warn("Unexpected catch", caught);
-
-                return (
-                  <html lang="en">
-                    <head>
-                      <meta charSet="utf-8" />
-                      <title>{caught.status} Uh-oh!</title>
-                      <Links />
-                    </head>
-                    <body>
-                      <div>
-                        <h1>
-                          {caught.status} {caught.statusText}
-                        </h1>
-                        {caught.data ? (
-                          <pre>
-                            <code>{JSON.stringify(caught.data, null, 2)}</code>
-                          </pre>
-                        ) : null}
-                      </div>
-                      <Scripts />
-                    </body>
-                  </html>
-                );
-            }
-          }
-
-          export function ErrorBoundary({ error }) {
-            console.error(error);
-            return (
-              <html lang="en">
-                <head>
-                  <meta charSet="utf-8" />
-                  <title>Oops!</title>
-                  <Links />
-                </head>
-                <body>
-                  <div>
-                    <h1>App Error Boundary</h1>
-                    <pre>{error.message}</pre>
-                  </div>
-                  <Scripts />
-                </body>
-              </html>
-            );
+              }
           }
         `,
 
