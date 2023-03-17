6d8a5815374472af06c9605c69ffeaf18d47e126
Matt Brophy <matt@brophy.org>
2023-03-17 13:57:10 -0400

    Change approach for removing isFetchActionRedirect

--- packages/router/router.ts
+++ packages/router/router.ts
@@ -1763,7 +1763,7 @@ export function createRouter(init: RouterInit): Router {
       updateState({ fetchers: new Map(state.fetchers) });
 
       return startRedirectNavigation(state, actionResult, {
-        submission,
+        fetcherSubmission: submission,
       });
     }
 
@@ -2071,9 +2071,11 @@ export function createRouter(init: RouterInit): Router {
     redirect: RedirectResult,
     {
       submission,
+      fetcherSubmission,
       replace,
     }: {
       submission?: Submission;
+      fetcherSubmission?: Submission;
       replace?: boolean;
     } = {}
   ) {
@@ -2113,7 +2115,14 @@ export function createRouter(init: RouterInit): Router {
     // Use the incoming submission if provided, fallback on the active one in
     // state.navigation
     let { formMethod, formAction, formEncType, formData } = state.navigation;
-    if (!submission && formMethod && formAction && formData && formEncType) {
+    if (
+      !submission &&
+      !fetcherSubmission &&
+      formMethod &&
+      formAction &&
+      formData &&
+      formEncType
+    ) {
       submission = {
         formMethod,
         formAction,
@@ -2125,32 +2134,17 @@ export function createRouter(init: RouterInit): Router {
     // If this was a 307/308 submission we want to preserve the HTTP method and
     // re-submit the GET/POST/PUT/PATCH/DELETE as a submission navigation to the
     // redirected location
+    let activeSubmission = submission || fetcherSubmission;
     if (
       redirectPreserveMethodStatusCodes.has(redirect.status) &&
-      submission &&
-      isMutationMethod(submission.formMethod)
+      activeSubmission &&
+      isMutationMethod(activeSubmission.formMethod)
     ) {
       await startNavigation(redirectHistoryAction, redirectLocation, {
         submission: {
-          ...submission,
+          ...activeSubmission,
           formAction: redirect.location,
         },
-        // Preserve this flag across redirects
-        preventScrollReset: pendingPreventScrollReset,
-      });
-    } else if (isFetchActionRedirect) {
-      // For a fetch action redirect, we kick off a new loading navigation
-      // without the fetcher submission, but we send it along for shouldRevalidate
-      await startNavigation(redirectHistoryAction, redirectLocation, {
-        overrideNavigation: {
-          state: "loading",
-          location: redirectLocation,
-          formMethod: undefined,
-          formAction: undefined,
-          formEncType: undefined,
-          formData: undefined,
-        },
-        fetcherSubmission: submission,
         // Preserve this flag across redirects
         preventScrollReset: pendingPreventScrollReset,
       });
@@ -2161,11 +2155,14 @@ export function createRouter(init: RouterInit): Router {
         overrideNavigation: {
           state: "loading",
           location: redirectLocation,
+          // Only include navigational submissions on the navigation
           formMethod: submission ? submission.formMethod : undefined,
           formAction: submission ? submission.formAction : undefined,
           formEncType: submission ? submission.formEncType : undefined,
           formData: submission ? submission.formData : undefined,
         },
+        // Send fetcher submissions through for shouldRevalidate
+        fetcherSubmission,
         // Preserve this flag across redirects
         preventScrollReset: pendingPreventScrollReset,
       });
