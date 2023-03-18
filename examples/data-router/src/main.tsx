3084e349577898614fe2470b3229028800c4baf4
Alexander Grekhnev <a.grekhnev@mypath.group>
2023-03-18 12:24:33 +0500

    refactor(main.tsx): cast type

--- examples/data-router/src/main.tsx
+++ examples/data-router/src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./app";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
