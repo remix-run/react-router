240b240b4a9beaff9c19a371a4cf07dc3dc95c2e
Matt Brophy <matt@brophy.org>
2023-03-29 16:39:49 -0400

    Add basic data router example

---
+++ examples/basic-data-router/src/app.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  useLoaderData,
+} from "react-router-dom";
+
+import "./index.css";
+
+let router = createBrowserRouter([
+  {
+    path: "/",
+    loader: () => ({ message: "Hello Data Router!" }),
+    Component() {
+      let data = useLoaderData() as { message: string };
+      return <h1>{data.message}</h1>;
+    },
+  },
+]);
+
+export default function App() {
+  return <RouterProvider router={router} fallbackElement={<p>Loading...</p>} />;
+}
+
+// @ts-expect-error
+if (import.meta.hot) {
+  // @ts-expect-error
+  import.meta.hot.dispose(() => router.dispose());
+}
