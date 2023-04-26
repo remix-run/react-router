b9e74019614341dcccc76b50ba06e80382d7f31a
Matt Brophy <matt@brophy.org>
2023-04-26 13:43:54 -0400

    Add missing static propr for StaticRouterProvider (#10401)

--- packages/react-router-dom/server.tsx
+++ packages/react-router-dom/server.tsx
@@ -136,6 +136,7 @@ export function StaticRouterProvider({
             location={state.location}
             navigationType={state.historyAction}
             navigator={dataRouterContext.navigator}
+            static={dataRouterContext.static}
           >
             <DataRoutes routes={router.routes} state={state} />
           </Router>
