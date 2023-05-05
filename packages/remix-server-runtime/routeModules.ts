b5404238d5d71df96d8cdb7381aa99b6696ed3f6
Mehdi Achour <machour@gmail.com>
2023-05-05 18:06:22 +0100

    fix(v2_meta): hint data as being potentially undefined (#6231)
    Co-authored-by: Matt Brophy <matt@brophy.org>

--- packages/remix-server-runtime/routeModules.ts
+++ packages/remix-server-runtime/routeModules.ts
@@ -200,7 +200,9 @@ export interface V2_ServerRuntimeMetaArgs<
   Loader extends LoaderFunction | unknown = unknown,
   MatchLoaders extends Record<string, unknown> = Record<string, unknown>
 > {
-  data: Loader extends LoaderFunction ? SerializeFrom<Loader> : AppData;
+  data:
+    | (Loader extends LoaderFunction ? SerializeFrom<Loader> : AppData)
+    | undefined;
   params: Params;
   location: Location;
   matches: V2_ServerRuntimeMetaMatches<MatchLoaders>;
