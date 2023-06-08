f4c7c808313d75b46156a7a19a322570b89510c9
Matt Brophy <matt@brophy.org>
2023-06-08 09:09:43 -0400

    Update examples folder vite configs to use port 3000

--- examples/ssr-data-router/vite.config.js
+++ examples/ssr-data-router/vite.config.js
@@ -5,6 +5,9 @@ import rollupReplace from "@rollup/plugin-replace";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 3000,
+  },
   plugins: [
     rollupReplace({
       preventAssignment: true,
