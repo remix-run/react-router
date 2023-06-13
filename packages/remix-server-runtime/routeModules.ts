66e1291dd7a5678c46570b1920aa0c93623b127a
Michaël De Boey <info@michaeldeboey.be>
2023-06-13 23:37:37 +0200

    fix(remix-server-runtime): don't use React types (#5713)
    Co-authored-by: Matt Brophy <matt@brophy.org>

--- packages/remix-server-runtime/routeModules.ts
+++ packages/remix-server-runtime/routeModules.ts
@@ -4,7 +4,6 @@ import type {
   Params,
   RouterState,
 } from "@remix-run/router";
-import type { ComponentType } from "react";
 
 import type { AppLoadContext, AppData } from "./data";
 import type { LinkDescriptor } from "./links";
@@ -48,23 +47,31 @@ export interface ActionFunction {
 /**
  * A React component that is rendered when the server throws a Response.
  *
- * @deprecated Please enable the v2_errorBoundary flag
+ * @deprecated Please enable the v2_errorBoundary flag to eliminate the need
+ * for this type.  If you are still using this, please use `@remix-run/react`'s
+ * `CatchBoundaryComponent` type
  */
-export type CatchBoundaryComponent = ComponentType;
+export type CatchBoundaryComponent = any;
 
 /**
  * A React component that is rendered when there is an error on a route.
  *
- * @deprecated Please enable the v2_errorBoundary flag
+ * @deprecated Please enable the v2_errorBoundary flag to eliminate the need
+ * for this type.  If you are still using this, please use `@remix-run/react`'s
+ * `ErrorBoundaryComponent` type
  */
-export type ErrorBoundaryComponent = ComponentType<{ error: Error }>;
+export type ErrorBoundaryComponent = any;
 
 /**
  * V2 version of the ErrorBoundary that eliminates the distinction between
  * Error and Catch Boundaries and behaves like RR 6.4 errorElement and captures
  * errors with useRouteError()
+ *
+ * @deprecated Please enable the v2_errorBoundary flag to eliminate the need
+ * for this type.  If you are still using this, please use `@remix-run/react`'s
+ * `V2_ErrorBoundaryComponent` type
  */
-export type V2_ErrorBoundaryComponent = ComponentType;
+export type V2_ErrorBoundaryComponent = any;
 
 export type HeadersArgs = {
   loaderHeaders: Headers;
@@ -262,8 +269,10 @@ type LdJsonValue = LdJsonPrimitive | LdJsonObject | LdJsonArray;
 
 /**
  * A React component that is rendered for a route.
+ *
+ * @deprecated Please use `@remix-run/react`'s `RouteComponent` type instead
  */
-export type RouteComponent = ComponentType<{}>;
+export type RouteComponent = any;
 
 /**
  * An arbitrary object that is associated with a route.
