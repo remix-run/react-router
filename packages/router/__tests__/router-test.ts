e0e521bc1448f98f16ddf71741096bba1b9caef2
Robb Traister <robb@seam.io>
2023-07-14 15:36:26 -0400

    add test for X-Remix-Reload-Document header

--- packages/router/__tests__/router-test.ts
+++ packages/router/__tests__/router-test.ts
@@ -7004,6 +7004,45 @@ describe("a router", () => {
       }
     });
 
+    it("processes redirects with document reload if header is present (assign)", async () => {
+      let urls = ["/redirect"];
+
+      for (let url of urls) {
+        let t = setup({ routes: REDIRECT_ROUTES });
+
+        let A = await t.navigate("/parent/child", {
+          formMethod: "post",
+          formData: createFormData({}),
+        });
+
+        await A.actions.child.redirectReturn(url, 301, {
+          "X-Remix-Reload-Document": "true",
+        });
+        expect(t.window.location.assign).toHaveBeenCalledWith(url);
+        expect(t.window.location.replace).not.toHaveBeenCalled();
+      }
+    });
+
+    it("processes redirects with document reload if header is present (replace)", async () => {
+      let urls = ["/redirect"];
+
+      for (let url of urls) {
+        let t = setup({ routes: REDIRECT_ROUTES });
+
+        let A = await t.navigate("/parent/child", {
+          formMethod: "post",
+          formData: createFormData({}),
+          replace: true,
+        });
+
+        await A.actions.child.redirectReturn(url, 301, {
+          "X-Remix-Reload-Document": "true",
+        });
+        expect(t.window.location.replace).toHaveBeenCalledWith(url);
+        expect(t.window.location.assign).not.toHaveBeenCalled();
+      }
+    });
+
     it("properly handles same-origin absolute URLs", async () => {
       let t = setup({ routes: REDIRECT_ROUTES });
 
