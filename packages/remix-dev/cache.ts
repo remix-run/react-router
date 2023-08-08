1453267d8385da3702dc8e00188d67d3b102c94d
Michaël De Boey <info@michaeldeboey.be>
2023-08-08 22:04:46 +0200

    chore(remix-dev): upgrade `cacache` to v17 (#7100)

--- packages/remix-dev/cache.ts
+++ packages/remix-dev/cache.ts
@@ -1,13 +1,7 @@
 import { put, get } from "cacache";
 
-export { put, get };
+export const putJson = async (cachePath: string, key: string, data: any) =>
+  put(cachePath, key, JSON.stringify(data));
 
-export function putJson(cachePath: string, key: string, data: any) {
-  return put(cachePath, key, JSON.stringify(data));
-}
-
-export function getJson(cachePath: string, key: string) {
-  return get(cachePath, key).then((obj) =>
-    JSON.parse(obj.data.toString("utf-8"))
-  );
-}
+export const getJson = async (cachePath: string, key: string) =>
+  get(cachePath, key).then((obj) => JSON.parse(obj.data.toString("utf-8")));
