dc7aee7a1ccc6e3fbc349b5666afff6a790337ad
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2023-11-24 08:26:34 +0900

    fix(remix-dev/vite): use global `Request` class in Vite dev server handler (#8062)
    Co-authored-by: Mark Dalgleish <mark.john.dalgleish@gmail.com>

--- packages/remix-dev/vite/node/adapter.ts
+++ packages/remix-dev/vite/node/adapter.ts
@@ -1,69 +1,23 @@
-// @ts-nocheck
-// adapted from https://github.com/solidjs/solid-start/blob/ccff60ce75e066f6613daf0272dbb43a196235a4/packages/start/node/fetch.js
-import { once } from "events";
-import { type IncomingMessage, type ServerResponse } from "http";
-import multipart from "parse-multipart-data";
+import type {
+  IncomingHttpHeaders,
+  IncomingMessage,
+  ServerResponse,
+} from "node:http";
+import { once } from "node:events";
+import { Readable } from "node:stream";
 import { splitCookiesString } from "set-cookie-parser";
-import { Readable } from "stream";
-import { File, FormData, Headers, Request as BaseNodeRequest } from "undici";
-import { type ServerBuild, installGlobals } from "@remix-run/node";
+import {
+  type ServerBuild,
+  installGlobals,
+  createReadableStreamFromReadable,
+} from "@remix-run/node";
 import { createRequestHandler as createBaseRequestHandler } from "@remix-run/server-runtime";
+
+import invariant from "../../invariant";
 
 installGlobals();
 
-function nodeToWeb(nodeStream) {
-  let destroyed = false;
-  let listeners = {};
-
-  function start(controller) {
-    listeners["data"] = onData;
-    listeners["end"] = onData;
-    listeners["end"] = onDestroy;
-    listeners["close"] = onDestroy;
-    listeners["error"] = onDestroy;
-    for (let name in listeners) nodeStream.on(name, listeners[name]);
-
-    nodeStream.pause();
-
-    function onData(chunk) {
-      if (destroyed) return;
-      controller.enqueue(chunk);
-      nodeStream.pause();
-    }
-
-    function onDestroy(err) {
-      if (destroyed) return;
-      destroyed = true;
-
-      for (let name in listeners)
-        nodeStream.removeListener(name, listeners[name]);
-
-      if (err) controller.error(err);
-      else controller.close();
-    }
-  }
-
-  function pull() {
-    if (destroyed) return;
-    nodeStream.resume();
-  }
-
-  function cancel() {
-    destroyed = true;
-
-    for (let name in listeners)
-      nodeStream.removeListener(name, listeners[name]);
-
-    nodeStream.push(null);
-    nodeStream.pause();
-    if (nodeStream.destroy) nodeStream.destroy();
-    else if (nodeStream.close) nodeStream.close();
-  }
-
-  return new ReadableStream({ start: start, pull: pull, cancel: cancel });
-}
-
-function createHeaders(requestHeaders) {
+function createHeaders(requestHeaders: IncomingHttpHeaders) {
   let headers = new Headers();
 
   for (let [key, values] of Object.entries(requestHeaders)) {
@@ -81,93 +35,33 @@ function createHeaders(requestHeaders) {
   return headers;
 }
 
-class NodeRequest extends BaseNodeRequest {
-  constructor(input, init) {
-    if (init && init.data && init.data.on) {
-      init = {
-        duplex: "half",
-        ...init,
-        body: init.data.headers["content-type"]?.includes("x-www")
-          ? init.data
-          : nodeToWeb(init.data),
-      };
-    }
-
-    super(input, init);
-  }
-
-  // async json() {
-  //   return JSON.parse(await this.text());
-  // }
-
-  async buffer() {
-    return Buffer.from(await super.arrayBuffer());
-  }
-
-  // async text() {
-  //   return (await this.buffer()).toString();
-  // }
-
-  // @ts-ignore
-  async formData() {
-    if (
-      this.headers.get("content-type") === "application/x-www-form-urlencoded"
-    ) {
-      return await super.formData();
-    } else {
-      let data = await this.buffer();
-      let input = multipart.parse(
-        data,
-        this.headers
-          .get("content-type")
-          .replace("multipart/form-data; boundary=", "")
-      );
-      let form = new FormData();
-      input.forEach(({ name, data, filename, type }) => {
-        // file fields have Content-Type set,
-        // whereas non-file fields must not
-        // https://html.spec.whatwg.org/multipage/form-control-infrastructure.html#multipart-form-data
-        let isFile = type !== undefined;
-        if (isFile) {
-          let value = new File([data], filename, { type });
-          form.append(name, value, filename);
-        } else {
-          let value = data.toString("utf-8");
-          form.append(name, value);
-        }
-      });
-      return form;
-    }
-  }
-
-  // @ts-ignore
-  clone() {
-    /** @type {BaseNodeRequest & { buffer?: () => Promise<Buffer>; formData?: () => Promise<FormData> }}  */
-    let el = super.clone();
-    el.buffer = this.buffer.bind(el);
-    el.formData = this.formData.bind(el);
-    return el;
-  }
-}
-
-function createRequest(req: IncomingMessage): Request {
+// Based on `createRemixRequest` in packages/remix-express/server.ts
+function createRequest(req: IncomingMessage, res: ServerResponse): Request {
   let origin =
     req.headers.origin && "null" !== req.headers.origin
       ? req.headers.origin
       : `http://${req.headers.host}`;
+  invariant(req.url, 'Expected "req.url" to be defined');
   let url = new URL(req.url, origin);
 
-  let init = {
+  let controller = new AbortController();
+  res.on("close", () => controller.abort());
+
+  let init: RequestInit = {
     method: req.method,
     headers: createHeaders(req.headers),
-    // https://developer.mozilla.org/en-US/docs/Web/HTTP/Methods
-    data: ["POST", "PUT", "DELETE", "PATCH"].includes(req.method) ? req : null,
+    signal: controller.signal,
   };
 
-  return new NodeRequest(url.href, init);
+  if (req.method !== "GET" && req.method !== "HEAD") {
+    init.body = createReadableStreamFromReadable(req);
+    (init as { duplex: "half" }).duplex = "half";
+  }
+
+  return new Request(url.href, init);
 }
 
-// Adapted from more recent version of `handleNodeResponse`:
+// Adapted from solid-start's `handleNodeResponse`:
 // https://github.com/solidjs/solid-start/blob/7398163869b489cce503c167e284891cf51a6613/packages/start/node/fetch.js#L162-L185
 async function handleNodeResponse(webRes: Response, res: ServerResponse) {
   res.statusCode = webRes.status;
@@ -186,7 +80,9 @@ async function handleNodeResponse(webRes: Response, res: ServerResponse) {
   }
 
   if (webRes.body) {
-    let readable = Readable.from(webRes.body);
+    // https://github.com/microsoft/TypeScript/issues/29867
+    let responseBody = webRes.body as unknown as AsyncIterable<Uint8Array>;
+    let readable = Readable.from(responseBody);
     readable.pipe(res);
     await once(readable, "end");
   } else {
@@ -196,15 +92,11 @@ async function handleNodeResponse(webRes: Response, res: ServerResponse) {
 
 export let createRequestHandler = (
   build: ServerBuild,
-  {
-    mode = "production",
-  }: {
-    mode?: string;
-  }
+  { mode = "production" }: { mode?: string }
 ) => {
   let handler = createBaseRequestHandler(build, mode);
   return async (req: IncomingMessage, res: ServerResponse) => {
-    let request = createRequest(req);
+    let request = createRequest(req, res);
     let response = await handler(request, {});
     handleNodeResponse(response, res);
   };
