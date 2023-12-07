df4897da3ffb97bf66bb5e045446a403a81903d6
Brendan Allan <brendonovich@outlook.com>
2023-12-07 15:05:01 +0800

    add replace to router redirect

--- packages/router/router.ts
+++ packages/router/router.ts
@@ -1796,7 +1796,9 @@ export function createRouter(init: RouterInit): Router {
           revalidatingFetchers[redirect.idx - matchesToLoad.length].key;
         fetchRedirectIds.add(fetcherKey);
       }
-      await startRedirectNavigation(state, redirect.result, { replace });
+      await startRedirectNavigation(state, redirect.result, {
+        replace: replace || redirect.result.replace,
+      });
       return { shortCircuited: true };
     }
 
@@ -2118,7 +2120,9 @@ export function createRouter(init: RouterInit): Router {
           revalidatingFetchers[redirect.idx - matchesToLoad.length].key;
         fetchRedirectIds.add(fetcherKey);
       }
-      return startRedirectNavigation(state, redirect.result);
+      return startRedirectNavigation(state, redirect.result, {
+        replace: redirect.result.replace,
+      });
     }
 
     // Process and commit output from loaders
@@ -4066,6 +4070,7 @@ async function callLoaderOrAction(
         type: ResultType.redirect,
         status,
         location,
+        replace: result.headers.get("X-Remix-Redirect-Replace") !== null,
         revalidate: result.headers.get("X-Remix-Revalidate") !== null,
         reloadDocument: result.headers.get("X-Remix-Reload-Document") !== null,
       };
