20230d5aa645e3190b960d4503ba42b9d8063c51
Jacob Ebey <jacob.ebey@live.com>
2023-12-11 15:33:58 -0800

    filter matches in fixture

--- examples/ssr-single-fetch/app/entry.server.tsx
+++ examples/ssr-single-fetch/app/entry.server.tsx
@@ -40,7 +40,11 @@ export async function render(
       if (isDataRequest && xRouteIds?.length) {
         let routesToLoad = new Set(xRouteIds);
 
-        await Promise.all(matches.map(m => m.route))
+        await Promise.all(
+          matches
+            .filter((m) => routesToLoad.has(m.route.id))
+            .map((m) => m.route)
+        );
         return Promise.all(
           matches.map((match) => {
             if (!routesToLoad!.has(match.route.id)) {
@@ -55,7 +59,7 @@ export async function render(
         );
       }
 
-      await Promise.all(matches.map(m => m.route))
+      await Promise.all(matches.map((m) => m.route));
       return Promise.all(
         matches.map((match) => {
           return defaultStrategy(match);
