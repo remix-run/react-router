4904383ba3146bb0b285a0c5c672f473b1b47060
Jacob Ebey <jacob.ebey@live.com>
2024-03-22 10:38:46 -0700

    chore: use undici / native node fetch

--- packages/react-router-dom/__tests__/setup.ts
+++ packages/react-router-dom/__tests__/setup.ts
@@ -2,25 +2,28 @@ import {
   TextEncoder as NodeTextEncoder,
   TextDecoder as NodeTextDecoder,
 } from "util";
-import { fetch, Request, Response, Headers } from "@remix-run/web-fetch";
 import { AbortController as NodeAbortController } from "abort-controller";
 
 // https://reactjs.org/blog/2022/03/08/react-18-upgrade-guide.html#configuring-your-testing-environment
 globalThis.IS_REACT_ACT_ENVIRONMENT = true;
 
 if (!globalThis.fetch) {
-  // Built-in lib.dom.d.ts expects `fetch(Request | string, ...)` but the web
-  // fetch API allows a URL so @remix-run/web-fetch defines
-  // `fetch(string | URL | Request, ...)`
-  // @ts-expect-error
+  const { TextDecoder, TextEncoder } = require("node:util");
+  global.TextDecoder = TextDecoder;
+  global.TextEncoder = TextEncoder;
+
+  const { ReadableStream, WritableStream } = require("node:stream/web");
+  global.ReadableStream = ReadableStream;
+  global.WritableStream = WritableStream;
+
+  const { fetch, FormData, Request, Response, Headers } = require("undici");
+
   globalThis.fetch = fetch;
-  // Same as above, lib.dom.d.ts doesn't allow a URL to the Request constructor
-  // @ts-expect-error
   globalThis.Request = Request;
-  // web-std/fetch Response does not currently implement Response.error()
-  // @ts-expect-error
   globalThis.Response = Response;
   globalThis.Headers = Headers;
+
+  global.FormData = global.FormData || FormData;
 }
 
 if (!globalThis.AbortController) {
--- packages/router/__tests__/setup.ts
+++ packages/router/__tests__/setup.ts
@@ -1,32 +1,25 @@
-import {
-  TextEncoder as NodeTextEncoder,
-  TextDecoder as NodeTextDecoder,
-} from "util";
-import { fetch, Request, Response, Headers } from "@remix-run/web-fetch";
 import { AbortController as NodeAbortController } from "abort-controller";
 
 if (!globalThis.fetch) {
-  // Built-in lib.dom.d.ts expects `fetch(Request | string, ...)` but the web
-  // fetch API allows a URL so @remix-run/web-fetch defines
-  // `fetch(string | URL | Request, ...)`
-  // @ts-expect-error
+  const { TextDecoder, TextEncoder } = require("node:util");
+  global.TextDecoder = global.TextDecoder || TextDecoder;
+  global.TextEncoder = global.TextEncoder || TextEncoder;
+
+  const { ReadableStream, WritableStream } = require("node:stream/web");
+  global.ReadableStream = ReadableStream;
+  global.WritableStream = WritableStream;
+
+  const { fetch, FormData, Request, Response, Headers } = require("undici");
+
   globalThis.fetch = fetch;
-  // Same as above, lib.dom.d.ts doesn't allow a URL to the Request constructor
-  // @ts-expect-error
   globalThis.Request = Request;
-  // web-std/fetch Response does not currently implement Response.error()
-  // @ts-expect-error
   globalThis.Response = Response;
   globalThis.Headers = Headers;
+
+  global.FormData = global.FormData || FormData;
 }
 
 if (!globalThis.AbortController) {
   // @ts-expect-error
   globalThis.AbortController = NodeAbortController;
-}
-
-if (!globalThis.TextEncoder || !globalThis.TextDecoder) {
-  globalThis.TextEncoder = NodeTextEncoder;
-  // @ts-expect-error
-  globalThis.TextDecoder = NodeTextDecoder;
 }
