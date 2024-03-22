6b856bc2337386f954bd77e9ba1181fcda90da13
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2024-03-22 15:54:16 +1100

    Use shared Jest config (#11362)

--- packages/router/jest.config.js
+++ packages/router/jest.config.js
@@ -1,19 +1,5 @@
 /** @type {import('jest').Config} */
 module.exports = {
+  ...require("../../jest/jest.config.shared"),
   testEnvironment: "jsdom",
-  testMatch: ["**/__tests__/*-test.(js|ts)"],
-  transform: {
-    "\\.[jt]sx?$": "./jest-transformer.js",
-  },
-  setupFiles: ["./__tests__/setup.ts"],
-  moduleNameMapper: {
-    "^@remix-run/router$": "<rootDir>/index.ts",
-    "^@remix-run/web-blob$": require.resolve("@remix-run/web-blob"),
-    "^@remix-run/web-fetch$": require.resolve("@remix-run/web-fetch"),
-    "^@remix-run/web-form-data$": require.resolve("@remix-run/web-form-data"),
-    "^@remix-run/web-stream$": require.resolve("@remix-run/web-stream"),
-    "^@web3-storage/multipart-parser$": require.resolve(
-      "@web3-storage/multipart-parser"
-    ),
-  },
 };
