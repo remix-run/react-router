7a099db76e43ccb1923babef4943ed4b7e230723
Matt Brophy <matt@brophy.org>
2024-04-01 15:50:47 -0400

    Fix node 20 snapshot

--- packages/router/__tests__/navigation-test.ts
+++ packages/router/__tests__/navigation-test.ts
@@ -169,11 +169,13 @@ describe("navigations", () => {
         })
       );
       expect(t.router.state.loaderData).toEqual({});
-      expect(t.router.state.errors).toMatchInlineSnapshot(`
-        {
-          "foo": [SyntaxError: Unexpected token } in JSON at position 15],
-        }
-      `);
+
+      // Node 18 and 20 output different errors here
+      // Node 18 and 20 output different errors here
+      let expected = process.version.startsWith("18")
+        ? "Unexpected token } in JSON at position 15"
+        : "Unexpected non-whitespace character after JSON at position 15";
+      expect(t.router.state.errors?.foo).toEqual(new SyntaxError(expected));
     });
 
     it("bubbles errors when unwrapping Responses", async () => {
@@ -204,11 +206,12 @@ describe("navigations", () => {
         })
       );
       expect(t.router.state.loaderData).toEqual({});
-      expect(t.router.state.errors).toMatchInlineSnapshot(`
-        {
-          "root": [SyntaxError: Unexpected token } in JSON at position 15],
-        }
-      `);
+
+      // Node 18 and 20 output different errors here
+      let expected = process.version.startsWith("18")
+        ? "Unexpected token } in JSON at position 15"
+        : "Unexpected non-whitespace character after JSON at position 15";
+      expect(t.router.state.errors?.root).toEqual(new SyntaxError(expected));
     });
 
     it("does not fetch unchanging layout data", async () => {
