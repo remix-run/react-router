c5f53642a6f703e8b701f6e5310fdb770e8a4e07
Matt Brophy <matt@brophy.org>
2024-04-03 10:50:56 -0400

    Bring Remix ScrollRestoration logic into react-router-dom

--- packages/react-router-dom/index.tsx
+++ packages/react-router-dom/index.tsx
@@ -82,6 +82,7 @@ import {
   shouldProcessLinkClick,
 } from "./dom";
 
+import type { ScriptProps } from "./ssr/components";
 import { RemixContext } from "./ssr/components";
 import type {
   AssetsManifest,
@@ -1801,10 +1802,10 @@ if (__DEV__) {
   Form.displayName = "Form";
 }
 
-export interface ScrollRestorationProps {
+export type ScrollRestorationProps = ScriptProps & {
   getKey?: GetScrollRestorationKeyFunction;
   storageKey?: string;
-}
+};
 
 /**
  * This component will emulate the browser's scroll restoration on location
@@ -1813,9 +1814,64 @@ export interface ScrollRestorationProps {
 export function ScrollRestoration({
   getKey,
   storageKey,
+  ...props
 }: ScrollRestorationProps) {
+  let remixContext = React.useContext(RemixContext);
+  let location = useLocation();
+  let matches = useMatches();
   useScrollRestoration({ getKey, storageKey });
-  return null;
+
+  // In order to support `getKey`, we need to compute a "key" here so we can
+  // hydrate that up so that SSR scroll restoration isn't waiting on React to
+  // hydrate. *However*, our key on the server is not the same as our key on
+  // the client!  So if the user's getKey implementation returns the SSR
+  // location key, then let's ignore it and let our inline <script> below pick
+  // up the client side history state key
+  let key = React.useMemo(
+    () => {
+      if (!getKey) return null;
+      let userKey = getKey(location, matches);
+      return userKey !== location.key ? userKey : null;
+    },
+    // Nah, we only need this the first time for the SSR render
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    []
+  );
+
+  // In SPA Mode, there's nothing to restore on initial render since we didn't
+  // render anything on the server
+  if (!remixContext || remixContext.isSpaMode) {
+    return null;
+  }
+
+  let restoreScroll = ((storageKey: string, restoreKey: string) => {
+    if (!window.history.state || !window.history.state.key) {
+      let key = Math.random().toString(32).slice(2);
+      window.history.replaceState({ key }, "");
+    }
+    try {
+      let positions = JSON.parse(sessionStorage.getItem(storageKey) || "{}");
+      let storedY = positions[restoreKey || window.history.state.key];
+      if (typeof storedY === "number") {
+        window.scrollTo(0, storedY);
+      }
+    } catch (error: unknown) {
+      console.error(error);
+      sessionStorage.removeItem(storageKey);
+    }
+  }).toString();
+
+  return (
+    <script
+      {...props}
+      suppressHydrationWarning
+      dangerouslySetInnerHTML={{
+        __html: `(${restoreScroll})(${JSON.stringify(
+          storageKey || SCROLL_RESTORATION_STORAGE_KEY
+        )}, ${JSON.stringify(key)})`,
+      }}
+    />
+  );
 }
 
 if (__DEV__) {
