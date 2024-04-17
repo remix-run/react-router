f286ad7145012da36d6b3b25d1bee8bb43e2c500
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2024-04-18 08:50:08 +1000

    Add playgrounds (#11464)

---
+++ playground/compiler-spa/app/entry.server.tsx
@@ -0,0 +1,19 @@
+import type { EntryContext } from "@react-router/node";
+import { RemixServer } from "react-router-dom";
+import { renderToString } from "react-dom/server";
+
+export default function handleRequest(
+  request: Request,
+  responseStatusCode: number,
+  responseHeaders: Headers,
+  reactRouterContext: EntryContext
+) {
+  let html = renderToString(
+    <RemixServer context={reactRouterContext} url={request.url} />
+  );
+  html = "<!DOCTYPE html>\n" + html;
+  return new Response(html, {
+    headers: { "Content-Type": "text/html" },
+    status: responseStatusCode,
+  });
+}
---
+++ playground/compiler/app/entry.client.tsx
@@ -0,0 +1,12 @@
+import { RouterProvider } from "react-router-dom";
+import { startTransition, StrictMode } from "react";
+import { hydrateRoot } from "react-dom/client";
+
+startTransition(() => {
+  hydrateRoot(
+    document,
+    <StrictMode>
+      <RouterProvider />
+    </StrictMode>
+  );
+});
