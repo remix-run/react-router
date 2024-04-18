6b2b27139cb866d4295820447b1aed255e4c6d37
Matt Brophy <matt@brophy.org>
2024-04-18 12:10:15 -0400

    fix playgrounds

--- playground/compiler-express/app/entry.client.tsx
+++ playground/compiler-express/app/entry.client.tsx
@@ -1,4 +1,4 @@
-import { RouterProvider } from "react-router-dom";
+import { HydratedRouter } from "react-router-dom";
 import { startTransition, StrictMode } from "react";
 import { hydrateRoot } from "react-dom/client";
 
@@ -6,7 +6,7 @@ startTransition(() => {
   hydrateRoot(
     document,
     <StrictMode>
-      <RouterProvider />
+      <HydratedRouter />
     </StrictMode>
   );
 });
