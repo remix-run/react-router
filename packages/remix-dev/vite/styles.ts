remix-run/remix

Fix Vite critical CSS resolution for route files with absolute paths (#9194)

packages/remix-dev/vite/styles.ts
@@ -198,7 +198,7 @@ export const getStylesForUrl = async ({
   let appPath = path.relative(process.cwd(), remixConfig.appDirectory);
   let documentRouteFiles =
     matchRoutes(routes, url, build.basename)?.map((match) =>
-      path.join(appPath, remixConfig.routes[match.route.id].file)
+      path.resolve(appPath, remixConfig.routes[match.route.id].file)
     ) ?? [];
 
   let styles = await getStylesForFiles({

remix-run/react-router

packages/remix-dev/vite/styles.ts
@@ -198,7 +198,7 @@ export const getStylesForUrl = async ({
   let appPath = path.relative(process.cwd(), reactRouterConfig.appDirectory);
   let documentRouteFiles =
     matchRoutes(routes, url, build.basename)?.map((match) =>
-      path.join(appPath, reactRouterConfig.routes[match.route.id].file)
+      path.resolve(appPath, reactRouterConfig.routes[match.route.id].file)
     ) ?? [];
 
   let styles = await getStylesForFiles({