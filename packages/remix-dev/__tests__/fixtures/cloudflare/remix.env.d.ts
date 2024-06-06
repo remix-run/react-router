46ebf70a4e26ec2dee074a8364ae0c201cab30a7
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2024-06-06 14:36:13 +1000

    Remove unused integration test templates (#11618)

--- packages/remix-dev/__tests__/fixtures/cloudflare/remix.env.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference types="vite/client" />
-/// <reference types="@react-router/cloudflare" />
-/// <reference types="@cloudflare/workers-types" />
--- packages/remix-dev/__tests__/fixtures/cloudflare/server.ts
+++
@@ -1,9 +0,0 @@
-import { logDevReady } from "@react-router/cloudflare";
-import { createPagesFunctionHandler } from "@react-router/cloudflare-pages";
-import * as build from "@react-router/dev/server-build";
-
-if (process.env.NODE_ENV === "development") {
-  logDevReady(build);
-}
-
-export const onRequest = createPagesFunctionHandler({ build });
