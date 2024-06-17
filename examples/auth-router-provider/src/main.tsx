00c05a224c142dc10bd78504095ef251f683edeb
Vatsal Patel <vatsal.acct01@gmail.com>
2024-06-17 10:51:57 +0530

    Fixed syntax error in createRoot parameter

--- examples/auth-router-provider/src/main.tsx
+++ examples/auth-router-provider/src/main.tsx
@@ -4,7 +4,7 @@ import * as ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
