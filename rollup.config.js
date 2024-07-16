577b1281e18a4a0145901161de05eb28e8626221
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2024-07-16 15:34:16 +1000

    Port Remix Architect package

--- rollup.config.js
+++ rollup.config.js
@@ -4,6 +4,7 @@ const path = require("path");
 module.exports = function rollup(options) {
   return [
     "react-router",
+    "react-router-architect",
     "react-router-cloudflare",
     "react-router-dom",
     "react-router-dev",
