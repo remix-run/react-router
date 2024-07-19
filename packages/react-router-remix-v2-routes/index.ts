9f3955167d5e876b8592107a60cc727fc9553184
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2024-07-19 16:27:13 +1000

    routes.ts: Support Remix routes option

--- packages/react-router-remix-v2-routes/index.ts
+++ packages/react-router-remix-v2-routes/index.ts
@@ -1,21 +1,46 @@
 import fs from "node:fs";
 import path from "node:path";
-import type { RouteManifestFunction } from "@react-router/dev";
+import type { RouteManifest, RouteManifestFunction } from "@react-router/dev";
 
 import { fileRoutes } from "./fileRoutes";
+import { defineRoutes, type DefineRoutesFunction } from "./defineRoutes";
 
 export function remixRoutes({
   ignoredRouteFiles,
   rootDirectory = "routes",
+  routes: customRoutes,
 }: {
   ignoredRouteFiles?: string[];
   rootDirectory?: string;
+
+  /**
+   * A function for defining custom routes, in addition to those already defined
+   * using the filesystem convention in `app/routes`. Both sets of routes will
+   * be merged.
+   */
+  routes?: (
+    defineRoutes: DefineRoutesFunction
+  ) =>
+    | ReturnType<DefineRoutesFunction>
+    | Promise<ReturnType<DefineRoutesFunction>>;
 } = {}): RouteManifestFunction {
-  return ({ appDirectory }) => {
-    if (!fs.existsSync(path.resolve(appDirectory, rootDirectory))) {
-      return {};
+  return async ({ appDirectory }) => {
+    let routes: RouteManifest = {};
+
+    if (fs.existsSync(path.resolve(appDirectory, rootDirectory))) {
+      routes = {
+        ...routes,
+        ...fileRoutes(appDirectory, ignoredRouteFiles, rootDirectory),
+      };
     }
 
-    return fileRoutes(appDirectory, ignoredRouteFiles, rootDirectory);
+    if (customRoutes) {
+      routes = {
+        ...routes,
+        ...(await customRoutes(defineRoutes)),
+      };
+    }
+
+    return routes;
   };
 }
