2bf428d4835be007fb3433789252fac3599fb533
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2024-07-23 17:22:15 +1000

    routes.ts: Maintain last valid routes in dev

---
+++ integration/routes-config-test.ts
@@ -0,0 +1,91 @@
+import { expect } from "@playwright/test";
+
+import {
+  type Files,
+  createProject,
+  build,
+  test,
+  viteConfig,
+  createEditor,
+} from "./helpers/vite.js";
+
+const js = String.raw;
+
+test.describe("routes config", () => {
+  test("fails the build if config is invalid", async () => {
+    let cwd = await createProject({
+      "app/routes.ts": `export default INVALID(`,
+    });
+    let buildResult = build({ cwd });
+    expect(buildResult.status).toBe(1);
+    expect(buildResult.stderr.toString()).toContain("Route config is invalid");
+  });
+
+  test("fails the dev process if config is initially invalid", async ({
+    dev,
+  }) => {
+    let files: Files = async ({ port }) => ({
+      "vite.config.js": await viteConfig.basic({ port }),
+      "app/routes.ts": `export default INVALID(`,
+    });
+    let devError: Error | undefined;
+    try {
+      await dev(files);
+    } catch (error: any) {
+      devError = error;
+    }
+    expect(devError?.toString()).toContain("Route config is invalid");
+  });
+
+  test("supports correcting an invalid config", async ({ page, dev }) => {
+    let files: Files = async ({ port }) => ({
+      "vite.config.js": await viteConfig.basic({ port }),
+      "app/routes.ts": js`
+        import { defineRoutes } from "@react-router/dev";
+
+        export default defineRoutes([
+          {
+            file: "test-route-1.tsx",
+            index: true,
+          },
+        ]);
+      `,
+      "app/test-route-1.tsx": `
+        export default () => <div data-test-route>Test route 1</div>
+      `,
+      "app/test-route-2.tsx": `
+        export default () => <div data-test-route>Test route 2</div>
+      `,
+    });
+    let { cwd, port } = await dev(files);
+
+    await page.goto(`http://localhost:${port}/`, { waitUntil: "networkidle" });
+    await expect(page.locator("[data-test-route]")).toHaveText("Test route 1");
+
+    let edit = createEditor(cwd);
+
+    // Make config invalid
+    await edit("app/routes.ts", (contents) => contents + "INVALID");
+
+    // Ensure dev server is still running with old config + HMR
+    await edit("app/test-route-1.tsx", (contents) =>
+      contents.replace("Test route 1", "Test route 1 updated")
+    );
+    await expect(page.locator("[data-test-route]")).toHaveText(
+      "Test route 1 updated"
+    );
+
+    // Fix config with new route
+    await edit("app/routes.ts", (contents) =>
+      contents.replace("INVALID", "").replace("test-route-1", "test-route-2")
+    );
+
+    await expect(async () => {
+      // Reload to pick up new route for current path
+      await page.reload();
+      await expect(page.locator("[data-test-route]")).toHaveText(
+        "Test route 2"
+      );
+    }).toPass();
+  });
+});
