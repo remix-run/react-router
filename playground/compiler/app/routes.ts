b4c0f7078348a404935d60d5a008fb4c079063e4
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2024-08-22 14:50:48 +1000

    Migrate playground to routes.ts

--- playground/compiler/app/routes.ts
+++ playground/compiler/app/routes.ts
@@ -1,3 +1,7 @@
-import { type RoutesConfig, index } from "@react-router/dev/routes";
+import { type RoutesConfig, route, index } from "@react-router/dev/routes";
 
-export const routes: RoutesConfig = [index("routes/_index.tsx")];
+export const routes: RoutesConfig = [
+  index("routes/_index.tsx"),
+  route("/chunkable", "routes/chunkable/route.tsx"),
+  route("/unchunkable", "routes/unchunkable.tsx"),
+];
