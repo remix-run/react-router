e54e8cabf81087971780a264d5e46ebdf4b72a06
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2024-08-26 19:33:12 +1000

    Add initial route chunk generation logic

--- packages/react-router-dev/vite/route-chunks-test.ts
+++ packages/react-router-dev/vite/route-chunks-test.ts
@@ -1,117 +1,279 @@
 import dedent from "dedent";
 
 import {
-  getTopLevelStatementsByExportName,
   hasChunkableExport,
+  getChunkedExport,
+  omitChunkedExports,
 } from "./route-chunks";
 
-describe("hasChunkableExport", () => {
-  function chunkable(code: string, exportName: string) {
-    const topLevelStatementsByExport = getTopLevelStatementsByExportName(code);
-    return hasChunkableExport(topLevelStatementsByExport, exportName);
-  }
-
+describe("route chunks", () => {
   describe("chunkable", () => {
-    test("default function export with no identifiers", () => {
+    test("functions with no identifiers", () => {
       const code = dedent`
-        export default function () {
-          return null;
-        }
-
-        export const other = () => {
-          return null;
-        }
+        export default function () { return null; }
+        export function target1() { return null; }
+        export function other1() { return null; }
+        export const target2 = () => null;
+        export const other2 = () => null;
       `;
-      expect(chunkable(code, "default")).toBe(true);
+      expect(hasChunkableExport(code, "default")).toBe(true);
+      expect(hasChunkableExport(code, "target1")).toBe(true);
+      expect(hasChunkableExport(code, "target2")).toBe(true);
+      expect(getChunkedExport(code, "default")?.code).toMatchInlineSnapshot(`
+        "export default function () {
+          return null;
+        }"
+      `);
+      expect(getChunkedExport(code, "target1")?.code).toMatchInlineSnapshot(`
+        "export function target1() {
+          return null;
+        }"
+      `);
+      expect(getChunkedExport(code, "target2")?.code).toMatchInlineSnapshot(
+        `"export const target2 = () => null;"`
+      );
+      expect(omitChunkedExports(code, ["default", "target1", "target2"])?.code)
+        .toMatchInlineSnapshot(`
+        "export function other1() {
+          return null;
+        }
+        export const other2 = () => null;"
+      `);
     });
 
-    test("const function containing no identifiers", () => {
+    test("functions referencing their own identifiers", () => {
       const code = dedent`
-        export const target = () => {
-          return null;
-        }
+        import { targetMessage1 } from "./targetMessage1";
+        import { targetMessage2 } from "./targetMessage2";
+        import { otherMessage1 } from "./otherMessage1";
+        import { otherMessage2 } from "./otherMessage2";
 
-        export const other = () => {
-          return null;
-        }
+        const getTargetMessage1 = () => targetMessage1;
+        const getOtherMessage1 = () => otherMessage1;
+        function getTargetMessage2() { return targetMessage2; }
+        function getOtherMessage2() { return otherMessage2; }
+
+        export function target1() { return getTargetMessage1(); }
+        export function other1() { return getOtherMessage1(); }
+        export const target2 = () => getTargetMessage2();
+        export const other2 = () => getOtherMessage2();
       `;
-      expect(chunkable(code, "target")).toBe(true);
+      expect(hasChunkableExport(code, "target1")).toBe(true);
+      expect(hasChunkableExport(code, "target2")).toBe(true);
+      expect(getChunkedExport(code, "target1")?.code).toMatchInlineSnapshot(`
+        "import { targetMessage1 } from "./targetMessage1";
+        const getTargetMessage1 = () => targetMessage1;
+        export function target1() {
+          return getTargetMessage1();
+        }"
+      `);
+      expect(getChunkedExport(code, "target2")?.code).toMatchInlineSnapshot(`
+        "import { targetMessage2 } from "./targetMessage2";
+        function getTargetMessage2() {
+          return targetMessage2;
+        }
+        export const target2 = () => getTargetMessage2();"
+      `);
+      expect(omitChunkedExports(code, ["target1", "target2"])?.code)
+        .toMatchInlineSnapshot(`
+        "import { otherMessage1 } from "./otherMessage1";
+        import { otherMessage2 } from "./otherMessage2";
+        const getOtherMessage1 = () => otherMessage1;
+        function getOtherMessage2() {
+          return otherMessage2;
+        }
+        export function other1() {
+          return getOtherMessage1();
+        }
+        export const other2 = () => getOtherMessage2();"
+      `);
+    });
+  });
+
+  describe("partially chunkable", () => {
+    test("function with no identifiers, one with shared identifiers", () => {
+      const code = dedent`
+      import { sharedMessage } from "./sharedMessage";
+      export default function () { return null; }
+      export function target1() { return null; }
+      export const target2 = () => sharedMessage;
+      export const other1 = () => sharedMessage;
+      export const other2 = () => sharedMessage;
+    `;
+      expect(hasChunkableExport(code, "default")).toBe(true);
+      expect(hasChunkableExport(code, "target1")).toBe(true);
+      expect(hasChunkableExport(code, "target2")).toBe(false);
+      expect(getChunkedExport(code, "default")?.code).toMatchInlineSnapshot(`
+        "export default function () {
+          return null;
+        }"
+      `);
+      expect(getChunkedExport(code, "target1")?.code).toMatchInlineSnapshot(`
+        "export function target1() {
+          return null;
+        }"
+      `);
+      expect(getChunkedExport(code, "target2")).toBeUndefined();
+      expect(omitChunkedExports(code, ["default", "target1", "target2"])?.code)
+        .toMatchInlineSnapshot(`
+        "import { sharedMessage } from "./sharedMessage";
+        export const target2 = () => sharedMessage;
+        export const other1 = () => sharedMessage;
+        export const other2 = () => sharedMessage;"
+      `);
     });
 
-    test("const function referencing its own identifiers", () => {
+    test("function referencing its own identifiers, another one sharing an identifier", () => {
       const code = dedent`
-        import { targetMessage } from "./targetMessage";
-        const getTargetMessage = () => targetMessage;
-        export const target = () => getTargetMessage();
+        import { targetMessage1 } from "./targetMessage1";
+        import { sharedMessage } from "./sharedMessage";
 
-        import { otherMessage } from "./otherMessage";
-        const getOtherMessage = () => otherMessage;
-        export const other = () => getOtherMessage();
+        const getTargetMessage1 = () => targetMessage1;
+        const getOtherMessage1 = () => sharedMessage;
+        function getTargetMessage2() { return sharedMessage; }
+        function getOtherMessage2() { return sharedMessage; }
+
+        export function target1() { return getTargetMessage1(); }
+        export function other1() { return getOtherMessage1(); }
+        export const target2 = () => getTargetMessage2();
+        export const other2 = () => getOtherMessage2();
       `;
-      expect(chunkable(code, "target")).toBe(true);
-    });
-
-    test("function declaration referencing its own identifiers", () => {
-      const code = dedent`
-        import { targetMessage } from "./targetMessage";
-        const getTargetMessage = () => targetMessage;
-        export function target() { return getTargetMessage(); }
-
-        import { otherMessage } from "./otherMessage";
-        const getOtherMessage = () => otherMessage;
-        export function other() { return getOtherMessage(); }
-      `;
-      expect(chunkable(code, "target")).toBe(true);
+      expect(hasChunkableExport(code, "target1")).toBe(true);
+      expect(hasChunkableExport(code, "target2")).toBe(false);
+      expect(getChunkedExport(code, "target1")?.code).toMatchInlineSnapshot(`
+        "import { targetMessage1 } from "./targetMessage1";
+        const getTargetMessage1 = () => targetMessage1;
+        export function target1() {
+          return getTargetMessage1();
+        }"
+      `);
+      expect(getChunkedExport(code, "target2")).toBeUndefined();
+      expect(omitChunkedExports(code, ["target1", "target2"])?.code)
+        .toMatchInlineSnapshot(`
+        "import { sharedMessage } from "./sharedMessage";
+        const getOtherMessage1 = () => sharedMessage;
+        function getTargetMessage2() {
+          return sharedMessage;
+        }
+        function getOtherMessage2() {
+          return sharedMessage;
+        }
+        export function other1() {
+          return getOtherMessage1();
+        }
+        export const target2 = () => getTargetMessage2();
+        export const other2 = () => getOtherMessage2();"
+      `);
     });
   });
 
   describe("not chunkable", () => {
-    test("export not present", () => {
+    test("exports not present", () => {
       const code = dedent`
         export default function () {}
       `;
-      expect(chunkable(code, "target")).toBe(false);
+      expect(hasChunkableExport(code, "target1")).toBe(false);
+      expect(getChunkedExport(code, "target1")).toBeUndefined();
+      expect(hasChunkableExport(code, "target2")).toBe(false);
+      expect(getChunkedExport(code, "target2")).toBeUndefined();
+      expect(
+        omitChunkedExports(code, ["target1", "target2"])?.code
+      ).toMatchInlineSnapshot(`"export default function () {}"`);
     });
 
-    test("const function interacting with shared import", () => {
+    test("functions sharing a variable", () => {
       const code = dedent`
-        import { sharedMessage } from "./sharedMessage";
-
-        const targetMessage = sharedMessage;
-        const getTargetMessage = () => targetMessage;
-        export const target = () => getTargetMessage();
-
-        const otherMessage = sharedMessage;
-        const getOtherMessage = () => otherMessage;
-        export const other = () => getOtherMessage();
+        const sharedMessage = "shared";
+        const getTargetMessage1 = () => sharedMessage;
+        const getTargetMessage2 = () => sharedMessage;
+        const getOtherMessage1 = () => sharedMessage;
+        const getOtherMessage2 = () => sharedMessage;
+        export const target1 = () => getTargetMessage1();
+        export const target2 = () => getTargetMessage2();
+        export const other1 = () => getOtherMessage1();
+        export const other2 = () => getOtherMessage2();
       `;
-      expect(chunkable(code, "target")).toBe(false);
+      expect(hasChunkableExport(code, "target1")).toBe(false);
+      expect(getChunkedExport(code, "target1")).toBeUndefined();
+      expect(hasChunkableExport(code, "target2")).toBe(false);
+      expect(getChunkedExport(code, "target2")).toBeUndefined();
+      expect(omitChunkedExports(code, ["target1", "target2"])?.code)
+        .toMatchInlineSnapshot(`
+        "const sharedMessage = "shared";
+        const getTargetMessage1 = () => sharedMessage;
+        const getTargetMessage2 = () => sharedMessage;
+        const getOtherMessage1 = () => sharedMessage;
+        const getOtherMessage2 = () => sharedMessage;
+        export const target1 = () => getTargetMessage1();
+        export const target2 = () => getTargetMessage2();
+        export const other1 = () => getOtherMessage1();
+        export const other2 = () => getOtherMessage2();"
+      `);
     });
 
-    test("const function interacting with shared import statement", () => {
+    test("functions sharing an import statement", () => {
       const code = dedent`
-        import { targetMessage, otherMessage } from "./messages";
-
-        const getTargetMessage = () => targetMessage;
-        export const target = () => getTargetMessage();
-
-        const getOtherMessage = () => otherMessage;
-        export const other = () => getOtherMessage();
+        import {
+          targetMessage1,
+          targetMessage2,
+          otherMessage1,
+          otherMessage2
+        } from "./messages";
+        const getTargetMessage1 = () => targetMessage1;
+        const getTargetMessage2 = () => targetMessage2;
+        const getOtherMessage1 = () => otherMessage1;
+        const getOtherMessage2 = () => otherMessage2;
+        export const target1 = () => getTargetMessage1();
+        export const target2 = () => getTargetMessage2();
+        export const other1 = () => getOtherMessage1();
+        export const other2 = () => getOtherMessage2();
       `;
-      expect(chunkable(code, "target")).toBe(false);
+      expect(hasChunkableExport(code, "target1")).toBe(false);
+      expect(getChunkedExport(code, "target1")).toBeUndefined();
+      expect(hasChunkableExport(code, "target2")).toBe(false);
+      expect(getChunkedExport(code, "target2")).toBeUndefined();
+      expect(omitChunkedExports(code, ["target1", "target2"])?.code)
+        .toMatchInlineSnapshot(`
+        "import { targetMessage1, targetMessage2, otherMessage1, otherMessage2 } from "./messages";
+        const getTargetMessage1 = () => targetMessage1;
+        const getTargetMessage2 = () => targetMessage2;
+        const getOtherMessage1 = () => otherMessage1;
+        const getOtherMessage2 = () => otherMessage2;
+        export const target1 = () => getTargetMessage1();
+        export const target2 = () => getTargetMessage2();
+        export const other1 = () => getOtherMessage1();
+        export const other2 = () => getOtherMessage2();"
+      `);
     });
 
-    test("const function interacting with shared named import", () => {
+    test("functions sharing a named import", () => {
       const code = dedent`
         import * as messages from "./messages";
-
-        const getTargetMessage = () => messages.targetMessage;
-        export const target = () => getTargetMessage();
-
-        const getOtherMessage = () => messages.otherMessage;
-        export const other = () => getOtherMessage();
+        const getTargetMessage1 = () => messages.targetMessage1;
+        const getTargetMessage2 = () => messages.targetMessage2;
+        const getOtherMessage1 = () => messages.otherMessage1;
+        const getOtherMessage2 = () => messages.otherMessage2;
+        export const target1 = () => getTargetMessage1();
+        export const target2 = () => getTargetMessage2();
+        export const other1 = () => getOtherMessage1();
+        export const other2 = () => getOtherMessage2();
       `;
-      expect(chunkable(code, "target")).toBe(false);
+      expect(hasChunkableExport(code, "target1")).toBe(false);
+      expect(getChunkedExport(code, "target1")).toBeUndefined();
+      expect(hasChunkableExport(code, "target2")).toBe(false);
+      expect(getChunkedExport(code, "target2")).toBeUndefined();
+      expect(omitChunkedExports(code, ["target1", "target2"])?.code)
+        .toMatchInlineSnapshot(`
+        "import * as messages from "./messages";
+        const getTargetMessage1 = () => messages.targetMessage1;
+        const getTargetMessage2 = () => messages.targetMessage2;
+        const getOtherMessage1 = () => messages.otherMessage1;
+        const getOtherMessage2 = () => messages.otherMessage2;
+        export const target1 = () => getTargetMessage1();
+        export const target2 = () => getTargetMessage2();
+        export const other1 = () => getOtherMessage1();
+        export const other2 = () => getOtherMessage2();"
+      `);
     });
   });
 });
