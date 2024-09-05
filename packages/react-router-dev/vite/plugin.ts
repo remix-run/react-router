a847ff42dd4c313a83704ea49d7f0553ed7b9967
Matt Brophy <matt@brophy.org>
2024-09-05 11:57:14 -0400

    handle cycles between manual chunks (Remix PR 9917)

--- packages/react-router-dev/vite/plugin.ts
+++ packages/react-router-dev/vite/plugin.ts
@@ -265,13 +265,13 @@ function resolveDependantChunks(
       return;
     }
 
+    chunks.add(chunk);
+
     if (chunk.imports) {
       for (let importKey of chunk.imports) {
         walk(viteManifest[importKey]);
       }
     }
-
-    chunks.add(chunk);
   }
 
   for (let entryChunk of entryChunks) {
