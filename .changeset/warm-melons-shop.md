b3b1e58f7bcd3108eb18a6b6d6219c058c2e56ba
Matt Brophy <matt@brophy.org>
2024-09-10 10:53:45 -0400

    Stabilize unstable_patchRoutesOnNavigation (#11970)

---
+++ .changeset/warm-melons-shop.md
@@ -0,0 +1,5 @@
+---
+"react-router": minor
+---
+
+Stabilize `unstable_patchRoutesOnNavigation`
--- packages/react-router/lib/dom/lib.tsx
+++ packages/react-router/lib/dom/lib.tsx
@@ -123,7 +123,7 @@ interface DOMRouterOpts {
   future?: Partial<FutureConfig>;
   hydrationData?: HydrationState;
   unstable_dataStrategy?: DataStrategyFunction;
-  unstable_patchRoutesOnNavigation?: PatchRoutesOnNavigationFunction;
+  patchRoutesOnNavigation?: PatchRoutesOnNavigationFunction;
   window?: Window;
 }
 
@@ -142,7 +142,7 @@ export function createBrowserRouter(
     routes,
     mapRouteProperties,
     unstable_dataStrategy: opts?.unstable_dataStrategy,
-    unstable_patchRoutesOnNavigation: opts?.unstable_patchRoutesOnNavigation,
+    patchRoutesOnNavigation: opts?.patchRoutesOnNavigation,
     window: opts?.window,
   }).initialize();
 }
@@ -162,7 +162,7 @@ export function createHashRouter(
     routes,
     mapRouteProperties,
     unstable_dataStrategy: opts?.unstable_dataStrategy,
-    unstable_patchRoutesOnNavigation: opts?.unstable_patchRoutesOnNavigation,
+    patchRoutesOnNavigation: opts?.patchRoutesOnNavigation,
     window: opts?.window,
   }).initialize();
 }
