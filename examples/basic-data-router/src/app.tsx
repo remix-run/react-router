e2f4c5081daadd0f8e48e6a8d72a6cfbcac11dca
Matt Brophy <matt@brophy.org>
2024-09-11 16:20:00 -0400

    Revert changes to example app

--- examples/basic-data-router/src/app.tsx
+++ examples/basic-data-router/src/app.tsx
@@ -1,10 +1,7 @@
-import * as React from "react";
 import {
-  Await,
   createBrowserRouter,
   RouterProvider,
-  useAsyncError,
-  useFetcher,
+  useLoaderData,
 } from "react-router-dom";
 
 import "./index.css";
@@ -12,71 +9,13 @@ import "./index.css";
 let router = createBrowserRouter([
   {
     path: "/",
+    loader: () => ({ message: "Hello Data Router!" }),
     Component() {
-      let fetcher = useFetcher();
-      return (
-        <>
-          <p>fetcher state: {fetcher.state}</p>
-          <button onClick={() => fetcher.load("/fetch")}>Load</button>
-          <br />
-          <br />
-          <br />
-          <button onClick={() => fetcher.abort()}>fetcher.abort()</button>{" "}
-          <button onClick={() => fetcher.abort({ data: fetcher.data })}>
-            {"fetcher.abort({ data:  })"}
-          </button>{" "}
-          <button onClick={() => fetcher.abort({ reason: new Error("oh no") })}>
-            {"fetcher.abort({ reason })"}
-          </button>{" "}
-          <button
-            onClick={() =>
-              fetcher.abort({ data: fetcher.data, reason: new Error("oh no") })
-            }
-          >
-            {"fetcher.abort({ data, reason })"}
-          </button>
-          <br />
-          {fetcher.data ? (
-            <>
-              <p>fetcher critical data: {fetcher.data?.critical}</p>
-              <p>
-                fetcher lazy data:{" "}
-                <React.Suspense fallback={"Loading..."}>
-                  <Await
-                    resolve={fetcher.data.lazy}
-                    errorElement={<ErrorElement />}
-                  >
-                    {(val) => val}
-                  </Await>
-                </React.Suspense>
-              </p>
-            </>
-          ) : null}
-        </>
-      );
-    },
-  },
-  {
-    path: "/fetch",
-    async loader({ request }) {
-      await new Promise((r) => setTimeout(r, 1000));
-      return {
-        critical: "CRITICAL",
-        lazy: new Promise((resolve, reject) => {
-          request.signal.addEventListener("abort", () =>
-            reject(request.signal.reason)
-          );
-          setTimeout(() => resolve("LAZY"), 3000);
-        }),
-      };
+      let data = useLoaderData() as { message: string };
+      return <h1>{data.message}</h1>;
     },
   },
 ]);
-
-function ErrorElement() {
-  let error = useAsyncError();
-  return <>Error! {error.message}</>;
-}
 
 export default function App() {
   return <RouterProvider router={router} fallbackElement={<p>Loading...</p>} />;
