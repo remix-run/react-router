7d1a62301af672f05a1335918535679de4a11079
Pedro Cattori <pcattori@gmail.com>
2024-09-19 11:37:42 -0400

    update playground for typegen

--- playground/compiler/app/routes.ts
+++ playground/compiler/app/routes.ts
@@ -1,3 +1,9 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
-export const routes: RouteConfig = [index("routes/_index.tsx")];
+export const routes: RouteConfig = [
+  index("routes/_index.tsx"),
+  route("marketing/:id?", "routes/marketing.tsx", [
+    route("another/:id?", "routes/another.tsx"),
+    route("blah/:blah", "routes/blah.tsx"),
+  ])
+];
