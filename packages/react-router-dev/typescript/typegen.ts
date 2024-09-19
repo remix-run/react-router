eed704c5c0ce1a12f24c0e4bd7066139c80f71c4
Pedro Cattori <pcattori@gmail.com>
2024-09-18 22:36:09 -0400

    params typegen

--- packages/react-router-dev/typescript/typegen.ts
+++ packages/react-router-dev/typescript/typegen.ts
@@ -1,6 +1,9 @@
+import fs from "node:fs";
+
 import Chokidar from "chokidar";
+import dedent from "dedent";
 import * as Path from "pathe";
-import glob from "fast-glob";
+import * as Pathe from "pathe/utils";
 
 import {
   configRoutesToRouteManifest,
@@ -21,6 +24,7 @@ function getDirectory(ctx: Context) {
 export function getPath(ctx: Context, route: RouteManifestEntry): string {
   return Path.join(
     getDirectory(ctx),
+    "app",
     Path.dirname(route.file),
     "+types." + Path.basename(route.file)
   );
@@ -70,35 +74,93 @@ export async function typegenAll(
   ctx: Context,
   routes: RouteManifest
 ): Promise<void> {
-  const actual = new Set(
-    await glob(`**/*`, {
-      cwd: getDirectory(ctx),
-      absolute: true,
-    })
-  );
-  const expected = new Set(
-    Object.values(routes).map((route) =>
-      Path.join(ctx.appDirectory, route.file)
-    )
-  );
-
-  const remove = setDifference(actual, expected);
-  if (remove.size > 0) {
-    console.log(`TODO typegen remove: ${Array.from(remove).join(",")}`);
-  }
-
-  const add = setDifference(expected, actual);
-  if (add.size > 0) {
-    console.log(`TODO typegen add: ${Array.from(add).join(",")}`);
-  }
+  fs.rmSync(getDirectory(ctx), { recursive: true, force: true });
+  Object.values(routes).forEach((route) => {
+    const typesPath = getPath(ctx, route);
+    const content = getModule(routes, route);
+    fs.mkdirSync(Path.dirname(typesPath), { recursive: true });
+    fs.writeFileSync(typesPath, content);
+  });
 }
 
-function setDifference<T>(a: Set<T>, b: Set<T>) {
-  const diff = new Set<T>();
-  a.forEach((value) => {
-    if (!b.has(value)) {
-      diff.add(value);
+function getModule(routes: RouteManifest, route: RouteManifestEntry): string {
+  return dedent`
+    // typegen: ${route.file}
+
+    type Route = typeof import("./${Pathe.filename(route.file)}")
+
+    export type Params = {${paramsTypeBody(routes, route)}}
+
+    export type LinksArgs = {} // TODO
+    export type LinksReturn = {} // TODO
+
+    export type ServerLoaderArgs = {} // TODO
+    export type ClientLoaderArgs = {} // TODO
+    export type LoaderData = {} // TODO
+
+    export type HydrateFallbackArgs = {} // TODO
+    export type HydrateFallbackReturn = import("react").ReactNode
+
+    export type ServerActionArgs = {} // TODO
+    export type ClientActionArgs = {} // TODO
+    export type ActionData = {} // TODO
+
+    export type DefaultArgs = {} // TODO
+    export type DefaultReturn = import("react").ReactNode
+
+    export type ErrorBoundaryArgs = {} // TODO
+    export type ErrorBoundaryReturn = import("react").ReactNode
+  `;
+}
+
+function paramsTypeBody(routes: RouteManifest, route: RouteManifestEntry) {
+  const urlpath = routeLineage(routes, route)
+    .map((route) => route.path)
+    .join("/");
+  const params = parseParams(urlpath);
+  const indent = "  ".repeat(3);
+  const paramTypeFields = Object.entries(params).map(([name, values]) => {
+    if (values.length === 1) {
+      const isOptional = values[0];
+      return isOptional
+        ? `${indent}${name}?: string`
+        : `${indent}${name}: string`;
     }
+    const items = values.map((isOptional) =>
+      isOptional ? "string | undefined" : "string"
+    );
+    return `${indent}${name}: [${items.join(", ")}]`;
   });
-  return diff;
+  return paramTypeFields.length ? "\n" + paramTypeFields.join("\n") + "\n" : "";
+}
+
+function routeLineage(routes: RouteManifest, route: RouteManifestEntry) {
+  const result: RouteManifestEntry[] = [];
+  while (route) {
+    result.push(route);
+    if (!route.parentId) break;
+    route = routes[route.parentId];
+  }
+  result.reverse();
+  return result;
+}
+
+function parseParams(urlpath: string) {
+  const result: Record<string, boolean[]> = {};
+
+  let segments = urlpath.split("/");
+  segments
+    .filter((s) => s.startsWith(":"))
+    .forEach((param) => {
+      param = param.slice(1); // omit leading `:`
+      let isOptional = param.endsWith("?");
+      if (isOptional) {
+        param = param.slice(0, -1); // omit trailing `?`
+      }
+
+      result[param] ??= [];
+      result[param].push(isOptional);
+      return;
+    });
+  return result;
 }
