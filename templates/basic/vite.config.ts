003f09a3a5784606dcedc91db3afd0b70c849e69
Brooks Lybrand <brookslybrand@gmail.com>
2024-10-03 14:22:03 -0500

    Update template to use pre-release tags (#12061)

--- templates/basic/vite.config.ts
+++ templates/basic/vite.config.ts
@@ -4,8 +4,5 @@ import tsconfigPaths from "vite-tsconfig-paths";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  optimizeDeps: {
-    include: ["react", "react-dom"],
-  },
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
 });
