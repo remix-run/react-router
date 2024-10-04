4131c27eeaa5f225ebcfd976f8d19222abc01c66
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2024-10-04 16:48:22 +1000

    Add route chunks playground

--- playground/route-chunks/app/routes/chunkable.tsx
+++ playground/route-chunks/app/routes/chunkable.tsx
@@ -1,8 +1,7 @@
 import { useLoaderData } from "react-router";
 
-export const clientLoader = async () => {
-  await new Promise((resolve) => setTimeout(resolve, 1000));
-  return "hello from chunkable client loader!";
+export const clientLoader = () => {
+  return "Hello from chunkable client loader";
 };
 
 export default function Hello() {
--- playground/route-chunks/app/routes/unchunkable.tsx
+++ playground/route-chunks/app/routes/unchunkable.tsx
@@ -3,9 +3,8 @@ import { useLoaderData } from "react-router";
 // Dummy variable to prevent route exports from being chunked
 let shared: null = null;
 
-export const clientLoader = async () => {
-  await new Promise((resolve) => setTimeout(resolve, 1000));
-  return shared ?? "hello from unchunkable client loader!";
+export const clientLoader = () => {
+  return shared ?? "Hello from unchunkable client loader";
 };
 
 export default function Hello() {
