5201677cf5aa754a248d581814e2af0206d10144
Brooks Lybrand <brookslybrand@gmail.com>
2024-10-04 11:00:06 -0500

    revert to tailwind 3

--- templates/basic/vite.config.ts
+++ templates/basic/vite.config.ts
@@ -1,8 +1,7 @@
-import tailwindcss from "@tailwindcss/vite";
 import { reactRouter } from "@react-router/dev/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  plugins: [reactRouter(), tsconfigPaths()],
 });
