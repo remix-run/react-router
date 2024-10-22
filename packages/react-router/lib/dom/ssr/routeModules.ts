4a84f79a21fdc7bb1396fb7e74aecf44646d42fa
Matt Brophy <matt@brophy.org>
2024-10-22 14:45:29 -0400

    Dedup meta/links types

--- packages/react-router/lib/dom/ssr/routeModules.ts
+++ packages/react-router/lib/dom/ssr/routeModules.ts
@@ -152,6 +152,56 @@ export interface MetaArgs<
   error?: unknown;
 }
 
+/**
+ * A function that returns an array of data objects to use for rendering
+ * metadata HTML tags in a route. These tags are not rendered on descendant
+ * routes in the route hierarchy. In other words, they will only be rendered on
+ * the route in which they are exported.
+ *
+ * @param Loader - The type of the current route's loader function
+ * @param MatchLoaders - Mapping from a parent route's filepath to its loader
+ * function type
+ *
+ * Note that parent route filepaths are relative to the `app/` directory.
+ *
+ * For example, if this meta function is for `/sales/customers/$customerId`:
+ *
+ * ```ts
+ * // app/root.tsx
+ * const loader = () => ({ hello: "world" })
+ * export type Loader = typeof loader
+ *
+ * // app/routes/sales.tsx
+ * const loader = () => ({ salesCount: 1074 })
+ * export type Loader = typeof loader
+ *
+ * // app/routes/sales/customers.tsx
+ * const loader = () => ({ customerCount: 74 })
+ * export type Loader = typeof loader
+ *
+ * // app/routes/sales/customers/$customersId.tsx
+ * import type { Loader as RootLoader } from "../../../root"
+ * import type { Loader as SalesLoader } from "../../sales"
+ * import type { Loader as CustomersLoader } from "../../sales/customers"
+ *
+ * const loader = () => ({ name: "Customer name" })
+ *
+ * const meta: MetaFunction<typeof loader, {
+ *  "root": RootLoader,
+ *  "routes/sales": SalesLoader,
+ *  "routes/sales/customers": CustomersLoader,
+ * }> = ({ data, matches }) => {
+ *   const { name } = data
+ *   //      ^? string
+ *   const { customerCount } = matches.find((match) => match.id === "routes/sales/customers").data
+ *   //      ^? number
+ *   const { salesCount } = matches.find((match) => match.id === "routes/sales").data
+ *   //      ^? number
+ *   const { hello } = matches.find((match) => match.id === "root").data
+ *   //      ^? "world"
+ * }
+ * ```
+ */
 export interface MetaFunction<
   Loader extends LoaderFunction | unknown = unknown,
   MatchLoaders extends Record<string, LoaderFunction | unknown> = Record<
--- packages/react-router/lib/server-runtime/routeModules.ts
+++ packages/react-router/lib/server-runtime/routeModules.ts
@@ -105,129 +105,6 @@ export interface HeadersFunction {
   (args: HeadersArgs): Headers | HeadersInit;
 }
 
-/**
- * A function that defines `<link>` tags to be inserted into the `<head>` of
- * the document on route transitions.
- */
-export interface LinksFunction {
-  (): LinkDescriptor[];
-}
-
-/**
- * A function that returns an array of data objects to use for rendering
- * metadata HTML tags in a route. These tags are not rendered on descendant
- * routes in the route hierarchy. In other words, they will only be rendered on
- * the route in which they are exported.
- *
- * @param Loader - The type of the current route's loader function
- * @param MatchLoaders - Mapping from a parent route's filepath to its loader
- * function type
- *
- * Note that parent route filepaths are relative to the `app/` directory.
- *
- * For example, if this meta function is for `/sales/customers/$customerId`:
- *
- * ```ts
- * // app/root.tsx
- * const loader = () => ({ hello: "world" })
- * export type Loader = typeof loader
- *
- * // app/routes/sales.tsx
- * const loader = () => ({ salesCount: 1074 })
- * export type Loader = typeof loader
- *
- * // app/routes/sales/customers.tsx
- * const loader = () => ({ customerCount: 74 })
- * export type Loader = typeof loader
- *
- * // app/routes/sales/customers/$customersId.tsx
- * import type { Loader as RootLoader } from "../../../root"
- * import type { Loader as SalesLoader } from "../../sales"
- * import type { Loader as CustomersLoader } from "../../sales/customers"
- *
- * const loader = () => ({ name: "Customer name" })
- *
- * const meta: MetaFunction<typeof loader, {
- *  "root": RootLoader,
- *  "routes/sales": SalesLoader,
- *  "routes/sales/customers": CustomersLoader,
- * }> = ({ data, matches }) => {
- *   const { name } = data
- *   //      ^? string
- *   const { customerCount } = matches.find((match) => match.id === "routes/sales/customers").data
- *   //      ^? number
- *   const { salesCount } = matches.find((match) => match.id === "routes/sales").data
- *   //      ^? number
- *   const { hello } = matches.find((match) => match.id === "root").data
- *   //      ^? "world"
- * }
- * ```
- */
-export interface ServerRuntimeMetaFunction<
-  Loader extends LoaderFunction | unknown = unknown,
-  ParentsLoaders extends Record<string, LoaderFunction | unknown> = Record<
-    string,
-    unknown
-  >
-> {
-  (
-    args: ServerRuntimeMetaArgs<Loader, ParentsLoaders>
-  ): ServerRuntimeMetaDescriptor[];
-}
-
-interface ServerRuntimeMetaMatch<
-  RouteId extends string = string,
-  Loader extends LoaderFunction | unknown = unknown
-> {
-  id: RouteId;
-  pathname: AgnosticRouteMatch["pathname"];
-  data: Loader extends LoaderFunction ? SerializeFrom<Loader> : unknown;
-  handle?: RouteHandle;
-  params: AgnosticRouteMatch["params"];
-  meta: ServerRuntimeMetaDescriptor[];
-  error?: unknown;
-}
-
-type ServerRuntimeMetaMatches<
-  MatchLoaders extends Record<string, LoaderFunction | unknown> = Record<
-    string,
-    unknown
-  >
-> = Array<
-  {
-    [K in keyof MatchLoaders]: ServerRuntimeMetaMatch<
-      Exclude<K, number | symbol>,
-      MatchLoaders[K]
-    >;
-  }[keyof MatchLoaders]
->;
-
-export interface ServerRuntimeMetaArgs<
-  Loader extends LoaderFunction | unknown = unknown,
-  MatchLoaders extends Record<string, LoaderFunction | unknown> = Record<
-    string,
-    unknown
-  >
-> {
-  data:
-    | (Loader extends LoaderFunction ? SerializeFrom<Loader> : AppData)
-    | undefined;
-  params: Params;
-  location: Location;
-  matches: ServerRuntimeMetaMatches<MatchLoaders>;
-  error?: unknown;
-}
-
-export type ServerRuntimeMetaDescriptor =
-  | { charSet: "utf-8" }
-  | { title: string }
-  | { name: string; content: string }
-  | { property: string; content: string }
-  | { httpEquiv: string; content: string }
-  | { "script:ld+json": LdJsonObject }
-  | { tagName: "meta" | "link"; [name: string]: string }
-  | { [name: string]: unknown };
-
 type LdJsonObject = { [Key in string]: LdJsonValue } & {
   [Key in string]?: LdJsonValue | undefined;
 };
@@ -249,7 +126,7 @@ export interface EntryRouteModule {
   default: any; // Weakly typed because server-runtime is not React-aware
   handle?: RouteHandle;
   links?: LinksFunction;
-  meta?: ServerRuntimeMetaFunction;
+  meta?: MetaFunction;
 }
 
 export interface ServerRouteModule extends EntryRouteModule {
