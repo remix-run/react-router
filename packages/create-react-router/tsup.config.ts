4f293f7ea7ffc5ff49a0aa28a903bf3186cba549
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2024-10-31 11:17:45 +1100

    Fix tsup config

--- packages/create-react-router/tsup.config.ts
+++ packages/create-react-router/tsup.config.ts
@@ -5,7 +5,7 @@ import { createBanner } from "../../build.utils.js";
 
 import pkg from "./package.json";
 
-const entry = ["index.ts"];
+const entry = ["cli.ts"];
 
 export default defineConfig([
   {
