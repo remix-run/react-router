9230e5d3f22bdc01ccbdff103cb15a5973b4218f
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2024-11-12 09:29:31 +1100

    Fix build

--- packages/react-router-dev/__tests__/fixtures/config/vite.config.ts
+++ packages/react-router-dev/__tests__/fixtures/config/vite.config.ts
@@ -3,5 +3,10 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [reactRouter(), tsconfigPaths()],
+  plugins: [
+    reactRouter({
+      appDirectory: "custom-app",
+    }),
+    tsconfigPaths(),
+  ],
 });
