aa5a9082e064dc0700392dbbdb6e27282a346ea3
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2024-11-13 11:05:55 +1100

    Add Config type

---
+++ packages/react-router-dev/config.ts
@@ -0,0 +1,1 @@
+export type { ReactRouterConfig as Config } from "./config/config";
--- playground/compiler/react-router.config.ts
+++ playground/compiler/react-router.config.ts
@@ -1,3 +1,5 @@
+import type { Config } from "@react-router/dev/config";
+
 export default {
   appDirectory: "app",
-};
+} satisfies Config;
