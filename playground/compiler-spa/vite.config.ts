a47a86a96906c9b71259b2c88c905f4bea63042d
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2024-11-13 14:30:19 +1100

    Fix compiler-spa playground config

--- playground/compiler-spa/vite.config.ts
+++ playground/compiler-spa/vite.config.ts
@@ -3,10 +3,5 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [
-    reactRouter({
-      ssr: false,
-    }),
-    tsconfigPaths(),
-  ],
+  plugins: [reactRouter(), tsconfigPaths()],
 });
