811760695c1e682cb9103c6f27bea1be04ef4b31
Brooks Lybrand <brookslybrand@gmail.com>
2024-11-20 13:02:22 -0600

    move ssr: true from vite.config to react-router.config in basic template

--- templates/basic/react-router.config.ts
+++ templates/basic/react-router.config.ts
@@ -2,4 +2,6 @@ import type { Config } from "@react-router/dev/config";
 
 export default {
   // Config options...
+  // Server-side render by default, to enable SPA mode set this to `false`
+  ssr: true,
 } satisfies Config;
