b5111928b9a54bcbed43881fa07a242a1b3c11ff
Pedro Cattori <pcattori@gmail.com>
2024-11-21 09:44:33 -0500

    fix: pass route error to errorboundary as a prop

--- packages/react-router-dev/vite/with-props.ts
+++ packages/react-router-dev/vite/with-props.ts
@@ -19,7 +19,7 @@ export const plugin: Plugin = {
     if (id !== VirtualModule.resolve(vmodId)) return;
     return dedent`
       import { createElement as h } from "react";
-      import { useActionData, useLoaderData, useMatches, useParams } from "react-router";
+      import { useActionData, useLoaderData, useMatches, useParams, useRouteError } from "react-router";
 
       export function withComponentProps(Component) {
         return function Wrapped() {
@@ -48,6 +48,7 @@ export const plugin: Plugin = {
             params: useParams(),
             loaderData: useLoaderData(),
             actionData: useActionData(),
+            error: useRouteError(),
           };
           return h(ErrorBoundary, props);
         };
