0f5072cb26729ee83acadc925d7be6284f34063b
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2024-12-06 13:44:26 +1100

    Bump Vite dep, fix typecheck

--- integration/helpers/vite-5-template/vite.config.ts
+++ integration/helpers/vite-5-template/vite.config.ts
@@ -3,5 +3,9 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [reactRouter(), tsconfigPaths()],
+  plugins: [
+    // @ts-expect-error `dev` depends on Vite 6, Plugin type is mismatched.
+    reactRouter(),
+    tsconfigPaths(),
+  ],
 });
