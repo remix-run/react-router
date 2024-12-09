c01b8235653cf20bb674e045540731e358a39446
Matt Brophy <matt@brophy.org>
2024-12-09 15:25:17 -0500

    Properly set node env

--- packages/react-router-dev/cli/index.ts
+++ packages/react-router-dev/cli/index.ts
@@ -3,6 +3,15 @@ import { parseArgs } from "./parse";
 
 let { input, flags, command } = parseArgs();
 
+// If not already set, default `NODE_ENV` so React loads the proper
+// version in it's CJS entry script.  We have to do this before importing `run.ts`
+// since that is what imports `react` (indirectly via `react-router`)
+if (command === "dev") {
+  process.env.NODE_ENV = process.env.NODE_ENV ?? "development";
+} else {
+  process.env.NODE_ENV = process.env.NODE_ENV ?? "production";
+}
+
 import("./run").then(({ run }) => {
   run(input, flags, command).then(
     () => {
