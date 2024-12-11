1d3377507efd1dfe4a8aa0d486c118e4b7575002
Ayc0 <ayc0.benj@gmail.com>
2024-12-11 11:01:26 +0100

    Remove withRef & Options in withRouter's types

--- types/lib/withRouter.d.ts
+++ types/lib/withRouter.d.ts
@@ -3,10 +3,6 @@ import { InjectedRouter, Params } from './Router'
 import { Location } from 'history'
 import { PlainRoute } from './Route'
 
-interface Options {
-    withRef?: boolean | undefined;
-}
-
 export interface WithRouterProps<P = Params, Q = any> {
     location: Location<Q>;
     params: P;
@@ -18,11 +14,9 @@ type ComponentConstructor<P> = ComponentClass<P> | FunctionComponent<P>;
 
 declare function withRouter<P, S>(
     component: ComponentConstructor<P & WithRouterProps> & S,
-    options?: Options
 ): ComponentClass<Omit<P, keyof WithRouterProps>> & S;
 declare function withRouter<P>(
     component: ComponentConstructor<P & WithRouterProps>,
-    options?: Options
 ): ComponentClass<Omit<P, keyof WithRouterProps>>;
 
 export default withRouter
