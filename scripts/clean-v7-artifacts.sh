7a7e41f147f4b4b5d57827e3285f2c2139f1816e
Matt Brophy <matt@brophy.org>
2025-01-03 11:48:58 -0500

    Add script to clean v7 artifacts

---
+++ scripts/clean-v7-artifacts.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e
+
+pnpm clean
+
+echo "Removing v7 directories..."
+set -x
+rm -rf packages/create-react-router/
+rm -rf packages/react-router-architect/
+rm -rf packages/react-router-cloudflare/
+rm -rf packages/react-router-dev/
+rm -rf packages/react-router-dom/.wireit/
+rm -rf packages/react-router-express/
+rm -rf packages/react-router-fs-routes/
+rm -rf packages/react-router-node/
+rm -rf packages/react-router-remix-routes-option-adapter/
+rm -rf packages/react-router-serve/
+rm -rf packages/react-router/.wireit/
+set +x
+
+echo "Installing and building..."
+pnpm install --frozen-lockfile
+pnpm build
