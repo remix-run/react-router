4c2cee3325cb503209af4ff03c28cd1413c20b1c
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2025-01-03 13:57:50 +1100

    Inline comments

--- packages/react-router-dev/vite/vite.ts
+++ packages/react-router-dev/vite/vite.ts
@@ -1,11 +1,3 @@
-// This file serves two purposes. First, it's used to avoid CJS deprecation
-// warnings in Vite since `@react-router/dev` currently compiles to CJS. We do
-// this by dynamically importing Vite, forcing it to use the ESM build. Second,
-// it's used to allow us to test against different versions of Vite by ensuring
-// that Vite is resolved from the current working directory when running within
-// this repo. If we don't do this, Vite will always be imported relative to this
-// package, which means that it will always resolve to Vite 6.
-
 import path from "pathe";
 
 import invariant from "../invariant";
@@ -15,15 +7,21 @@ import { isReactRouterRepo } from "../config/is-react-router-repo";
 type Vite = typeof import("vite");
 let vite: Vite | undefined;
 
-const viteImportSpecifier = !isReactRouterRepo()
-  ? "vite"
-  : `file:///${path
+const viteImportSpecifier = isReactRouterRepo()
+  ? // Support testing against different versions of Vite by ensuring that Vite
+    // is resolved from the current working directory when running within this
+    // repo. If we don't do this, Vite will always be imported relative to this
+    // file, which means that it will always resolve to Vite 6.
+    `file:///${path
       .normalize(
         require.resolve("vite/package.json", { paths: [process.cwd()] })
       )
-      .replace("package.json", "dist/node/index.js")}`;
+      .replace("package.json", "dist/node/index.js")}`
+  : "vite";
 
 export async function preloadVite(): Promise<void> {
+  // Use a dynamic import to force Vite to use the ESM build. If we don't do
+  // this, Vite logs CJS deprecation warnings.
   vite = await import(viteImportSpecifier);
 }
 
