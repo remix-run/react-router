93f02299504e4890229026dcaff6b6b56059fde1
Matt Brophy <matt@brophy.org>
2025-01-09 14:55:17 -0500

    Wire up server middleware e2e tests - reidrects still failing

--- integration/middleware-test.ts
+++ integration/middleware-test.ts
@@ -610,11 +610,10 @@ test.describe("Middleware", () => {
     });
   });
 
-  test.describe.skip("Server Middleware", () => {
-    test.only("calls middleware before/after loaders", async ({ page }) => {
+  test.describe("Server Middleware", () => {
+    test("calls middleware before/after loaders", async ({ page }) => {
       let fixture = await createFixture({
         files: {
-          "react-router.config.ts": reactRouterConfig({}),
           "vite.config.ts": js`
             import { defineConfig } from "vite";
             import { reactRouter } from "@react-router/dev/vite";
@@ -679,6 +678,222 @@ test.describe("Middleware", () => {
       expect(await page.locator("[data-route]").textContent()).toBe(
         "About: c,d"
       );
+
+      appFixture.close();
+    });
+
+    test("calls middleware before/after actions", async ({ page }) => {
+      let fixture = await createFixture({
+        files: {
+          "vite.config.ts": js`
+            import { defineConfig } from "vite";
+            import { reactRouter } from "@react-router/dev/vite";
+
+            export default defineConfig({
+              build: { manifest: true },
+              plugins: [reactRouter()],
+            });
+          `,
+          "app/routes/_index.tsx": js`
+            import { Form } from 'react-router'
+
+            export const middleware = [
+              ({ context }) => { context.order = ['a']; },
+              ({ context }) => { context.order.push('b'); }
+            ];
+
+            export async function action({ request, context }) {
+              return context.order.join(',');
+            }
+
+            export async function loader({ request, context }) {
+              return context.order.join(',');
+            }
+
+            export default function Component({ loaderData, actionData }) {
+              return (
+                <>
+                  <h2 data-route>Index: {loaderData} - {actionData || 'empty'}</h2>
+                  <Form method="post">
+                    <input name="name" />
+                    <button type="submit">Submit</button>
+                  </Form>
+                </>
+               );
+            }
+          `,
+        },
+      });
+
+      let appFixture = await createAppFixture(fixture);
+
+      let app = new PlaywrightFixture(appFixture, page);
+      await app.goto("/");
+      await page.waitForSelector('[data-route]:has-text("Index")');
+      expect(await page.locator("[data-route]").textContent()).toBe(
+        "Index: a,b - empty"
+      );
+
+      (await page.getByRole("button"))?.click();
+      await new Promise((r) => setTimeout(r, 1000));
+      await page.waitForSelector('[data-route]:has-text("- a,b")');
+      expect(await page.locator("[data-route]").textContent()).toBe(
+        "Index: a,b - a,b"
+      );
+
+      appFixture.close();
+    });
+
+    test("handles redirects thrown on the way down", async ({ page }) => {
+      let fixture = await createFixture({
+        files: {
+          "vite.config.ts": js`
+            import { defineConfig } from "vite";
+            import { reactRouter } from "@react-router/dev/vite";
+
+            export default defineConfig({
+              build: { manifest: true },
+              plugins: [reactRouter()],
+            });
+          `,
+          "app/routes/_index.tsx": js`
+            import { Link } from 'react-router'
+
+            export default function Component({ loaderData, actionData }) {
+              return <Link to="/redirect">Link</Link>;
+            }
+          `,
+          "app/routes/redirect.tsx": js`
+            import { Link, redirect } from 'react-router'
+            export const middleware = [
+              ({ request, context }) => { throw redirect('/target'); }
+            ]
+            export default function Component() {
+              return <h1>Redirect</h1>
+            }
+          `,
+          "app/routes/target.tsx": js`
+            export default function Component() {
+              return <h1>Target</h1>
+            }
+          `,
+        },
+      });
+
+      let appFixture = await createAppFixture(fixture);
+
+      let app = new PlaywrightFixture(appFixture, page);
+      await app.goto("/");
+      await page.waitForSelector('a:has-text("Link")');
+
+      (await page.getByRole("link"))?.click();
+      await page.waitForSelector('h1:has-text("Target")');
+
+      appFixture.close();
+    });
+
+    test.only("handles redirects thrown on the way up", async ({ page }) => {
+      let fixture = await createFixture({
+        files: {
+          "vite.config.ts": js`
+            import { defineConfig } from "vite";
+            import { reactRouter } from "@react-router/dev/vite";
+
+            export default defineConfig({
+              build: { manifest: true },
+              plugins: [reactRouter()],
+            });
+          `,
+          "app/routes/_index.tsx": js`
+            import { Link } from 'react-router'
+
+            export default function Component({ loaderData, actionData }) {
+              return <Link to="/redirect">Link</Link>;
+            }
+          `,
+          "app/routes/redirect.tsx": js`
+            import { Link, redirect } from 'react-router'
+            export const middleware = [
+              async ({ request, context }, next) => {
+                await next();
+                throw redirect('/target');
+              }
+            ]
+            export default function Component() {
+              return <h1>Redirect</h1>
+            }
+          `,
+          "app/routes/target.tsx": js`
+            export default function Component() {
+              return <h1>Target</h1>
+            }
+          `,
+        },
+      });
+
+      let appFixture = await createAppFixture(fixture);
+
+      let app = new PlaywrightFixture(appFixture, page);
+      await app.goto("/");
+      await page.waitForSelector('a:has-text("Link")');
+
+      (await page.getByRole("link"))?.click();
+      await new Promise((r) => setTimeout(r, 1000));
+      console.log(await app.getHtml());
+      await page.waitForSelector('h1:has-text("Target")');
+
+      appFixture.close();
+    });
+
+    test("handles errors thrown on the way down", async ({ page }) => {
+      let fixture = await createFixture({
+        files: {
+          "vite.config.ts": js`
+            import { defineConfig } from "vite";
+            import { reactRouter } from "@react-router/dev/vite";
+
+            export default defineConfig({
+              build: { manifest: true },
+              plugins: [reactRouter()],
+            });
+          `,
+          "app/routes/_index.tsx": js`
+            import { Link } from 'react-router'
+
+            export default function Component() {
+              return <Link to="/private/page">Link</Link>;
+            }
+          `,
+          "app/routes/private.tsx": js`
+            import { Link, redirect } from 'react-router'
+            export const middleware = [
+              async ({ request, context }, next) => {
+                throw new Response(null, { status: 401 });
+              }
+            ]
+            export default function Component() {
+              return <Outlet/>
+            }
+            export function ErrorBoundary() {
+              return <h1>Access Denied</h1>
+            }
+          `,
+          "app/routes/private.page.tsx": js`
+            export default function Component() {
+              return <h1>Target</h1>
+            }
+          `,
+        },
+      });
+
+      let appFixture = await createAppFixture(fixture);
+
+      let app = new PlaywrightFixture(appFixture, page);
+      await app.goto("/");
+      await page.waitForSelector('a:has-text("Link")');
+
+      (await page.getByRole("link"))?.click();
+      await page.waitForSelector('h1:has-text("Access Denied")');
 
       appFixture.close();
     });
