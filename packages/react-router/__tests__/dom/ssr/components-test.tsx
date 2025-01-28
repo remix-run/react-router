a0394711d148194f1c88c949f05f47fe9cea0823
Matt Brophy <matt@brophy.org>
2025-01-28 13:31:13 -0500

    Limit matchRoutes optimization to client side routers (#12882)

--- packages/react-router/__tests__/dom/ssr/components-test.tsx
+++ packages/react-router/__tests__/dom/ssr/components-test.tsx
@@ -150,9 +150,9 @@ describe("<NavLink />", () => {
 
 describe("<ServerRouter>", () => {
   it("handles empty default export objects from the compiler", async () => {
-    let staticHandlerContext = await createStaticHandler([
-      { id: "root", path: "/", children: [{ id: "empty", index: true }] },
-    ]).query(new Request("http://localhost/"));
+    let staticHandlerContext = await createStaticHandler([{ path: "/" }]).query(
+      new Request("http://localhost/")
+    );
 
     invariant(
       !(staticHandlerContext instanceof Response),
