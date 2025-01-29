920f80789ce35798429cb6ddc321f7cba686315e
Matt Brophy <matt@brophy.org>
2025-01-29 15:01:51 -0500

    Add signal as parameter to patchRoutesOnNavigation

---
+++ .changeset/seven-countries-divide.md
@@ -0,0 +1,7 @@
+---
+"@remix-run/router": minor
+---
+
+Provide the request `signal` as a parameter to `patchRoutesOnNavigation`
+
+- This can be used to abort any manifest fetches if the in-flight navigation/fetcher is aborted
--- packages/router/router.ts
+++ packages/router/router.ts
@@ -3293,6 +3293,7 @@ export function createRouter(init: RouterInit): Router {
       let localManifest = manifest;
       try {
         await patchRoutesOnNavigationImpl({
+          signal,
           path: pathname,
           matches: partialMatches,
           patch: (routeId, children) => {
