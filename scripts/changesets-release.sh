e1b29c8a5251074e2c9dac12e942555eca2b83f2
Matt Brophy <matt@brophy.org>
2025-01-30 11:27:24 -0500

    fix release script to use v6 tag

--- scripts/changesets-release.sh
+++ scripts/changesets-release.sh
@@ -4,7 +4,7 @@ MODE=$(node -e "console.log(require('./.changeset/pre.json').mode)")
 
 # Can only provide a custom --tag when not in prerelease mode
 # The prerelease tags come from the pre.json "tag" field
-if [ $MODE == "exit" ]; then
+if [ "${MODE}" == "exit" ]; then
   pnpm exec changeset publish --tag v6
 else
   pnpm exec changeset publish
