6e86d6d4d99948930b14fdd81bafc18d3aa67118
Matt Brophy <matt@brophy.org>
2025-01-31 11:10:08 -0500

    Use NODE_ENV=developemnt when no args are provided

--- packages/react-router-dev/bin.js
+++ packages/react-router-dev/bin.js
@@ -6,7 +6,7 @@ let arg = require("arg");
 // We have to do this before importing `run.ts` since that is what imports
 // `react` (indirectly via `react-router`)
 let args = arg({}, { argv: process.argv.slice(2), permissive: true });
-if (args._[0] === "dev") {
+if (args._.length === 0 || args._[0] === "dev") {
   process.env.NODE_ENV = process.env.NODE_ENV ?? "development";
 } else {
   process.env.NODE_ENV = process.env.NODE_ENV ?? "production";
