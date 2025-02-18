57233638e7c16e47f1c255e7ee2a578add6a3d9e
Matt Brophy <matt@brophy.org>
2025-02-18 11:36:18 -0500

    Fix lint issue

--- integration/vite-prerender-test.ts
+++ integration/vite-prerender-test.ts
@@ -1,7 +1,8 @@
 import fs from "node:fs";
 import path from "node:path";
 import { PassThrough } from "node:stream";
-import { test, expect, Page } from "@playwright/test";
+import type { Page } from "@playwright/test";
+import { test, expect } from "@playwright/test";
 
 import {
   createAppFixture,
