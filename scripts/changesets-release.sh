e8c2d0a5f20040bc538c7329c56b24d3a47cbfff
Matt Brophy <matt@brophy.org>
2025-02-25 10:28:49 -0500

    Add logging to release script

--- scripts/changesets-release.sh
+++ scripts/changesets-release.sh
@@ -5,7 +5,9 @@ MODE=$(node -e "console.log(require('./.changeset/pre.json').mode)")
 # Can only provide a custom --tag when not in prerelease mode
 # The prerelease tags come from the pre.json "tag" field
 if [ "${MODE}" == "exit" ]; then
+  echo "Publishing with v6 tag (stable release)"
   pnpm exec changeset publish --tag v6
 else
+  echo "Publishing with default changesets tag (pre-release)"
   pnpm exec changeset publish
 fi
