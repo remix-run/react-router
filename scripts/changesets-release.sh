81423a5174b92b7f889a27b6789e62256d467544
Matt Brophy <matt@brophy.org>
2025-02-27 11:56:16 -0500

    pre.json does not exist on stable releases so check didn't work

--- scripts/changesets-release.sh
+++ scripts/changesets-release.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-MODE=$(node -e "console.log(require('./.changeset/pre.json').mode)")
-
 # Can only provide a custom --tag when not in prerelease mode
-# The prerelease tags come from the pre.json "tag" field
-if [ "${MODE}" == "exit" ]; then
-  echo "Publishing with v6 tag (stable release)"
-  pnpm exec changeset publish --tag v6
-else
+# The prerelease tags come from the `pre.json`` "tag" field
+if [ -f ".changeset/pre.json" ];
   echo "Publishing with default changesets tag (pre-release)"
-  pnpm exec changeset publish
+  #pnpm exec changeset publish
+else
+  echo "Publishing with v6 tag (stable release)"
+  #pnpm exec changeset publish --tag v6
 fi
