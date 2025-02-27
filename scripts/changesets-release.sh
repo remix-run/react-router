6d7a4da0165f7805ad902a27e7640e29290e1371
Matt Brophy <matt@brophy.org>
2025-02-27 11:56:39 -0500

    oops uncomment

--- scripts/changesets-release.sh
+++ scripts/changesets-release.sh
@@ -4,8 +4,8 @@
 # The prerelease tags come from the `pre.json`` "tag" field
 if [ -f ".changeset/pre.json" ];
   echo "Publishing with default changesets tag (pre-release)"
-  #pnpm exec changeset publish
+  pnpm exec changeset publish
 else
   echo "Publishing with v6 tag (stable release)"
-  #pnpm exec changeset publish --tag v6
+  pnpm exec changeset publish --tag v6
 fi
