79f956e20e6279e8fca55793ef0c76b92be82060
Fahrzin Hemmati <fahhem@users.noreply.github.com>
2025-02-27 18:49:30 -0800

    Switch server.hot to server.ws for vite

--- packages/react-router-dev/vite/plugin.ts
+++ packages/react-router-dev/vite/plugin.ts
@@ -1952,7 +1952,7 @@ export const reactRouterVitePlugin: ReactRouterVitePlugin = () => {
           }
         }
 
-        server.hot.send({
+        server.ws.send({
           type: "custom",
           event: "react-router:hmr",
           data: hmrEventData,
