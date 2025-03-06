5d61a3e26a51f822e30ea775b59039e2b04bef85
Jasper Van Gestel <vjee2e@gmail.com>
2025-03-06 09:31:12 +0100

    Add failing tests

--- packages/react-router/__tests__/href-test.ts
+++ packages/react-router/__tests__/href-test.ts
@@ -17,9 +17,22 @@ describe("href", () => {
     );
   });
 
+  it("works with optional segments", () => {
+    expect(href("/a/b?/:c", { c: "hello" })).toBe("/a/hello");
+    expect(href("/a/b?/:c", { b: true, c: "hello" })).toBe("/a/b/hello");
+  });
+
+  it("works with slpats", () => {
+    expect(href("/a/*", { "*": "hello" })).toBe("/a/hello");
+    expect(href("/a/*", { "*": "hello/world" })).toBe("/a/hello/world");
+  });
+
   it("throws when required params are missing", () => {
     expect(() => href("/a/:b")).toThrow(
       `Path '/a/:b' requires param 'b' but it was not provided`
+    );
+    expect(() => href("/a/*")).toThrow(
+      `Path '/a/*' requires param '*' but it was not provided`
     );
   });
 });
