336ef8598285ca9296e707cef2d14fcba1a8ef2b
Matt Brophy <matt@brophy.org>
2025-04-02 15:40:01 -0400

    Revert "Refactor dataStrategy for easier RSC abstraction"
    This reverts commit 65920e8ca46191189b1dd63ac675ebd3b2b4f400.

--- integration/single-fetch-test.ts
+++ integration/single-fetch-test.ts
@@ -2,7 +2,6 @@ import { test, expect } from "@playwright/test";
 import {
   UNSAFE_ServerMode as ServerMode,
   UNSAFE_SingleFetchRedirectSymbol as SingleFetchRedirectSymbol,
-  UNSAFE_ServerMode,
 } from "react-router";
 
 import {
