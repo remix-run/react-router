95384006c82339a86891160d82f2f575ef6e79dc
Matt Brophy <matt@brophy.org>
2025-05-01 14:11:22 -0400

    Update MSW to v2 (#13519)
    Co-authored-by: Artem Zakharchenko <kettanaito@gmail.com>

--- packages/create-react-router/__tests__/msw.ts
+++ packages/create-react-router/__tests__/msw.ts
@@ -1,43 +1,39 @@
 import path from "node:path";
 import fsp from "node:fs/promises";
 import { setupServer } from "msw/node";
-import { rest } from "msw";
+import { http, type RequestHandler } from "msw";
 
 import { githubHandlers } from "./github-mocks";
 
-type RequestHandler = Parameters<typeof setupServer>[0];
-
 let miscHandlers: Array<RequestHandler> = [
-  rest.get(
-    "https://registry.npmjs.org/react-router/latest",
-    async (req, res, ctx) => {
-      return res(ctx.body(JSON.stringify({ version: "123.0.0" })));
-    }
-  ),
-  rest.head(
-    "https://example.com/error/:status/template.tar.gz",
-    async (req, res, ctx) => {
-      return res(ctx.status(Number(req.params.status)));
-    }
-  ),
-  rest.get(
-    "https://example.com/error/:status/template.tar.gz",
-    async (req, res, ctx) => {
-      return res(ctx.status(Number(req.params.status)));
-    }
-  ),
-  rest.head("https://example.com/template.tar.gz", async (req, res, ctx) => {
-    return res(ctx.status(200));
+  http.get("https://registry.npmjs.org/react-router/latest", () => {
+    return Response.json({ version: "123.0.0" });
   }),
-  rest.get("https://example.com/template.tar.gz", async (req, res, ctx) => {
+  http.head<{ status: string }>(
+    "https://example.com/error/:status/template.tar.gz",
+    ({ params }) => {
+      return new Response(null, { status: Number(params.status) });
+    }
+  ),
+  http.get<{ status: string }>(
+    "https://example.com/error/:status/template.tar.gz",
+    ({ params }) => {
+      return new Response(null, { status: Number(params.status) });
+    }
+  ),
+  http.head("https://example.com/template.tar.gz", () => {
+    return new Response();
+  }),
+  http.get("https://example.com/template.tar.gz", async () => {
     let fileBuffer = await fsp.readFile(
       path.join(__dirname, "fixtures", "template.tar.gz")
     );
 
-    return res(
-      ctx.body(fileBuffer),
-      ctx.set("Content-Type", "application/x-gzip")
-    );
+    return new Response(fileBuffer, {
+      headers: {
+        "Content-Type": "application/x-gzip",
+      },
+    });
   }),
 ];
 
