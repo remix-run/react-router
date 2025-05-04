84931b6bed75d8ad823bb60afb2720d3373801a9
omahs <73983677+omahs@users.noreply.github.com>
2025-05-04 11:39:07 +0200

    fix typo

--- integration/helpers/vite-plugin-cloudflare-template/worker-configuration.d.ts
+++ integration/helpers/vite-plugin-cloudflare-template/worker-configuration.d.ts
@@ -5135,7 +5135,7 @@ declare module "assets:*" {
 //     https://opensource.org/licenses/Apache-2.0
 declare abstract class PipelineTransform {
   /**
-   * transformJson recieves an array of javascript objects which can be
+   * transformJson receives an array of javascript objects which can be
    * mutated and returned to the pipeline
    * @param data The data to be mutated
    * @returns A promise containing the mutated data
