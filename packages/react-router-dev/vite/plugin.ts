7cea871f5853a5b0eabf9b10f1c3044073b6f3e4
chris <chr33s@icloud.com>
2025-05-15 10:19:58 +0700

    update: refactor to use endsWith

--- packages/react-router-dev/vite/plugin.ts
+++ packages/react-router-dev/vite/plugin.ts
@@ -1576,7 +1576,7 @@ export const reactRouterVitePlugin: ReactRouterVitePlugin = () => {
         if (ctx.reactRouterConfig.future.unstable_viteEnvironmentApi) {
           viteDevServer.middlewares.use(async (req, res, next) => {
             let [reqPathname, reqSearch] = (req.url ?? "").split("?");
-            if (reqPathname === getRelativePathName(`${ctx.publicPath}@react-router/critical.css`)) {
+            if (reqPathname.endsWith("/@react-router/critical.css")) {
               let pathname = new URLSearchParams(reqSearch).get("pathname");
               if (!pathname) {
                 return next("No pathname provided");
@@ -2377,10 +2377,6 @@ export const reactRouterVitePlugin: ReactRouterVitePlugin = () => {
     },
   ];
 };
-
-export function getRelativePathName(url: string) {
-  return new URL(url, "http://localhost").pathname;
-}
 
 function getParentClientNodes(
   clientModuleGraph: Vite.EnvironmentModuleGraph,
