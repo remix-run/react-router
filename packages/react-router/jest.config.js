5c18446be9f72169ba349720b3864e8abe4552c8
Michaël De Boey <info@michaeldeboey.be>
2025-06-02 13:28:24 +0200

    chore(react-router): cleanup test setup

--- packages/react-router/jest.config.js
+++ packages/react-router/jest.config.js
@@ -3,5 +3,5 @@ module.exports = {
   ...require("../../jest/jest.config.shared"),
   setupFiles: ["<rootDir>/__tests__/setup.ts"],
   setupFilesAfterEnv: ["@testing-library/jest-dom"],
-  testEnvironment: "jsdom",
+  testEnvironment: "jest-fixed-jsdom",
 };
