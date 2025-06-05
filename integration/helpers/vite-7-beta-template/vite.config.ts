9398cceaa99e7254cd42268479a817ccf5dda246
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2025-06-05 15:11:22 +1000

    fix types

--- integration/helpers/vite-7-beta-template/vite.config.ts
+++ integration/helpers/vite-7-beta-template/vite.config.ts
@@ -3,5 +3,6 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  // @ts-expect-error `dev` depends on Vite 6, Plugin type is mismatched.
   plugins: [reactRouter(), tsconfigPaths()],
 });
