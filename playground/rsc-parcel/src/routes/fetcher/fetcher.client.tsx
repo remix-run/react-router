e539a5f984e8d2f46552c76a45f7ae2fb5d6dff4
Jacob Ebey <jacob.ebey@live.com>
2025-06-06 23:17:47 -0700

    silence redirect errors

--- playground/rsc-parcel/src/routes/fetcher/fetcher.client.tsx
+++ playground/rsc-parcel/src/routes/fetcher/fetcher.client.tsx
@@ -1,14 +1,21 @@
 "use client";
 
 import { useFetcher } from "react-router";
-import type { loader } from "../resource/resource";
+import type { action } from "../resource/resource";
 
 export default function FetcherRoute() {
-  const fetcher = useFetcher<typeof loader>();
+  const fetcher = useFetcher<typeof action>();
 
   return (
     <main>
-      <button onClick={() => fetcher.load("/resource")}>
+      <button
+        onClick={() =>
+          fetcher.submit(
+            { hello: "world" },
+            { action: "/resource", method: "post", encType: "application/json" }
+          )
+        }
+      >
         Load fetcher data
       </button>
       <pre>{JSON.stringify(fetcher.data, null, 2)}</pre>
