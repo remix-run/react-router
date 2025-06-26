c3f9f23862f42a8d9e7f4e9b7652a16e6a071188
Jacob Ebey <jacob.ebey@live.com>
2025-06-26 13:17:18 -0700

    remove comment

--- packages/react-router/lib/components.tsx
+++ packages/react-router/lib/components.tsx
@@ -1232,7 +1232,6 @@ export let createRoutesFromElements = createRoutesFromChildren;
 export function renderMatches(
   matches: RouteMatch[] | null
 ): React.ReactElement | null {
-  // Is this legacy? No params relevant to how it's used???? Seems lacking.
   return _renderMatches(matches);
 }
 
