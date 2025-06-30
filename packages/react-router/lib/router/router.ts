633bbe77ee26a0b263d6db981c6ac6bfedfcaf09
Dmitry Khorkin <32310771+DimaAmega@users.noreply.github.com>
2025-06-30 22:57:02 +0400

    fix(react-router): scroll with revalidation (#13671)

--- packages/react-router/lib/router/router.ts
+++ packages/react-router/lib/router/router.ts
@@ -327,7 +327,7 @@ export interface RouterState {
   /**
    * Current scroll position we should start at for a new view
    *  - number -> scroll position to restore to
-   *  - false -> do not restore scroll at all (used during submissions)
+   *  - false -> do not restore scroll at all (used during submissions/revalidations)
    *  - null -> don't have a saved position, scroll to hash or top of page
    */
   restoreScrollPosition: number | false | null;
@@ -1310,6 +1310,11 @@ export function createRouter(init: RouterInit): Router {
       blockers.forEach((_, k) => blockers.set(k, IDLE_BLOCKER));
     }
 
+    // Don't restore on router.revalidate()
+    let restoreScrollPosition = isUninterruptedRevalidation ? 
+      false : 
+      getSavedScrollPosition(location, newState.matches || state.matches);
+
     // Always respect the user flag.  Otherwise don't reset on mutation
     // submission navigations unless they redirect
     let preventScrollReset =
@@ -1378,10 +1383,7 @@ export function createRouter(init: RouterInit): Router {
         initialized: true,
         navigation: IDLE_NAVIGATION,
         revalidation: "idle",
-        restoreScrollPosition: getSavedScrollPosition(
-          location,
-          newState.matches || state.matches
-        ),
+        restoreScrollPosition,
         preventScrollReset,
         blockers,
       },
