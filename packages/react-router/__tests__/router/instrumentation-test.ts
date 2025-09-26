09f230392ccf819e3168bb8ee94971a95dc197a6
Matt Brophy <matt@brophy.org>
2025-09-26 15:40:13 -0400

    Handle instrumentation of lazy

--- packages/react-router/__tests__/router/instrumentation-test.ts
+++ packages/react-router/__tests__/router/instrumentation-test.ts
@@ -1,5 +1,6 @@
+import type { LoaderFunction } from "../../lib/router/utils";
 import { cleanup, setup } from "./utils/data-router-setup";
-import { createFormData } from "./utils/utils";
+import { createDeferred, createFormData, tick } from "./utils/utils";
 
 // Detect any failures inside the router navigate code
 afterEach(() => {
@@ -7,6 +8,44 @@ afterEach(() => {
 });
 
 describe("instrumentation", () => {
+  it("allows instrumentation of lazy", async () => {
+    let spy = jest.fn();
+    let lazyDfd = createDeferred<{ loader: LoaderFunction }>();
+    let t = setup({
+      routes: [
+        {
+          index: true,
+        },
+        {
+          id: "page",
+          path: "/page",
+          lazy: () => lazyDfd.promise,
+        },
+      ],
+      unstable_instrumentRoute: (route) => {
+        route.instrument({
+          async lazy(lazy) {
+            spy("start");
+            await lazy();
+            spy("end");
+          },
+        });
+      },
+    });
+
+    await t.navigate("/page");
+    expect(spy.mock.calls).toEqual([["start"]]);
+
+    await lazyDfd.resolve({ loader: () => "PAGE" });
+    await tick();
+    expect(spy.mock.calls).toEqual([["start"], ["end"]]);
+    expect(t.router.state).toMatchObject({
+      navigation: { state: "idle" },
+      location: { pathname: "/page" },
+      loaderData: { page: "PAGE" },
+    });
+  });
+
   it("allows instrumentation of loaders", async () => {
     let spy = jest.fn();
     let t = setup({
@@ -78,6 +117,211 @@ describe("instrumentation", () => {
       location: { pathname: "/page" },
       actionData: { page: "PAGE" },
     });
+  });
+
+  it("allows instrumentation of loaders when lazy is used", async () => {
+    let spy = jest.fn();
+    let lazyDfd = createDeferred<{ loader: LoaderFunction }>();
+    let loaderDfd = createDeferred();
+    let t = setup({
+      routes: [
+        {
+          index: true,
+        },
+        {
+          id: "page",
+          path: "/page",
+          lazy: () => lazyDfd.promise,
+        },
+      ],
+      unstable_instrumentRoute: (route) => {
+        route.instrument({
+          async loader(loader) {
+            spy("start");
+            await loader();
+            spy("end");
+          },
+        });
+      },
+    });
+
+    await t.navigate("/page");
+    expect(spy).not.toHaveBeenCalled();
+
+    await lazyDfd.resolve({ loader: () => loaderDfd.promise });
+    expect(spy.mock.calls).toEqual([["start"]]);
+
+    await loaderDfd.resolve("PAGE");
+    await tick();
+    expect(spy.mock.calls).toEqual([["start"], ["end"]]);
+
+    await tick();
+    expect(t.router.state).toMatchObject({
+      navigation: { state: "idle" },
+      location: { pathname: "/page" },
+      loaderData: { page: "PAGE" },
+    });
+  });
+
+  it("allows instrumentation of actions when lazy is used", async () => {
+    let spy = jest.fn();
+    let lazyDfd = createDeferred<{ loader: LoaderFunction }>();
+    let actionDfd = createDeferred();
+    let t = setup({
+      routes: [
+        {
+          index: true,
+        },
+        {
+          id: "page",
+          path: "/page",
+          lazy: () => lazyDfd.promise,
+        },
+      ],
+      unstable_instrumentRoute: (route) => {
+        route.instrument({
+          async action(action) {
+            spy("start");
+            await action();
+            spy("end");
+          },
+        });
+      },
+    });
+
+    await t.navigate("/page", {
+      formMethod: "POST",
+      formData: createFormData({}),
+    });
+    expect(spy).not.toHaveBeenCalled();
+
+    await lazyDfd.resolve({ action: () => actionDfd.promise });
+    expect(spy.mock.calls).toEqual([["start"]]);
+
+    await actionDfd.resolve("PAGE");
+    await tick();
+    expect(spy.mock.calls).toEqual([["start"], ["end"]]);
+
+    await tick();
+    expect(t.router.state).toMatchObject({
+      navigation: { state: "idle" },
+      location: { pathname: "/page" },
+      actionData: { page: "PAGE" },
+    });
+  });
+
+  it("does not double-instrument when a static `loader` is used alongside `lazy`", async () => {
+    let spy = jest.fn();
+    let lazyDfd = createDeferred<{ loader: LoaderFunction }>();
+    let warnSpy = jest.spyOn(console, "warn").mockImplementation(() => {});
+    let t = setup({
+      routes: [
+        {
+          index: true,
+        },
+        {
+          id: "page",
+          path: "/page",
+          loader: true,
+          lazy: () => lazyDfd.promise,
+        },
+      ],
+      unstable_instrumentRoute: (route) => {
+        route.instrument({
+          async loader(loader) {
+            spy("start");
+            await loader();
+            spy("end");
+          },
+        });
+      },
+    });
+
+    let A = await t.navigate("/page");
+    expect(spy.mock.calls).toEqual([["start"]]);
+    await lazyDfd.resolve({ action: () => "ACTION", loader: () => "WRONG" });
+    await A.loaders.page.resolve("PAGE");
+    expect(spy.mock.calls).toEqual([["start"], ["end"]]);
+    expect(t.router.state).toMatchObject({
+      navigation: { state: "idle" },
+      location: { pathname: "/page" },
+      loaderData: { page: "PAGE" },
+    });
+    spy.mockClear();
+
+    await t.navigate("/");
+
+    let C = await t.navigate("/page");
+    expect(spy.mock.calls).toEqual([["start"]]);
+    await C.loaders.page.resolve("PAGE");
+    expect(spy.mock.calls).toEqual([["start"], ["end"]]);
+    expect(t.router.state).toMatchObject({
+      navigation: { state: "idle" },
+      location: { pathname: "/page" },
+      loaderData: { page: "PAGE" },
+    });
+    spy.mockClear();
+
+    warnSpy.mockRestore();
+  });
+
+  it("does not double-instrument when a static `action` is used alongside `lazy`", async () => {
+    let spy = jest.fn();
+    let lazyDfd = createDeferred<{ loader: LoaderFunction }>();
+    let warnSpy = jest.spyOn(console, "warn").mockImplementation(() => {});
+    let t = setup({
+      routes: [
+        {
+          index: true,
+        },
+        {
+          id: "page",
+          path: "/page",
+          action: true,
+          lazy: () => lazyDfd.promise,
+        },
+      ],
+      unstable_instrumentRoute: (route) => {
+        route.instrument({
+          async action(action) {
+            spy("start");
+            await action();
+            spy("end");
+          },
+        });
+      },
+    });
+
+    let A = await t.navigate("/page", {
+      formMethod: "POST",
+      formData: createFormData({}),
+    });
+    expect(spy.mock.calls).toEqual([["start"]]);
+    await lazyDfd.resolve({ action: () => "WRONG" });
+    await A.actions.page.resolve("PAGE");
+    expect(spy.mock.calls).toEqual([["start"], ["end"]]);
+    expect(t.router.state).toMatchObject({
+      navigation: { state: "idle" },
+      location: { pathname: "/page" },
+      actionData: { page: "PAGE" },
+    });
+    spy.mockClear();
+
+    let B = await t.navigate("/page", {
+      formMethod: "POST",
+      formData: createFormData({}),
+    });
+    expect(spy.mock.calls).toEqual([["start"]]);
+    await B.actions.page.resolve("PAGE");
+    expect(spy.mock.calls).toEqual([["start"], ["end"]]);
+    expect(t.router.state).toMatchObject({
+      navigation: { state: "idle" },
+      location: { pathname: "/page" },
+      actionData: { page: "PAGE" },
+    });
+    spy.mockClear();
+
+    warnSpy.mockRestore();
   });
 
   it("provides read-only information to instrumentation wrappers", async () => {
