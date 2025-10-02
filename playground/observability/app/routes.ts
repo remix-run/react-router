4e44d47be3b99e35cbe170a2525e9bae45231f75
Matt Brophy <matt@brophy.org>
2025-10-02 11:02:21 -0400

    Remove playground

--- playground/observability/app/routes.ts
+++
@@ -1,6 +0,0 @@
-import { type RouteConfig, index, route } from "@react-router/dev/routes";
-
-export default [
-  index("routes/index.tsx"),
-  route(":slug", "routes/slug.tsx"),
-] satisfies RouteConfig;
--- playground/observability/react-router.config.ts
+++
@@ -1,7 +0,0 @@
-import type { Config } from "@react-router/dev/config";
-
-export default {
-  future: {
-    v8_middleware: true,
-  },
-} satisfies Config;
