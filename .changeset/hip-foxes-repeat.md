9e422e04223ed4c5f91ac16c85584d4156e97dd4
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2025-10-21 17:41:13 +1100

    fix: retain CSS for dynamic imports on navigation

---
+++ .changeset/hip-foxes-repeat.md
@@ -0,0 +1,5 @@
+---
+"@react-router/dev": patch
+---
+
+Ensure route navigation doesn't inadvertently remove CSS `link` elements injected by dynamic imports
---
+++ integration/vite-css-lazy-loading-test.ts
@@ -0,0 +1,146 @@
+import { test, expect } from "@playwright/test";
+
+import { PlaywrightFixture } from "./helpers/playwright-fixture.js";
+import {
+  type Fixture,
+  type AppFixture,
+  createAppFixture,
+  createFixture,
+  css,
+  js,
+} from "./helpers/create-fixture.js";
+
+test.describe("Vite CSS lazy loading", () => {
+  let fixture: Fixture;
+  let appFixture: AppFixture;
+
+  test.beforeAll(async () => {
+    fixture = await createFixture({
+      files: {
+        "app/components/css-component.module.css": css`
+          .test {
+            color: rgb(0, 128, 0);
+            font-family: sans-serif;
+            font-weight: bold;
+          }
+        `,
+
+        "app/components/css-component.tsx": js`
+          import styles from "./css-component.module.css";
+          export default function CssComponent() {
+            return <p data-css-component className={styles.test}>This text should be green.</p>;
+          }
+        `,
+
+        "app/components/load-lazy-css-component.tsx": js`
+          import { lazy, useState } from "react";
+          const LazyCssComponent = lazy(() => import("./css-component"));
+          export function LoadLazyCssComponent() {
+            const [show, setShow] = useState(false);
+            return (
+              <>
+                <button data-load-lazy-css-component onClick={() => setShow(true)}>Load Lazy CSS Component</button>
+                {show && <LazyCssComponent />}
+              </>
+            );
+          }
+        `,
+
+        "app/routes/_layout.tsx": js`
+          import { Link, Outlet } from "react-router";
+          import { LoadLazyCssComponent } from "../components/load-lazy-css-component";
+          export default function Layout() {
+            return (
+              <>
+                <nav>
+                  <ul>
+                    <li>
+                      <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                      <Link to="/with-css-component">Route with CSS Component</Link>
+                    </li>
+                    <li>
+                      <Link to="/without-css-component">Route Without CSS Component</Link>
+                    </li>
+                  </ul>
+                </nav>
+                <LoadLazyCssComponent />
+                <Outlet />
+              </>
+            );
+          }
+        `,
+
+        "app/routes/_layout._index.tsx": js`
+          export default function Index() {
+            return <h2 data-route-home>Home</h2>;
+          }
+        `,
+
+        "app/routes/_layout.with-css-component.tsx": js`
+          import CssComponent from "../components/css-component";
+          export default function RouteWithCssComponent() {
+            return (
+              <>
+                <h2 data-route-with-css-component>Route with CSS Component</h2>
+                <CssComponent />
+              </>
+            );
+          }
+        `,
+
+        "app/routes/_layout.without-css-component.tsx": js`
+          export default function RouteWithoutCssComponent() {
+            return <h2 data-route-without-css-component>Route Without CSS Component</h2>;
+          }
+        `,
+      },
+    });
+
+    appFixture = await createAppFixture(fixture);
+  });
+
+  test.afterAll(() => {
+    appFixture.close();
+  });
+
+  test("retains CSS from dynamic imports on navigation if the same CSS is also imported by a route", async ({
+    page,
+  }) => {
+    let app = new PlaywrightFixture(appFixture, page);
+
+    const ANY_CSS_LINK_SELECTOR =
+      "link[rel='stylesheet'][href*='css-component']";
+    // Links with a trailing hash are only ever managed by React Router, not
+    // Vite's dynamic CSS injection logic
+    const ROUTE_CSS_LINK_SELECTOR = `${ANY_CSS_LINK_SELECTOR}[href$='#']`;
+
+    function getCssComponentColor() {
+      return page
+        .locator("[data-css-component]")
+        .evaluate((el) => window.getComputedStyle(el).color);
+    }
+
+    await app.goto("/with-css-component");
+    await page.waitForSelector("[data-route-with-css-component]");
+    expect(await page.locator("[data-css-component]").count()).toBe(1);
+    expect(await page.locator(ANY_CSS_LINK_SELECTOR).count()).toBe(1);
+    expect(await page.locator(ROUTE_CSS_LINK_SELECTOR).count()).toBe(1);
+
+    expect(await getCssComponentColor()).toBe("rgb(0, 128, 0)");
+
+    await page.locator("[data-load-lazy-css-component]").click();
+    await page.waitForSelector("[data-css-component]");
+    expect(await page.locator(ANY_CSS_LINK_SELECTOR).count()).toBe(2);
+    expect(await page.locator(ROUTE_CSS_LINK_SELECTOR).count()).toBe(1);
+
+    await app.clickLink("/without-css-component");
+    await page.waitForSelector("[data-route-without-css-component]");
+    expect(await page.locator("[data-css-component]").count()).toBe(1);
+    expect(await page.locator(ANY_CSS_LINK_SELECTOR).count()).toBe(1);
+    expect(await page.locator(ROUTE_CSS_LINK_SELECTOR).count()).toBe(0);
+
+    expect(await getCssComponentColor()).toBe("rgb(0, 128, 0)");
+  });
+});
