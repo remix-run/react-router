56928de2fabab2a033dca891b5d59a774cdfc251
Matt Brophy <matt@brophy.org>
2025-11-13 11:53:15 -0500

    Normalize double slashes in resolvePath

--- packages/react-router/__tests__/resolvePath-test.tsx
+++ packages/react-router/__tests__/resolvePath-test.tsx
@@ -1,9 +1,37 @@
 import { resolvePath } from "react-router";
 
 describe("resolvePath", () => {
+  it("does not touch with protocol-less absolute paths", () => {
+    expect(resolvePath("//google.com")).toMatchObject({
+      pathname: "//google.com",
+    });
+
+    expect(resolvePath("//google.com/../../path")).toMatchObject({
+      pathname: "//google.com/../../path",
+    });
+
+    expect(resolvePath("//google.com?q=query#hash")).toMatchObject({
+      pathname: "//google.com",
+      search: "?q=query",
+      hash: "#hash",
+    });
+  });
+
   it('resolves absolute paths irrespective of the "from" pathname', () => {
     expect(resolvePath("/search", "/inbox")).toMatchObject({
       pathname: "/search",
+    });
+
+    expect(resolvePath("/search/../123", "/inbox")).toMatchObject({
+      pathname: "/123",
+    });
+
+    expect(resolvePath("/search/../../123", "/inbox")).toMatchObject({
+      pathname: "/123",
+    });
+
+    expect(resolvePath("/search/user/../../123", "/inbox")).toMatchObject({
+      pathname: "/123",
     });
   });
 
@@ -23,6 +51,32 @@ describe("resolvePath", () => {
     expect(resolvePath("search", "/inbox")).toMatchObject({
       pathname: "/inbox/search",
     });
+
+    expect(resolvePath("search/../123", "/inbox")).toMatchObject({
+      pathname: "/inbox/123",
+    });
+
+    expect(resolvePath("search/../../123", "/inbox")).toMatchObject({
+      pathname: "/123",
+    });
+
+    expect(resolvePath("search/../../../123", "/inbox")).toMatchObject({
+      pathname: "/123",
+    });
+  });
+
+  it("normalizes any mid-path double-slashes", () => {
+    let spy = jest.spyOn(console, "warn").mockImplementation(() => {});
+
+    expect(resolvePath("/search/../..//foo")).toMatchObject({
+      pathname: "/foo",
+    });
+
+    expect(resolvePath("search/../..//foo", "/inbox")).toMatchObject({
+      pathname: "/foo",
+    });
+
+    spy.mockRestore();
   });
 
   it('ignores trailing slashes on the "from" pathname when resolving relative paths', () => {
