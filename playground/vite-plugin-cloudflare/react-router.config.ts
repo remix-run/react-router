b643537e8c6702fe77d2a2badc132fe1b56f65ed
Edmund Hung <me@edmund.dev>
2026-01-30 01:02:52 +0000

    feat: prerendering with vite preview server (#14650)

--- playground/vite-plugin-cloudflare/react-router.config.ts
+++ playground/vite-plugin-cloudflare/react-router.config.ts
@@ -3,5 +3,7 @@ import type { Config } from "@react-router/dev/config";
 export default {
   future: {
     v8_viteEnvironmentApi: true,
+    unstable_previewServerPrerendering: true,
   },
+  prerender: ["/static"],
 } satisfies Config;
