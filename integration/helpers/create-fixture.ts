375065b0efab4b6791e21a8d46a4a24685b82522
Jacob Ebey <jacob.ebey@live.com>
2026-02-06 11:13:00 -0800

    enabled debug logs

--- integration/helpers/create-fixture.ts
+++ integration/helpers/create-fixture.ts
@@ -604,11 +604,11 @@ function reactRouterBuild(
   });
 
   // These logs are helpful for debugging. Remove comments if needed.
-  // console.log("spawning node " + buildArgs.join(" ") + ":\n");
-  // console.log("  STDOUT:");
-  // console.log("  " + buildSpawn.stdout.toString("utf-8"));
-  // console.log("  STDERR:");
-  // console.log("  " + buildSpawn.stderr.toString("utf-8"));
+  console.log("spawning node " + buildArgs.join(" ") + ":\n");
+  console.log("  STDOUT:");
+  console.log("  " + buildSpawn.stdout.toString("utf-8"));
+  console.log("  STDERR:");
+  console.log("  " + buildSpawn.stderr.toString("utf-8"));
 
   if (buildStdio) {
     buildStdio.write(buildSpawn.stdout.toString("utf-8"));
