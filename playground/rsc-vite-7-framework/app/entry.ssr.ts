4bece3414716c9a5f4b0ba34741de4b95ec3517a
Jacob Ebey <jacob.ebey@live.com>
2026-03-24 10:57:45 -0700

    remove custom entry from playground

--- playground/rsc-vite-7-framework/app/entry.ssr.ts
+++
@@ -1,8 +0,0 @@
-// @ts-ignore - on 18 types, requires 19
-import { generateHTML as defaultGenerateHTML } from "@react-router/dev/config/default-rsc-entries/entry.ssr";
-
-export function generateHTML(request: Request, serverResponse: Response) {
-  console.log("custom entry.ssr");
-
-  return defaultGenerateHTML(request, serverResponse);
-}
