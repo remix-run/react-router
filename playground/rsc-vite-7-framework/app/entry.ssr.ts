1a0fedadd043bb5ece3af29cd1c1b7256dd56de7
Jacob Ebey <jacob.ebey@live.com>
2026-03-24 10:48:15 -0700

    fixup types

--- playground/rsc-vite-7-framework/app/entry.ssr.ts
+++ playground/rsc-vite-7-framework/app/entry.ssr.ts
@@ -1,3 +1,4 @@
+// @ts-ignore - on 18 types, requires 19
 import { generateHTML as defaultGenerateHTML } from "@react-router/dev/config/default-rsc-entries/entry.ssr";
 
 export function generateHTML(request: Request, serverResponse: Response) {
