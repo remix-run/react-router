cdaa4f4ab704999cbdd2a246f61f34c7bf68dd90
Vincent Gao <gaobing1230@gmail.com>
2026-06-19 09:16:21 +0200

    Fix Bun default import handling for Babel

--- packages/react-router-dev/vite/babel.ts
+++ packages/react-router-dev/vite/babel.ts
@@ -5,12 +5,19 @@ import * as t from "@babel/types";
 import _traverse from "@babel/traverse";
 import _generate from "@babel/generator";
 
-// These `require`s were needed to support building within vite-ecosystem-ci,
-// otherwise we get errors that `traverse` and `generate` are not functions.
-const traverse = (_traverse as any)
-  .default as typeof import("@babel/traverse").default;
-const generate = (_generate as any)
-  .default as typeof import("@babel/generator").default;
+type DefaultImport<T> = T | { default: T };
+
+export function unwrapDefault<T>(value: DefaultImport<T>): T {
+  return (value as { default?: T }).default ?? (value as T);
+}
+
+// Babel's CommonJS packages are exposed differently across runtimes.
+const traverse = unwrapDefault(
+  _traverse as DefaultImport<typeof import("@babel/traverse").default>,
+);
+const generate = unwrapDefault(
+  _generate as DefaultImport<typeof import("@babel/generator").default>,
+);
 
 export { traverse, generate, parse, t };
 export type { Babel, NodePath, ParseResult };
