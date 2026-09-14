e79b2314cf7d03b239e8ae162212e8b883e600b2
Matt Brophy <matt@brophy.org>
2026-09-14 16:14:41 -0400

    Rename route-pattern preload module
    Assisted-By: devx/1b77856c-9d25-4e6f-b55e-5dce2807ffc7

--- packages/react-router/route-pattern.ts
+++ packages/react-router/route-pattern.ts
@@ -2,4 +2,4 @@
  * @module route-pattern
  */
 
-export { unstable_preloadRoutePattern } from "./lib/router/route-pattern-preload";
+export { unstable_preloadRoutePattern } from "./lib/router/matcher-route-pattern.preload";
